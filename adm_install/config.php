4ad36b496a3ed5335f4df2b9f25a6dbc4e897581
Markus Faßbender <markus.fassbender@gmail.com>
2005-10-04 16:54:20 +0000

    Umstellung auf neue Ordnerstruktur

---
+++ adm_install/config.php
@@ -0,0 +1,80 @@
+<?php
+/******************************************************************************
+ * Konfigurationsdatei von Admidio
+ *
+ * Copyright    : (c) 2004 - 2005 The Admidio Team
+ * Homepage     : http://www.admidio.org
+ * Module-Owner : Markus Fassbender
+ *
+ ******************************************************************************
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *****************************************************************************/
+ 
+   // Kennzeichen, ob Scripte im Internet oder lokal auf dem Rechner ausgefuehrt werden
+   // Lokal muessen z.B. keine Mails verschickt werden, das fuehrt nur zum Fehler im Script
+   // 1 = Internet
+   // 0 = Local
+   $g_internet = 1;
+   
+   // Daten für die MySQL-Datenbank-Verbindung
+   $g_adm_srv = "%SERVER%";      // Server
+   $g_adm_usr = "%USER%";        // Benutzer
+   $g_adm_pw  = "%PASSWORD%";    // Passwort
+   $g_adm_db  = "%DATABASE%";    // Datenbank
+      
+   // Root-Pfad für das System auf dem es installiert ist 
+   // Der Pfad muss bis zu dem Verzeichnis, in dem die admidio.html-Datei liegt, angegeben werden !!!
+   // Beispiel: "http://www.admidio.org/beispiel"
+   $g_root_path = "%ROOT_PATH%";
+   
+   // Startseite der Homepage
+   // Relativer Pfad von g_root_path aus gesehen
+   $g_main_page = "admidio.html";
+   
+   // Kurzbezeichnung der Gruppierung, des Vereins oder der Organisation auf der Admidio läuft
+   // Diese muss der Eingabe auf der Installationsseite entsprechen !!!
+   // Beispiel: "ADMIDIO"
+   // Maximal 10 Zeichen !!!
+   $g_organization = "%ORGANIZATION%";
+   
+   // Domainname auf dem Admidio laufen soll
+   // Ohne http:// oder www. und auch keine Subdomains angeben !!!
+   // Beispiel: "admidio.org"
+   $g_domain   = "%DOMAIN%";
+
+   // URL der Homepage
+   $g_homepage = "www.admidio.org";
+   
+   // Titel der Homepage, der später im Browser angezeigt werden soll
+   $g_title    = "Admidio - Das Online-Verwaltungssystem für Vereine, Gruppen und Organisationen";
+      
+   
+   // Forumspezifisch
+   
+   // Forum integriert
+   // 1 = ja
+   // 0 = nein
+   $g_forum = 0;
+
+   // Praefix der Tabellen des phpBB-Forums
+   $g_forum_praefix = 'adm_';
+
+   $g_forum_srv = "server";
+   $g_forum_usr = "user";
+   $g_forum_pw  = "passwort";
+   $g_forum_db  = "datenbank";
+?>
---
+++ adm_install/upd_1_1_konv.php
@@ -0,0 +1,94 @@
+<?php
+/******************************************************************************
+ * Datenkonvertierung fuer die Version 1.1
+ *
+ * Copyright    : (c) 2004 - 2005 The Admidio Team
+ * Homepage     : http://www.admidio.org
+ * Module-Owner : Markus Fassbender
+ *
+ ******************************************************************************
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *****************************************************************************/
+
+$aim_id   = 0;
+$icq_id   = 0;
+$msn_id   = 0;
+$yahoo_id = 0;
+
+// IDs der einzelnen Messenger auslesen
+
+$sql = "SELECT auf_id FROM adm_user_field WHERE auf_type = 'MESSENGER' AND auf_name = 'AIM' ";
+$result = mysql_query($sql, $connection);
+if(!$result) showError(mysql_error());
+$row = mysql_fetch_object($result);
+$aim_id = $row->auf_id;
+
+$sql = "SELECT auf_id FROM adm_user_field WHERE auf_type = 'MESSENGER' AND auf_name = 'ICQ' ";
+$result = mysql_query($sql, $connection);
+if(!$result) showError(mysql_error());
+$row = mysql_fetch_object($result);
+$icq_id = $row->auf_id;
+
+$sql = "SELECT auf_id FROM adm_user_field WHERE auf_type = 'MESSENGER' AND auf_name = 'MSN' ";
+$result = mysql_query($sql, $connection);
+if(!$result) showError(mysql_error());
+$row = mysql_fetch_object($result);
+$msn_id = $row->auf_id;
+
+$sql = "SELECT auf_id FROM adm_user_field WHERE auf_type = 'MESSENGER' AND auf_name = 'YAHOO' ";
+$result = mysql_query($sql, $connection);
+if(!$result) showError(mysql_error());
+$row = mysql_fetch_object($result);
+$yahoo_id = $row->auf_id;
+
+// alle User selektieren, die einen Messenger zugeordnet haben
+
+$sql = "SELECT au_id, au_messenger, au_messenger_id FROM adm_user WHERE au_messenger > 0 ";
+$result_user = mysql_query($sql, $connection);
+if(!$result_user) showError(mysql_error());
+
+while($row_usr = mysql_fetch_object($result_user))
+{
+   $messenger_id = 0;
+   switch($row_usr->au_messenger)
+   {
+      case 1:
+         $messenger_id = $icq_id;
+         break;
+      case 2:
+         $messenger_id = $aim_id;
+         break;
+      case 3:
+         $messenger_id = $msn_id;
+         break;
+      case 4:
+         $messenger_id = $yahoo_id;
+         break;
+   }
+   
+   // neuen Satz in adm_user_data einfuegen
+   $sql = "INSERT INTO adm_user_data (aud_au_id, aud_auf_id, aud_value) VALUES ($row_usr->au_id, $messenger_id, '$row_usr->au_messenger_id') ";
+   $result = mysql_query($sql, $connection);
+   if(!$result) showError(mysql_error());
+}
+
+// die beiden alten Messengerfelder in adm_user loeschen
+$sql = "ALTER TABLE `adm_user` DROP `au_messenger`, DROP `au_messenger_id` ";
+$result = mysql_query($sql, $connection);
+if(!$result) showError(mysql_error());
+
+?>
