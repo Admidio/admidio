206447da4acfa8e8137c3c1a6c030cee776be77e
Jochen Erkens <jochen.erkens@gmail.com>
2005-11-18 16:15:33 +0000

    Erst Lauff�hige Version: - Man kann ausw�hlen ob alle user angezeigt werden oder nur die die keine Rolle haben -  Zu besseren �bersicht werden Vor jedem neuen Anfangsbuchstaben Gro�e Buchstaben angezeigt (ab 50 Namen) und darunter Das Alphbeth mit Links (ab 100 Namen)

--- source/adm_program/modules/lists/members.php
+++ source/adm_program/modules/lists/members.php
@@ -12,7 +12,10 @@
  * popup   : 0 - (Default) Fenster wird normal mit Homepagerahmen angezeigt
  *           1 - Fenster wurde im Popupmodus aufgerufen
  * url:        - URL auf die danach weitergeleitet wird
- *
+ * restrict:	Begrenzte Userzahl:
+ * 				m - (Default) nur Mitglieder
+ * 				u - alle in der Datenbank gespeicherten user
+ * 
  ******************************************************************************
  *
  * This program is free software; you can redistribute it and/or
@@ -35,7 +38,6 @@ require("../../system/function.php");
 require("../../system/date.php");
 require("../../system/tbl_user.php");
 require("../../system/session_check_login.php");
-require("../../system/string.php");
 
 // nur Webmaster & Moderatoren d&uuml;rfen Rollen zuweisen
 if(!isModerator() && !isGroupLeader() && !editUser())
@@ -65,6 +67,61 @@ $result_role = mysql_query($sql, $g_adm_con);
          	db_error($result, true);
 $role= mysql_fetch_array($result_role);
 
+//Übername ob nur Mitglieder oder alle User der Datenbank angezeigt werden sollen
+$restrict=$_GET["restrict"];
+if($restrict=="")$restrict="m";
+
+//Falls gefordert, nur Aufruf von inhabern der Rolle Mitglied
+if($restrict=="m"){
+	$sql = "
+		SELECT DISTINCT au_id, au_name, au_vorname, au_geburtstag
+		FROM adm_user, adm_mitglieder, adm_rolle
+		WHERE au_id = am_au_id
+		AND ar_ag_shortname = '$g_organization'
+		AND am_ar_id = ar_id
+		AND am_valid = 1
+		AND ar_valid = 1
+		ORDER BY au_name, au_vorname ASC ";
+	$result_user = mysql_query($sql, $g_adm_con);
+	db_error($result_user);
+	//Zählen wieviele Leute in der Datenbank stehen
+	$user_anzahl = mysql_num_rows($result_user);
+}
+
+//Falls gefordert, aufrufen alle Leute aus der Datenbank
+if($restrict=="u"){
+	$sql = "
+		SELECT au_id, au_name, au_vorname, au_geburtstag
+		FROM adm_user
+		ORDER BY au_name, au_vorname ASC ";
+	$result_user = mysql_query($sql, $g_adm_con);
+	db_error($result_user);
+	//Zählen wieviele Leute in der Datenbank stehen
+	$user_anzahl = mysql_num_rows($result_user);
+}
+
+//Zählen wieviele Leute in der Datenbank stehen
+$sql = "SELECT COUNT(*) FROM adm_user";
+$result_user_anzahl = mysql_query($sql, $g_adm_con);
+db_error($result_user_anzahl);
+$user_anzahl = mysql_fetch_array($result_user_anzahl);
+
+//Erfassen wer die Rolle bereits hat oder schon mal hatte
+$sql = "
+	SELECT am_au_id, am_ar_id, am_valid
+	FROM adm_mitglieder
+	WHERE am_ar_id = '$role_id'";
+$result_role_member = mysql_query($sql, $g_adm_con);
+db_error($result_role_member);
+         	
+//Schreiben der User-IDs die die Rolle bereits haben oder hatten in Array
+$role_member= array();
+for($y=0; $member = mysql_fetch_array($result_role_member); $y++){
+	if($member['am_valid']==1)$role_member[$y]= $member['am_au_id'];
+}
+
+
+//Beginn HTML
 echo "
 <!-- (c) 2004 - 2005 The Admidio Team - http://www.admidio.org - Version: ". getVersion(). " -->\n
 <!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">
@@ -90,6 +147,16 @@ echo "<div style=\"margin-top: 10px; margin-bottom: 10px;\" align=\"center\">
    <a name=\"Anfang\"></a>
 	<form action=\"members_save.php?role_id=".$role_id. "&amp;popup=". $_GET['popup']. "&amp;url=$url\" method=\"post\" name=\"Mitglieder\">
 	   <h2>Mitglieder zu $role[2] zuordnen</h2>";
+     	//Button Alle bzw. nur Mitglieder anzeigen
+     	if($restrict=="m")
+     		echo"	<button name=\"back\" type=\"button\" value=\"back\" style=\"width: 140px;\" onclick=\"self.location.href='members.php?ar_id=role_id&amp;popup=1&amp;restrict=u'\">
+         			<img src=\"../../../adm_program/images/gruppe.png\" style=\"vertical-align: middle;\" align=\"top\" vspace=\"1\" width=\"16\" height=\"16\" border=\"0\" alt=\"\">&nbsp;Alle anzeigen
+         		</button>";
+     	if($restrict=="u")
+     		echo"	<button name=\"back\" type=\"button\" value=\"back\" style=\"width: 140px;\" onclick=\"self.location.href='members.php?ar_id=role_id&amp;popup=1&amp;restrict=m'\">
+         			<img src=\"../../../adm_program/images/person.png\" style=\"vertical-align: middle;\" align=\"top\" vspace=\"1\" width=\"16\" height=\"16\" border=\"0\" alt=\"\">&nbsp;Nur Mitglieder
+         		</button>";;
+     	//Anfang Tabelle
      	echo"
 		<br><br>
 		<table class=\"tableList\" cellpadding=\"3\" cellspacing=\"0\" ";
@@ -102,50 +169,37 @@ echo "<div style=\"margin-top: 10px; margin-bottom: 10px;\" align=\"center\">
 					<th class=\"tableHeader\" style=\"text-align: center;\">Geburtsdatum</th>
 					<th class=\"tableHeader\" style=\"text-align: center;\">Mitglied</th>
          	</tr>";
-         	//Aufrufen alle Leute aus der Datenbank
-         	$sql = "
-         		SELECT au_id, au_name, au_vorname, au_geburtstag
-            	FROM adm_user
-					ORDER BY au_name, au_vorname ASC ";
-   			$result_user = mysql_query($sql, $g_adm_con);
-   			db_error($result_user);
-         	
-         	//Erfassen wer die Rolle bereits hat
-         	$sql = "
-         		SELECT am_au_id, am_ar_id, am_valid
-            	FROM adm_mitglieder
-					WHERE am_ar_id = '$role_id'";
-   			$result_role_member = mysql_query($sql, $g_adm_con);
-   			db_error($result_role_member);
-         	
-         	//Schreiben der User-IDs in Array
-         	$role_member= array();
-         	for($y=0; $member = mysql_fetch_array($result_role_member); $y++){
-         		if($member['am_valid']==1)$role_member[$y]= $member['am_au_id'];
-         	} 
-         	//Ausgabe der Tabellenzeilen einfügen von Ankern
-         	$letter_first = "A";
+ 
+  //Ausgabe der Tabellenzeilen, ggf. einfügen von Ankern
          	$user = mysql_fetch_array($result_user);
-         	for($alpha_first=1; $alpha_first<=26; $alpha_first++){
-         		echo "<tr><td style=\"text-align: center;\" colspan=\"4\">";
-     				$letter_menu = "A";
-     				//Aktueller Anfangsbuchstabe plus Anker
-     				echo"<a name=\"$letter_first\"></a><h2>$letter_first</h2>";
-     				echo"<a href=\"#Anfang\">Anfang</a>&nbsp;";
-     				for($alpha_menu=1; $alpha_menu<=26; $alpha_menu++){
-     					//Falls Aktueller Anfangsbuchstabe, Nur Buchstabe ausgeben
-     					if($letter_first==$letter_menu)
-     						echo"$letter_menu&nbsp;";
-     					//Falls Nicht Link zu Anker
-     					if($letter_first!=$letter_menu)
-     						echo"<a href=\"#$letter_menu\">$letter_menu</a>&nbsp;";
-     					$letter_menu = strNextLetter($letter_menu);
-     				}
-         		echo"
-						<a href=\"#Ende\">Ende</a>
-					</td></tr>";
-	        		$member_counter=0;
-	        		while(stripos($user['au_name'], $letter_first)===0){
+         //Für alle Namen die mit Zahlen beginnen z.B. 123GmbH
+         	$ascii = array(48, 49, 50, 51, 52, 53, 54, 55, 56, 57);
+           	
+           	if(in_array(ord($user['au_name']), $ascii)){
+         		//große Anfangsbuchstaben werden erst ab 50 Personen angezeigt
+         		if($user_anzahl[0]>50){
+         			echo "<tr><td style=\"text-align: center;\" colspan=\"4\">";           			
+      					//Aktueller Anfangsbuchstabe plus Anker
+     						$letter_string = chr($letter);          			
+           				echo"<a name=\"$letter_string\"></a><h2>$letter_string</h2>";
+ 						//Buchstaben Links zu Ankern wenn mehr als 100 Namen angezeigt werden sollen    			
+							if($user_anzahl[0]>100){     			
+     							echo"<a href=\"#Anfang\">Anfang</a>&nbsp;";
+     							for($menu_letter=65; $menu_letter<=90; $menu_letter++){
+     							//Falls Aktueller Anfangsbuchstabe, Nur Buchstabe ausgeben
+      							$menu_letter_string = chr($menu_letter);    					
+     								if($letter==$menu_letter)echo"$menu_letter_string&nbsp;";
+     							//Falls Nicht Link zu Anker
+     								if($letter!=$menu_letter)echo"<a href=\"#$menu_letter_string\">$menu_letter_string</a>&nbsp;";
+     							}//for
+         				echo"<a href=\"#Ende\">Ende</a>";
+							}//User_anzahl>100
+         			echo"</td></tr>";
+           		}//Ende 
+           	for($letter=48; $letter<=57; $letter++){
+         		//Ausgabe aller Personen mit entsprechendem Anfangsbuchstaben
+	        		$user_name = $user['au_name'];
+	        		while(ord($user['au_name'])==$letter ||ord($user['au_name'])==$letter+32){
          			echo"
 						<tr>
 							<td style=\"text-align: center;\">". $user['au_name']."</td>
@@ -163,8 +217,52 @@ echo "<div style=\"margin-top: 10px; margin-bottom: 10px;\" align=\"center\">
 					";
 					$user = mysql_fetch_array($result_user);
          		}//Ende Whileschleife
-         		$letter_first = strNextLetter($letter_first);
+				}//Ende for-Schleife
+         }//Ende Namen mit Zahlen 	
+         	
+         //Für alle Namen die mit Buchstaben beginnen egal ob klein oder Groß
+         	for($letter=65; $letter<=90; $letter++){
+           	//große Anfangsbuchstaben werden erst ab 50 Personen angezeigt 
+           		if($letter==ord($user['au_name']) && $user_anzahl[0]>50){
+         			echo "<tr><td style=\"text-align: center;\" colspan=\"4\">";           			
+      				//Aktueller Anfangsbuchstabe plus Anker
+     					$letter_string = chr($letter);          			
+           			echo"<a name=\"$letter_string\"></a><h2>$letter_string</h2>";
+ 					//Buchstaben Links zu Ankern wenn mehr als 100 Namen angezeigt werden sollen    			
+						if($user_anzahl[0]>100){     			
+     						echo"<a href=\"#Anfang\">Anfang</a>&nbsp;";
+     						for($menu_letter=65; $menu_letter<=90; $menu_letter++){
+     						//Falls Aktueller Anfangsbuchstabe, Nur Buchstabe ausgeben
+      						$menu_letter_string = chr($menu_letter);    					
+     							if($letter==$menu_letter)echo"$menu_letter_string&nbsp;";
+     						//Falls Nicht Link zu Anker
+     							if($letter!=$menu_letter)echo"<a href=\"#$menu_letter_string\">$menu_letter_string</a>&nbsp;";
+     						}//for
+         			echo"<a href=\"#Ende\">Ende</a>";
+						}//User_anzahl>10
+         			echo"</td></tr>";
+           		}//Ende $letter==$letter_int					
+	        	//Ausgabe aller Personen mit entsprechendem Anfangsbuchstaben
+	        		while(ord($user['au_name'])==$letter ||ord($user['au_name'])==$letter+32){
+         			echo"
+						<tr>
+							<td style=\"text-align: center;\">". $user['au_name']."</td>
+							<td style=\"text-align: center;\">". $user['au_vorname']."</td>
+							<td style=\"text-align: center;\">". $user['au_geburtstag']."</td>
+							<td style=\"text-align: center;\">";
+							if(in_array($user['au_id'], $role_member)){
+								echo"<input type=\"checkbox\" id=\"$user[0]\" name=\"$user[0]\" checked value=\"1\">";
+							}
+							else{
+								echo"<input type=\"checkbox\" id=\"$user[0]\" name=\"$user[0]\" value=\"1\">";
+							}
+							echo"</td>
+						</tr>
+					";
+					$user = mysql_fetch_array($result_user);
+         		}//Ende Whileschleife
          	}//Ende for-Schleife
+         	//Buttons schließen oder Speichern
          	echo"
 					<tr><td colspan=4><a name=\"Ende\"></a><hr width=\"85%\" /></td></tr>
 					<tr class=\"listMouseOut\">
