acb96d56bb82cc279372bfba91c192fb706004f9
Markus Faßbender <markus.fassbender@gmail.com>
2005-11-21 08:27:19 +0000

    Default von BBCode auf 1 gesetzt

--- adm_install/upd_1_2_db.sql
+++ adm_install/upd_1_2_db.sql
@@ -2,5 +2,5 @@ ALTER TABLE `adm_gruppierung` CHANGE `ag_mutter` `ag_mother` VARCHAR( 10 ) NULL 
 ALTER TABLE `adm_gruppierung` ADD `ag_mail_extern` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT '0';
 ALTER TABLE `adm_gruppierung` ADD `ag_homepage` VARCHAR( 30 ) NULL DEFAULT 'www.admidio.org';
 ALTER TABLE `adm_gruppierung` ADD `ag_mail_attachment_size` SMALLINT UNSIGNED NOT NULL DEFAULT '1024';
-ALTER TABLE `adm_gruppierung` ADD `ag_bbcode` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `adm_gruppierung` ADD `ag_bbcode` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT '1';
 ALTER TABLE `adm_gruppierung` ADD `ag_enable_rss` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT '1';
