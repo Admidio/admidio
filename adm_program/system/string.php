a7e24c9dd7d61e6865eabd574d813146eb08d9ca
Markus Faßbender <markus.fassbender@gmail.com>
2006-01-17 14:32:24 +0000

    Dateinamen mit Leerzeichen k�nnen nicht hochgeladen werden BUG: 1408232

--- adm_program/system/string.php
+++ adm_program/system/string.php
@@ -181,7 +181,7 @@ function isValidFileName($file_name, $check_ext = false)
    if(strlen(trim($file_name)) > 0)
    {
       // nur gueltige Zeichen zulassen
-      $anz = strspn($file_name, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@.-_+");
+      $anz = strspn($file_name, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.-_+ ");
 
       if($anz == strlen($file_name))
       {
