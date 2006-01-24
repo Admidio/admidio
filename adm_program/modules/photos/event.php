c5fcdc487211afbeec01122199e9f59c11f2f3b4
Jochen Erkens <jochen.erkens@gmail.com>
2006-01-24 15:45:53 +0000

    Hilfepopufenstergr��e angepasst

--- adm_program/modules/photos/event.php
+++ adm_program/modules/photos/event.php
@@ -379,7 +379,7 @@ if($_GET["aufgabe"]=="change" || $_GET["aufgabe"]=="new"){
             <div style=\"margin-top: 6px;\">
             <hr width=\"85%\" />
                Hilfe: <img src=\"$g_root_path/adm_program/images/help.png\" style=\"cursor: pointer; vertical-align: top;\" vspace=\"1\" width=\"16\" height=\"16\" border=\"0\" alt=\"Hilfe\" title=\"Hilfe\"
-                     onclick=\"window.open('$g_root_path/adm_program/system/msg_window.php?err_code=veranst_help','Message','width=500, height=230, left=310,top=200,scrollbars=no')\">
+                     onclick=\"window.open('$g_root_path/adm_program/system/msg_window.php?err_code=veranst_help','Message','width=500, height=350, left=310,top=200,scrollbars=no')\">
             <hr width=\"85%\" />
             <div style=\"margin-top: 6px;\">
                <button name=\"submit\" type=\"submit\" value=\"speichern\">
