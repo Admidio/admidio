0c8a19e39f02f5123299301805a09476c0776846
Markus Faßbender <markus.fassbender@gmail.com>
2006-02-06 08:04:39 +0000

    Link zu Ehemaligenlisten korrigiert

--- adm_program/index.php
+++ adm_program/index.php
@@ -209,7 +209,7 @@ require("../adm_config/body_top.php");
       </div>
       <div style=\"text-align: left; margin-left: 45px;\">
          <span class=\"textHead\"><a href=\"$g_root_path/adm_program/modules/lists/lists.php\">Listen</a></span>&nbsp;&nbsp;
-         <span class=\"textHeadSmall\">&#91; <a href=\"$g_root_path/adm_program/modules/lists/lists.php?type=former\">Ehemalige</a> &#93;</span><br />
+         <span class=\"textHeadSmall\">&#91; <a href=\"$g_root_path/adm_program/modules/lists/lists.php?active_role=0\">Ehemalige</a> &#93;</span><br />
          <span class=\"textDescription\">Verschiedene Benutzerlisten der Rollen (Gruppen / Kurse / Abteilungen) anzeigen.</span>
       </div>
 
