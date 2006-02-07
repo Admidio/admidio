c841611b4ae8a79bd3c56f9462b6ed08bdacb741
Elmar Meuthen <e.meuthen@admidio.org>
2006-02-07 16:38:21 +0000

    weiter gebastelt...

--- adm_program/system/email_class.php
+++ adm_program/system/email_class.php
@@ -46,14 +46,12 @@
  *
  *****************************************************************************/
 
+require("./common.php");
+
 // Email - Klasse
 class newEmail
 {
-	var $headerOptions;
-	var $mailBoundary;
-	var $text;
-	var $attachments;
-	var $copyToSender;
+
 
 
 //Konstruktor der Klasse. Hier werden auch automatisch Encoding-Optionen etc. gesetzt
@@ -65,9 +63,14 @@ function newEmail()
 
 }
 
-function addSender($sender)
+function setSender($address, $name='')
 {
-	$this->headerOptions['From'] = $sender;
+	if (isValidEmailAddress($address))
+	{
+		$this->headerOptions['From'] = $name. " <". $address. ">";
+		return TRUE;
+	}
+	return FALSE;
 }
 
 function addSubject($subject)
@@ -75,40 +78,55 @@ function addSubject($subject)
 	$this->headerOptions['Subject'] = $subject;
 }
 
-function addRecipient($to)
+function addRecipient($address, $name='')
 {
-	if (!isset($this->headerOptions['To']))
+	if (isValidEmailAddress($address))
 	{
-		$this->headerOptions['To'] = $to;
+		if (!isset($this->headerOptions['To']))
+		{
+			$this->headerOptions['To'] = $name. " <". $address. ">";
+		}
+		else
+		{
+		$this->headerOptions['To'] = $this->headerOptions['To']. ", ". $name. " <". $address. ">";
+		}
+		return TRUE;
 	}
-	else
-	{
-		$this->headerOptions['To'] = $this->headerOptions['To']. ", " .$to;
-	}
+	return FALSE;
 }
 
-function addCopy($cc)
+function addCopy($address, $name='')
 {
-	if (!isset($this->headerOptions['Cc']))
+	if (isValidEmailAddress($address))
 	{
-		$this->headerOptions['Cc'] = $cc;
+		if (!isset($this->headerOptions['Cc']))
+		{
+			$this->headerOptions['Cc'] = $name. " <". $address. ">";
+		}
+		else
+		{
+		$this->headerOptions['Cc'] = $this->headerOptions['Cc']. ", ". $name. " <". $address. ">";
+		}
+		return TRUE;
 	}
-	else
-	{
-		$this->headerOptions['Cc'] = $this->headerOptions['Cc']. ", " .$cc;
-	}
+	return FALSE;
 }
 
-function addBlindCopy($bcc)
+function addBlindCopy($address, $name='')
 {
-	if (!isset($this->headerOptions['Bcc']))
+	if (isValidEmailAddress($address))
 	{
-		$this->headerOptions['Bcc'] = $bcc;
+		if (!isset($this->headerOptions['Bcc']))
+		{
+			$this->headerOptions['Bcc'] = $name. " <". $address. ">";
+		}
+		else
+		{
+		$this->headerOptions['Bcc'] = $this->headerOptions['Bcc']. ", ". $name. " <". $address. ">";
+		}
+		return TRUE;
 	}
-	else
-	{
-		$this->headerOptions['Bcc'] = $this->headerOptions['Bcc']. ", " .$bcc;
-	}
+	return FALSE;
 }
 
 function setText($text)
@@ -123,6 +141,7 @@ function addAttachment($file, $name = '', $c_type='application/octet-stream', $e
             'name'		=> $name,
             'c_type'	=> $c_type,
             'encoding'	=> $encoding);
+    $this->headerOptions['Content-Type'] = "multipart/mixed;\n\tboundary=\"$mailBoundary\"";
 }
 
 function setCopyToSenderFlag()
@@ -130,14 +149,65 @@ function setCopyToSenderFlag()
 	$this->copyToSender = TRUE;
 }
 
-function buildEmail()
-{
-
-}
-
 function sendEmail()
 {
+	// Erst mal einen leeren Mailbody erstellen...
+	$mail_body = '';
 
+
+	// Wenn keine Absenderadresse gesetzt wurde, ist hier Ende im Gelände...
+	if (!isset($this->headerOptions['From']))
+	{
+		return FALSE;
+	}
+
+	// Wenn keine Empfänger gesetzt wurde, ist hier auch Ende...
+	if (!isset($this->headerOptions['To']) and !isset($this->headerOptions['Cc']) and !isset($this->headerOptions['Bcc']))
+	{
+		return FALSE;
+	}
+
+	//Hier werden die HauptMailempfänger gesetzt und aus dem Header genommen...
+	$recipient = '';
+	if (isset($this->headerOptions['To']))
+	{
+		$recipient = $this->headerOptions['To'];
+		unset($this->headerOptions['To']);
+	}
+
+	// Hier wird das MailSubject gesetzt und aus dem Header genommen...
+	$subject = '';
+	if (isset($this->headerOptions['Subject']))
+	{
+		$subject = $this->headerOptions['Subject'];
+		unset($this->headerOptions['Subject']);
+	}
+
+	// Hier wird der Header für die Mail aufbereitet...
+	$mail_properties = '';
+	foreach ($this->headerOptions as $key => $value)
+	{
+		$mail_properties = $mail_properties. $key. ": ". $value. "\r\n";
+	}
+
+	// Eigentlichen Mail-Text hinzufügen...
+	$mail_body = $this->text;
+
+
+	// Mail wir jetzt versendet...
+	mail($recipient, $subject, $mail_body, $mail_properties);
+
+	// Eventuell noch eine Kopie an den Absender:
+	if ($this->copyToSender)
+	{
+		//TODO!!!!
+	}
+
+	//DebugKrempel von Elmar
+	echo "$mail_properties";
+
+	//Ende DebugKrempel von Elmar
+	return TRUE;
 }
 
 
