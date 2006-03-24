1ed55c02d3a1ef9b27913a4c3f87f463445840e9
Markus Faßbender <markus.fassbender@gmail.com>
2006-03-24 18:13:20 +0000

    Index nicht über Create anlegen BUG: 1457662

--- source/adm_install/upd_1_3_db.sql
+++ source/adm_install/upd_1_3_db.sql
@@ -9,28 +9,18 @@ create table %PRAEFIX%_folders
 )
 type = InnoDB
 auto_increment = 1;
-create index FOL_ORG_FK on %PRAEFIX%_folders
-(
-   fol_org_shortname
-);
-create index FOL_FOL_PARENT_FK on %PRAEFIX%_folders
-(
-   fol_fol_id_parent
-);
+alter table %PRAEFIX%_folders add index FOL_ORG_FK (fol_org_shortname);
+alter table %PRAEFIX%_folders add index FOL_FOL_PARENT_FK (fol_fol_id_parent);
+
 create table %PRAEFIX%_folder_roles
 (
    flr_fol_id                     int(11) unsigned               not null,
    flr_rol_id                     int(11) unsigned               not null
 )
 type = InnoDB;
-create index FLR_FOL_FK on %PRAEFIX%_folder_roles
-(
-   flr_fol_id
-);
-create index FOL_ROL_FK on %PRAEFIX%_folder_roles
-(
-   flr_rol_id
-);
+alter table %PRAEFIX%_folder_roles add index FLR_FOL_FK (flr_fol_id);
+alter table %PRAEFIX%_folder_roles add index FOL_ROL_FK (flr_rol_id);
+
 alter table adm_folder_roles add constraint %PRAEFIX%_FK_FOL_ROL foreign key (flr_rol_id)
       references adm_roles (rol_id) on delete restrict on update restrict;
 alter table adm_folder_roles add constraint %PRAEFIX%_FK_FLR_FOL foreign key (flr_fol_id)
