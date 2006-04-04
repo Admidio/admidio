651489f45603639980f5016c603e36d5499617ce
Elmar Meuthen <e.meuthen@admidio.org>
2006-04-04 14:02:17 +0000

    Im css die Einrückungen auf 4 Blanks umgesetzt und in body_bottom.php im alt Tag den Umlaut umgesetzt

--- adm_config_official/body_bottom.php
+++ adm_config_official/body_bottom.php
@@ -4,8 +4,8 @@
 -->
 
 <p>
-   <div align="center">
-      <a href="http://www.admidio.org" target="_blank"><img src="<?php echo $g_root_path ?>/adm_program/images/admidio_logo_20.png" align="middle" border="0" alt="Das Online-Verwaltungssystem für Vereine, Gruppen und Organisationen" /></a>
-      <span style="font-size: 9pt;">&nbsp;&nbsp;&copy; 2004 - 2006&nbsp;&nbsp;Admidio Team</span>
-   </div>
+    <div align="center">
+        <a href="http://www.admidio.org" target="_blank"><img src="<?php echo $g_root_path ?>/adm_program/images/admidio_logo_20.png" align="middle" border="0" alt="Das Online-Verwaltungssystem f&uuml;r Vereine, Gruppen und Organisationen" /></a>
+        <span style="font-size: 9pt;">&nbsp;&nbsp;&copy; 2004 - 2006&nbsp;&nbsp;Admidio Team</span>
+    </div>
 </p>
