3f55026524b84a8e6207fbfff72d8b3949367067
Markus Faßbender <markus.fassbender@gmail.com>
2006-04-18 14:41:53 +0000

    Profil optisch ueberarbeitet und vCard-Export eingebaut

--- adm_program/modules/profile/profile_edit.php
+++ adm_program/modules/profile/profile_edit.php
@@ -409,13 +409,13 @@ if($popup == 0)
          echo "<hr width=\"80%\" />
 
          <div style=\"margin-top: 6px;\">
-            <button name=\"speichern\" type=\"submit\" value=\"speichern\">
-               <img src=\"$g_root_path/adm_program/images/disk.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Speichern\">
-                 &nbsp;Speichern</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
             <button name=\"zurueck\" type=\"button\" value=\"zurueck\" onclick=\"history.back()\">
             <img src=\"$g_root_path/adm_program/images/back.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Zur&uuml;ck\">
-              &nbsp;Zur&uuml;ck</button>
+              &nbsp;Zur&uuml;ck</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              
+            <button name=\"speichern\" type=\"submit\" value=\"speichern\">
+               <img src=\"$g_root_path/adm_program/images/disk.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Speichern\">
+                 &nbsp;Speichern</button>
          </div>";
 
          if($user->usr_id_change > 0)
--- adm_program/modules/profile/roles_save.php
+++ adm_program/modules/profile/roles_save.php
@@ -35,45 +35,45 @@ require("../../system/tbl_role_dependencies.php");
 
 // nur Webmaster & Moderatoren duerfen Rollen zuweisen
 if(!isModerator() && !isGroupLeader() && !editUser())
- {
-   $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=norights";
-   header($location);
-   exit();
- }
+{
+    $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=norights";
+    header($location);
+    exit();
+}
 
 if(isModerator())
 {
-   // Alle Rollen der Gruppierung auflisten
-   $sql    = "SELECT rol_id, rol_name 
-   				 FROM ". TBL_ROLES. "
-               WHERE rol_org_shortname = '$g_organization'
-                 AND rol_valid        = 1
-               ORDER BY rol_name";
+    // Alle Rollen der Gruppierung auflisten
+    $sql    = "SELECT rol_id, rol_name 
+                 FROM ". TBL_ROLES. "
+                WHERE rol_org_shortname = '$g_organization'
+                  AND rol_valid        = 1
+                ORDER BY rol_name";
 }
 elseif(isGroupLeader())
 {
-   // Alle Rollen auflisten, bei denen das Mitglied Leiter ist
-   $sql    = "SELECT rol_id, rol_name 
-                FROM ". TBL_MEMBERS. ", ". TBL_ROLES. "
-               WHERE mem_usr_id  = $g_current_user->id
-                 AND mem_valid  = 1
-                 AND mem_leader = 1
-                 AND rol_id     = mem_rol_id
-                 AND rol_org_shortname = '$g_organization'
-                 AND rol_valid        = 1
-                 AND rol_locked     = 0
-               ORDER BY rol_name";
+    // Alle Rollen auflisten, bei denen das Mitglied Leiter ist
+    $sql    = "SELECT rol_id, rol_name 
+                 FROM ". TBL_MEMBERS. ", ". TBL_ROLES. "
+                WHERE mem_usr_id  = $g_current_user->id
+                  AND mem_valid  = 1
+                  AND mem_leader = 1
+                  AND rol_id     = mem_rol_id
+                  AND rol_org_shortname = '$g_organization'
+                  AND rol_valid        = 1
+                  AND rol_locked     = 0
+                ORDER BY rol_name";
 }
 elseif(editUser())
 {
-   // Alle Rollen auflisten, die keinen Moderatorenstatus haben
-   $sql    = "SELECT rol_id, rol_name 
-   				 FROM ". TBL_ROLES. "
-               WHERE rol_org_shortname = '$g_organization'
-                 AND rol_valid        = 1
-                 AND rol_moderation = 0
-                 AND rol_locked     = 0
-               ORDER BY rol_name";
+    // Alle Rollen auflisten, die keinen Moderatorenstatus haben
+    $sql    = "SELECT rol_id, rol_name 
+                 FROM ". TBL_ROLES. "
+                WHERE rol_org_shortname = '$g_organization'
+                  AND rol_valid        = 1
+                  AND rol_moderation = 0
+                  AND rol_locked     = 0
+                ORDER BY rol_name";
 }
 $result_rolle = mysql_query($sql, $g_adm_con);
 db_error($result_rolle);
@@ -86,148 +86,152 @@ $parentRoles = array();
 
 while($row = mysql_fetch_object($result_rolle))
 {
-	// der Webmaster-Rolle duerfen nur Webmaster neue Mitglieder zuweisen
-	if($row->rol_name != 'Webmaster' || hasRole('Webmaster'))
-	{
-		if($key == "role-$i")
-		{
-			$function = 1;
-			$value    = next($_POST);
-			$key      = key($_POST);
-		}
-		else
-			$function = 0;
+    // der Webmaster-Rolle duerfen nur Webmaster neue Mitglieder zuweisen
+    if($row->rol_name != 'Webmaster' || hasRole('Webmaster'))
+    {
+        if($key == "role-$i")
+        {
+            $function = 1;
+            $value    = next($_POST);
+            $key      = key($_POST);
+        }
+        else
+        {
+            $function = 0;
+        }
 
-		if($key == "leader-$i")
-		{
-			$leiter   = 1;
-			$value    = next($_POST);
-			$key      = key($_POST);
-		}
-		else
-			$leiter   = 0;
+        if($key == "leader-$i")
+        {
+            $leiter   = 1;
+            $value    = next($_POST);
+            $key      = key($_POST);
+        }
+        else
+        {
+            $leiter   = 0;
+        }
 
-		$sql    = "SELECT * FROM ". TBL_MEMBERS. ", ". TBL_ROLES. "
-						WHERE mem_rol_id = $row->rol_id
-						  AND mem_usr_id = {0}
-						  AND mem_rol_id = rol_id ";
-		$sql    = prepareSQL($sql, array($_GET['user_id']));
-		$result = mysql_query($sql, $g_adm_con);
-		db_error($result);
+        $sql    = "SELECT * FROM ". TBL_MEMBERS. ", ". TBL_ROLES. "
+                    WHERE mem_rol_id = $row->rol_id
+                      AND mem_usr_id = {0}
+                      AND mem_rol_id = rol_id ";
+        $sql    = prepareSQL($sql, array($_GET['user_id']));
+        $result = mysql_query($sql, $g_adm_con);
+        db_error($result);
 
-		$user_found = mysql_num_rows($result);
+        $user_found = mysql_num_rows($result);
 
-		if($user_found > 0)
-		{
-			// neue Mitgliederdaten zurueckschreiben
-			if($function == 1)
-			{
-				$sql = "UPDATE ". TBL_MEMBERS. " SET mem_valid  = 1
-															, mem_end   = '0000-00-00'
-															, mem_leader = $leiter
-							WHERE mem_rol_id = $row->rol_id
-							  AND mem_usr_id = {0}";
-				$count_assigned++;
-			}
-			else
-			{
-				$sql = "UPDATE ". TBL_MEMBERS. " SET mem_valid  = 0
-															, mem_end   = NOW()
-															, mem_leader = $leiter
-							WHERE mem_rol_id = $row->rol_id
-							  AND mem_usr_id = {0}";
-			}
-		}
-		else
-		{
-			// neue Mitgliederdaten einfuegen, aber nur, wenn auch ein Haeckchen da ist
-			if($function == 1)
-			{
-				$sql = "INSERT INTO ". TBL_MEMBERS. " (mem_rol_id, mem_usr_id, mem_begin,mem_end, mem_valid, mem_leader)
-						  VALUES ($row->rol_id, {0}, NOW(),0000-00-00, 1, $leiter) ";
-				$count_assigned++;
-			}
-		}
-		$sql    = prepareSQL($sql, array($_GET['user_id']));
-		$result = mysql_query($sql, $g_adm_con);
-		db_error($result);
-		
-		//find the parent roles
-		if($function == 1 && $user_found < 1)
-		{
-			//$roleDepSrc = new TblRoleDependencies($g_adm_con);
-			$tmpRoles = TblRoleDependencies::getParentRoles($g_adm_con,$row->rol_id);
-			
-			foreach($tmpRoles as $tmpRole)
-			{
-				if(!in_array($tmpRole,$parentRoles))
-				$parentRoles[] = $tmpRole;
-			} 		
-			
-		}
-		
-	}
+        if($user_found > 0)
+        {
+            // neue Mitgliederdaten zurueckschreiben
+            if($function == 1)
+            {
+                $sql = "UPDATE ". TBL_MEMBERS. " SET mem_valid  = 1
+                                                   , mem_end   = '0000-00-00'
+                                                   , mem_leader = $leiter
+                            WHERE mem_rol_id = $row->rol_id
+                              AND mem_usr_id = {0}";
+                $count_assigned++;
+            }
+            else
+            {
+                $sql = "UPDATE ". TBL_MEMBERS. " SET mem_valid  = 0
+                                                   , mem_end   = NOW()
+                                                   , mem_leader = $leiter
+                            WHERE mem_rol_id = $row->rol_id
+                              AND mem_usr_id = {0}";
+            }
+        }
+        else
+        {
+            // neue Mitgliederdaten einfuegen, aber nur, wenn auch ein Haeckchen da ist
+            if($function == 1)
+            {
+                $sql = "INSERT INTO ". TBL_MEMBERS. " (mem_rol_id, mem_usr_id, mem_begin,mem_end, mem_valid, mem_leader)
+                          VALUES ($row->rol_id, {0}, NOW(),0000-00-00, 1, $leiter) ";
+                $count_assigned++;
+            }
+        }
+        $sql    = prepareSQL($sql, array($_GET['user_id']));
+        $result = mysql_query($sql, $g_adm_con);
+        db_error($result);
+        
+        //find the parent roles
+        if($function == 1 && $user_found < 1)
+        {
+            //$roleDepSrc = new TblRoleDependencies($g_adm_con);
+            $tmpRoles = TblRoleDependencies::getParentRoles($g_adm_con,$row->rol_id);
+            
+            foreach($tmpRoles as $tmpRole)
+            {
+                if(!in_array($tmpRole,$parentRoles))
+                $parentRoles[] = $tmpRole;
+            }       
+            
+        }
+        
+    }
 
    $i++;
 }
 
 foreach($parentRoles as $actRole)
-			{
-				$sql = "INSERT INTO ". TBL_MEMBERS. " (mem_rol_id, mem_usr_id, mem_begin,mem_end, mem_valid, mem_leader)
-						  VALUES ($actRole, {0}, NOW(), 0000-00-00, 1, $leiter)
-						  		 ON DUPLICATE KEY UPDATE mem_end = 0000-00-00";
-						  
-						  
-				$sql    = prepareSQL($sql, array($_GET['user_id']));
-				$result = mysql_query($sql, $g_adm_con);				
-				db_error($result);
-			}
+{
+    $sql = "INSERT INTO ". TBL_MEMBERS. " (mem_rol_id, mem_usr_id, mem_begin,mem_end, mem_valid, mem_leader)
+              VALUES ($actRole, {0}, NOW(), 0000-00-00, 1, $leiter)
+                     ON DUPLICATE KEY UPDATE mem_end = 0000-00-00";
+
+
+    $sql    = prepareSQL($sql, array($_GET['user_id']));
+    $result = mysql_query($sql, $g_adm_con);                
+    db_error($result);
+}
 
 if($_GET['new_user'] == 1 && $count_assigned == 0)
 {
-   // Neuem User wurden keine Rollen zugewiesen
-   $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=norolle";
-   header($location);
-   exit();
+    // Neuem User wurden keine Rollen zugewiesen
+    $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=norolle";
+    header($location);
+    exit();
 }
 
 if($_GET['popup'] == 1)
 {
-   echo "
-   <?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?". ">
-   <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 TRANSITIONAL//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
-   <html xmlns=\"http://www.w3.org/1999/xhtml\">
-   <head>
-      <!-- (c) 2004 - 2006 The Admidio Team - http://www.admidio.org - Version: ". getVersion(). " -->
-      <title>Funktionen zuordnen</title>
-      <meta http-equiv=\"content-type\" content=\"text/html; charset=ISO-8859-1\" />
-      <link rel=\"stylesheet\" type=\"text/css\" href=\"$g_root_path/adm_config/main.css\" />
-      
-      <!--[if gte IE 5.5000]>
-      <script language=\"JavaScript\" src=\"$g_root_path/adm_program/system/correct_png.js\"></script>
-      <![endif]-->
-   </head>
+    echo "
+    <?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?". ">
+    <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 TRANSITIONAL//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
+    <html xmlns=\"http://www.w3.org/1999/xhtml\">
+    <head>
+        <!-- (c) 2004 - 2006 The Admidio Team - http://www.admidio.org - Version: ". getVersion(). " -->
+        <title>Funktionen zuordnen</title>
+        <meta http-equiv=\"content-type\" content=\"text/html; charset=ISO-8859-1\" />
+        <link rel=\"stylesheet\" type=\"text/css\" href=\"$g_root_path/adm_config/main.css\" />
 
-   <body>
-   	  <div align=\"center\"><br />
-         <div class=\"groupBox\" align=\"left\" style=\"padding: 10px\">
-            <p>Die &Auml;nderungen wurden erfolgreich gespeichert.</p>
-            <p>Bitte denk daran, das Profil im Browser neu zu laden,
-            damit die ge&auml;nderten Rollen angezeigt werden.</p>
-         </div>
-         <div style=\"padding-top: 10px;\" align=\"center\">
-            <button name=\"schliessen\" type=\"button\" value=\"schliessen\" onclick=\"window.close()\">
-            <img src=\"$g_root_path/adm_program/images/door_ing.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\">
-            &nbsp;Schlie&szlig;en</button>
-         </div>
-      </div>
-   </body>
-   </html>";
+        <!--[if gte IE 5.5000]>
+        <script language=\"JavaScript\" src=\"$g_root_path/adm_program/system/correct_png.js\"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+        <div align=\"center\"><br />
+            <div class=\"groupBox\" align=\"left\" style=\"padding: 10px\">
+                <p>Die &Auml;nderungen wurden erfolgreich gespeichert.</p>
+                <p>Bitte denk daran, das Profil im Browser neu zu laden,
+                damit die ge&auml;nderten Rollen angezeigt werden.</p>
+            </div>
+            <div style=\"padding-top: 10px;\" align=\"center\">
+                <button name=\"schliessen\" type=\"button\" value=\"schliessen\" onclick=\"window.close()\">
+                <img src=\"$g_root_path/adm_program/images/door_ing.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\">
+                &nbsp;Schlie&szlig;en</button>
+            </div>
+        </div>
+    </body>
+    </html>";
 }
 else
 {
-   // zur Ausgangsseite zurueck
-   $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=save&url=". $_GET['url']. "&timer=2000";
-   header($location);
-   exit();
+    // zur Ausgangsseite zurueck
+    $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=save&url=". urlencode($_GET['url']). "&timer=2000";
+    header($location);
+    exit();
 }
