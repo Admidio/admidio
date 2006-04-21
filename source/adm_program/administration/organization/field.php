3aae173f03e951059379c82df4eb3f26b74687ab
Markus Faßbender <markus.fassbender@gmail.com>
2006-04-21 12:49:01 +0000

    Focus bei Formularen auf das erste Feld setzen BUG: 1334049

--- source/adm_program/administration/organization/field.php
+++ source/adm_program/administration/organization/field.php
@@ -36,16 +36,20 @@ require("../../system/login_valid.php");
 // nur Moderatoren duerfen Profilfelder erfassen & verwalten
 if(!isModerator())
 {
-   $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=norights";
-   header($location);
-   exit();
+    $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=norights";
+    header($location);
+    exit();
 }
 
 // wenn URL uebergeben wurde zu dieser gehen, ansonsten zurueck
 if(array_key_exists('url', $_GET))
-   $url = $_GET['url'];
+{
+    $url = $_GET['url'];
+}
 else
-   $url = urlencode(getHttpReferer());
+{
+    $url = urlencode(getHttpReferer());
+}
 
 $field_type        = "";
 $field_name        = "";
@@ -56,106 +60,137 @@ $field_locked      = 0;
 // -> Felder mit Daten des Feldes vorbelegen
 
 if ($_GET["usf_id"] != 0)
- {
-   $sql    = "SELECT * FROM ". TBL_USER_FIELDS. " WHERE usf_id = {0}";
-   $sql    = prepareSQL($sql, array($_GET['usf_id']));
-   $result = mysql_query($sql, $g_adm_con);
-   db_error($result);
+{
+    $sql    = "SELECT * FROM ". TBL_USER_FIELDS. " WHERE usf_id = {0}";
+    $sql    = prepareSQL($sql, array($_GET['usf_id']));
+    $result = mysql_query($sql, $g_adm_con);
+    db_error($result);
 
-   if (mysql_num_rows($result) > 0)
-   {
-      $row_usf = mysql_fetch_object($result);
+    if (mysql_num_rows($result) > 0)
+    {
+        $row_usf = mysql_fetch_object($result);
 
-      $field_type        = $row_usf->usf_type;
-      $field_name        = $row_usf->usf_name;
-      $field_description = $row_usf->usf_description;
-      $field_locked      = $row_usf->usf_locked;
-   }
- }
+        $field_type        = $row_usf->usf_type;
+        $field_name        = $row_usf->usf_name;
+        $field_description = $row_usf->usf_description;
+        $field_locked      = $row_usf->usf_locked;
+    }
+}
 
 echo "
 <!-- (c) 2004 - 2006 The Admidio Team - http://www.admidio.org - Version: ". getVersion(). " -->\n
 <!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">
 <html>
 <head>
-   <title>$g_current_organization->longname - Rolle</title>
-   <link rel=\"stylesheet\" type=\"text/css\" href=\"$g_root_path/adm_config/main.css\">
+    <title>$g_current_organization->longname - Rolle</title>
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"$g_root_path/adm_config/main.css\">
 
-   <!--[if gte IE 5.5000]>
-   <script type=\"text/javascript\" src=\"$g_root_path/adm_program/system/correct_png.js\"></script>
-   <![endif]-->";
+    <!--[if gte IE 5.5000]>
+    <script type=\"text/javascript\" src=\"$g_root_path/adm_program/system/correct_png.js\"></script>
+    <![endif]-->";
 
-   require("../../../adm_config/header.php");
+    require("../../../adm_config/header.php");
 echo "</head>";
 
 require("../../../adm_config/body_top.php");
-   echo "
-   <div style=\"margin-top: 10px; margin-bottom: 10px;\" align=\"center\">
+    echo "<div style=\"margin-top: 10px; margin-bottom: 10px;\" align=\"center\">
+        <form action=\"field_function.php?usf_id=". $_GET['usf_id']. "&amp;mode=1&amp;url=$url\" method=\"post\" id=\"edit_field\">
+            <div class=\"formHead\" style=\"width: 400px\">";
+                if($_GET['usf_id'] > 0)
+                {
+                    echo strspace("Feld ändern", 2);
+                }
+                else
+                {
+                    echo strspace("Feld anlegen", 2);
+                }
+            echo "</div>
+            <div class=\"formBody\" style=\"width: 400px\">
+                <div>
+                    <div style=\"text-align: right; width: 28%; float: left;\">Name:</div>
+                    <div style=\"text-align: left; margin-left: 29%;\">
+                        <input type=\"text\" id=\"name\" name=\"name\" size=\"30\" maxlength=\"100\" value=\"". htmlspecialchars($field_name, ENT_QUOTES). "\">
+                    </div>
+                </div>
+                <div style=\"margin-top: 6px;\">
+                    <div style=\"text-align: right; width: 28%; float: left;\">Beschreibung:</div>
+                    <div style=\"text-align: left; margin-left: 29%;\">
+                        <input type=\"text\" name=\"description\" size=\"38\" maxlength=\"255\" value=\"". htmlspecialchars($field_description, ENT_QUOTES). "\">
+                    </div>
+                </div>
+                <div style=\"margin-top: 6px;\">
+                    <div style=\"text-align: right; width: 28%; float: left;\">Datentyp:</div>
+                    <div style=\"text-align: left; margin-left: 29%;\">
+                        <select size=\"1\" name=\"typ\">
+                            <option value=\"\""; 
+                                if(strlen($field_type) == 0) 
+                                {
+                                    echo " selected=\"selected\"";
+                                }
+                                echo ">&nbsp;</option>\n
+                            <option value=\"TEXT\"";     
+                                if($field_type == "TEXT") 
+                                {
+                                    echo " selected=\"selected\""; 
+                                }
+                                echo ">Text (30 Zeichen)</option>\n
+                            <option value=\"TEXT_BIG\""; 
+                                if($field_type == "TEXT_BIG") 
+                                {
+                                    echo " selected=\"selected\""; 
+                                }
+                                echo ">Text (255 Zeichen)</option>\n
+                            <option value=\"NUMERIC\"";  
+                                if($field_type == "NUMERIC") 
+                                {
+                                    echo " selected=\"selected\""; 
+                                }
+                                echo ">Zahl</option>\n
+                            <option value=\"CHECKBOX\""; 
+                                if($field_type == "CHECKBOX") 
+                                {
+                                    echo " selected=\"selected\""; 
+                                }
+                                echo ">Ja / Nein</option>\n
+                        </select>
+                    </div>
+                </div>
+                <div style=\"margin-top: 6px;\">
+                    <div style=\"text-align: right; width: 28%; float: left;\">
+                        <img src=\"$g_root_path/adm_program/images/lock.png\" alt=\"Feld nur für Moderatoren sichtbar\">
+                    </div>
+                    <div style=\"text-align: left; margin-left: 29%;\">
+                        <input type=\"checkbox\" id=\"locked\" name=\"locked\" ";
+                        if($field_locked == 1)
+                        {
+                            echo " checked ";
+                        }
+                        echo " value=\"1\" />
+                        <label for=\"locked\">Feld nur für Moderatoren sichtbar&nbsp;</label>
+                        <img src=\"$g_root_path/adm_program/images/help.png\" style=\"cursor: pointer; vertical-align: middle;\" vspace=\"1\" align=\"top\" width=\"16\" height=\"16\" border=\"0\" alt=\"Hilfe\" title=\"Hilfe\"
+                        onclick=\"window.open('$g_root_path/adm_program/system/msg_window.php?err_code=field_locked','Message','width=400,height=200,left=310,top=200,scrollbars=yes')\">
+                    </div>
+                </div>
 
-   <form action=\"field_function.php?usf_id=". $_GET['usf_id']. "&amp;mode=1&amp;url=$url\" method=\"post\" id=\"edit_field\">
-      <div class=\"formHead\" style=\"width: 400px\">";
-         if($_GET['usf_id'] > 0)
-            echo strspace("Feld ändern", 2);
-         else
-            echo strspace("Feld anlegen", 2);
-      echo "</div>
-      <div class=\"formBody\" style=\"width: 400px\">
-         <div>
-            <div style=\"text-align: right; width: 28%; float: left;\">Name:</div>
-            <div style=\"text-align: left; margin-left: 29%;\">
-               <input type=\"text\" name=\"name\" size=\"30\" maxlength=\"100\" value=\"". htmlspecialchars($field_name, ENT_QUOTES). "\">
-            </div>
-         </div>
-         <div style=\"margin-top: 6px;\">
-            <div style=\"text-align: right; width: 28%; float: left;\">Beschreibung:</div>
-            <div style=\"text-align: left; margin-left: 29%;\">
-               <input type=\"text\" name=\"description\" size=\"38\" maxlength=\"255\" value=\"". htmlspecialchars($field_description, ENT_QUOTES). "\">
-            </div>
-         </div>
-         <div style=\"margin-top: 6px;\">
-            <div style=\"text-align: right; width: 28%; float: left;\">Datentyp:</div>
-            <div style=\"text-align: left; margin-left: 29%;\">
-               <select size=\"1\" name=\"typ\">
-                  <option value=\"\""; if(strlen($field_type) == 0) echo " selected=\"selected\""; echo ">&nbsp;</option>\n
-                  <option value=\"TEXT\"";     if($field_type == "TEXT") echo " selected=\"selected\""; echo ">Text (30 Zeichen)</option>\n
-                  <option value=\"TEXT_BIG\""; if($field_type == "TEXT_BIG") echo " selected=\"selected\""; echo ">Text (255 Zeichen)</option>\n
-                  <option value=\"NUMERIC\"";  if($field_type == "NUMERIC") echo " selected=\"selected\""; echo ">Zahl</option>\n
-                  <option value=\"CHECKBOX\""; if($field_type == "CHECKBOX") echo " selected=\"selected\""; echo ">Ja / Nein</option>\n
-               </select>
-            </div>
-         </div>
-         <div style=\"margin-top: 6px;\">
-            <div style=\"text-align: right; width: 28%; float: left;\">
-               <img src=\"$g_root_path/adm_program/images/lock.png\" alt=\"Feld nur für Moderatoren sichtbar\">
-            </div>
-            <div style=\"text-align: left; margin-left: 29%;\">
-               <input type=\"checkbox\" id=\"locked\" name=\"locked\" ";
-               if($field_locked == 1)
-                  echo " checked ";
-               echo " value=\"1\" />
-               <label for=\"locked\">Feld nur für Moderatoren sichtbar&nbsp;</label>
-               <img src=\"$g_root_path/adm_program/images/help.png\" style=\"cursor: pointer; vertical-align: middle;\" vspace=\"1\" align=\"top\" width=\"16\" height=\"16\" border=\"0\" alt=\"Hilfe\" title=\"Hilfe\"
-               onclick=\"window.open('$g_root_path/adm_program/system/msg_window.php?err_code=field_locked','Message','width=400,height=200,left=310,top=200,scrollbars=yes')\">
-            </div>
-         </div>
+                <hr width=\"85%\" />
 
-         <hr width=\"85%\" />
+                <div style=\"margin-top: 6px;\">
+                    <button name=\"zurueck\" type=\"button\" value=\"zurueck\" onclick=\"history.back()\">
+                    <img src=\"$g_root_path/adm_program/images/back.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Zur&uuml;ck\">
+                    &nbsp;Zur&uuml;ck</button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <button name=\"speichern\" type=\"submit\" value=\"speichern\">
+                    <img src=\"$g_root_path/adm_program/images/disk.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Speichern\">
+                    &nbsp;Speichern</button>
+                </div>";
+            echo "</div>
+        </form>
+    </div>
+    <script type=\"text/javascript\"><!--
+        document.getElementById('name').focus();
+    --></script>";
 
-         <div style=\"margin-top: 6px;\">
-            <button name=\"zurueck\" type=\"button\" value=\"zurueck\" onclick=\"history.back()\">
-            <img src=\"$g_root_path/adm_program/images/back.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Zur&uuml;ck\">
-            &nbsp;Zur&uuml;ck</button>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <button name=\"speichern\" type=\"submit\" value=\"speichern\">
-            <img src=\"$g_root_path/adm_program/images/disk.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Speichern\">
-            &nbsp;Speichern</button>
-         </div>";
-      echo "</div>
-   </form>
-
-   </div>";
-
-   require("../../../adm_config/body_bottom.php");
+    require("../../../adm_config/body_bottom.php");
 echo "</body>
 </html>";
 ?>
--- source/adm_program/administration/roles/categories.php
+++ source/adm_program/administration/roles/categories.php
@@ -36,16 +36,20 @@ require("../../system/login_valid.php");
 // nur Moderatoren duerfen Kategorien erfassen & verwalten
 if(!isModerator())
 {
-   $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=norights";
-   header($location);
-   exit();
+    $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=norights";
+    header($location);
+    exit();
 }
 
 // wenn URL uebergeben wurde zu dieser gehen, ansonsten zurueck
 if(array_key_exists('url', $_GET))
-   $url = $_GET['url'];
+{
+    $url = $_GET['url'];
+}
 else
-   $url = urlencode(getHttpReferer());
+{
+    $url = urlencode(getHttpReferer());
+}
 
 $category_name = "";
 
@@ -53,71 +57,74 @@ $category_name = "";
 // -> Felder mit Daten des Feldes vorbelegen
 
 if ($_GET["rlc_id"] != 0)
- {
-   $sql    = "SELECT * FROM ". TBL_ROLE_CATEGORIES. " WHERE rlc_id = {0}";
-   $sql    = prepareSQL($sql, array($_GET['rlc_id']));
-   $result = mysql_query($sql, $g_adm_con);
-   db_error($result);
+{
+    $sql    = "SELECT * FROM ". TBL_ROLE_CATEGORIES. " WHERE rlc_id = {0}";
+    $sql    = prepareSQL($sql, array($_GET['rlc_id']));
+    $result = mysql_query($sql, $g_adm_con);
+    db_error($result);
 
-   if (mysql_num_rows($result) > 0)
-   {
-      $row_rlc = mysql_fetch_object($result);
-
-      $category_name = $row_rlc->rlc_name;
-   }
- }
+    if (mysql_num_rows($result) > 0)
+    {
+        $row_rlc = mysql_fetch_object($result);
+        $category_name = $row_rlc->rlc_name;
+    }
+}
 
 echo "
 <!-- (c) 2004 - 2006 The Admidio Team - http://www.admidio.org - Version: ". getVersion(). " -->\n
 <!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">
 <html>
 <head>
-   <title>$g_current_organization->longname - Kategorie</title>
-   <link rel=\"stylesheet\" type=\"text/css\" href=\"$g_root_path/adm_config/main.css\">
+    <title>$g_current_organization->longname - Kategorie</title>
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"$g_root_path/adm_config/main.css\">
 
-   <!--[if gte IE 5.5000]>
-   <script type=\"text/javascript\" src=\"$g_root_path/adm_program/system/correct_png.js\"></script>
-   <![endif]-->";
+    <!--[if gte IE 5.5000]>
+    <script type=\"text/javascript\" src=\"$g_root_path/adm_program/system/correct_png.js\"></script>
+    <![endif]-->";
 
-   require("../../../adm_config/header.php");
+    require("../../../adm_config/header.php");
 echo "</head>";
 
 require("../../../adm_config/body_top.php");
-   echo "
-   <div style=\"margin-top: 10px; margin-bottom: 10px;\" align=\"center\">
+    echo "<div style=\"margin-top: 10px; margin-bottom: 10px;\" align=\"center\">
+        <form action=\"categories_function.php?rlc_id=". $_GET['rlc_id']. "&amp;mode=1&amp;url=$url\" method=\"post\" id=\"edit_category\">
+            <div class=\"formHead\" style=\"width: 300px\">";
+                if($_GET['rlc_id'] > 0)
+                {
+                    echo strspace("Kategorie ändern");
+                }
+                else
+                {
+                    echo strspace("Kategorie anlegen");
+                }
+            echo "</div>
+            <div class=\"formBody\" style=\"width: 300px\">
+                <div>
+                    <div style=\"text-align: right; width: 23%; float: left;\">Name:</div>
+                    <div style=\"text-align: left; margin-left: 24%;\">
+                        <input type=\"text\" id=\"name\" name=\"name\" size=\"30\" maxlength=\"100\" value=\"". htmlspecialchars($category_name, ENT_QUOTES). "\">
+                    </div>
+                </div>
 
-   <form action=\"categories_function.php?rlc_id=". $_GET['rlc_id']. "&amp;mode=1&amp;url=$url\" method=\"post\" id=\"edit_category\">
-      <div class=\"formHead\" style=\"width: 300px\">";
-         if($_GET['rlc_id'] > 0)
-            echo strspace("Kategorie ändern");
-         else
-            echo strspace("Kategorie anlegen");
-      echo "</div>
-      <div class=\"formBody\" style=\"width: 300px\">
-         <div>
-            <div style=\"text-align: right; width: 23%; float: left;\">Name:</div>
-            <div style=\"text-align: left; margin-left: 24%;\">
-               <input type=\"text\" name=\"name\" size=\"30\" maxlength=\"100\" value=\"". htmlspecialchars($category_name, ENT_QUOTES). "\">
-            </div>
-         </div>
+                <hr width=\"85%\" />
 
-         <hr width=\"85%\" />
+                <div style=\"margin-top: 6px;\">
+                    <button id=\"zurueck\" type=\"button\" value=\"zurueck\" onclick=\"history.back()\">
+                    <img src=\"$g_root_path/adm_program/images/back.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Zur&uuml;ck\">
+                    &nbsp;Zur&uuml;ck</button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <button id=\"speichern\" type=\"submit\" value=\"speichern\">
+                    <img src=\"$g_root_path/adm_program/images/disk.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Speichern\">
+                    &nbsp;Speichern</button>
+                </div>";
+            echo "</div>
+        </form>
+    </div>
+    <script type=\"text/javascript\"><!--
+        document.getElementById('name').focus();
+    --></script>";
 
-         <div style=\"margin-top: 6px;\">
-            <button id=\"zurueck\" type=\"button\" value=\"zurueck\" onclick=\"history.back()\">
-            <img src=\"$g_root_path/adm_program/images/back.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Zur&uuml;ck\">
-            &nbsp;Zur&uuml;ck</button>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <button id=\"speichern\" type=\"submit\" value=\"speichern\">
-            <img src=\"$g_root_path/adm_program/images/disk.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Speichern\">
-            &nbsp;Speichern</button>
-         </div>";
-      echo "</div>
-   </form>
-
-   </div>";
-
-   require("../../../adm_config/body_bottom.php");
+    require("../../../adm_config/body_bottom.php");
 echo "</body>
 </html>";
 ?>
