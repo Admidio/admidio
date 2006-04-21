9ae97820ef85ff6ab1fe956ff941af1dcf81938b
Markus Faßbender <markus.fassbender@gmail.com>
2006-04-21 12:29:37 +0000

    "Zurueck"-Buttons alle Links im Dialog RFE: 1472963 Focus bei Formularen auf das erste Feld setzen BUG: 1334049

--- source/adm_program/modules/download/rename.php
+++ source/adm_program/modules/download/rename.php
@@ -28,108 +28,113 @@
  *
  *****************************************************************************/
 
-	require("../../system/common.php");
-	require("../../system/login_valid.php");
-   
-   // erst prüfen, ob der User auch die entsprechenden Rechte hat
-   if(!editDownload())
-   {
-      $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=norights";
-      header($location);
-      exit();
-   }
-   
+require("../../system/common.php");
+require("../../system/login_valid.php");
 
-   $default_folder = urldecode($_GET['default_folder']);
-   $folder     = urldecode($_GET['folder']);
-   $file       = $_GET['file'];
-   $act_folder = "../../../adm_my_files/download";
+// erst prüfen, ob der User auch die entsprechenden Rechte hat
+if(!editDownload())
+{
+    $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=norights";
+    header($location);
+    exit();
+}
 
-   // uebergebene Ordner auf Gueltigkeit pruefen
-   // und Ordnerpfad zusammensetzen
-   if(strlen($default_folder) > 0)
-   {
-      if(strpos($default_folder, "..") !== false)
-      {
-         $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=invalid_folder";
-         header($location);
-         exit();
-      }
-      $act_folder = "$act_folder/$default_folder";
-   }
 
-   if(strlen($folder) > 0)
-   {
-      if(strpos($folder, "..") !== false)
-      {
-         $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=invalid_folder";
-         header($location);
-         exit();
-      }
-      $act_folder = "$act_folder/$folder";
-   }
-   
-   if(strpos($file, "..") !== false
-   || strlen($file) == 0)
-   {
-      $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=invalid_file";
-      header($location);
-      exit();
-   }
+$default_folder = urldecode($_GET['default_folder']);
+$folder     = urldecode($_GET['folder']);
+$file       = $_GET['file'];
+$act_folder = "../../../adm_my_files/download";
 
-   //Beginn der Seite
-   echo "
-   <!-- (c) 2004 - 2006 The Admidio Team - http://www.admidio.org - Version: ". getVersion(). " -->\n
-   <!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">
-   <html>
-   <head>
-      <title>$g_current_organization->longname - Umbenennen</title>
-      <link rel=\"stylesheet\" type=\"text/css\" href=\"$g_root_path/adm_config/main.css\">
+// uebergebene Ordner auf Gueltigkeit pruefen
+// und Ordnerpfad zusammensetzen
+if(strlen($default_folder) > 0)
+{
+    if(strpos($default_folder, "..") !== false)
+    {
+        $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=invalid_folder";
+        header($location);
+        exit();
+    }
+    $act_folder = "$act_folder/$default_folder";
+}
 
-      <!--[if gte IE 5.5000]>
-      <script type=\"text/javascript\" src=\"$g_root_path/adm_program/system/correct_png.js\"></script>
-      <![endif]-->";
+if(strlen($folder) > 0)
+{
+    if(strpos($folder, "..") !== false)
+    {
+        $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=invalid_folder";
+        header($location);
+        exit();
+    }
+    $act_folder = "$act_folder/$folder";
+}
 
-      require("../../../adm_config/header.php");
-   echo "</head>";
-   
-   require("../../../adm_config/body_top.php");
-   //Beginn des Inhaltes
-   echo "<div style=\"margin-top: 10px; margin-bottom: 10px;\" align=\"center\">";
-   
-   $datei = explode(".","$file");
-   echo "<p>&nbsp;</p>
-      <form method=\"POST\" action=\"download_function.php?mode=4&amp;folder=". urlencode($folder). "&amp;default_folder=". urlencode($default_folder). "&amp;file=". urlencode($file). "\">
-         <div class=\"formHead\" style=\"width: 400px\">Datei/Ordner umbenennen</div>
-         <div class=\"formBody\" style=\"width: 400px\">
-            <div>
-               <div style=\"text-align: right; width: 35%; float: left;\">Bisheriger Name:</div>
-               <div style=\"text-align: left; margin-left: 37%;\">$datei[0]</div>
+if(strpos($file, "..") !== false
+|| strlen($file) == 0)
+{
+    $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=invalid_file";
+    header($location);
+    exit();
+}
+
+//Beginn der Seite
+echo "
+<!-- (c) 2004 - 2006 The Admidio Team - http://www.admidio.org - Version: ". getVersion(). " -->\n
+<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">
+<html>
+<head>
+    <title>$g_current_organization->longname - Umbenennen</title>
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"$g_root_path/adm_config/main.css\">
+
+    <!--[if gte IE 5.5000]>
+    <script type=\"text/javascript\" src=\"$g_root_path/adm_program/system/correct_png.js\"></script>
+    <![endif]-->";
+    require("../../../adm_config/header.php");
+echo "</head>";
+
+require("../../../adm_config/body_top.php");
+    //Beginn des Inhaltes
+    echo "<div style=\"margin-top: 10px; margin-bottom: 10px;\" align=\"center\">";
+        $datei = explode(".","$file");
+        echo "<p>&nbsp;</p>
+        <form method=\"POST\" action=\"download_function.php?mode=4&amp;folder=". urlencode($folder). "&amp;default_folder=". urlencode($default_folder). "&amp;file=". urlencode($file). "\">
+            <div class=\"formHead\" style=\"width: 400px\">Datei/Ordner umbenennen</div>
+            <div class=\"formBody\" style=\"width: 400px\">
+                <div>
+                    <div style=\"text-align: right; width: 35%; float: left;\">Bisheriger Name:</div>
+                    <div style=\"text-align: left; margin-left: 37%;\">$datei[0]</div>
+                </div>
+                <div style=\"margin-top: 10px;\">
+                    <div style=\"text-align: right; width: 35%; float: left;\">Neuer Name:</div>
+                    <div style=\"text-align: left; margin-left: 37%;\">
+                        <input type=\"text\" id=\"new_name\" name=\"new_name\" size=\"25\" tabindex=\"1\">";
+                        if(strlen($datei[1]) > 0)
+                        {
+                            echo ".$datei[1]";
+                        }
+                        echo "&nbsp;<img src=\"$g_root_path/adm_program/images/help.png\" style=\"cursor: pointer; vertical-align: top;\" vspace=\"1\" width=\"16\" height=\"16\" border=\"0\" alt=\"Hilfe\" title=\"Hilfe\"
+                        onclick=\"window.open('$g_root_path/adm_program/system/msg_window.php?err_code=dateiname','Message','width=400,height=250,left=310,top=200,scrollbars=yes')\">
+                    </div>
+                </div>
+
+                <hr style=\"margin-top: 10px; margin-bottom: 10px;\" width=\"85%\" />
+
+                <div style=\"margin-top: 6px;\">
+                    <button name=\"zurueck\" type=\"button\" value=\"zurueck\" onclick=\"history.back()\">
+                    <img src=\"$g_root_path/adm_program/images/back.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Zur&uuml;ck\">
+                    &nbsp;Zur&uuml;ck</button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <button name=\"umbenennen\" type=\"submit\" value=\"umbenennen\" tabindex=\"2\">
+                    <img src=\"$g_root_path/adm_program/images/edit.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Hochladen\">
+                    &nbsp;Umbenennen</button>
+                </div>
             </div>
-            <div style=\"margin-top: 10px;\">
-               <div style=\"text-align: right; width: 35%; float: left;\">Neuer Name:</div>
-               <div style=\"text-align: left; margin-left: 37%;\">
-                  <input type=\"text\" name=\"new_name\" size=\"25\" tabindex=\"1\">.$datei[1]
-                  &nbsp;<img src=\"$g_root_path/adm_program/images/help.png\" style=\"cursor: pointer; vertical-align: top;\" vspace=\"1\" width=\"16\" height=\"16\" border=\"0\" alt=\"Hilfe\" title=\"Hilfe\"
-                  onclick=\"window.open('$g_root_path/adm_program/system/msg_window.php?err_code=dateiname','Message','width=400,height=250,left=310,top=200,scrollbars=yes')\">               </div>
-            </div>
-
-            <hr style=\"margin-top: 10px; margin-bottom: 10px;\" width=\"85%\" />
-
-            <div style=\"margin-top: 6px;\">
-               <button name=\"umbenennen\" type=\"submit\" value=\"umbenennen\" tabindex=\"2\">
-               <img src=\"$g_root_path/adm_program/images/edit.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Hochladen\">
-               &nbsp;Umbenennen</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-               <button name=\"zurueck\" type=\"button\" value=\"zurueck\" onclick=\"history.back()\">
-               <img src=\"$g_root_path/adm_program/images/back.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Zur&uuml;ck\">
-               &nbsp;Zur&uuml;ck</button>
-            </div>
-         </div>
-      </form>
-   </div>";
-   //Ende des Seiten Inhalts
-   require("../../../adm_config/body_bottom.php");
-   echo "</body>
-   </html>";
+        </form>
+    </div>
+    <script type=\"text/javascript\"><!--
+        document.getElementById('new_name').focus();
+    --></script>";   
+    require("../../../adm_config/body_bottom.php");
+echo "</body>
+</html>";
 ?>
