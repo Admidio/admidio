4987a97cca11912c20f9fbe2987fdc194b9cc78b
Jochen Erkens <jochen.erkens@gmail.com>
2006-04-29 05:27:18 +0000

    Vollstaendig neu In Admidiostandart umgesetzt. Kontrolle ob maximale Mitgliederzahl ueberschritten wurde fehlt noch

--- adm_program/modules/lists/members_save.php
+++ adm_program/modules/lists/members_save.php
@@ -8,9 +8,9 @@
  *
  * Uebergaben:
  *
- * user_id: Benutzer deren zuordnung geändert werden soll
+ * user_id: Benutzer deren zuordnung geï¿½ndert werden soll
  * url:     URL auf die danach weitergeleitet wird
- * role_id: Rolle zu denen die Zuordnug geändert werden soll
+ * role_id: Rolle zu denen die Zuordnug geï¿½ndert werden soll
  *
  ******************************************************************************
  *
@@ -32,18 +32,19 @@
 
 require("../../system/common.php");
 require("../../system/login_valid.php");
-//Übernahme der Rolle die bearbeitet werden soll
+//Uebernahme der Rolle die bearbeitet werden soll
 $role_id = $_GET["role_id"];
 
-//Erfassen der übergeben Rolle
-$sql    =   "SELECT * FROM ". TBL_ROLES. "
-              WHERE rol_id = '$role_id'";
+//Erfassen der uebergeben Rolle
+$sql="SELECT * FROM ". TBL_ROLES. "
+      WHERE rol_id = '$role_id'";
 $result_role = mysql_query($sql, $g_adm_con);
-            db_error($result);
+db_error($result);
 $role = mysql_fetch_object($result_role);
-// nur Moderatoren dürfen Rollen zuweisen
+
+// nur Moderatoren duerfen Rollen zuweisen
 // nur Webmaster duerfen die Rolle Webmaster zuweisen
-// beide müssen mitglied der richtigen Gliedgemeinschaft sein
+// beide muessen mitglied der richtigen Gliedgemeinschaft sein
 if((!isModerator() && !isGroupLeader($role_id) && !editUser()) || (!hasRole("Webmaster") && $role->rol_name=="Webmaster") || $role->rol_org_shortname!=$g_organization)
 {
    $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=norights";
@@ -52,87 +53,114 @@ if((!isModerator() && !isGroupLeader($role_id) && !editUser()) || (!hasRole("Web
 }
 
 //Veraarbeitung der Daten
-    //Abfrag aller Datensätze die mit der Rolle zu tun haben
-    $sql =" SELECT *
-                FROM ". TBL_MEMBERS. "
-                WHERE mem_rol_id = $role_id";
-    $result_role = mysql_query($sql, $g_adm_con);
-   db_error($result);
-   //Schreiben der Datensätze in Array sortiert nach zugewiesenen Benutzern (id)
-   $mitglieder_array= array(array());
-   for($x=0; $role= mysql_fetch_array($result_role); $x++){
-    for($y=0; $y<=6; $y++){
+//Abfrag aller Datensaetze die mit der Rolle zu tun haben
+$sql =" SELECT *
+        FROM ". TBL_MEMBERS. "
+        WHERE mem_rol_id = $role_id";
+$result_role = mysql_query($sql, $g_adm_con);
+db_error($result);
+   
+//Schreiben der Datensaetze in Array sortiert nach zugewiesenen Benutzern (id)
+$mitglieder_array= array(array());
+for($x=0; $role= mysql_fetch_array($result_role); $x++)
+{
+    for($y=0; $y<=6; $y++)
+    {
         $mitglieder_array["$role[2]"][$y]=$role[$y];
     }
-   }
-   //Aufrufen alle Leute aus der Datenbank
-    $sql =" SELECT *
-              FROM ". TBL_USERS. "
-             WHERE usr_valid = 1 ";
-            $result_user = mysql_query($sql, $g_adm_con);
-            db_error($result_user);
-    //Datensätze durchgehen und sehen ob für den Benutzer eine Änderung vorliegt
-    while($user= mysql_fetch_array($result_user)){
-        //Falls User Mitglied der Rolle ist oder schonmal war
-        if(array_key_exists($user["usr_id"], $mitglieder_array)){
-            //Kontolle ob Zuweisung geändert wurde wen ja entsprechenden SQL-Befehl zusammensetzen
-            //Falls abgewählt wurde (automatisch auch als Leiter abmelden)
-            if($mitglieder_array[$user["usr_id"]][5]==1 && $_POST["member_".$user["usr_id"]]==false){
-                $mem_id = $mitglieder_array[$user["usr_id"]][0];
-                $sql =" UPDATE ". TBL_MEMBERS. " SET mem_valid  = 0
-                                          , mem_end   = NOW()
-                                                        , mem_leader = 0
-                            WHERE mem_id = '$mem_id'";                             
-                $result = mysql_query($sql, $g_adm_con);
-            db_error($result);
-            }
-            //Falls wieder angemeldet wurde
-            if($mitglieder_array[$user["usr_id"]][5]==0 && $_POST["member_".$user["usr_id"]]==true){
-                $mem_id = $mitglieder_array[$user["usr_id"]][0];
-                $sql =" UPDATE ". TBL_MEMBERS. " SET mem_valid  = 1
-                                          , mem_end   = '0000-00-00'";
-            //Falls jemand auch Leiter werden soll
-            if($_POST["leader_".$user["usr_id"]]==true)$sql .=", mem_leader = 1 ";
-            $sql .= "WHERE mem_id = '$mem_id'";
-                $result = mysql_query($sql, $g_adm_con);
-            db_error($result);          
-            }
-            //Falls nur Leiterfunktion hinzugefügt/entfernt werden soll under der user Mitglied ist/bleibt
-            if($mitglieder_array[$user["usr_id"]][5]==1 && $_POST["member_".$user["usr_id"]]==true){
-                $mem_id = $mitglieder_array[$user["usr_id"]][0];
-                //Falls Leiter hinzugefügt werden soll
-                if($_POST["leader_".$user["usr_id"]]==true && $mitglieder_array[$user["usr_id"]][6]==0){
-                    $sql =" UPDATE ". TBL_MEMBERS. " SET mem_leader  = 1 
-                                WHERE mem_id = '$mem_id'";
-                    $result = mysql_query($sql, $g_adm_con);
-                db_error($result);
-                }
-                //Falls Leiter entfernt werden soll
-                if($_POST["leader_".$user["usr_id"]]==false && $mitglieder_array[$user["usr_id"]][6]==1){
-                    $sql =" UPDATE ". TBL_MEMBERS. " SET mem_leader  = 0 
-                                WHERE mem_id = '$mem_id'";
-                    $result = mysql_query($sql, $g_adm_con);
-                db_error($result);
-                }
-            }
-        }
-        //Falls noch nie angemeldet gewesen aber jetzt werden soll
-        else if(!array_key_exists($user["usr_id"], $mitglieder_array) && $_POST["member_".$user["usr_id"]]==true){
-            $usr_id = $user["usr_id"];
-            $sql = "INSERT INTO ". TBL_MEMBERS. " (mem_rol_id, mem_usr_id, mem_begin, mem_valid, mem_leader)
-                 VALUES ($role_id, $usr_id, NOW(), 1";
-         //Falls jemand direkt Leiter werden soll
-         if($_POST["leader_".$user["usr_id"]]==true)$sql .=", 1) ";
-         else $sql .=", 0) ";
+}
+
+//Aufrufen alle Leute aus der Datenbank
+$sql =" SELECT *
+        FROM ". TBL_USERS. "
+        WHERE usr_valid = 1 ";
+$result_user = mysql_query($sql, $g_adm_con);
+db_error($result_user);
+    
+//Datensaetze durchgehen und sehen ob faer den Benutzer eine aenderung vorliegt
+while($user= mysql_fetch_array($result_user))
+{
+    //Falls User Mitglied der Rolle ist oder schonmal war
+    if(array_key_exists($user["usr_id"], $mitglieder_array))
+    {
+        //Kontolle ob Zuweisung geaendert wurde wen ja entsprechenden SQL-Befehl zusammensetzen
+        
+        //Falls abgewaehlt wurde (automatisch auch als Leiter abmelden)
+        if($mitglieder_array[$user["usr_id"]][5]==1 && $_POST["member_".$user["usr_id"]]==false)
+        {
+            $mem_id = $mitglieder_array[$user["usr_id"]][0];
+            $sql =" UPDATE ". TBL_MEMBERS. " 
+                    SET mem_valid  = 0, 
+                        mem_end   = NOW(), 
+                        mem_leader = 0
+                    WHERE mem_id = '$mem_id'";                             
             $result = mysql_query($sql, $g_adm_con);
-        db_error($result);
+            db_error($result);
+        }
+           
+        //Falls wieder angemeldet wurde
+        if($mitglieder_array[$user["usr_id"]][5]==0 && $_POST["member_".$user["usr_id"]]==true)
+        {
+            $mem_id = $mitglieder_array[$user["usr_id"]][0];
+            $sql =" UPDATE ". TBL_MEMBERS. " 
+                    SET mem_valid  = 1, 
+                        mem_end   = '0000-00-00'";
+            
+            //Falls jemand auch Leiter werden soll
+            if($_POST["leader_".$user["usr_id"]]==true)
+            {
+                $sql .=", mem_leader = 1 ";
+            }
+            
+            $sql .= "WHERE mem_id = '$mem_id'";
+            $result = mysql_query($sql, $g_adm_con);
+            db_error($result);          
+        }
+            
+        //Falls nur Leiterfunktion hinzugefuegt/entfernt werden soll under der user Mitglied ist/bleibt
+        if($mitglieder_array[$user["usr_id"]][5]==1 && $_POST["member_".$user["usr_id"]]==true)
+        {
+            $mem_id = $mitglieder_array[$user["usr_id"]][0];
+            
+            //Falls Leiter hinzugefuegt werden soll
+                if($_POST["leader_".$user["usr_id"]]==true && $mitglieder_array[$user["usr_id"]][6]==0)
+                {
+                    $sql =" UPDATE ". TBL_MEMBERS. " SET mem_leader  = 1 
+                            WHERE mem_id = '$mem_id'";
+                    $result = mysql_query($sql, $g_adm_con);
+                    db_error($result);
+                }
+                
+                //Falls Leiter entfernt werden soll
+                if($_POST["leader_".$user["usr_id"]]==false && $mitglieder_array[$user["usr_id"]][6]==1)
+                {
+                    $sql =" UPDATE ". TBL_MEMBERS. " SET mem_leader  = 0 
+                            WHERE mem_id = '$mem_id'";
+                    $result = mysql_query($sql, $g_adm_con);
+                db_error($result);
+                }
         }
     }
+        
+    //Falls noch nie angemeldet gewesen aber jetzt werden soll
+    else if(!array_key_exists($user["usr_id"], $mitglieder_array) && $_POST["member_".$user["usr_id"]]==true)
+    {
+        $usr_id = $user["usr_id"];
+        $sql="  INSERT INTO ". TBL_MEMBERS. " (mem_rol_id, mem_usr_id, mem_begin, mem_valid, mem_leader)
+                VALUES ($role_id, $usr_id, NOW(), 1";
+        
+        //Falls jemand direkt Leiter werden soll
+        if($_POST["leader_".$user["usr_id"]]==true)
+        {
+            $sql .=", 1) ";
+        }
+        else $sql .=", 0) ";
+        $result = mysql_query($sql, $g_adm_con);
+        db_error($result);
+    }
+}
 
 
-
-if($_GET['popup'] == 1)
-{
    echo "
    <?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?". ">
    <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 TRANSITIONAL//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
@@ -163,11 +191,3 @@ if($_GET['popup'] == 1)
       </div>
    </body>
    </html>";
-}
-else
-{
-   // zur Ausgangsseite zurueck
-   $location = "location: $g_root_path/adm_program/system/err_msg.php?err_code=save&url=". $_GET['url']. "&timer=2000";
-   header($location);
-   exit();
-}
