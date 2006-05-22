c9109e25877df6f8b33fa4a05ac11704e1c6ba78
Markus Faßbender <markus.fassbender@gmail.com>
2006-05-22 09:02:51 +0000

    Updatescript auf 1.3 bricht auf der Spielwiese ab BUG: 1492777

--- adm_install/upd_1_3_db.sql
+++ adm_install/upd_1_3_db.sql
@@ -1,11 +1,10 @@
 ALTER TABLE %PRAEFIX%_roles ADD rol_announcements TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 AFTER rol_moderation;
-ALTER TABLE %PRAEFIX%_users ADD usr_photo_upload blob AFTER usr_password;
 ALTER TABLE %PRAEFIX%_users ADD usr_photo blob AFTER usr_password;
 
 create table %PRAEFIX%_folders
 (
    fol_id                         int(11) unsigned               not null AUTO_INCREMENT,
-   fol_org_shortname              varchar(10)                    not null,
+   fol_org_id                     tinyint(4)                     not null,
    fol_fol_id_parent              int(11) unsigned,
    fol_type                       varchar(10)                    not null,
    fol_name                       varchar(255)                   not null,
@@ -13,7 +12,7 @@ create table %PRAEFIX%_folders
 )
 type = InnoDB
 auto_increment = 1;
-alter table %PRAEFIX%_folders add index FOL_ORG_FK (fol_org_shortname);
+alter table %PRAEFIX%_folders add index FOL_ORG_FK (fol_org_id);
 alter table %PRAEFIX%_folders add index FOL_FOL_PARENT_FK (fol_fol_id_parent);
 
 create table %PRAEFIX%_folder_roles
@@ -29,7 +28,7 @@ alter table %PRAEFIX%_folder_roles add index FOL_ROL_FK (flr_rol_id);
 create table %PRAEFIX%_guestbook
 (
    gbo_id                         int(11) unsigned               not null AUTO_INCREMENT,
-   gbo_org_id                     int(4) unsigned                not null,
+   gbo_org_id                     tinyint(4)                     not null,
    gbo_usr_id                     int(11) unsigned,
    gbo_name                       varchar(60)                    not null,
    gbo_text                       text                           not null,
@@ -62,7 +61,7 @@ alter table %PRAEFIX%_guestbook_comments add index GBC_USR_FK (gbc_usr_id);
 create table %PRAEFIX%_links
 (
    lnk_id                         int(11) unsigned               not null AUTO_INCREMENT,
-   lnk_org_id                     int(4) unsigned                not null,
+   lnk_org_id                     tinyint(4)                     not null,
    lnk_name                       varchar(255)                   not null,
    lnk_description                text,
    lnk_url                        varchar(255)                   not null,
@@ -77,7 +76,7 @@ alter table %PRAEFIX%_links add index LNK_USR_FK (lnk_usr_id);
 create table %PRAEFIX%_preferences
 (
    prf_id                         int(11) unsigned               not null AUTO_INCREMENT,
-   prf_org_id                     int(4) unsigned                not null,
+   prf_org_id                     tinyint(4)                     not null,
    prf_name                       varchar(30)                    not null,
    prf_value                      varchar(255),
    primary key (prf_id)
@@ -91,21 +90,21 @@ alter table %PRAEFIX%_folder_roles add constraint %PRAEFIX%_FK_FLR_FOL foreign k
       references %PRAEFIX%_folders (fol_id) on delete restrict on update restrict;
 alter table %PRAEFIX%_folders add constraint %PRAEFIX%_FK_FOL_FOL_PARENT foreign key (fol_fol_id_parent)
       references %PRAEFIX%_folders (fol_id) on delete restrict on update restrict;
-alter table %PRAEFIX%_folders add constraint %PRAEFIX%_FK_FOL_ORG foreign key (fol_org_shortname)
-      references %PRAEFIX%_organizations (org_shortname) on delete restrict on update restrict;
-alter table %PRAEFIX%_guestbook add constraint FK_GBO_ORG foreign key (gbo_org_id)
+alter table %PRAEFIX%_folders add constraint %PRAEFIX%_FK_FOL_ORG foreign key (fol_org_id)
       references %PRAEFIX%_organizations (org_id) on delete restrict on update restrict;
-alter table %PRAEFIX%_guestbook add constraint FK_GBO_USR foreign key (gbo_usr_id)
+alter table %PRAEFIX%_guestbook add constraint %PRAEFIX%_FK_GBO_ORG foreign key (gbo_org_id)
+      references %PRAEFIX%_organizations (org_id) on delete restrict on update restrict;
+alter table %PRAEFIX%_guestbook add constraint %PRAEFIX%_FK_GBO_USR foreign key (gbo_usr_id)
       references %PRAEFIX%_users (usr_id) on delete set null on update restrict;
-alter table %PRAEFIX%_guestbook add constraint FK_GBO_USR_CHANGE foreign key (gbo_usr_id_change)
+alter table %PRAEFIX%_guestbook add constraint %PRAEFIX%_FK_GBO_USR_CHANGE foreign key (gbo_usr_id_change)
       references %PRAEFIX%_users (usr_id) on delete set null on update restrict;
-alter table %PRAEFIX%_guestbook_comments add constraint FK_GBC_GBO foreign key (gbc_gbo_id)
+alter table %PRAEFIX%_guestbook_comments add constraint %PRAEFIX%_FK_GBC_GBO foreign key (gbc_gbo_id)
       references %PRAEFIX%_guestbook (gbo_id) on delete restrict on update restrict;
-alter table %PRAEFIX%_guestbook_comments add constraint FK_GBC_USR foreign key (gbc_usr_id)
+alter table %PRAEFIX%_guestbook_comments add constraint %PRAEFIX%_FK_GBC_USR foreign key (gbc_usr_id)
       references %PRAEFIX%_users (usr_id) on delete restrict on update restrict;
-alter table %PRAEFIX%_links add constraint FK_LNK_ORG foreign key (lnk_org_id)
+alter table %PRAEFIX%_links add constraint %PRAEFIX%_FK_LNK_ORG foreign key (lnk_org_id)
       references %PRAEFIX%_organizations (org_id) on delete restrict on update restrict;
-alter table %PRAEFIX%_links add constraint FK_LNK_USR foreign key (lnk_usr_id)
+alter table %PRAEFIX%_links add constraint %PRAEFIX%_FK_LNK_USR foreign key (lnk_usr_id)
       references %PRAEFIX%_users (usr_id) on delete restrict on update restrict;
-alter table %PRAEFIX%_preferences add constraint FK_PRF_ORG foreign key (prf_org_id)
+alter table %PRAEFIX%_preferences add constraint %PRAEFIX%_FK_PRF_ORG foreign key (prf_org_id)
       references %PRAEFIX%_organizations (org_id) on delete restrict on update restrict;
