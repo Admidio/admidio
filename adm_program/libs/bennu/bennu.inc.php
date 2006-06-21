f1049d45fd1878f1bfa14f38817c8ca098321867
Roland Meuthen <roland.meuthen@gmx.de>
2006-06-21 20:54:41 +0000

    Bug 1504896 Diverse Fehler in iCalendar export behoben iCalendar library Bennu http://bennu.sourceforge.net/ eingebaut

---
+++ adm_program/libs/bennu/bennu.inc.php
@@ -0,0 +1,11 @@
+<?php // $Id: bennu.inc.php,v 1.3 2005/07/21 22:31:44 defacer Exp $
+
+if(!defined('_BENNU_VERSION')) {
+    define('_BENNU_VERSION', '0.1');
+    include('iCalendar_rfc2445.php');
+    include('iCalendar_components.php');
+    include('iCalendar_properties.php');
+    include('iCalendar_parameters.php');
+}
+
+?>
