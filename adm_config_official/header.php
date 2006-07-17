2b7909603165d49b18686b03df57580df57922b7
Markus Faßbender <markus.fassbender@gmail.com>
2006-07-17 07:44:00 +0000

    content-type eingebaut, da es sonst schon mal Probleme bei der automatischen Erkennung geben kann, wenn keiner angegeben wird.

--- adm_config_official/header.php
+++ adm_config_official/header.php
@@ -1,4 +1,6 @@
 
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+
 <!-- Hier koennen Sie Ihren HTML-Code einbauen, der in jedem <head></head> Bereich
      einer Admidio-Modul-Seite erscheinen soll.
 -->
