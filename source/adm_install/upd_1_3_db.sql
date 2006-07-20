d210f9074974406f8020fe1eb56983758be1fc18
Markus Faßbender <markus.fassbender@gmail.com>
2006-07-20 06:53:35 +0000

    Folder-Tabellen entfernt, werden in der 1.3 nicht gebraucht

--- source/adm_install/upd_1_3_db.sql
+++ source/adm_install/upd_1_3_db.sql
@@ -5,30 +5,6 @@ ALTER TABLE %PRAEFIX%_roles ADD rol_guestbook     TINYINT(1) UNSIGNED NOT NULL D
 ALTER TABLE %PRAEFIX%_roles ADD rol_weblinks      TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 AFTER rol_photo;
 ALTER TABLE %PRAEFIX%_users ADD usr_photo blob AFTER usr_password;
 
-create table %PRAEFIX%_folders
-(
-   fol_id                         int(11) unsigned               not null AUTO_INCREMENT,
-   fol_org_id                     tinyint(4)                     not null,
-   fol_fol_id_parent              int(11) unsigned,
-   fol_type                       varchar(10)                    not null,
-   fol_name                       varchar(255)                   not null,
-   primary key (fol_id)
-)
-type = InnoDB
-auto_increment = 1;
-alter table %PRAEFIX%_folders add index FOL_ORG_FK (fol_org_id);
-alter table %PRAEFIX%_folders add index FOL_FOL_PARENT_FK (fol_fol_id_parent);
-
-create table %PRAEFIX%_folder_roles
-(
-   flr_fol_id                     int(11) unsigned               not null,
-   flr_rol_id                     int(11) unsigned               not null,
-   primary key (flr_fol_id, flr_rol_id)
-)
-type = InnoDB;
-alter table %PRAEFIX%_folder_roles add index FLR_FOL_FK (flr_fol_id);
-alter table %PRAEFIX%_folder_roles add index FOL_ROL_FK (flr_rol_id);
-
 create table %PRAEFIX%_guestbook
 (
    gbo_id                         int(11) unsigned               not null AUTO_INCREMENT,
@@ -90,14 +66,6 @@ create table %PRAEFIX%_preferences
 type = InnoDB;
 alter table %PRAEFIX%_preferences add index PRF_ORG_FK (prf_org_id);
 
-alter table %PRAEFIX%_folder_roles add constraint %PRAEFIX%_FK_FOL_ROL foreign key (flr_rol_id)
-      references %PRAEFIX%_roles (rol_id) on delete restrict on update restrict;
-alter table %PRAEFIX%_folder_roles add constraint %PRAEFIX%_FK_FLR_FOL foreign key (flr_fol_id)
-      references %PRAEFIX%_folders (fol_id) on delete restrict on update restrict;
-alter table %PRAEFIX%_folders add constraint %PRAEFIX%_FK_FOL_FOL_PARENT foreign key (fol_fol_id_parent)
-      references %PRAEFIX%_folders (fol_id) on delete restrict on update restrict;
-alter table %PRAEFIX%_folders add constraint %PRAEFIX%_FK_FOL_ORG foreign key (fol_org_id)
-      references %PRAEFIX%_organizations (org_id) on delete restrict on update restrict;
 alter table %PRAEFIX%_guestbook add constraint %PRAEFIX%_FK_GBO_ORG foreign key (gbo_org_id)
       references %PRAEFIX%_organizations (org_id) on delete restrict on update restrict;
 alter table %PRAEFIX%_guestbook add constraint %PRAEFIX%_FK_GBO_USR foreign key (gbo_usr_id)
