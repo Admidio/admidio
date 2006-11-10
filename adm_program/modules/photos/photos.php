67c754cfbc86e2bde219bd8157c6bc470143cc1c
Jochen Erkens <jochen.erkens@gmail.com>
2006-11-10 15:14:05 +0000

    Link für RSS-Feed im Kopf eingearbeitet

--- adm_program/modules/photos/photos.php
+++ adm_program/modules/photos/photos.php
@@ -222,8 +222,15 @@ echo "
 <html>
     <head>
         <title>$g_current_organization->longname - Fotogalerien</title>
-        <link rel=\"stylesheet\" type=\"text/css\" href=\"$g_root_path/adm_config/main.css\">
+        <link rel=\"stylesheet\" type=\"text/css\" href=\"$g_root_path/adm_config/main.css\">";
 
+        if($g_preferences['enable_rss'] == 1)
+        {
+            echo "<link type=\"application/rss+xml\" rel=\"alternate\" title=\"$g_current_organization->longname - Fotos\"
+            href=\"$g_root_path/adm_program/modules/photos/rss_photos.php\">";
+        };
+
+        echo"
         <!--[if lt IE 7]>
             <script type=\"text/javascript\" src=\"$g_root_path/adm_program/system/correct_png.js\"></script>
         <![endif]-->";
