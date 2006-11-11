71121f1e881a6164ae574aab45f3d04edc464423
Jochen Erkens <jochen.erkens@gmail.com>
2006-11-11 17:41:52 +0000

    Techniische Aenderung beim anzeigen von Bildern in einer Liste: Die Bilder werden in eine Session gepackt. Die Datei die später die Bilder anzeigt ruft sich das entsprechende Bild ab und loescht es aus der Session raus.
    Grund,  vorher war für jedes Bild ein neuer Datenbankaufruf noetig

---
+++ adm_program/modules/lists/photo_show.php
@@ -0,0 +1,37 @@
+<?php
+   /******************************************************************************
+ * Photoresizer
+ *
+ * Copyright    : (c) 2004 - 2006 The Admidio Team
+ * Homepage     : http://www.admidio.org
+ * Module-Owner : Jochen Erkens
+ *
+ * Uebergaben:
+ *
+ * usr_id : die ID des Users dessen Bild angezeigt werden soll
+ ******************************************************************************
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *****************************************************************************/
+require("../../system/common.php");
+require("../../system/login_valid.php");
+
+header("Content-Type: image/jpeg");
+$id = $_GET['usr_id'];
+echo $_SESSION['profilphoto'][$id];
+unset($_SESSION['profilphoto'][$id]);
+
+?>
