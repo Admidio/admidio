6f5af3014a57320057778871f110d73abfdd6dc3
Markus Faßbender <markus.fassbender@gmail.com>
2007-01-01 18:41:12 +0000

    Zuordnung der Registrierung überarbeitet Besseres Handling bei mehreren Orgas, wenn der Benutzer schon existiert, aber noch kein Mitglied der aktuellen Orga ist Siehe http://sourceforge.net/tracker/index.php?func=detail&aid=1396959&group_id=134961&atid=731561

--- source/adm_program/administration/new_user/new_user_assign.php
+++ source/adm_program/administration/new_user/new_user_assign.php
@@ -1,6 +1,6 @@
 <?php
 /******************************************************************************
- * Uebersicht mit allen Benutzer anzeigen zum Zuordnen des Neuen
+ * Zeigt eine Liste mit moeglichen Zuordnungen an
  *
  * Copyright    : (c) 2004 - 2006 The Admidio Team
  * Homepage     : http://www.admidio.org
@@ -9,7 +9,6 @@
  * Uebergaben:
  *
  * new_user_id: ID des Users, der angezeigt werden soll
- * letter:      Anfangsbuchstabe, der Nachnamen, die angezeigt werden sollen
  *
  ******************************************************************************
  *
@@ -44,95 +43,39 @@ if($g_preferences['registration_mode'] == 0)
     $g_message->show("module_disabled");
 }
 
-if(isset($_GET['new_user_id']) && is_numeric($_GET['new_user_id']) == false)
+if(isset($_GET['new_user_id']) == false || is_numeric($_GET['new_user_id']) == false)
 {
     $g_message->show("invalid");
 }
 
-if(isset($_GET['letter']) && strlen($_GET["letter"]) > 2)
+// neuen User erst einmal als Objekt erzeugen
+$new_user = new User($g_adm_con);
+$new_user->getUser($_GET['new_user_id']);
+
+// alle User aus der DB selektieren, die denselben Vor- und Nachnamen haben
+$sql = "SELECT * " .
+	   "  FROM ". TBL_USERS. 
+	   " WHERE UPPER(usr_last_name)  LIKE UPPER('$new_user->last_name')" .
+	   "   AND UPPER(usr_first_name) LIKE UPPER('$new_user->first_name') " .
+	   "   AND usr_valid      = 1 ";
+$result_usr   = mysql_query($sql, $g_adm_con);
+$member_found = mysql_num_rows($result_usr);
+
+if($member_found == 0)
 {
-    $g_message->show("invalid");
+	// kein User mit dem Namen gefunden, dann direkt neuen User erzeugen und dieses Script verlassen
+	header("Location: $g_root_path/adm_program/modules/profile/profile_new.php?user_id=". $_GET['new_user_id']. "&new_user=3");
+	exit();
 }
 
-$restrict = "";
-$listname = "";
-$i = 0;
-
-if($_GET['new_user_id'] > 0)
-{
-   $new_user = new User($g_adm_con);
-   $new_user->getUser($_GET['new_user_id']);
-
-   if(!array_key_exists("letter", $_GET))
-      $_GET["letter"] = substr($new_user->last_name, 0, 1);
-}
-
-if(!array_key_exists("letter", $_GET))
-   $_GET["letter"] = "A%";
-else
-   $_GET["letter"] = $_GET["letter"]. "%";
-
-if($_GET["all"] == 0)
-{
-   // Mitglieder mit gleichem Nachname der Gruppierung zur Auswahl selektieren
-   $sql    = "SELECT *
-                FROM ". TBL_ROLES. ", ". TBL_MEMBERS. ", ". TBL_USERS. "
-               WHERE rol_org_shortname = '$g_organization'
-                 AND rol_valid         = 1
-                 AND mem_rol_id        = rol_id
-                 AND mem_valid         = 1
-                 AND mem_usr_id        = usr_id
-                 AND usr_last_name LIKE '$new_user->last_name'
-                 AND usr_valid         = 1
-               GROUP BY usr_last_name, usr_first_name
-               ORDER BY usr_last_name, usr_first_name ";
-   $result_all = mysql_query($sql, $g_adm_con);
-   $member_found = mysql_num_rows($result_all);
-
-   // wenn keine mit gleichem Nachnamen gefunden, dann alle selektieren
-   if($member_found == 0)
-   {
-      // alle Mitglieder der Gruppierung zur Auswahl selektieren
-      $sql    = "SELECT *
-                   FROM ". TBL_ROLES. ", ". TBL_MEMBERS. ", ". TBL_USERS. "
-                  WHERE rol_org_shortname = '$g_organization'
-                    AND rol_valid         = 1
-                    AND mem_rol_id        = rol_id
-                    AND mem_valid         = 1
-                    AND mem_usr_id        = usr_id
-                    AND usr_last_name LIKE {0}
-                    AND usr_valid         = 1
-                  GROUP BY usr_last_name, usr_first_name
-                  ORDER BY usr_last_name, usr_first_name ";
-      $sql    = prepareSQL($sql, array($_GET['letter']));
-      $result_all = mysql_query($sql, $g_adm_con);
-      $all    = 1;
-   }
-}
-else
-{
-   // alle Mitglieder der Gruppierung zur Auswahl selektieren
-   $sql    = "SELECT *
-                FROM ". TBL_ROLES. ", ". TBL_MEMBERS. ", ". TBL_USERS. "
-               WHERE rol_org_shortname = '$g_organization'
-                 AND rol_valid         = 1
-                 AND mem_rol_id        = rol_id
-                 AND mem_valid         = 1
-                 AND mem_usr_id        = usr_id
-                 AND usr_last_name LIKE {0}
-                 AND usr_valid         = 1
-               GROUP BY usr_last_name, usr_first_name
-               ORDER BY usr_last_name, usr_first_name ";
-   $sql    = prepareSQL($sql, array($_GET['letter']));
-   $result_all = mysql_query($sql, $g_adm_con);
-}
+$_SESSION['navigation']->addUrl($g_current_url);
 
 echo "
 <!-- (c) 2004 - 2006 The Admidio Team - http://www.admidio.org - Version: ". getVersion(). " -->\n
 <!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">
 <html>
 <head>
-   <title>$g_current_organization->longname - Neue User zuordnen</title>
+   <title>$g_current_organization->longname - Neuen Benutzer zuordnen</title>
    <link rel=\"stylesheet\" type=\"text/css\" href=\"$g_root_path/adm_config/main.css\">
 
    <!--[if lt IE 7]>
@@ -143,89 +86,97 @@ echo "
 echo "</head>";
 
 require("../../../adm_config/body_top.php");
-echo "<div align=\"center\">";
-
-if($_GET['new_user_id'] > 0)
-   echo "<h1>Anmeldung von $new_user->first_name $new_user->last_name zuordnen</h1>";
 echo "
-<h1>- ". str_replace("%", "", $_GET["letter"]). " -</h1>";
-
-if($_GET["all"] == 1 || $member_found == 0)
-{
-   echo "<p>W&auml;hle den Anfangsbuchstaben des Nachnamens aus:</p>
-   <p>";
-
-   $letter_menu = "A";
-   for($i = 0; $i < 26;$i++)
-   {
-      // Anzahl Mitglieder zum entsprechenden Buchstaben ermitteln
-      $sql    = "SELECT COUNT(*)
-                   FROM ". TBL_ROLES. ", ". TBL_MEMBERS. ", ". TBL_USERS. "
-                  WHERE rol_org_shortname = '$g_organization'
-                    AND rol_valid         = 1
-                    AND mem_rol_id        = rol_id
-                    AND mem_valid         = 1
-                    AND mem_usr_id        = usr_id
-                    AND usr_last_name LIKE '$letter_menu%'
-                    AND usr_valid         = 1
-                  GROUP BY usr_last_name, usr_first_name ";
-      $result = mysql_query($sql, $g_adm_con);
-      db_error($result);
-      $row = mysql_fetch_array($result);
-
-      if($row[0] > 0)
-         echo "<a href=\"new_user_assign.php?new_user_id=$new_user->id&amp;all=1&amp;letter=$letter_menu\">$letter_menu</a>";
-      else
-         echo $letter_menu;
-
-      echo "&nbsp;&nbsp;&nbsp;";
-      // naechsten Buchstaben anwaehlen
-      $letter_menu = strNextLetter($letter_menu);
-   }
-
-   echo "</p>";
-}
-
-echo "
-<table class=\"tableList\" cellpadding=\"2\" cellspacing=\"0\">
-   <tr>
-      <th class=\"tableHeader\" style=\"text-align: left;\">&nbsp;Name</th>
-      <th class=\"tableHeader\" style=\"text-align: left;\">&nbsp;Benutzername</th>
-      <th class=\"tableHeader\" style=\"text-align: left;\">&nbsp;E-Mail</th>
-      <th class=\"tableHeader\" style=\"text-align: left;\">&nbsp;Funktion</th>
-   </tr>";
-
-   while($row = mysql_fetch_object($result_all))
-   {
-      echo "<tr class=\"listMouseOut\" onMouseOver=\"this.className='listMouseOver'\" onMouseOut=\"this.className='listMouseOut'\">
-               <td style=\"text-align: left;\">&nbsp;<a href=\"$g_root_path/adm_program/modules/profile/profile.php?user_id=$row->usr_id\">$row->usr_last_name,&nbsp;$row->usr_first_name</a></td>
-               <td style=\"text-align: left;\">&nbsp;$row->usr_login_name</td>
-               <td style=\"text-align: left;\">&nbsp;";
-               if($g_preferences['enable_mail_module'] != 1)
-                  echo "<a href=\"mailto:$row->usr_email\">";
-               else
-                  echo "<a href=\"$g_root_path/adm_program/modules/mail/mail.php?usr_id=$row->usr_id\">";
-               echo "$row->usr_email</a></td>
-               <td style=\"text-align: left;\">&nbsp;";
-      if(strlen($row->usr_login_name) > 0)
-         echo "Angemeldet";
-      else
-      {
-         $load_url = urlencode("$g_root_path/adm_program/administration/new_user/new_user_function.php?new_user_id=". $_GET['new_user_id']. "&amp;user_id=$row->usr_id&amp;mode=1");
-         echo "<a href=\"$g_root_path/adm_program/system/err_msg.php?err_code=zuordnen&amp;err_text=$row->usr_first_name $row->usr_last_name&amp;err_head=Webanmeldung zuordnen&amp;button=2&amp;url=$load_url\">Zuordnen</a>";
-      }
-
-      echo "</td></tr>";
-   }
-echo "
-</table>
-
-<p>Falls der Benutzer noch nicht existiert:</p>
-<p><button name=\"neu\" type=\"button\" value=\"neu\"
-   onclick=\"self.location.href='$g_root_path/adm_program/modules/profile/profile_new.php?user_id=". $_GET['new_user_id']. "&amp;new_user=3&amp;url=". urlencode("$g_root_path/adm_program/administration/new_user/new_user.php"). "'\">
-<img src=\"$g_root_path/adm_program/images/add.png\" style=\"vertical-align: middle; padding-bottom: 1px;\" width=\"16\" height=\"16\" border=\"0\" alt=\"Neuer Benutzer\">
-&nbsp;Neuer Benutzer</button></p>
-
+<div style=\"margin-top: 10px; margin-bottom: 10px;\" align=\"center\">
+    <div class=\"formHead\" style=\"width: 400px;\">Anmeldung zuordnen</div>
+    <div class=\"formBody\" style=\"width: 400px;\">
+    	Es wurde bereits ein Benutzer unter dem Namen <b>$new_user->first_name $new_user->last_name</b> 
+        in der Datenbank gefunden.<br>
+        <div class=\"groupBox\" style=\"margin-top: 10px; text-align: left;\">
+            <div class=\"groupBoxHeadline\">Gefundene Benutzer</div>";
+            $i = 0;
+        	while($row = mysql_fetch_object($result_usr))
+            {
+            	if($i > 0)
+                {
+                	echo "<hr width=\"85%\">";
+                }
+            	echo "<div style=\"margin-left: 20px;\">
+                    <i>$row->usr_last_name, $row->usr_first_name</i><br>
+                	$row->usr_address<br>
+                	$row->usr_zip_code $row->usr_city<br>";
+                    if($g_preferences['enable_mail_module'] == 1)
+                    {
+                        echo "<a href=\"$g_root_path/adm_program/modules/mail/mail.php?usr_id=$row->usr_id\">$row->usr_email</a><br>";
+                    }
+                    else
+                    {
+                        echo "<a href=\"mailto:$row->usr_email\">$row->usr_email</a><br>";
+                    }
+                    
+                    if(isMember($row->usr_id) == false && strlen($row->usr_login_name) == 0)
+                    {
+                    	// kein Mitlgied dieser Orga und auch keine Logindaten vorhanden
+                    	echo "<br>Dieser Benutzer ist noch kein Mitglied der Organisation $g_organization und 
+                        besitzt auch keine Logindaten.<br><br>
+                        <span class=\"iconLink\">
+                            <a class=\"iconLink\" href=\"$g_root_path/adm_program/administration/new_user/new_user_function.php?new_user_id=". $_GET['new_user_id']. "&amp;user_id=$row->usr_id&amp;mode=2\"><img
+                             class=\"iconLink\" src=\"$g_root_path/adm_program/images/properties.png\" style=\"vertical-align: middle;\" border=\"0\" title=\"Rollen und Logindaten diesem Benutzer zuordnen\" alt=\"Rollen und Logindaten diesem Benutzer zuordnen\"></a>
+                            <a class=\"iconLink\" href=\"$g_root_path/adm_program/administration/new_user/new_user_function.php?new_user_id=". $_GET['new_user_id']. "&amp;user_id=$row->usr_id&amp;mode=2\">Mitgliedschaft und Logindaten diesem Benutzer zuordnen</a>
+                        </span>";
+                    }               
+                    elseif(isMember($row->usr_id) == false && strlen($row->usr_login_name) > 0)
+                    {
+                        // kein Mitlgied dieser Orga und Logindaten sind bereits vorhanden
+                        echo "<br>Dieser Benutzer ist noch kein Mitglied der Organisation $g_organization, besitzt aber bereits Logindaten.<br><br>
+                        <span class=\"iconLink\">
+                            <a class=\"iconLink\" href=\"$g_root_path/adm_program/administration/new_user/new_user_function.php?new_user_id=". $_GET['new_user_id']. "&amp;user_id=$row->usr_id&amp;mode=2\"><img
+                             class=\"iconLink\" src=\"$g_root_path/adm_program/images/properties.png\" style=\"vertical-align: middle;\" border=\"0\" title=\"Mitgliedschaft zuweisen\" alt=\"Mitgliedschaft zuweisen\"></a>
+                            <a class=\"iconLink\" href=\"$g_root_path/adm_program/administration/new_user/new_user_function.php?new_user_id=". $_GET['new_user_id']. "&amp;user_id=$row->usr_id&amp;mode=2\">Mitgliedschaft zuweisen</a>
+                        </span>";
+                    }               
+                    else
+                    {
+                    	if(isMember($row->usr_id) == true)
+                        {
+                        	// der Benutzer ist bereits Mitglied dieser Orga, also nur Logindaten neu zuschicken                    
+                            echo "<br>Dieser Benutzer besitzt schon ein g&uuml;ltiges Login. 
+                                M&ouml;chtest du ihm seinen Loginnamen mit Passwort als Erinnerung zuschicken ?<br>
+                            <div style=\"margin-top: 5px;\">
+                                <span class=\"iconLink\">
+                                    <a class=\"iconLink\" href=\"$g_root_path/adm_program/administration/new_user/new_user_function.php?new_user_id=". $_GET['new_user_id']. "&amp;user_id=$row->usr_id&amp;mode=6\"><img
+                                     class=\"iconLink\" src=\"$g_root_path/adm_program/images/key.png\" style=\"vertical-align: middle;\" border=\"0\" title=\"E-Mail mit Benutzernamen und neuem Passwort zuschicken\" alt=\"E-Mail mit Benutzernamen und neuem Passwort zuschicken\"></a>
+                                    <a class=\"iconLink\" href=\"$g_root_path/adm_program/administration/new_user/new_user_function.php?new_user_id=". $_GET['new_user_id']. "&amp;user_id=$row->usr_id&amp;mode=6\">Zugangsdaten zuschicken</a>
+                                </span>
+                            </div>";
+                        }
+                    }
+                echo "</div>";
+                $i++;
+            }
+        echo "</div>
+        <div class=\"groupBox\" style=\"margin-top: 10px; text-align: left;\">
+            <div class=\"groupBoxHeadline\">Neuen Benutzer anlegen</div>
+            <div style=\"margin-left: 20px;\">
+                Falls der neue Benutzer nicht bei den oben aufgelisteten Benutzern dabei ist, 
+                kannst du auch einen neuen Benutzer anlegen.<br>
+                <div style=\"margin-top: 5px;\">
+                    <span class=\"iconLink\">
+                        <a class=\"iconLink\" href=\"$g_root_path/adm_program/modules/profile/profile_new.php?user_id=". $_GET['new_user_id']. "&new_user=3\"><img
+                         class=\"iconLink\" src=\"$g_root_path/adm_program/images/add.png\" style=\"vertical-align: middle;\" border=\"0\" title=\"Neuen Benutzer anlegen\" alt=\"Neuen Benutzer anlegen\"></a>
+                        <a class=\"iconLink\" href=\"$g_root_path/adm_program/modules/profile/profile_new.php?user_id=". $_GET['new_user_id']. "&new_user=3\">Benutzer anlegen</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div style=\"margin-top: 20px;\">
+            <span class=\"iconLink\">
+                <a class=\"iconLink\" href=\"$g_root_path/adm_program/system/back.php\"><img
+                 class=\"iconLink\" src=\"$g_root_path/adm_program/images/back.png\" style=\"vertical-align: middle;\" border=\"0\" alt=\"Zur&uuml;ck\"></a>
+                <a class=\"iconLink\" href=\"$g_root_path/adm_program/system/back.php\">Zur&uuml;ck</a>
+            </span>
+        </div>
+    </div>
 </div>";
 
 require("../../../adm_config/body_bottom.php");
