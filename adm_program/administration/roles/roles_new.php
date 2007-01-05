f16222933794e72fe28b8a1ec97987a1c0bbfaaa
Markus Faßbender <markus.fassbender@gmail.com>
2007-01-05 13:40:56 +0000

    Warningausgaben bei der Zurücknavigation nach einer Fehlermeldung entfernt Prüfung eingebaut, ob Rollen zur aktuellen Orga gehören Erstellen von gleichen Rollennamen unterschiedlicher Kategorien ermöglicht

--- adm_program/administration/roles/roles_new.php
+++ adm_program/administration/roles/roles_new.php
@@ -53,6 +53,24 @@ else
 
 $_SESSION['navigation']->addUrl($g_current_url);
 
+if ($rol_id > 0)
+{
+    // Daten der Rolle auslesen und gleichzeitig pruefen, 
+    // ob die Rolle auch zur aktuellen Orga gehoert
+    $sql    = "SELECT * FROM ". TBL_ROLES. " 
+                WHERE rol_id = {0}
+                  AND rol_org_shortname = '$g_organization' ";
+    $sql    = prepareSQL($sql, array($rol_id));
+    $result = mysql_query($sql, $g_adm_con);
+    db_error($result);
+    
+    if (mysql_num_rows($result) == 0)
+    {
+        $g_message->show("invalid");
+    }
+    $row_ar = mysql_fetch_object($result);
+}
+
 if(isset($_SESSION['roles_request']))
 {
    $form_values = $_SESSION['roles_request'];
@@ -72,7 +90,7 @@ else
     $form_values['downloads']     = 0;
     $form_values['guestbook']     = 0;
     $form_values['guestbook_comments'] = 0;
-    $form_values['profile'] 	  = 1;
+    $form_values['profile']       = 1;
     $form_values['mail_logout']   = 0;
     $form_values['mail_login']    = 0;
     $form_values['links']         = 0;
@@ -90,58 +108,48 @@ else
 
     if ($rol_id > 0)
     {
-        $sql    = "SELECT * FROM ". TBL_ROLES. " WHERE rol_id = {0}";
-        $sql    = prepareSQL($sql, array($rol_id));
-        $result = mysql_query($sql, $g_adm_con);
-        db_error($result);
-
-        if (mysql_num_rows($result) > 0)
+        // Rolle Webmaster darf nur vom Webmaster selber erstellt oder gepflegt werden
+        if($row_ar->rol_name == "Webmaster" && !hasRole("Webmaster"))
         {
-            $row_ar = mysql_fetch_object($result);
-
-            // Rolle Webmaster darf nur vom Webmaster selber erstellt oder gepflegt werden
-            if($row_ar->rol_name == "Webmaster" && !hasRole("Webmaster"))
+            if($g_current_user->id != $row_ar->rol_usr_id)
             {
-                if($g_current_user->id != $row_ar->rol_usr_id)
-                {
-                    $g_message->show("norights");
-                }
+                $g_message->show("norights");
             }
+        }
 
-            $form_values['name']          = $row_ar->rol_name;
-            $form_values['description']   = $row_ar->rol_description;
-            $form_values['category']      = $row_ar->rol_cat_id;
-            $form_values['locked']        = $row_ar->rol_locked;
-            $form_values['moderation']    = $row_ar->rol_moderation;
-            $form_values['users']         = $row_ar->rol_edit_user;
-            $form_values['announcements'] = $row_ar->rol_announcements;
-            $form_values['dates']         = $row_ar->rol_dates;
-            $form_values['photos']        = $row_ar->rol_photo;
-            $form_values['downloads']     = $row_ar->rol_download;
-            $form_values['guestbook']     = $row_ar->rol_guestbook;
-            $form_values['guestbook_comments'] = $row_ar->rol_guestbook_comments;
-            $form_values['guestbook_comments'] = $row_ar->rol_guestbook_comments;
-            $form_values['profile'] 	  = $row_ar->rol_profile;
-            $form_values['mail_logout']   = $row_ar->rol_mail_logout;
-            $form_values['mail_login']    = $row_ar->rol_mail_login;
-            $form_values['links']         = $row_ar->rol_weblinks;
-            $form_values['max_members']   = $row_ar->rol_max_members;
-            $form_values['start_date']    = mysqldate("d.m.y", $row_ar->rol_start_date);
-            $form_values['end_date']      = mysqldate("d.m.y", $row_ar->rol_end_date);
-            $form_values['start_time']    = mysqltime("h:i",   $row_ar->rol_start_time);
-            $form_values['end_time']      = mysqltime("h:i",   $row_ar->rol_end_time);
-            $form_values['weekday']       = $row_ar->rol_weekday;
-            $form_values['location']      = $row_ar->rol_location;
-            $form_values['cost']          = $row_ar->rol_cost;
+        $form_values['name']          = $row_ar->rol_name;
+        $form_values['description']   = $row_ar->rol_description;
+        $form_values['category']      = $row_ar->rol_cat_id;
+        $form_values['locked']        = $row_ar->rol_locked;
+        $form_values['moderation']    = $row_ar->rol_moderation;
+        $form_values['users']         = $row_ar->rol_edit_user;
+        $form_values['announcements'] = $row_ar->rol_announcements;
+        $form_values['dates']         = $row_ar->rol_dates;
+        $form_values['photos']        = $row_ar->rol_photo;
+        $form_values['downloads']     = $row_ar->rol_download;
+        $form_values['guestbook']     = $row_ar->rol_guestbook;
+        $form_values['guestbook_comments'] = $row_ar->rol_guestbook_comments;
+        $form_values['guestbook_comments'] = $row_ar->rol_guestbook_comments;
+        $form_values['profile']       = $row_ar->rol_profile;
+        $form_values['mail_logout']   = $row_ar->rol_mail_logout;
+        $form_values['mail_login']    = $row_ar->rol_mail_login;
+        $form_values['links']         = $row_ar->rol_weblinks;
+        $form_values['max_members']   = $row_ar->rol_max_members;
+        $form_values['start_date']    = mysqldate("d.m.y", $row_ar->rol_start_date);
+        $form_values['end_date']      = mysqldate("d.m.y", $row_ar->rol_end_date);
+        $form_values['start_time']    = mysqltime("h:i",   $row_ar->rol_start_time);
+        $form_values['end_time']      = mysqltime("h:i",   $row_ar->rol_end_time);
+        $form_values['weekday']       = $row_ar->rol_weekday;
+        $form_values['location']      = $row_ar->rol_location;
+        $form_values['cost']          = $row_ar->rol_cost;
 
-            if ($form_values['start_time'] == "00:00")
-            {
-                $form_values['start_time'] = "";
-            }
-            if ($form_values['end_time'] == "00:00")
-            {
-                $form_values['end_time'] = "";
-            }
+        if ($form_values['start_time'] == "00:00")
+        {
+            $form_values['start_time'] = "";
+        }
+        if ($form_values['end_time'] == "00:00")
+        {
+            $form_values['end_time'] = "";
         }
     }
 }
@@ -255,7 +263,7 @@ require("../../../adm_config/body_top.php");
                     </div>
                     <div style=\"text-align: left; margin-left: 30%;\">
                         <input type=\"checkbox\" id=\"locked\" name=\"locked\" ";
-                            if($form_values['locked'] == 1)
+                            if(isset($form_values['locked']) && $form_values['locked'] == 1)
                             {
                                 echo " checked ";
                             }
@@ -272,7 +280,7 @@ require("../../../adm_config/body_top.php");
                     <div style=\"margin-top: 6px;\">
                         <div style=\"text-align: right; width: 10%; float: left;\">
                             <input type=\"checkbox\" id=\"moderation\" name=\"moderation\" ";
-                            if($form_values['moderation'] == 1)
+                            if(isset($form_values['moderation']) && $form_values['moderation'] == 1)
                             {
                                 echo " checked ";
                             }
@@ -292,7 +300,7 @@ require("../../../adm_config/body_top.php");
                     <div style=\"margin-top: 6px;\">
                         <div style=\"text-align: right; width: 10%; float: left;\">
                             <input type=\"checkbox\" id=\"users\" name=\"users\" ";
-                            if($form_values['users'] == 1)
+                            if(isset($form_values['users']) && $form_values['users'] == 1)
                             {
                                 echo " checked ";
                             }
@@ -308,7 +316,7 @@ require("../../../adm_config/body_top.php");
                     <div style=\"margin-top: 6px;\">
                         <div style=\"text-align: right; width: 10%; float: left;\">
                             <input type=\"checkbox\" id=\"announcements\" name=\"announcements\" ";
-                            if($form_values['announcements'] == 1)
+                            if(isset($form_values['announcements']) && $form_values['announcements'] == 1)
                                 echo " checked ";
                             echo " value=\"1\" />&nbsp;
                             <label for=\"announcements\"><img src=\"$g_root_path/adm_program/images/note.png\" alt=\"Ank&uuml;ndigungen anlegen und bearbeiten\"></label>
@@ -320,7 +328,7 @@ require("../../../adm_config/body_top.php");
                     <div style=\"margin-top: 6px;\">
                         <div style=\"text-align: right; width: 10%; float: left;\">
                             <input type=\"checkbox\" id=\"dates\" name=\"dates\" ";
-                            if($form_values['dates'] == 1)
+                            if(isset($form_values['dates']) && $form_values['dates'] == 1)
                                 echo " checked ";
                             echo " value=\"1\" />&nbsp;
                             <label for=\"dates\"><img src=\"$g_root_path/adm_program/images/date.png\" alt=\"Termine anlegen und bearbeiten\"></label>
@@ -332,7 +340,7 @@ require("../../../adm_config/body_top.php");
                     <div style=\"margin-top: 6px;\">
                         <div style=\"text-align: right; width: 10%; float: left;\">
                             <input type=\"checkbox\" id=\"photos\" name=\"photos\" ";
-                            if($form_values['photos'] == 1)
+                            if(isset($form_values['photos']) && $form_values['photos'] == 1)
                                 echo " checked ";
                             echo " value=\"1\" />&nbsp;
                             <label for=\"photos\"><img src=\"$g_root_path/adm_program/images/photo.png\" alt=\"Fotos hochladen und bearbeiten\"></label>
@@ -344,7 +352,7 @@ require("../../../adm_config/body_top.php");
                     <div style=\"margin-top: 6px;\">
                         <div style=\"text-align: right; width: 10%; float: left;\">
                             <input type=\"checkbox\" id=\"downloads\" name=\"downloads\" ";
-                            if($form_values['downloads'] == 1)
+                            if(isset($form_values['downloads']) && $form_values['downloads'] == 1)
                                 echo " checked ";
                             echo " value=\"1\" />&nbsp;
                             <label for=\"downloads\"><img src=\"$g_root_path/adm_program/images/folder_down.png\" alt=\"Downloads hochladen und bearbeiten\"></label>
@@ -356,7 +364,7 @@ require("../../../adm_config/body_top.php");
                     <div style=\"margin-top: 6px;\">
                         <div style=\"text-align: right; width: 10%; float: left;\">
                             <input type=\"checkbox\" id=\"guestbook\" name=\"guestbook\" ";
-                            if($form_values['guestbook'] == 1)
+                            if(isset($form_values['guestbook']) && $form_values['guestbook'] == 1)
                                 echo " checked ";
                             echo " value=\"1\" />&nbsp;
                             <label for=\"guestbook\"><img src=\"$g_root_path/adm_program/images/comment.png\" alt=\"G&auml;stebucheintr&auml;ge bearbeiten und l&ouml;schen\"></label>
@@ -368,7 +376,7 @@ require("../../../adm_config/body_top.php");
                     <div style=\"margin-top: 6px;\">
                         <div style=\"text-align: right; width: 10%; float: left;\">
                             <input type=\"checkbox\" id=\"guestbook_comments\" name=\"guestbook_comments\" ";
-                            if($form_values['guestbook_comments'] == 1)
+                            if(isset($form_values['guestbook_comments']) && $form_values['guestbook_comments'] == 1)
                                 echo " checked ";
                             echo " value=\"1\" />&nbsp;
                             <label for=\"guestbook_comments\"><img src=\"$g_root_path/adm_program/images/comments.png\" alt=\"Kommentare zu G&auml;stebucheintr&auml;gen anlegen\"></label>
@@ -380,7 +388,7 @@ require("../../../adm_config/body_top.php");
                     <div style=\"margin-top: 6px;\">
                         <div style=\"text-align: right; width: 10%; float: left;\">
                             <input type=\"checkbox\" id=\"mail_logout\" name=\"mail_logout\" ";
-                            if($form_values['mail_logout'] == 1)
+                            if(isset($form_values['mail_logout']) && $form_values['mail_logout'] == 1)
                                 echo " checked ";
                             echo " value=\"1\" />&nbsp;
                             <label for=\"mail_logout\"><img src=\"$g_root_path/adm_program/images/mail.png\" alt=\"Besucher (ausgeloggt) k&ouml;nnen E-Mails an diese Rolle schreiben\"></label>
@@ -394,7 +402,7 @@ require("../../../adm_config/body_top.php");
                     <div style=\"margin-top: 6px;\">
                         <div style=\"text-align: right; width: 10%; float: left;\">
                             <input type=\"checkbox\" id=\"mail_login\" name=\"mail_login\" ";
-                            if($form_values['mail_login'] == 1)
+                            if(isset($form_values['mail_login']) && $form_values['mail_login'] == 1)
                                 echo " checked ";
                             echo " value=\"1\" />&nbsp;
                             <label for=\"mail_login\"><img src=\"$g_root_path/adm_program/images/mail_key.png\" alt=\"Eingeloggte Benutzer k&ouml;nnen E-Mails an diese Rolle schreiben\"></label>
@@ -408,7 +416,7 @@ require("../../../adm_config/body_top.php");
                     <div style=\"margin-top: 6px;\">
                         <div style=\"text-align: right; width: 10%; float: left;\">
                             <input type=\"checkbox\" id=\"links\" name=\"links\" ";
-                            if($form_values['links'] == 1)
+                            if(isset($form_values['links']) && $form_values['links'] == 1)
                                 echo " checked ";
                             echo " value=\"1\" />&nbsp;
                             <label for=\"links\"><img src=\"$g_root_path/adm_program/images/globe.png\" alt=\"Weblinks anlegen und bearbeiten\"></label>
@@ -420,7 +428,7 @@ require("../../../adm_config/body_top.php");
                     <div style=\"margin-top: 6px;\">
                         <div style=\"text-align: right; width: 10%; float: left;\">
                             <input type=\"checkbox\" id=\"profile\" name=\"profile\" ";
-                            if($form_values['profile'] == 1)
+                            if(isset($form_values['profile']) && $form_values['profile'] == 1)
                                 echo " checked ";
                             echo " value=\"1\" />&nbsp;
                             <label for=\"profile\"><img src=\"$g_root_path/adm_program/images/user.png\" alt=\"Eigenes Profil bearbeiten\"></label>
