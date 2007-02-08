388804aaa163a54951f5c18f3bd6cad75f75a618
Thomas Thoss <t.thoss@admidio.org>
2007-02-08 19:16:59 +0000

    habe mal auf die schnelle ein kleines Online Plugin bebaut.

---
+++ adm_plugins/sidebar_online/config.php
@@ -0,0 +1,16 @@
+<?php
+/******************************************************************************
+ * Konfigurationsdatei fuer Sidebar-Online
+ * ein Admidio-Plugin
+ *
+ * Copyright    : (c) 2004 - 2007 The Admidio Team
+ * Homepage     : http://www.admidio.org
+ * Module-Owner : Thomas Thoss
+ *
+ *****************************************************************************/
+
+// Zeit in der die User noch als Aktiv gelten (Default = 10)
+$onlinezeit = 10;
+
+
+?>
