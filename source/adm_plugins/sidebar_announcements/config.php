7d2c3f75d0decb806d177a63853861bf7eab1c1c
Markus Faßbender <markus.fassbender@gmail.com>
2007-02-26 13:17:38 +0000

    Sidebar Announcements wiederhergestellt

--- source/adm_plugins/sidebar_announcements/config.php
+++ source/adm_plugins/sidebar_announcements/config.php
@@ -9,6 +9,13 @@
  *
  *****************************************************************************/
 
+// Anzahl der Termine, die angezeigt werden sollen (Default = 2)
+$plg_announcements_count = 2;
+
+// Name einer CSS-Klasse fuer Links
+// Nur noetig, falls die Links ein anderes Aussehen bekommen sollen
+$plg_link_class = '';
+
 // Angabe des Ziels (target) in dem die Inhalte der Links geöffnet werden sollen
 // Hier koennen die ueblichen targets (_self, _top ...) oder Framenamen angegeben werden
 $plg_link_target = '_self';
@@ -18,24 +25,6 @@ $plg_link_target = '_self';
 $plg_max_char_per_word = 0;
 
 // Wahlweise kann hier ein anderer Titel fuer die Ankuendigungen angegeben werden
-$plg_headline = 'Fotos';
+$plg_headline = 'Ank&uuml;ndigungen';
 
-//Maximale Photobreite
-//Angabe in px, (Default = 150)
-$plg_photos_max_width = 150;
-
-//Maximale Photohoehe
-//Angabe in px, (Default = 200)
-$plg_photos_max_height = 200;
-
-//Zahl der Veranstaltungen aus denen das Foto kommen darf, gezählt wird ab der aktuellsten
-//Default = 0 (Keine Einschraenkung)
-$plg_photos_events = 0;
-
-//Bildauswahl
-// =1 (erstes Bild) etc. =0 (Zufall,(Default)) 
-$plg_photos_picnr = 0;
-
-//Soll der Link zur Veranstaltung unter dem Bild angezeigt werden?
-$plg_photos_show_link = false;
 ?>
