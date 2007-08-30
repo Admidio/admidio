71030921ed6e35f7d232ba5611097647f17671d0
Markus Faßbender <markus.fassbender@gmail.com>
2007-08-30 11:33:24 +0000

    - "Sidebar Login" in "Login Form" umbenannt - Feld fuer automatischen Login eingebaut - Layout kann jetzt über eine CSS-Datei angepasst werden - beliebig viele Kopieen des Plugins können gleichzeitig laufen

---
+++ adm_plugins/login_form/config.php
@@ -0,0 +1,42 @@
+<?php
+/******************************************************************************
+ * Konfigurationsdatei fuer das Admidio-Plugin Login Form
+ *
+ * Copyright    : (c) 2004 - 2007 The Admidio Team
+ * Homepage     : http://www.admidio.org
+ * Module-Owner : Markus Fassbender
+ * License      : GNU Public License 2 http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ *****************************************************************************/
+
+// Zeigt einen Link zum Registrieren unter dem Loginbutton an
+// 1 = (Default) Link wird angezeigt
+// 0 = Link wird nicht angezeigt
+$plg_show_register_link = 1;
+
+// Zeigt einen Link um eine E-Mail an den Webmaster zu schreiben, 
+// falls es Probleme beim Login gibt
+// 1 = (Default) Link wird angezeigt
+// 0 = Link wird nicht angezeigt
+$plg_show_email_link = 1;
+
+// Zeigt nach dem Einloggen einen Link zum Ausloggen an
+// 1 = (Default) Link wird angezeigt
+// 0 = Link wird nicht angezeigt
+$plg_show_logout_link = 1;
+
+// Angabe des Ziels (target) in dem die Inhalte der Links geöffnet werden sollen
+// Hier koennen die ueblichen targets (_self, _top ...) oder Framenamen angegeben werden
+$plg_link_target = '_self';
+
+// eine kleine Spielerei
+// hier kann man Raenge eingeben, der Benutzer sieht nach dem Einloggen dann seinen Rang
+// in der Seitenleiste und kann sich daran erfreuen :)
+// Falls dies nicht gewuenscht ist, einfach alle Zeilen mit den Raengen loeschen
+$plg_rank = array(
+    "0"   => "Neumitglied",
+    "50"  => "Mitglied",
+    "100" => "Seniormitglied",
+    "200" => "Ehrenmitglied",
+    );
+?>
