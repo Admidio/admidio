b120d9462ead844f443b4c1c874157cf6fc350c8
Markus Faßbender <markus.fassbender@gmail.com>
2007-08-30 11:28:38 +0000

    Link-Text wird vor der Benutzung initialisiert Es gab vorher Probleme beim Aufruf der Benutzerverwaltung

--- source/adm_plugins/random_photo/random_photo.php
+++ source/adm_plugins/random_photo/random_photo.php
@@ -12,21 +12,7 @@
  * Copyright    : (c) 2004 - 2007 The Admidio Team
  * Homepage     : http://www.admidio.org
  * Module-Owner : Jochen Erkens
- *
- ******************************************************************************
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * License      : GNU Public License 2 http://www.gnu.org/licenses/gpl-2.0.html
  *
  *****************************************************************************/
 
@@ -142,6 +128,7 @@ $bildgroesse = getimagesize($picpath);
 //Popupfenstergröße
 $popup_height = $g_preferences['photo_show_height']+210;
 $popup_width  = $g_preferences['photo_show_width']+70;
+$link_text    = "";
 
 if($plg_photos_show_link && $plg_max_char_per_word > 0)
 {
