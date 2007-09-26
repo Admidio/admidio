d707a729eb33fbf92e9735b305d5c8eae75e2942
Markus Faßbender <markus.fassbender@gmail.com>
2007-09-26 17:56:24 +0000

    Und nun noch der Rest

---
+++ adm_install/db_scripts/upd_1_3_conv.php
@@ -0,0 +1,118 @@
+<?php
+/******************************************************************************
+ * Datenkonvertierung fuer die Version 1.3
+ *
+ * Copyright    : (c) 2004 - 2007 The Admidio Team
+ * Homepage     : http://www.admidio.org
+ * Module-Owner : Markus Fassbender
+ * License      : GNU Public License 2 http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ *****************************************************************************/
+
+// Alle Moderatoren bekommen das Recht fuer Ankuendigungen
+$sql = "UPDATE ". TBL_ROLES. " SET rol_announcements = 1
+         WHERE rol_moderation = 1 ";
+$result = mysql_query($sql, $connection);
+if(!$result) showError(mysql_error());
+    
+// die Uhrzeit bei dat_end wurde nicht immer korrekt gesetzt
+$sql = "SELECT * FROM ". TBL_DATES. "
+         WHERE (   HOUR(dat_end)   = 0
+               AND MINUTE(dat_end) = 0
+               AND SECOND(dat_end) = 0 )
+           AND (  HOUR(dat_begin)   <> 0
+               OR MINUTE(dat_begin) <> 0
+               OR SECOND(dat_begin) <> 0 ) ";
+$dat_result = mysql_query($sql, $connection);
+if(!$dat_result) showError(mysql_error());
+
+while($dat_row = mysql_fetch_object($dat_result))
+{
+    $date = mysqldatetime("y-m-d", $dat_row->dat_end);
+    $time = mysqldatetime("h:i:s", $dat_row->dat_begin);
+    $datetime = "$date $time";
+    
+    $sql = "UPDATE ". TBL_DATES. " SET dat_end = '$datetime'
+             WHERE dat_id = $dat_row->dat_id ";
+    $result = mysql_query($sql, $connection);
+    if(!$result) showError(mysql_error());
+}
+
+// Orga-Felder in adm_preferences umwandeln
+$sql = "SELECT * FROM ". TBL_ORGANIZATIONS;
+$result_orga = mysql_query($sql, $connection);
+if(!$result_orga) showError(mysql_error());
+
+while($row_orga = mysql_fetch_object($result_orga))
+{
+    $sql = "INSERT INTO ". TBL_PREFERENCES. " (prf_org_id, prf_name, prf_value)
+            VALUES ($row_orga->org_id, 'max_email_attachment_size', $row_orga->org_mail_size)";
+    $result = mysql_query($sql, $connection);
+    if(!$result) showError(mysql_error());
+
+    $sql = "INSERT INTO ". TBL_PREFERENCES. " (prf_org_id, prf_name, prf_value)
+            VALUES ($row_orga->org_id, 'max_file_upload_size', $row_orga->org_upload_size)";
+    $result = mysql_query($sql, $connection);
+    if(!$result) showError(mysql_error());
+
+    $sql = "INSERT INTO ". TBL_PREFERENCES. " (prf_org_id, prf_name, prf_value)
+            VALUES ($row_orga->org_id, 'send_email_extern', $row_orga->org_mail_extern)";
+    $result = mysql_query($sql, $connection);
+    if(!$result) showError(mysql_error());
+
+    $sql = "INSERT INTO ". TBL_PREFERENCES. " (prf_org_id, prf_name, prf_value)
+            VALUES ($row_orga->org_id, 'enable_rss', $row_orga->org_enable_rss)";
+    $result = mysql_query($sql, $connection);
+    if(!$result) showError(mysql_error());
+
+    $sql = "INSERT INTO ". TBL_PREFERENCES. " (prf_org_id, prf_name, prf_value)
+            VALUES ($row_orga->org_id, 'enable_bbcode', $row_orga->org_bbcode)";
+    $result = mysql_query($sql, $connection);
+    if(!$result) showError(mysql_error());
+    
+    $sql = "INSERT INTO ". TBL_PREFERENCES. " (prf_org_id, prf_name, prf_value)
+            VALUES ($row_orga->org_id, 'email_administrator', 'webmaster@". $_SERVER['HTTP_HOST']. "')";
+    $result = mysql_query($sql, $connection);
+    if(!$result) showError(mysql_error());
+    
+    $sql = "INSERT INTO ". TBL_PREFERENCES. " (prf_org_id, prf_name, prf_value)
+            VALUES ($row_orga->org_id, 'default_country', 'Deutschland')";
+    $result = mysql_query($sql, $connection);
+    if(!$result) showError(mysql_error());
+    
+    $sql = "INSERT INTO ". TBL_PREFERENCES. " (prf_org_id, prf_name, prf_value)
+            VALUES ($row_orga->org_id, 'photo_save_scale', '640')";
+    $result = mysql_query($sql, $connection);
+    if(!$result) showError(mysql_error());
+}
+
+// unnoetige Orga-Felder koennen jetzt geloescht werden
+$sql = "ALTER TABLE ". TBL_ORGANIZATIONS. " DROP org_mail_size";
+$result = mysql_query($sql, $connection);
+if(!$result) showError(mysql_error());
+
+$sql = "ALTER TABLE ". TBL_ORGANIZATIONS. " DROP org_upload_size";
+$result = mysql_query($sql, $connection);
+if(!$result) showError(mysql_error());
+
+$sql = "ALTER TABLE ". TBL_ORGANIZATIONS. " DROP org_photo_size";
+$result = mysql_query($sql, $connection);
+if(!$result) showError(mysql_error());
+
+$sql = "ALTER TABLE ". TBL_ORGANIZATIONS. " DROP org_mail_extern";
+$result = mysql_query($sql, $connection);
+if(!$result) showError(mysql_error());
+
+$sql = "ALTER TABLE ". TBL_ORGANIZATIONS. " DROP org_enable_rss";
+$result = mysql_query($sql, $connection);
+if(!$result) showError(mysql_error());
+
+$sql = "ALTER TABLE ". TBL_ORGANIZATIONS. " DROP org_bbcode";
+$result = mysql_query($sql, $connection);
+if(!$result) showError(mysql_error());
+
+$sql = "ALTER TABLE ". TBL_ORGANIZATIONS. " DROP org_font";
+$result = mysql_query($sql, $connection);
+if(!$result) showError(mysql_error());
+
+?>
---
+++ adm_install/db_scripts/upd_1_4_db.sql
@@ -0,0 +1,85 @@
+
+-- Kategorientabelle konvertieren
+ALTER TABLE %PRAEFIX%_role_categories DROP FOREIGN KEY %PRAEFIX%_FK_RLC_ORG;
+ALTER TABLE %PRAEFIX%_roles DROP FOREIGN KEY %PRAEFIX%_FK_ROL_RLC;
+
+create table %PRAEFIX%_categories
+(
+   cat_id                         int (11) unsigned              not null AUTO_INCREMENT,
+   cat_org_id                     tinyint(4)                     not null,
+   cat_type                       varchar(10)                    not null,
+   cat_name                       varchar(30)                    not null,
+   cat_hidden                     tinyint(1) unsigned            not null default 0,
+   primary key (cat_id)
+)
+type = InnoDB
+auto_increment = 1;
+
+alter table %PRAEFIX%_categories add index CAT_ORG_FK (cat_org_id);
+
+ALTER TABLE %PRAEFIX%_roles DROP index ROL_RLC_FK;
+ALTER TABLE %PRAEFIX%_roles CHANGE COLUMN `rol_rlc_id` `rol_cat_id` INTEGER(11) UNSIGNED NOT NULL DEFAULT 0;
+alter table %PRAEFIX%_roles add index ROL_CAT_FK (rol_cat_id);
+ALTER TABLE %PRAEFIX%_roles ADD COLUMN `rol_profile` tinyint(1) unsigned NOT NULL DEFAULT 0 AFTER `rol_photo`;
+
+ALTER TABLE %PRAEFIX%_links ADD COLUMN `lnk_cat_id` INTEGER(11) UNSIGNED NOT NULL DEFAULT 0 AFTER `lnk_org_id`;
+alter table %PRAEFIX%_links add index LNK_CAT_FK (lnk_cat_id);
+
+alter table %PRAEFIX%_links add index LNK_USR_CHANGE_FK (lnk_usr_id_change);
+
+alter table %PRAEFIX%_categories add constraint %PRAEFIX%_FK_CAT_ORG foreign key (cat_org_id)
+      references %PRAEFIX%_organizations (org_id) on delete restrict on update restrict;
+      
+alter table %PRAEFIX%_links add constraint %PRAEFIX%_FK_LNK_USR_CHANGE foreign key (lnk_usr_id_change)
+      references %PRAEFIX%_users (usr_id) on delete set null on update restrict;      
+
+-- Texttabelle
+create table %PRAEFIX%_texts
+(
+   txt_id                         int(11) unsigned               not null,
+   txt_org_id                     tinyint(4)                     not null,
+   txt_name                       varchar(30)                    not null,
+   txt_text                       text,
+   primary key (txt_id)
+)
+type = InnoDB
+auto_increment = 1;
+
+alter table %PRAEFIX%_texts add index TXT_ORG_FK (txt_org_id);
+
+alter table %PRAEFIX%_texts add constraint %PRAEFIX%_FK_TXT_ORG foreign key (txt_org_id)
+      references %PRAEFIX%_organizations (org_id) on delete restrict on update restrict;
+
+-- Guestbook-Comments erweitern
+ALTER TABLE %PRAEFIX%_guestbook_comments
+ ADD COLUMN gbc_name VARCHAR(50) NOT NULL DEFAULT '' AFTER `gbc_usr_id`,
+ ADD COLUMN gbc_email VARCHAR(50) AFTER `gbc_text`,
+ ADD COLUMN gbc_ip_address VARCHAR(15) NOT NULL DEFAULT '0' AFTER `gbc_email`,
+ ADD COLUMN gbc_last_change DATETIME AFTER `gbc_ip_address`,
+ ADD COLUMN gbc_usr_id_change INTEGER(11) UNSIGNED AFTER `gbc_last_change`;
+ 
+ALTER TABLE %PRAEFIX%_guestbook_comments MODIFY COLUMN `gbc_usr_id` INTEGER UNSIGNED;
+
+alter table %PRAEFIX%_guestbook_comments add index GBC_USR_CHANGE_FK (gbc_usr_id_change);
+alter table %PRAEFIX%_guestbook_comments add constraint %PRAEFIX%_FK_GBC_USR_CHANGE foreign key (gbc_usr_id_change)
+      references %PRAEFIX%_users (usr_id) on delete set null on update restrict;
+
+-- Sessiontabelle bearbeiten
+ALTER TABLE %PRAEFIX%_sessions DROP COLUMN ses_longer_session;
+ALTER TABLE %PRAEFIX%_sessions ADD  COLUMN ses_blob blob AFTER ses_ip_address;
+
+-- Memberstabelle pflegen
+UPDATE %PRAEFIX%_members SET mem_end = NULL WHERE mem_end = '0000-00-00';
+
+-- Rollentabelle pflegen
+UPDATE %PRAEFIX%_roles SET rol_start_date = NULL WHERE rol_start_date = '0000-00-00';
+UPDATE %PRAEFIX%_roles SET rol_end_date   = NULL WHERE rol_end_date   = '0000-00-00';
+UPDATE %PRAEFIX%_roles SET rol_start_time = NULL WHERE rol_start_time = '00:00:00';
+UPDATE %PRAEFIX%_roles SET rol_end_time   = NULL WHERE rol_end_time   = '00:00:00';
+
+-- Usertabelle pflegen
+UPDATE %PRAEFIX%_users SET usr_birthday     = NULL WHERE usr_birthday     = '0000-00-00';
+UPDATE %PRAEFIX%_users SET usr_last_login   = NULL WHERE usr_last_login   = '0000-00-00 00:00:00';
+UPDATE %PRAEFIX%_users SET usr_actual_login = NULL WHERE usr_actual_login = '0000-00-00 00:00:00';
+UPDATE %PRAEFIX%_users SET usr_date_invalid = NULL WHERE usr_date_invalid = '0000-00-00 00:00:00';
+UPDATE %PRAEFIX%_users SET usr_last_change  = NULL WHERE usr_last_change  = '0000-00-00 00:00:00';
