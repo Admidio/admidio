dc61b2fefc9cdb2c48154d3e88a844273da2a0c3
Markus Faßbender <markus.fassbender@gmail.com>
2008-02-18 12:47:17 +0000

    - unnötigen Code entfernt

--- source/adm_program/modules/ecards/ecard_form.php
+++ source/adm_program/modules/ecards/ecard_form.php
@@ -5,8 +5,6 @@
  * Copyright    : (c) 2004 - 2007 The Admidio Team
  * Homepage     : http://www.admidio.org
  * Module-Owner : Roland Eischer 
- * Based on     : Jochen Erkens: Photogalerien &
- *                Elmar Meuthen: E-Mails verschicken
  * License      : GNU Public License 2 http://www.gnu.org/licenses/gpl-2.0.html
  *
  * Uebergaben:
