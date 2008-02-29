b6f37c4fe41c4c310f4da0a12a363244d0590abf
Markus Faßbender <markus.fassbender@gmail.com>
2008-02-29 09:31:27 +0000

    - Favicon wird jetzt über den eigenen Header geladen, so dass es vom User einfacher ersetzt werden kann

--- source/adm_themes/classic/my_header.php
+++ source/adm_themes/classic/my_header.php
@@ -2,3 +2,5 @@
 <!-- Hier koennen Sie Ihren HTML-Code einbauen, der in jedem <head></head> Bereich
      einer Admidio-Modul-Seite erscheinen soll.
 -->
+
+<link rel="shortcut icon" type="image/ico" href="<?php echo THEME_PATH; ?>/icons/favicon.ico" />
--- source/adm_themes/modern/my_header.php
+++ source/adm_themes/modern/my_header.php
@@ -3,5 +3,6 @@
      einer Admidio-Modul-Seite erscheinen soll.
 -->
 
+<link rel="shortcut icon" type="image/ico" href="<?php echo THEME_PATH; ?>/icons/favicon.ico" />
 <link rel="stylesheet" type="text/css" href="<?php echo THEME_PATH; ?>/css/modern.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo $g_root_path; ?>/adm_plugins/login_form/login_form.css" />
