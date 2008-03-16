fb0f89684b619ca04624347ab2a693ea7c6eaa0e
Markus Faßbender <markus.fassbender@gmail.com>
2008-03-16 20:44:12 +0000

    - Admidio-Schriftzug wieder zentriert darstellen

--- source/adm_themes/modern/my_body_bottom.php
+++ source/adm_themes/modern/my_body_bottom.php
@@ -3,97 +3,97 @@
      einer Admidio-Modul-Seite erscheinen soll.
 -->
 
-		&nbsp;</div>
-		<div><img class="img_border" src="<?php echo THEME_PATH; ?>/images/border_bottom_big.png" alt="border" /></div>
-	</div>
-	<div id="right_block">
-		<div><img class="img_border" src="<?php echo THEME_PATH; ?>/images/border_top_small.png" alt="border" /></div>
-		<div id="sidebar" class="content">
-			<?php
-			if($g_valid_login)
-			{
-				echo "<h3>Angemeldet als</h3>";
-			}
-			else
-			{
-				echo "<h3>Anmelden</h3>";
-			}
-			include(SERVER_PATH. "/adm_plugins/login_form/login_form.php"); ?>
-			
-			<br />
-			<h3>Module</h3>
-            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/announcements/announcements.php"><img 
-            	style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/announcements_small.png" alt="Ankündigungen" title="Ankündigungen" /></a>
-            	<a href="<?php echo $g_root_path; ?>/adm_program/modules/announcements/announcements.php">Ankündigungen</a></span>
-            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/download/download.php"><img 
-            	style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/download_small.png" alt="Downloads" title="Downloads" /></a>
-            	<a href="<?php echo $g_root_path; ?>/adm_program/modules/download/download.php">Downloads</a></span>
-            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/mail/mail.php"><img 
-            	style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/email_small.png" alt="E-Mail" title="E-Mail" /></a>
-            	<a href="<?php echo $g_root_path; ?>/adm_program/modules/mail/mail.php">E-Mail</a></span>
-            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/photos/photos.php"><img 
-            	style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/photo_small.png" alt="Fotos" title="Fotos" /></a>
-            	<a href="<?php echo $g_root_path; ?>/adm_program/modules/photos/photos.php">Fotos</a></span>
-            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/guestbook/guestbook.php"><img 
-            	style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/guestbook_small.png" alt="Gästebuch" title="Gästebuch" /></a>
-            	<a href="<?php echo $g_root_path; ?>/adm_program/modules/guestbook/guestbook.php">Gästebuch</a></span>
-            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/lists/lists.php"><img 
-            	style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/list_small.png" alt="Listen" title="Listen" /></a>
-            	<a href="<?php echo $g_root_path; ?>/adm_program/modules/lists/lists.php">Listen</a></span>
-            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/lists/mylist.php"><img 
-            	style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/list_small.png" alt="Eigene Listen" title="Eigene Listen" /></a>
-            	<a href="<?php echo $g_root_path; ?>/adm_program/modules/lists/mylist.php">Eigene Listen</a></span>
-            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/profile/profile.php"><img 
-            	style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/profile_small.png" alt="Mein Profil" title="Mein Profil" /></a>
-            	<a href="<?php echo $g_root_path; ?>/adm_program/modules/profile/profile.php">Mein Profil</a></span>
-            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/dates/dates.php"><img 
-            	style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/dates_small.png" alt="Termine" title="Termine" /></a>
-            	<a href="<?php echo $g_root_path; ?>/adm_program/modules/dates/dates.php">Termine</a></span>
-            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/links/links.php"><img 
-            	style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/weblinks_small.png" alt="Weblinks" title="Weblinks" /></a>
-            	<a href="<?php echo $g_root_path; ?>/adm_program/modules/links/links.php">Weblinks</a></span>
-            
-			<?php      
-			      
-			if($g_preferences['enable_forum_interface'])
+        &nbsp;</div>
+        <div><img class="img_border" src="<?php echo THEME_PATH; ?>/images/border_bottom_big.png" alt="border" /></div>
+    </div>
+    <div id="right_block">
+        <div><img class="img_border" src="<?php echo THEME_PATH; ?>/images/border_top_small.png" alt="border" /></div>
+        <div id="sidebar" class="content">
+            <?php
+            if($g_valid_login)
             {
-            	echo '<span class="menu"><a href="'. $g_forum->url. '"><img 
-            		style="vertical-align: middle;" src="'. THEME_PATH. '/icons/forum_small.png" alt="Forum" title="Forum" /></a>
-            		<a href="'. $g_forum->url. '">Forum</a></span>';
+                echo "<h3>Angemeldet als</h3>";
             }
-			
+            else
+            {
+                echo "<h3>Anmelden</h3>";
+            }
+            include(SERVER_PATH. "/adm_plugins/login_form/login_form.php"); ?>
+            
+            <br />
+            <h3>Module</h3>
+            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/announcements/announcements.php"><img 
+                style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/announcements_small.png" alt="Ankündigungen" title="Ankündigungen" /></a>
+                <a href="<?php echo $g_root_path; ?>/adm_program/modules/announcements/announcements.php">Ankündigungen</a></span>
+            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/download/download.php"><img 
+                style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/download_small.png" alt="Downloads" title="Downloads" /></a>
+                <a href="<?php echo $g_root_path; ?>/adm_program/modules/download/download.php">Downloads</a></span>
+            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/mail/mail.php"><img 
+                style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/email_small.png" alt="E-Mail" title="E-Mail" /></a>
+                <a href="<?php echo $g_root_path; ?>/adm_program/modules/mail/mail.php">E-Mail</a></span>
+            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/photos/photos.php"><img 
+                style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/photo_small.png" alt="Fotos" title="Fotos" /></a>
+                <a href="<?php echo $g_root_path; ?>/adm_program/modules/photos/photos.php">Fotos</a></span>
+            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/guestbook/guestbook.php"><img 
+                style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/guestbook_small.png" alt="Gästebuch" title="Gästebuch" /></a>
+                <a href="<?php echo $g_root_path; ?>/adm_program/modules/guestbook/guestbook.php">Gästebuch</a></span>
+            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/lists/lists.php"><img 
+                style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/list_small.png" alt="Listen" title="Listen" /></a>
+                <a href="<?php echo $g_root_path; ?>/adm_program/modules/lists/lists.php">Listen</a></span>
+            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/lists/mylist.php"><img 
+                style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/list_small.png" alt="Eigene Listen" title="Eigene Listen" /></a>
+                <a href="<?php echo $g_root_path; ?>/adm_program/modules/lists/mylist.php">Eigene Listen</a></span>
+            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/profile/profile.php"><img 
+                style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/profile_small.png" alt="Mein Profil" title="Mein Profil" /></a>
+                <a href="<?php echo $g_root_path; ?>/adm_program/modules/profile/profile.php">Mein Profil</a></span>
+            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/dates/dates.php"><img 
+                style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/dates_small.png" alt="Termine" title="Termine" /></a>
+                <a href="<?php echo $g_root_path; ?>/adm_program/modules/dates/dates.php">Termine</a></span>
+            <span class="menu"><a href="<?php echo $g_root_path; ?>/adm_program/modules/links/links.php"><img 
+                style="vertical-align: middle;" src="<?php echo THEME_PATH; ?>/icons/weblinks_small.png" alt="Weblinks" title="Weblinks" /></a>
+                <a href="<?php echo $g_root_path; ?>/adm_program/modules/links/links.php">Weblinks</a></span>
+            
+            <?php      
+                  
+            if($g_preferences['enable_forum_interface'])
+            {
+                echo '<span class="menu"><a href="'. $g_forum->url. '"><img 
+                    style="vertical-align: middle;" src="'. THEME_PATH. '/icons/forum_small.png" alt="Forum" title="Forum" /></a>
+                    <a href="'. $g_forum->url. '">Forum</a></span>';
+            }
+            
             if($g_current_user->isWebmaster() || $g_current_user->assignRoles() || $g_current_user->approveUsers() || $g_current_user->editUser())
             {
-            	echo '<h3>Administration</h3>';
-            	if($g_current_user->editUser())
-					echo '<span class="menu"><a href="'. $g_root_path. '/adm_program/administration/new_user/new_user.php"><img 
-            		style="vertical-align: middle;" src="'. THEME_PATH. '/icons/new_registrations_small.png" alt="Neue Anmeldungen" title="Neue Anmeldungen" /></a>
-            		<a href="'. $g_root_path. '/adm_program/administration/new_user/new_user.php">Neue Anmeldungen</a></span>';
-				if($g_current_user->approveUsers())
-					echo '<span class="menu"><a href="'. $g_root_path. '/adm_program/administration/members/members.php"><img 
-            		style="vertical-align: middle;" src="'. THEME_PATH. '/icons/user_administration_small.png" alt="Benutzerverwaltung" title="Benutzerverwaltung" /></a>
-            		<a href="'. $g_root_path. '/adm_program/administration/members/members.php">Benutzerverwaltung</a></span>';
-				if($g_current_user->assignRoles())
-					echo '<span class="menu"><a href="'. $g_root_path. '/adm_program/administration/roles/roles.php"><img 
-            		style="vertical-align: middle;" src="'. THEME_PATH. '/icons/roles_administration_small.png" alt="Rollenverwaltung" title="Rollenverwaltung" /></a>
-            		<a href="'. $g_root_path. '/adm_program/administration/roles/roles.php">Rollenverwaltung</a></span>';
-				if($g_current_user->isWebmaster())
-					echo '<span class="menu"><a href="'. $g_root_path. '/adm_program/administration/organization/organization.php"><img 
-            		style="vertical-align: middle;" src="'. THEME_PATH. '/icons/options_small.png" alt="Organisationseinstellungen" title="Organisationseinstellungen" /></a>
-            		<a href="'. $g_root_path. '/adm_program/administration/organization/organization.php">Organisationseinstellungen</a></span>';
+                echo '<h3>Administration</h3>';
+                if($g_current_user->editUser())
+                    echo '<span class="menu"><a href="'. $g_root_path. '/adm_program/administration/new_user/new_user.php"><img 
+                    style="vertical-align: middle;" src="'. THEME_PATH. '/icons/new_registrations_small.png" alt="Neue Anmeldungen" title="Neue Anmeldungen" /></a>
+                    <a href="'. $g_root_path. '/adm_program/administration/new_user/new_user.php">Neue Anmeldungen</a></span>';
+                if($g_current_user->approveUsers())
+                    echo '<span class="menu"><a href="'. $g_root_path. '/adm_program/administration/members/members.php"><img 
+                    style="vertical-align: middle;" src="'. THEME_PATH. '/icons/user_administration_small.png" alt="Benutzerverwaltung" title="Benutzerverwaltung" /></a>
+                    <a href="'. $g_root_path. '/adm_program/administration/members/members.php">Benutzerverwaltung</a></span>';
+                if($g_current_user->assignRoles())
+                    echo '<span class="menu"><a href="'. $g_root_path. '/adm_program/administration/roles/roles.php"><img 
+                    style="vertical-align: middle;" src="'. THEME_PATH. '/icons/roles_administration_small.png" alt="Rollenverwaltung" title="Rollenverwaltung" /></a>
+                    <a href="'. $g_root_path. '/adm_program/administration/roles/roles.php">Rollenverwaltung</a></span>';
+                if($g_current_user->isWebmaster())
+                    echo '<span class="menu"><a href="'. $g_root_path. '/adm_program/administration/organization/organization.php"><img 
+                    style="vertical-align: middle;" src="'. THEME_PATH. '/icons/options_small.png" alt="Organisationseinstellungen" title="Organisationseinstellungen" /></a>
+                    <a href="'. $g_root_path. '/adm_program/administration/organization/organization.php">Organisationseinstellungen</a></span>';
             }
             ?>
-		</div>
-		<div><img class="img_border" src="<?php echo THEME_PATH; ?>/images/border_bottom_small.png" alt="border" /></div>
-		
-		<div style="clear: both;"></div>
-	</div>
+        </div>
+        <div><img class="img_border" src="<?php echo THEME_PATH; ?>/images/border_bottom_small.png" alt="border" /></div>
+        
+        <div style="clear: both;"></div>
+    </div>
 </div>
 
 <p>
-	<a href="http://www.admidio.org"><img 
-	src="<?php echo THEME_PATH ?>/images/admidio_logo_20.png" style="border: 0px; vertical-align: bottom;"
-	 alt="Das Online-Verwaltungssystem für Vereine, Gruppen und Organisationen"
-	 title="Das Online-Verwaltungssystem für Vereine, Gruppen und Organisationen" /></a>
-	<span style="font-size: 9pt;">&nbsp;&nbsp;&copy; 2004 - <? echo date('Y', time()); ?>&nbsp;&nbsp;Admidio Team</span>
+    <a href="http://www.admidio.org"><img 
+    src="<?php echo THEME_PATH ?>/images/admidio_logo_20.png" style="border: 0px; vertical-align: bottom;"
+     alt="Das Online-Verwaltungssystem für Vereine, Gruppen und Organisationen"
+     title="Das Online-Verwaltungssystem für Vereine, Gruppen und Organisationen" /></a>
+    <span style="font-size: 9pt;">&nbsp;&nbsp;&copy; 2004 - <? echo date('Y', time()); ?>&nbsp;&nbsp;Admidio Team</span>
 </p>
