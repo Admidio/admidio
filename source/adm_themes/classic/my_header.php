31f5bf258f61aa0edc829bf9b275955bac6b1f69
Markus Faßbender <markus.fassbender@gmail.com>
2008-04-03 07:20:13 +0000

    - Favicon ist jetzt ein PNG mit transparenz - Version von Sidebar-Online hochgesetzt

--- source/adm_themes/classic/my_header.php
+++ source/adm_themes/classic/my_header.php
@@ -3,4 +3,4 @@
      einer Admidio-Modul-Seite erscheinen soll.
 -->
 
-<link rel="shortcut icon" type="image/ico" href="<?php echo THEME_PATH; ?>/icons/favicon.ico" />
+<link rel="shortcut icon" type="image/x-icon" href="<?php echo THEME_PATH; ?>/icons/favicon.png" />
