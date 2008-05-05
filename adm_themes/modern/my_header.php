f04c820b74d8505137cf1e9c6f159c2147831b48
Jochen Erkens <jochen.erkens@gmail.com>
2008-05-05 12:11:19 +0000

    webclipicon hinzugefuegt

--- adm_themes/modern/my_header.php
+++ adm_themes/modern/my_header.php
@@ -4,5 +4,6 @@
 -->
 
 <link rel="shortcut icon" type="image/x-icon" href="<?php echo THEME_PATH; ?>/icons/favicon.png" />
+<link rel="apple-touch-icon" href="<?php echo THEME_PATH; ?>/icons/webclipicon.png" />
 <link rel="stylesheet" type="text/css" href="<?php echo THEME_PATH; ?>/css/modern.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo $g_root_path; ?>/adm_plugins/login_form/login_form.css" />
