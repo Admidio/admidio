6de51bd5234a67d2a3f689d83d723d015b8167d8
Jochen Erkens <jochen.erkens@gmail.com>
2008-05-05 12:11:19 +0000

    webclipicon hinzugefuegt

--- source/adm_themes/classic/my_header.php
+++ source/adm_themes/classic/my_header.php
@@ -4,3 +4,4 @@
 -->
 
 <link rel="shortcut icon" type="image/x-icon" href="<?php echo THEME_PATH; ?>/icons/favicon.png" />
+<link rel="apple-touch-icon" href="<?php echo THEME_PATH; ?>/icons/webclipicon.png" />
