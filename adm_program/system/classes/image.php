1c6ce38ad803a6cae9132b5334ae39373aac217a
Markus Faßbender <markus.fassbender@gmail.com>
2008-07-20 08:21:38 +0000

    - Image-Klassenstruktur aufgebaut. Damit ist es jetzt sehr einfach möglich weitere Bildtypen zu unterstützen. JPG und PNG implementiert. Erst einmal nur beim Profilbild eingebaut.

---
+++ adm_program/system/classes/image.php
@@ -0,0 +1,46 @@
+<?php
+/******************************************************************************
+ * Diese Factory-Klasse erzeugt automatisch das entsprechende Bildobjekt aufgrund
+ * eines uebergebenen Bildpfades
+ *
+ * Copyright    : (c) 2004 - 2008 The Admidio Team
+ * Homepage     : http://www.admidio.org
+ * Module-Owner : Markus Fassbender
+ * License      : GNU Public License 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * 
+ * Das Bildobjekt erhaelt man ueber folgenden Aufruf im Code
+ *
+ * $new_image = Image::getImageObject("/pfad_zum_bild/bild.jpg");
+ *
+ *****************************************************************************/
+
+class Image
+{
+    // Methode prueft, welcher Bildtyp (JPG,PNG,GIF) vorhanden ist und gibt das
+    // entsprechende Objekt zurueck
+    function getImageObject($pathAndFilename)
+    {
+        if(file_exists($pathAndFilename))
+        {
+            $imageProperties = getimagesize($pathAndFilename);
+            //print_r(getimagesize($pathAndFilename)); exit();
+            
+            if($imageProperties['mime'] == "image/jpeg")
+            {
+                require_once("image_jpg.php");
+                return new ImageJPG($pathAndFilename);
+            }
+            elseif($imageProperties['mime'] == "image/png")
+            {
+                require_once("image_png.php");
+                return new ImagePNG($pathAndFilename);
+            }
+            else
+            {
+                return NULL;
+            }
+        }
+        return NULL;
+    }
+}
+?>
---
+++ adm_program/system/classes/image_png.php
@@ -0,0 +1,49 @@
+<?php
+/******************************************************************************
+ * Diese Klasse dient dazu JPG-Bilder zu verarbeiten
+ *
+ * Copyright    : (c) 2004 - 2008 The Admidio Team
+ * Homepage     : http://www.admidio.org
+ * Module-Owner : Markus Fassbender
+ * License      : GNU Public License 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * 
+ * Neben den Methoden der Elternklasse ImageMain, stehen noch zusaetzlich
+ * folgende Methoden zur Verfuegung:
+ *
+ * create()         - Es wird eine Bildresource erzeugt und zurueckgegeben
+ * copyToFile($image, $pathAndFilename = "", $quality = 95)
+ *                  - Methode kopiert die uebergebene Bildresource in die uebergebene Datei bzw. der 
+ *                    hinterlegten Datei des Objekts
+ *
+ *****************************************************************************/
+
+require_once(SERVER_PATH. "/adm_program/system/classes/image_main.php");
+
+class ImagePNG extends ImageMain
+{
+    // Konstruktor
+    function ImagePNG($pathAndFilename = "")
+    {
+        $this->setImage($pathAndFilename);
+        $this->imageType = "PNG";
+    }
+    
+    // Es wird eine Bildresource erzeugt und zurueckgegeben
+    function create()
+    {
+        return imagecreatefrompng($this->imagePath);
+    }
+    
+    // Methode kopiert die uebergebene Bildresource in die uebergebene Datei bzw. der 
+    // hinterlegten Datei des Objekts
+    // Rueckgabe: true, falls erfolgreich
+    function copyToFile($image, $pathAndFilename = "", $quality = 95)
+    {
+        if(strlen($pathAndFilename) == 0)
+        {
+            $pathAndFilename = $this->imagePath;
+        }
+        return imagepng($image, $pathAndFilename, $quality);
+    }
+}
+?>
