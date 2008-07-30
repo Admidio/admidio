cc1d6300de2fad1a68fcfd7aeaaaab43cf49085a
Markus Faßbender <markus.fassbender@gmail.com>
2008-07-30 18:56:24 +0000

    - Feldlänge der Überschrift auf 100 erhöht - include-Fehler beim Anlegen neuer Organisationen

--- adm_install/db_scripts/upd_2_1_0_db.sql
+++ adm_install/db_scripts/upd_2_1_0_db.sql
@@ -1,3 +1,7 @@
 
 -- Texttabelle anpassen
 ALTER TABLE %PRAEFIX%_texts MODIFY COLUMN `txt_id` int(11) unsigned not null AUTO_INCREMENT;
+
+-- Ueberschriftgroessen anpasssen
+ALTER TABLE %PRAEFIX%_announcements MODIFY COLUMN `ann_headline` VARCHAR(100) NOT NULL;
+ALTER TABLE %PRAEFIX%_dates MODIFY COLUMN `dat_headline` VARCHAR(100) NOT NULL;
