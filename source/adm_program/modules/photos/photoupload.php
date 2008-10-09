230184cf1dce9619b4ff2d481002bfe1f4e52e31
Markus Faßbender <markus.fassbender@gmail.com>
2008-10-09 20:14:47 +0000

    - Falsche Weiterleitung nach Fotoupload BUG: https://sourceforge.net/tracker/index.php?func=detail&aid=2155698&group_id=134961&atid=731558
    - Upload von PNG-Bildern ermöglichen
    FRE: https://sourceforge.net/tracker/index.php?func=detail&aid=1289716&group_id=134961&atid=731561

--- source/adm_program/modules/photos/photoupload.php
+++ source/adm_program/modules/photos/photoupload.php
@@ -9,15 +9,15 @@
  *
  * Uebergaben:
  *
- * pho_id: id des Albums zu dem die Bilder hinzugefuegt werden sollen
- * mode: 1 - Klassisches Formular zur Bilderazswahl
- * 		 2 - Flexuploder 
+ * pho_id : id des Albums zu dem die Bilder hinzugefuegt werden sollen
+ * mode   : 1 - Klassisches Formular zur Bilderauswahl (Default)
+ * 		    2 - Flexuploder 
  * 
  *****************************************************************************/
 
-require("../../system/classes/photo_album.php");
-require("../../system/common.php");
-require("../../system/login_valid.php");
+require_once("../../system/classes/photo_album.php");
+require_once("../../system/common.php");
+require_once("../../system/login_valid.php");
 require_once("../../libs/flexupload/class.flexupload.inc.php");
 
 // pruefen ob das Modul ueberhaupt aktiviert ist
@@ -45,9 +45,10 @@ if(isset($_GET["pho_id"]) && is_numeric($_GET["pho_id"]) == false)
     $g_message->show("invalid");
 }
 
-if(is_numeric($_GET["mode"]) == false || $_GET["mode"] < 1 || $_GET["mode"] > 2)
+// im Zweifel den klassischen Upload nehmen
+if(!isset($_GET["mode"]) || $_GET["mode"] < 1 || $_GET["mode"] > 2)
 {
-    $g_message->show("invalid");
+    $_GET["mode"] = 1;
 }
 
 //Kontrolle ob Server Dateiuploads zulaesst
@@ -91,58 +92,59 @@ require(THEME_SERVER_PATH. "/overall_header.php");
 /**************************Klassisches Formular********************************************************/
 if($_GET["mode"] == 1 && ($g_preferences['photo_upload_mode'] == 0 || $g_preferences['photo_upload_mode'] == 2))
 {
-	echo"
-	<form method=\"post\" action=\"$g_root_path/adm_program/modules/photos/photoupload_do.php?pho_id=". $_GET['pho_id']. "\" enctype=\"multipart/form-data\">
-	<div class=\"formLayout\" id=\"photo_upload_form\">
-	    <div class=\"formHead\">Bilder hochladen</div>
-	    <div class=\"formBody\">
+	echo '
+	<form method="post" action="'.$g_root_path.'/adm_program/modules/photos/photoupload_do.php?pho_id='. $_GET['pho_id']. '" enctype="multipart/form-data">
+	<div class="formLayout" id="photo_upload_form">
+	    <div class="formHead">Bilder hochladen</div>
+	    <div class="formBody">
 	        <p>
-	            Die Bilder werden zu dem Album <strong>".$photo_album->getValue("pho_name")."</strong> hinzugefügt.<br />"
-	            ."(Beginn: ". mysqldate("d.m.y", $photo_album->getValue("pho_begin")). ")
+	            Die Bilder werden zu dem Album <strong>'.$photo_album->getValue("pho_name").'</strong> hinzugefügt.<br />
+	            (Beginn: '. mysqldate("d.m.y", $photo_album->getValue("pho_begin")). ')
 	        </p>
 	
-	        <ul class=\"formFieldList\">
+	        <ul class="formFieldList">
 	            <li><dl>
-	                <dt><label for=\"bilddatei1\">Bild 1:</label></dt>
-	                <dd><input type=\"file\" id=\"bilddatei1\" name=\"bilddatei[]\" value=\"durchsuchen\" /></dd>
+	                <dt><label for="bilddatei1">Bild 1:</label></dt>
+	                <dd><input type="file" id="bilddatei1" name="bilddatei[]" value="durchsuchen" /></dd>
 	            </dl></li>
 	            <li><dl>
-	                <dt><label for=\"bilddatei1\">Bild 2:</label></dt>
-	                <dd><input type=\"file\" id=\"bilddatei2\" name=\"bilddatei[]\" value=\"durchsuchen\" /></dd>
+	                <dt><label for="bilddatei1">Bild 2:</label></dt>
+	                <dd><input type="file" id="bilddatei2" name="bilddatei[]" value="durchsuchen" /></dd>
 	            </dl></li>
 	            <li><dl>
-	                <dt><label for=\"bilddatei1\">Bild 3:</label></dt>
-	                <dd><input type=\"file\" id=\"bilddatei3\" name=\"bilddatei[]\" value=\"durchsuchen\" /></dd>
+	                <dt><label for="bilddatei1">Bild 3:</label></dt>
+	                <dd><input type="file" id="bilddatei3" name="bilddatei[]" value="durchsuchen" /></dd>
 	            </dl></li>
 	            <li><dl>
-	                <dt><label for=\"bilddatei1\">Bild 4:</label></dt>
-	                <dd><input type=\"file\" id=\"bilddatei4\" name=\"bilddatei[]\" value=\"durchsuchen\" /></dd>
+	                <dt><label for="bilddatei1">Bild 4:</label></dt>
+	                <dd><input type="file" id="bilddatei4" name="bilddatei[]" value="durchsuchen" /></dd>
 	            </dl></li>
 	            <li><dl>
-	                <dt><label for=\"bilddatei1\">Bild 5:</label></dt>
-	                <dd><input type=\"file\" id=\"bilddatei5\" name=\"bilddatei[]\" value=\"durchsuchen\" /></dd>
+	                <dt><label for="bilddatei1">Bild 5:</label></dt>
+	                <dd><input type="file" id="bilddatei5" name="bilddatei[]" value="durchsuchen" /></dd>
 	            </dl></li>
 	        </ul>
 	        <hr />
-	        <div class=\"formSubmit\">
-	            <button name=\"upload\" type=\"submit\" value=\"speichern\"><img src=\"". THEME_PATH. "/icons/photo_upload.png\" alt=\"Speichern\" />&nbsp;Bilder hochladen</button>
+	        <div class="formSubmit">
+	            <button name="upload" type="submit" value="speichern"><img src="'. THEME_PATH. '/icons/photo_upload.png" alt="Speichern" />&nbsp;Bilder hochladen</button>
 	        </div>
 	   </div>
-	</form>";
+	</form>';
 }
 /**************************Flexuploader********************************************************/
 elseif($_GET["mode"] == 2 && ($g_preferences['photo_upload_mode'] == 0 || $g_preferences['photo_upload_mode'] == 1))
 {
-	echo"<h2>Bilder hochladen</h2>
+	echo '<h2>Bilder hochladen</h2>
 		<p>
-	        Die Bilder werden zu dem Album <strong>".$photo_album->getValue("pho_name")."</strong> hinzugefügt. (Beginn: ". mysqldate("d.m.y", $photo_album->getValue("pho_begin")). ")
-	    </p>";
+	        Die Bilder werden zu dem Album <strong>'.$photo_album->getValue("pho_name").'</strong> hinzugefügt.<br />
+            (Beginn: '. mysqldate("d.m.y", $photo_album->getValue("pho_begin")). ')
+	    </p>';
     //neues Objekt erzeugen mit Ziel was mit den Dateien passieren soll
-	$fup = new FlexUpload("$g_root_path/adm_program/modules/photos/photoflexupload_do.php?pho_id=".$_GET['pho_id']."");
+	$fup = new FlexUpload($g_root_path."/adm_program/modules/photos/photoflexupload_do.php?pho_id=".$_GET['pho_id']);
 	//Pfad zum swf-File
-	$fup->setPathToSWF("$g_root_path/adm_program/libs/flexupload/");
+	$fup->setPathToSWF($g_root_path."/adm_program/libs/flexupload/");
 	//Pfad der Sprachdatei
-	$fup->setLocale("$g_root_path/adm_program/libs/flexupload/de.xml");
+	$fup->setLocale($g_root_path."/adm_program/libs/flexupload/de.xml");
 	//maximale Dateigröße
 	$fup->setMaxFileSize(5*1024*1024);
 	//maximale Dateianzahl
@@ -161,31 +163,31 @@ else
     $g_message->show("invalid");
 }
 
-echo "
+echo '
 </div>
-<ul class=\"iconTextLinkList\">
+<ul class="iconTextLinkList">
     <li>
-        <span class=\"iconTextLink\">
-            <a href=\"$g_root_path/adm_program/system/back.php\"><img 
-            src=\"". THEME_PATH. "/icons/back.png\" alt=\"Zurück\" /></a>
-            <a href=\"$g_root_path/adm_program/system/back.php\">Zurück</a>
+        <span class="iconTextLink">
+            <a href="'.$g_root_path.'/adm_program/system/back.php"><img 
+            src="'. THEME_PATH. '/icons/back.png" alt="Zurück" /></a>
+            <a href="'.$g_root_path.'/adm_program/system/back.php">Zurück</a>
         </span>
     </li>    
     <li>
-        <span class=\"iconTextLink\">
-            <img class=\"iconHelpLink\" src=\"". THEME_PATH. "/icons/help.png\" alt=\"Hilfe\" title=\"\"
-                onclick=\"window.open('$g_root_path/adm_program/system/msg_window.php?err_code=photo_up_help&amp;window=true','Message','width=600,height=600,left=310,top=200,scrollbars=yes')\"  
-                onmouseover=\"ajax_showTooltip(event,'$g_root_path/adm_program/system/msg_window.php?err_code=photo_up_help',this);\" onmouseout=\"ajax_hideTooltip()\" />   
-            <a href=\"#\" onclick=\"window.open('$g_root_path/adm_program/system/msg_window.php?err_code=photo_up_help&amp;window=true','Message','width=600,height=600,left=310,top=200,scrollbars=yes')\">Hilfe</a>
+        <span class="iconTextLink">
+            <img class="iconHelpLink" src="'. THEME_PATH. '/icons/help.png" alt="Hilfe" title=""
+                onclick="window.open(\''.$g_root_path.'/adm_program/system/msg_window.php?err_code=photo_up_help&amp;window=trueß\',\'Message\',\'width=600,height=600,left=310,top=200,scrollbars=yes\')"  
+                onmouseover="ajax_showTooltip(event,\''.$g_root_path.'/adm_program/system/msg_window.php?err_code=photo_up_help\',this);" onmouseout="ajax_hideTooltip()" />   
+            <a href="#" onclick="window.open(\''.$g_root_path.'/adm_program/system/msg_window.php?err_code=photo_up_help&amp;window=true\',\'Message\',\'width=600,height=600,left=310,top=200,scrollbars=yes\')">Hilfe</a>
         </span>
     </li>
-</ul>";
+</ul>';
 
 //Seitenende
-echo"
-<script type=\"text/javascript\"><!--
-        document.getElementById('bilddatei1').focus();
---></script>";
+echo '
+<script type="text/javascript"><!--
+        document.getElementById(\'bilddatei1\').focus();
+--></script>';
 
 require(THEME_SERVER_PATH. "/overall_footer.php");
 
--- source/adm_program/modules/photos/photoupload_do.php
+++ source/adm_program/modules/photos/photoupload_do.php
@@ -74,26 +74,26 @@ if($_POST["upload"])
 {
     //zaehlen wieviele Bilder hochgeladen werden sollen und ob alle Uploads Fehlerfrei sind
     $counter=0;
-    for($x=0; $x<=4; $x++)
+    for($act_upload_nr = 0; $act_upload_nr < 5; $act_upload_nr++)
     {
         //Datei wurde hochgeladen
-        if(isset($_FILES["bilddatei"]["name"]["$x"]))
+        if(isset($_FILES["bilddatei"]["name"][$act_upload_nr]))
         {
             //Es liegt kein Fehler vor, die Datei wurde erfolgreich hochgeladen.
-            if($_FILES["bilddatei"]["error"]["$x"]==0)
+            if($_FILES["bilddatei"]["error"][$act_upload_nr]==0)
             {
                 $counter++;
 
-                //Dateiendungskontrolle
-                $bildinfo=getimagesize($_FILES["bilddatei"]["tmp_name"][$x]);
-                if ($_FILES["bilddatei"]["name"][$x]!=NULL && $bildinfo['mime']!="image/jpeg")
+                // Nur JPG und PNG koennen hochgeladen werden
+                $bildinfo = getimagesize($_FILES["bilddatei"]["tmp_name"][$act_upload_nr]);
+                if ($bildinfo['mime'] != "image/jpeg" && $bildinfo['mime'] != "image/png")
                 {
                     $g_message->show("dateiendungphotoup");
                 }
             }
 
             //Die hochgeladene Datei ueberschreitet die in der Anweisung upload_max_filesize in php.ini festgelegte Groesse.
-            if($_FILES["bilddatei"]["error"]["$x"]==1)
+            if($_FILES["bilddatei"]["error"][$act_upload_nr]==1)
             {
                 $g_message->show("photo_2big", ini_get("upload_max_filesize"));
             }
@@ -123,27 +123,26 @@ if($_POST["upload"])
 {
     //bei selbstaufruf der Datei Hinweise zu hochgeladenen Dateien und Kopieren der Datei in Ordner
     //Anlegen des Berichts
-    echo"<h1 class=\"moduleHeadline\">Fotogalerien - Upload</h1>
-    <div class=\"photoModuleContainer\">
+    echo '<h1 class="moduleHeadline">Fotogalerien - Upload</h1>
+    <div class="photoModuleContainer">
         Bitte einen Moment Geduld ...<br /><br />
-        Die Bilder wurden dem Album <strong>".$photo_album->getValue("pho_name")."</strong>
-        erfolgreich hinzugefügt, wenn sie hier angezeigt werden.<br />";
+        Die Bilder wurden dem Album <strong>'.$photo_album->getValue("pho_name").'</strong>
+        erfolgreich hinzugefügt, wenn sie hier angezeigt werden.<br />';
 
         //Verarbeitungsschleife fuer die einzelnen Bilder
-        $bildnr=$photo_album->getValue("pho_quantity");
-        for($x=0; $x<=4; $x=$x+1)
+        $bildnr = $photo_album->getValue("pho_quantity");
+        for($act_upload_nr = 0; $act_upload_nr < 5; $act_upload_nr++)
         {
-            $y=$x+1;
-            if($_FILES["bilddatei"]["name"][$x]!=NULL && $ordner!=NULL)
+            if($_FILES["bilddatei"]["name"][$act_upload_nr]!=NULL && $ordner!=NULL)
             {
                 //errechnen der neuen Bilderzahl
                 $bildnr++;
-                echo "<br />Bild $bildnr:<br />";
+                echo '<br />Albumbild '.$bildnr.':<br />';
 
                 //Bild in Tempordner verschieben, groeße aendern und speichern
-                if(move_uploaded_file($_FILES["bilddatei"]["tmp_name"][$x], SERVER_PATH. "/adm_my_files/photos/temp".$y.".jpg"))
+                if(move_uploaded_file($_FILES["bilddatei"]["tmp_name"][$act_upload_nr], SERVER_PATH. "/adm_my_files/photos/temp".$act_upload_nr.".jpg"))
                 {
-                    $temp_bild=SERVER_PATH. "/adm_my_files/photos/temp".$y.".jpg";
+                    $temp_bild = SERVER_PATH. "/adm_my_files/photos/temp".$act_upload_nr.".jpg";
 
                     //Bild skalliert speichern
                     $image = new Image($temp_bild);
@@ -165,20 +164,18 @@ if($_POST["upload"])
                     $image->delete();
 
                     //Loeschen des Bildes aus Arbeitsspeicher
-                    if(file_exists(SERVER_PATH. "/adm_my_files/photos/temp".$y.".jpg"))
+                    if(file_exists(SERVER_PATH. "/adm_my_files/photos/temp".$act_upload_nr.".jpg"))
                     {
-                        unlink(SERVER_PATH. "/adm_my_files/photos/temp".$y.".jpg");
+                        unlink(SERVER_PATH. "/adm_my_files/photos/temp".$act_upload_nr.".jpg");
                     }           
-                    
-                
                 }//Ende Bild speichern
 
 
                 //Kontrolle
                 if(file_exists($ordner."/".$bildnr.".jpg"))
                 {
-                    echo"<img src=\"photo_show.php?scal=300&amp;pic_nr=".$bildnr."&amp;pho_id=".$photo_album->getValue("pho_id")."&amp;pho_begin=".$photo_album->getValue("pho_begin")."\"
-                            class=\"photoOutput\" /><br /><br />";
+                    echo '<img src="photo_show.php?scal=300&amp;pic_nr='.$bildnr.'&amp;pho_id='.$photo_album->getValue("pho_id").'&amp;pho_begin='.$photo_album->getValue("pho_begin").'"
+                            class="photoOutput" /><br /><br />';
 
                     //Aendern der Datenbankeintaege
                     $photo_album->setValue("pho_quantity", $photo_album->getValue("pho_quantity")+1);
@@ -187,32 +184,31 @@ if($_POST["upload"])
                 else
                 {
                     $bildnr--;
-                    echo"Das Bild konnte nicht verarbeitet werden.";
+                    echo "Das Bild konnte nicht verarbeitet werden.";
                 }
-                unset($y);
             }//if($bilddatei!= "")
         }//for
 
         //Buttons
-        echo"
+        echo '
         <hr />
-        <ul class=\"iconTextLinkList\">
+        <ul class="iconTextLinkList">
             <li>
-                <span class=\"iconTextLink\">
-                    <a href=\"$g_root_path/adm_program/modules/photos/photos.php?pho_id=$pho_id\"><img 
-                    src=\"". THEME_PATH. "/icons/application_view_tile.png\" alt=\"Übersicht\" /></a>
-                    <a href=\"$g_root_path/adm_program/modules/photos/photos.php?pho_id=$pho_id\">Übersicht</a>
+                <span class="iconTextLink">
+                    <a href="'.$g_root_path.'/adm_program/modules/photos/photos.php?pho_id='.$photo_album->getValue("pho_id").'"><img 
+                    src="'. THEME_PATH. '/icons/application_view_tile.png" alt="Übersicht" /></a>
+                    <a href="'.$g_root_path.'/adm_program/modules/photos/photos.php?pho_id='.$photo_album->getValue("pho_id").'">Übersicht</a>
                 </span>
             </li>
             <li>
-                <span class=\"iconTextLink\">
-                    <a href=\"$g_root_path/adm_program/modules/photos/photoupload.php?pho_id=$pho_id\"><img 
-                    src=\"". THEME_PATH. "/icons/photo_upload.png\" alt=\"Weitere Bilder hochladen\" /></a>
-                    <a href=\"$g_root_path/adm_program/modules/photos/photoupload.php?pho_id=$pho_id\">Weitere Bilder hochladen</a>
+                <span class="iconTextLink">
+                    <a href="'.$g_root_path.'/adm_program/modules/photos/photoupload.php?pho_id='.$photo_album->getValue("pho_id").'&amp;mode=1"><img 
+                    src="'. THEME_PATH. '/icons/photo_upload.png" alt="Weitere Bilder hochladen" /></a>
+                    <a href="'.$g_root_path.'/adm_program/modules/photos/photoupload.php?pho_id='.$photo_album->getValue("pho_id").'&amp;mode=1">Weitere Bilder hochladen</a>
                 </span>
             </li>
          </ul>
-    </div><br /><br />";
+    </div><br /><br />';
 }//if($upload)
 
 //Seitenende
