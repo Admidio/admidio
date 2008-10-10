e6ec8bfdacd7dc4ec1cf3406eeb9c3acd239c572
Markus Faßbender <markus.fassbender@gmail.com>
2008-10-10 08:24:46 +0000

    Tippfehler korrigiert

--- adm_program/modules/photos/photoupload.php
+++ adm_program/modules/photos/photoupload.php
@@ -176,7 +176,7 @@ echo '
     <li>
         <span class="iconTextLink">
             <img class="iconHelpLink" src="'. THEME_PATH. '/icons/help.png" alt="Hilfe" title=""
-                onclick="window.open(\''.$g_root_path.'/adm_program/system/msg_window.php?err_code=photo_up_help&amp;window=trueß\',\'Message\',\'width=600,height=600,left=310,top=200,scrollbars=yes\')"  
+                onclick="window.open(\''.$g_root_path.'/adm_program/system/msg_window.php?err_code=photo_up_help&amp;window=true\',\'Message\',\'width=600,height=600,left=310,top=200,scrollbars=yes\')"  
                 onmouseover="ajax_showTooltip(event,\''.$g_root_path.'/adm_program/system/msg_window.php?err_code=photo_up_help\',this);" onmouseout="ajax_hideTooltip()" />   
             <a href="#" onclick="window.open(\''.$g_root_path.'/adm_program/system/msg_window.php?err_code=photo_up_help&amp;window=true\',\'Message\',\'width=600,height=600,left=310,top=200,scrollbars=yes\')">Hilfe</a>
         </span>
