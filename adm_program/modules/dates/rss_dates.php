1c6f0da04139c9be7babbcc4a6319794f710c8d0
Markus Faßbender <markus.fassbender@gmail.com>
2008-10-16 13:03:55 +0000

    Falsches Bis-Datum bei ganztägige Terminen im RSS-Feed BUG: https://sourceforge.net/tracker2/?func=detail&aid=2171588&group_id=134961&atid=731558

--- adm_program/modules/dates/rss_dates.php
+++ adm_program/modules/dates/rss_dates.php
@@ -17,6 +17,7 @@
 require("../../system/common.php");
 require("../../system/classes/ubb_parser.php");
 require("../../system/classes/rss.php");
+require("../../system/classes/date.php");
 
 // Nachschauen ob RSS ueberhaupt aktiviert ist bzw. das Modul oeffentlich zugaenglich ist
 if ($g_preferences['enable_rss'] != 1)
@@ -61,74 +62,76 @@ $result = $g_db->query($sql);
 // ab hier wird der RSS-Feed zusammengestellt
 
 // Ein RSSfeed-Objekt erstellen
-$rss = new RSSfeed("http://". $g_current_organization->getValue("org_homepage"), $g_current_organization->getValue("org_longname"). " - Termine", "Die 10 naechsten Termine");
+$rss  = new RSSfeed("http://". $g_current_organization->getValue("org_homepage"), $g_current_organization->getValue("org_longname"). " - Termine", "Die 10 naechsten Termine");
+$date = new Date($g_db);
 
 // Dem RSSfeed-Objekt jetzt die RSSitems zusammenstellen und hinzufuegen
 while ($row = $g_db->fetch_object($result))
 {
+    // ausgelesene Termindaten in Date-Objekt schieben
+    $date->clear();
+    $date->setArray($row);
+
     // Die Attribute fuer das Item zusammenstellen
-    $title = mysqldatetime("d.m.y", $row->dat_begin). " ". $row->dat_headline;
-    $link  = "$g_root_path/adm_program/modules/dates/dates.php?id=". $row->dat_id;
-    $description = "<b>$row->dat_headline</b> <br />". mysqldatetime("d.m.y", $row->dat_begin);
-
-    if (mysqldatetime("h:i", $row->dat_begin) != "00:00")
+    $title = mysqldatetime("d.m.y", $date->getValue("dat_begin"));
+    if(mysqldatetime("d.m.y", $date->getValue("dat_begin")) != mysqldatetime("d.m.y", $date->getValue("dat_end")))
     {
-        $description = $description. " um ". mysqldatetime("h:i", $row->dat_begin). " Uhr";
+        $title = $title. ' - '. mysqldatetime("d.m.y", $date->getValue("dat_end"));
     }
+    $title = $title. " ". $date->getValue("dat_headline");
+    $link  = $g_root_path."/adm_program/modules/dates/dates.php?id=". $date->getValue("dat_id");
+    $description = "<b>".$date->getValue("dat_headline")."</b> <br />". mysqldatetime("d.m.y", $date->getValue("dat_begin"));
 
-    if ($row->dat_begin != $row->dat_end)
+    if ($date->getValue("dat_all_day") == 0)
     {
-        $description = $description. "<br /> bis <br />";
-
-        if (mysqldatetime("d.m.y", $row->dat_begin) != mysqldatetime("d.m.y", $row->dat_end))
+        $description = $description. " von ". mysqldatetime("h:i", $date->getValue("dat_begin")). " Uhr bis ";
+        if(mysqldatetime("d.m.y", $date->getValue("dat_begin")) != mysqldatetime("d.m.y", $date->getValue("dat_end")))
         {
-            $description = $description. mysqldatetime("d.m.y", $row->dat_end);
-
-            if (mysqldatetime("h:i", $row->dat_end) != "00:00")
-            {
-                $description = $description. " um ";
-            }
+            $description = $description. mysqldatetime("d.m.y", $date->getValue("dat_end")). " ";
         }
-
-        if (mysqldatetime("h:i", $row->dat_end) != "00:00")
+        $description = $description. mysqldatetime("h:i", $date->getValue("dat_end")). " Uhr";
+    }
+    else
+    {
+        if(mysqldatetime("d.m.y", $date->getValue("dat_begin")) != mysqldatetime("d.m.y", $date->getValue("dat_end")))
         {
-            $description = $description. mysqldatetime("h:i", $row->dat_end). " Uhr";
+            $description = $description. " bis ". mysqldatetime("d.m.y", $date->getValue("dat_end"));
         }
     }
 
-    if ($row->dat_location != "")
+    if ($date->getValue("dat_location") != "")
     {
-        $description = $description. "<br /><br />Treffpunkt:&nbsp;". $row->dat_location;
+        $description = $description. "<br /><br />Treffpunkt:&nbsp;". $date->getValue("dat_location");
     }
 
     //eventuell noch die Beschreibung durch den UBB-Parser schicken...
     if ($g_preferences['enable_bbcode'] == 1)
     {
-        $description = $description. "<br /><br />". $bbcode->parse($row->dat_description);
+        $description = $description. "<br /><br />". $bbcode->parse($date->getValue("dat_description"));
     }
     else
     {
-        $description = $description. "<br /><br />". nl2br($row->dat_description);
+        $description = $description. "<br /><br />". nl2br($date->getValue("dat_description"));
     }
 
-    $description = $description. "<br /><br /><a href=\"$link\">Link auf ". $g_current_organization->getValue("org_homepage"). "</a>";
+    $description = $description. '<br /><br /><a href="'.$link.'">Link auf '. $g_current_organization->getValue("org_homepage"). '</a>';
     
     //i-cal downloadlink
-    $description = $description. "<br /><br /><a href=\"$g_root_path/adm_program/modules/dates/dates_function.php?dat_id=$row->dat_id&mode=4\">Termin in meinen Kalender &uuml;bernehmen</a>";
+    $description = $description. '<br /><br /><a href="'.$g_root_path.'/adm_program/modules/dates/dates_function.php?dat_id='.$date->getValue("dat_id").'&mode=4">Termin in meinen Kalender übernehmen</a>';
     
     // Den Autor und letzten Bearbeiter der Ankuendigung ermitteln und ausgeben
-    $user = new User($g_db, $row->dat_usr_id_create);
+    $user = new User($g_db, $date->getValue("dat_usr_id_create"));
     $description = $description. "<br /><br /><i>Angelegt von ". $user->getValue("Vorname"). " ". $user->getValue("Nachname");
-    $description = $description. " am ". mysqldatetime("d.m.y h:i", $row->dat_timestamp_create). "</i>";
+    $description = $description. " am ". mysqldatetime("d.m.y h:i", $date->getValue("dat_timestamp_create")). "</i>";
 
-    if($row->dat_usr_id_change > 0)
+    if($date->getValue("dat_usr_id_change") > 0)
     {
-        $user_change = new User($g_db, $row->dat_usr_id_change);
+        $user_change = new User($g_db, $date->getValue("dat_usr_id_change"));
         $description = $description. "<br /><i>Zuletzt bearbeitet von ". $user_change->getValue("Vorname"). " ". $user_change->getValue("Nachname");
-        $description = $description. " am ". mysqldatetime("d.m.y h:i", $row->dat_timestamp_change). "</i>";
+        $description = $description. " am ". mysqldatetime("d.m.y h:i", $date->getValue("dat_timestamp_change")). "</i>";
     }
     
-    $pubDate = date('r',strtotime($row->dat_timestamp_create));
+    $pubDate = date('r',strtotime($date->getValue("dat_timestamp_create")));
 
 
     //Item hinzufuegen
