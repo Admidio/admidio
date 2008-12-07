ba52ea6f04bc73753a19899a4af8e3ee0168d272
Thomas Thoss <t.thoss@admidio.org>
2008-12-07 20:05:10 +0000

    Rev Änderung 2509, 2508 und 2507 inklusiver dieser wegen Umbenennung von PM in Messages.

--- adm_program/system/classes/messages.php
+++ adm_program/system/classes/messages.php
@@ -10,15 +10,16 @@
  * Diese Klasse dient dazu einen Nachrichtenobjekt zu erstellen.
  * Die Nachrichten können ueber diese Klasse verwaltet werden
  *
- * Das Objekt wird erzeugt durch Aufruf des Konstruktors:
- * $pm = new Pm();
+ * Das Objekt wird erzeugt durch Aufruf des Konstruktors und der Uebergabe der
+ * aktuellen Datenbankverbindung:
+ * $messages = new Messages($g_db);
  *
  *
  * Folgende Funktionen stehen nun zur Verfuegung:
  *
  * Clear()				  - Daten loeschen
  *
- * GetPm($userid)   	  - Die Anzahl der Nachrichten des aktuellen Users werden gelesen
+ * GetMessages($userid)   - Die Anzahl der Nachrichten des aktuellen Users werden gelesen
  *							und die Ausgabevariablien gesetzt.
  * 							$userid = Die User ID des Users 
  *
@@ -26,15 +27,15 @@
  *
  ******************************************************************************/
 
-class Pm
+class Messages
 {
 	// Allgemeine Variablen
-	var $pm;							//Anzahl der Nachrichten
-	var $pm_text;						//Nachrichtentext
-	var $pm_icon;						//Icon für die Nachrichtenanzeige
+	var $msg;							//Anzahl der Nachrichten
+	var $msg_text;						//Nachrichtentext
+	var $msg_icon;						//Icon für die Nachrichtenanzeige
 
 	// Konstruktor
-	function Pm(&$db)
+	function messages(&$db)
 	{
 		$this->db =& $db;
 	}
@@ -43,45 +44,45 @@ class Pm
 	function Clear()
 	{
 		// Session-Valid und Userdaten loeschen
-		$this->pm		= 0;
-		$this->pm_text	= "keine neue Nachricht";
-		$this->pm_icon	= "pm_new.gif";
+		$this->msg		= 0;
+		$this->msg_text	= "keine neue Nachricht";
+		$this->msg_icon	= "pm_new.gif";
 	}
 
 	// Funktion prueft auf neue PM
-	function GetPm($userid)
+	function GetMessages($userid)
 	{
-		// PMs suchen
+		// Nachrichten suchen
 		//$sql = "SELECT count(*)
-		//          FROM adm_pm 
-		//         WHERE pm_user_id_to = ".$userid." 
-		//           AND pm_read_date IS NULL";
+		//          FROM adm_messages 
+		//         WHERE msg_user_id_to = ".$userid." 
+		//           AND msg_read_date IS NULL";
 		//$result = $this->db->query($sql);
 		//$row = $this->db->fetch_array($result);
-		//$this->pm = $row[0];
+		//$this->msg = $row[0];
 		
-		$this->pm = 0;
+		$this->msg = 0;
 		
-		if ($this->pm == 0)
+		if ($this->msg == 0)
 		{
-		    $this->pm_text = "keine neue Nachricht";
-		    $this->pm_icon = "pm_new.gif";
+		    $this->msg_text = "keine neue Nachricht";
+		    $this->msg_icon = "message_new.gif";
 		}
-		elseif ($this->pm == 1)
+		elseif ($this->msg == 1)
 		{
-		    $this->pm_text = "<b>1</b> neue Nachricht";
-		    $this->pm_icon = "pm_new_ani.gif";
+		    $this->msg_text = "<b>1</b> neue Nachricht";
+		    $this->msg_icon = "message_new_ani.gif";
 		}
 		else
 		{
-		    $this->pm_text = $this->pm." neue Nachrichten";
-		 	$this->pm_icon = "pm_new_ani.gif";
+		    $this->msg_text = $this->msg." neue Nachrichten";
+		 	$this->msg_icon = "message_new_ani.gif";
 		}
 	}
 
 	// Eine Funktion, die nichts, rein garnichts macht. 
 	// Wenn der Returncode gleich TRUE, wurde auch in der Tat das nichts machen bestaetigt
-	function nix()
+	function Nix()
 	{
 		return TRUE;
 	}
