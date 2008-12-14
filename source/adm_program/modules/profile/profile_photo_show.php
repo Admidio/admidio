e311d3ee5c7299d3f3bee1cf92d896a9422b8c8e
Jochen Erkens <jochen.erkens@gmail.com>
2008-12-14 12:14:17 +0000

    da war noch ne undefinierte Variable reingerutscht

--- source/adm_program/modules/profile/profile_photo_show.php
+++ source/adm_program/modules/profile/profile_photo_show.php
@@ -16,12 +16,71 @@
  *****************************************************************************/
 require("../../system/common.php");
 require("../../system/login_valid.php");
-require("../../system/classes/image.php");
+//require("../../system/classes/image.php");
 
+// lokale Variablen der Uebergabevariablen initialisieren
+$req_usr_id    = 0;
+$req_tmp_photo = 0;
+
+// Uebergabevariablen pruefen
+
+if(  (isset($_GET["usr_id"]) 
+   && is_numeric($_GET["usr_id"]) == false)
+|| isset($_GET["usr_id"]) == false)
+{
+    $g_message->show("invalid");
+}
+else
+{
+    $req_usr_id = $_GET["usr_id"];
+}
+
+if(isset($_GET["tmp_photo"]) && $_GET["tmp_photo"] == 1)
+{
+    $req_tmp_photo = 1;
+}
+
+//Testen ob Recht besteht Profil einzusehn
+if(!$g_current_user->viewProfile($req_usr_id))
+{
+    $g_message->show("norights");
+}
+// Foto aus der Datenbank lesen und ausgeben
+
+header("Content-Type: image/jpeg");
+if($req_tmp_photo == true)
+{
+    echo $g_current_session->getValue("ses_blob");
+}
+else
+{
+    if($_GET["usr_id"] == $g_current_user->getValue("usr_id"))
+    {
+        $user = $g_current_user;
+    }
+    else
+    {
+        $user = new User($g_db, $_GET["usr_id"]);
+    }
+    
+    if(strlen($user->getValue("usr_photo")) > 0)
+    {
+        echo $user->getValue("usr_photo");
+    }
+    else
+    {
+        // es wurde kein Bild gefunden, dann ein Dummy-Bild zurueckgeben
+        $no_profile_pic = imagecreatefrompng(THEME_SERVER_PATH. "/images/no_profile_pic.png");
+        echo imagepng($no_profile_pic);
+    }
+}
+
+/*
 // lokale Variablen der Uebergabevariablen initialisieren
 $req_usr_id    = 0;
 $req_new_photo = 0;
 $image         = null;
+$picpath       = THEME_SERVER_PATH. "/images/no_profile_pic.png";
 
 // Uebergabevariablen pruefen
 
@@ -36,7 +95,7 @@ else
 
 if(isset($_GET["new_photo"]) && $_GET["new_photo"] == 1)
 {
-    $req_tmp_photo = 1;
+    $req_new_photo = 1;
 }
 
 //Testen ob Recht besteht Profil einzusehn
@@ -45,26 +104,39 @@ if(!$g_current_user->viewProfile($req_usr_id))
     $g_message->show("norights");
 }
 
-//Pfad zusammensetzen
-if($req_tmp_photo == 1)
-{
-	$picpath = SERVER_PATH. "/adm_my_files/user_profile_photos/".$req_usr_id."_new.jpg";
-}
-else
+
+//Bild aus adm_my_files
+if($g_preferences['profile_photo_storage'] == 1 && $req_new_photo == 0)
 {
 	$picpath = SERVER_PATH. "/adm_my_files/user_profile_photos/".$req_usr_id.".jpg";
-}
-
-//Nachsehen ob angefordertes Bild existiert
-if(file_exists($picpath))
-{
 	$image = new Image($picpath);
 }
+//Bild der Datenbank
+elseif($g_preferences['profile_photo_storage'] == 0 && $req_new_photo == 0)
+{
+	$user = new User($g_db, $req_usr_id);
+	if(strlen($user->getValue("usr_photo")) != NULL)
+    {
+        header("Content-Type: image/jpeg");
+        $test = $user->getValue("usr_photo");
+        echo addslashes($test); 
+        exit();
+        $image = new Image();
+        $image->setImageFromData(addslashes($user->getValue("usr_photo")));
+    }
+    else
+    {
+    	$image = new Image($picpath);
+    }
+}
+//neues Bild
 else
 {
-	$image = new Image( THEME_SERVER_PATH. "/images/no_profile_pic.png");
+	$picpath = SERVER_PATH. "/adm_my_files/user_profile_photos/".$req_usr_id."_new.jpg";
+	$image = new Image($picpath);
 }
-header("Content-Type: ". $image->getMimeType());
+
+//header("Content-Type: ". $image->getMimeType());
 $image->copyToBrowser();
-$image->delete();
+$image->delete();*/
 ?>
