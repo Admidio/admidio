8af588d5fc4f13cbdbd42e0310e09638e46074be
Roland Meuthen <roland.meuthen@gmx.de>
2009-01-02 10:27:01 +0000

    Link fuer Backup hinzugefuegt

--- source/adm_themes/modern/my_body_bottom.php
+++ source/adm_themes/modern/my_body_bottom.php
@@ -162,6 +162,13 @@
                     style="vertical-align: middle;" src="'. THEME_PATH. '/icons/options.png" alt="Organisationseinstellungen" title="Organisationseinstellungen" /></a>
                     <a href="'. $g_root_path. '/adm_program/administration/organization/organization.php">Organisationseinstellungen</a></span>';
                 }
+				if($g_current_user->isWebmaster())
+                {
+                    echo '<span class="menu"><a href="'. $g_root_path. '/adm_program/administration/backup/backupDB.php"><img
+                    style="vertical-align: middle;" src="'. THEME_PATH. '/icons/options.png" alt="Datenbank Backup" title="Datenbank Backup" /></a>
+                    <a href="'. $g_root_path. '/adm_program/administration/backup/backupDB.php">Datenbank Backup</a></span>';
+                }
+
             }
             
             ?>
