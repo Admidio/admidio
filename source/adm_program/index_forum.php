520cc7f38aa76fcf8e640a6873d71e5a8d5bd2bf
Roland Eischer <roland_203@yahoo.de>
2009-01-10 13:27:13 +0000

    Doctype hinzugefügt + Javascript angepasst

--- source/adm_program/index_forum.php
+++ source/adm_program/index_forum.php
@@ -50,7 +50,7 @@ if(isset($g_layout['header']) == false)
 {
     $g_layout['header'] = "";
 }
-
+$g_layout['onload'] = 'javascript:resizeIframe(\'sizeframe\');';
 if(isset($g_layout['onload']))
 {
     $g_layout['onload'] = " onload=\"". $g_layout['onload']. "\"";
@@ -71,8 +71,7 @@ if(isset($g_current_organization))
 }
 
 header('Content-type: text/html; charset=utf-8'); 
-echo '
-
+echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="de" xml:lang="de">
 <head>
     <!-- (c) 2004 - 2008 The Admidio Team - http://www.admidio.org -->
@@ -92,8 +91,7 @@ echo '
     ';
     
     echo $g_layout['header']. '
-
-    <!--[if lt IE 7]>
+	 <!--[if lt IE 7]>
     <script type="text/javascript">
         window.attachEvent("onload", correctPNG);
     </script>
@@ -130,21 +128,30 @@ echo "</head>
 
 // Html des Modules ausgeben
 ?>
-
 <script type="text/javascript"> 
 <!--
-notIE=document.getElementById&&!document.all
-heightOffset=20
-function resizeIframe(obj)
-{
-	this.obj=obj
-	this.obj.style.height=""
-	setTimeout("this.obj.style.height=this.obj.contentWindow.document.body.scrollHeight+(notIE?heightOffset:0)",10)
-}
+	
+	function resizeIframe(id)
+	{
+	alert("lol");
+		var notIE = document.getElementById&&!document.all
+		var heightOffset = 20;
+		var IFrameObj = document.getElementById(id);
+		IFrameObj.style.height="";
+		var IFrameDoc;
+		if (notIE) {
+			// For NS6
+			IFrameDoc = IFrameObj.contentDocument; 
+		} else {
+			// For IE5.5 and IE6
+			IFrameDoc = IFrameObj.document;
+		}
+		IFrameObj.style.height=IFrameDoc.body.scrollHeight+(notIE?heightOffset:0)+"px";
+	}
 // -->
 </script>
 <br />
-<iframe id="sizeframe" name="sizeframe" marginheight="0px" marginwidth="0px" width="<?php if ($g_preferences['forum_width']){echo $g_preferences['forum_width'];}else{echo "570";}?>px" height="100px" scrolling="no" frameborder="no" allowtransparency="true" background-color="transparent" src="<?php echo $g_forum->url; ?>" onload="resizeIframe(this)"></iframe>
+<iframe id="sizeframe" name="sizeframe" style="padding:0px; margin:0px; width:<?php if ($g_preferences['forum_width']){echo $g_preferences['forum_width'];}else{echo "570";}?>px ;height:100px;" scrolling="no" frameborder="no" allowtransparency="true" background-color="transparent" marginheight="0" marginwidth="0"  src="<?php echo $g_forum->url; ?>" onload="resizeIframe('sizeframe');"></iframe>
 <br />
 <?
 require(THEME_SERVER_PATH. "/overall_footer.php");
