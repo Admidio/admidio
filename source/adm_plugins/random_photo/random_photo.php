322bee14e87309b42da2177ac58f02b9bf85d2cd
Jochen Erkens <jochen.erkens@gmail.com>
2009-01-19 16:57:07 +0000

    - Strings vereinheitlicht - Es wurde noch versucht die Klasse PhotoAlbum statt tablePhotos aufgerufen

--- source/adm_plugins/random_photo/random_photo.php
+++ source/adm_plugins/random_photo/random_photo.php
@@ -17,17 +17,19 @@
  *****************************************************************************/
 
 // Pfad des Plugins ermitteln
-$plugin_folder_pos = strpos(__FILE__, "adm_plugins") + 11;
-$plugin_file_pos   = strpos(__FILE__, "random_photo.php");
+$plugin_folder_pos = strpos(__FILE__, 'adm_plugins') + 11;
+$plugin_file_pos   = strpos(__FILE__, 'random_photo.php');
 $plugin_folder     = substr(__FILE__, $plugin_folder_pos+1, $plugin_file_pos-$plugin_folder_pos-2);
 
 if(!defined('PLUGIN_PATH'))
 {
     define('PLUGIN_PATH', substr(__FILE__, 0, $plugin_folder_pos));
 }
-require_once(PLUGIN_PATH. "/../adm_program/system/common.php");
-require_once(PLUGIN_PATH. "/../adm_program/system/classes/table_photos.php");
-require_once(PLUGIN_PATH. "/$plugin_folder/config.php");
+require_once(PLUGIN_PATH. '/../adm_program/system/common.php');
+require_once(PLUGIN_PATH. '/../adm_program/system/classes/table_photos.php');
+require_once(PLUGIN_PATH. '/'.$plugin_folder.'/config.php');
+//require_once(PLUGIN_PATH. '/../adm_program/system/classes/image.php');
+//require_once(PLUGIN_PATH. '/../adm_program/system/classes/htaccess.php');
 
 // pruefen, ob alle Einstellungen in config.php gesetzt wurden
 // falls nicht, hier noch mal die Default-Werte setzen
@@ -42,7 +44,7 @@ if(isset($plg_link_class))
 }
 else
 {
-    $plg_link_class = "";
+    $plg_link_class = '';
 }
 
 if(isset($plg_link_target))
@@ -51,7 +53,7 @@ if(isset($plg_link_target))
 }
 else
 {
-    $plg_link_target = "_self";
+    $plg_link_target = '_self';
 }
 
 if(isset($plg_headline))
@@ -60,7 +62,7 @@ if(isset($plg_headline))
 }
 else
 {
-    $plg_headline = "Fotos";
+    $plg_headline = 'Fotos';
 }
 
 if(!isset($plg_photos_max_width) || !is_numeric($plg_photos_max_width))
@@ -92,12 +94,12 @@ echo '<div id="plugin_'. $plugin_folder. '">';
 
 // Fotoalben Aufrufen
 // Bedingungen: freigegeben,Anzahllimit, Bilder enthalten 
-$sql="      SELECT *
-            FROM ". TBL_PHOTOS. "
-            WHERE pho_org_shortname ='$g_organization' 
+$sql='      SELECT *
+            FROM '. TBL_PHOTOS. '
+            WHERE pho_org_shortname ="'.$g_organization.'" 
             AND pho_locked = 0
             AND pho_quantity>0
-            ORDER BY pho_begin DESC";
+            ORDER BY pho_begin DESC';
 
 //Limit setzen falls gefordert
 if($plg_photos_albums != 0)
@@ -110,9 +112,9 @@ $result = $g_db->query($sql);
 // Variablen initialisieren
 $i         = 0;
 $picnr     = 0;
-$picpath   = "";
-$link_text = "";
-$album = new PhotoAlbum($g_db);
+$picpath   = '';
+$link_text = '';
+$album = new TablePhotos($g_db);
 
 // Schleife, falls nicht direkt ein Bild gefunden wird, aber auf 20 Durchlaeufe begrenzen
 while(!file_exists($picpath) && $i < 20 && $g_db->num_rows($result) > 0)
@@ -134,13 +136,13 @@ while(!file_exists($picpath) && $i < 20 && $g_db->num_rows($result) > 0)
     }
     
     //Bilpfad zusammensetzen
-    $picpath = PLUGIN_PATH. "/../adm_my_files/photos/".$album->getValue('pho_begin')."_".$album->getValue('pho_id')."/".$picnr.".jpg";
+    $picpath = PLUGIN_PATH. '/../adm_my_files/photos/'.$album->getValue('pho_begin').'_'.$album->getValue('pho_id').'/'.$picnr.'.jpg';
     $i++;
 }
 
 if(!file_exists($picpath))
 {
-    $picpath = THEME_SERVER_PATH. "/images/nopix.jpg";
+    $picpath = THEME_SERVER_PATH. '/images/nopix.jpg';
 }
 
 //Ermittlung der Original Bildgroesse
@@ -153,18 +155,18 @@ $popup_width  = $g_preferences['photo_show_width']+70;
 if($plg_photos_show_link && $plg_max_char_per_word > 0)
 {
     //Linktext umbrechen wenn noetig
-    $words = explode(" ", $album->getValue('pho_name'));
+    $words = explode(' ', $album->getValue('pho_name'));
     
     for($i = 0; $i < count($words); $i++)
     {
         if(strlen($words[$i]) > $plg_max_char_per_word)
         {
-            $link_text = "$link_text ". substr($words[$i], 0, $plg_max_char_per_word). "-<br />". 
+            $link_text = $link_text.substr($words[$i], 0, $plg_max_char_per_word). '-<br />'. 
                             substr($words[$i], $plg_max_char_per_word);
         }
         else
         {
-            $link_text = "$link_text ". $words[$i];
+            $link_text = $link_text. $words[$i];
         }
     }
 }
@@ -180,19 +182,19 @@ echo '<a class="$plg_link_class" href="'. $g_root_path. '/adm_program/modules/ph
 //Entscheidung ueber scallierung
 if($bildgroesse[0]/$plg_photos_max_width > $bildgroesse[1]/$plg_photos_max_height)
 {
-   echo "<img style=\"vertical-align: middle; cursor: pointer;\" src=\"$g_root_path/adm_program/modules/photos/photo_show.php?pho_id=".$pho_id."&amp;pic_nr=".$picnr."&amp;pho_begin=".$album->getValue('pho_begin')."&amp;scal=".$plg_photos_max_width."&amp;side=x\"  border=\"0\" alt=\"Zufallsbild\" />";
+   echo '<img style="vertical-align: middle; cursor: pointer;" src="'.$g_root_path.'/adm_program/modules/photos/photo_show.php?pho_id='.$pho_id.'&amp;pic_nr='.$picnr.'&amp;pho_begin='.$album->getValue('pho_begin').'&amp;scal='.$plg_photos_max_width.'&amp;side=x"  border="0" alt="Zufallsbild" />';
 }
 else
 {
-   echo "<img style=\"vertical-align: middle; cursor: pointer;\" src=\"$g_root_path/adm_program/modules/photos/photo_show.php?pho_id=".$pho_id."&amp;pic_nr=".$picnr."&amp;pho_begin=".$album->getValue('pho_begin')."&amp;scal=".$plg_photos_max_height."&amp;side=y\"  border=\"0\" alt=\"Zufallsbild\" />";
+   echo '<img style="vertical-align: middle; cursor: pointer;" src="'.$g_root_path.'/adm_program/modules/photos/photo_show.php?pho_id='.$pho_id.'&amp;pic_nr='.$picnr.'&amp;pho_begin='.$album->getValue('pho_begin').'&amp;scal='.$plg_photos_max_height.'&amp;side=y"  border="0" alt="Zufallsbild" />';
 }
 
-echo "</a>";
+echo '</a>';
 
 //Link zum Album
 if($plg_photos_show_link)
 {
-    echo"<br /><a class=\"$plg_link_class\" href=\"$g_root_path/adm_program/modules/photos/photos.php?pho_id=".$pho_id."\" target=\"$plg_link_target\">".$link_text."</a>";
+    echo'<br /><a class="'.$plg_link_class.'" href="'.$g_root_path.'/adm_program/modules/photos/photos.php?pho_id='.$pho_id.'" target="'.$plg_link_target.'">'.$link_text.'</a>';
 }
 
 echo '</div>';
