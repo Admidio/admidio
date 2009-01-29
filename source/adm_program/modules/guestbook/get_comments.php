c70c990ec41bf9f43fec9135311baf372d5e1901
Jochen Erkens <jochen.erkens@gmail.com>
2009-01-29 15:40:00 +0000

    Stringschreibweise vereinheitlichen

--- source/adm_program/modules/guestbook/get_comments.php
+++ source/adm_program/modules/guestbook/get_comments.php
@@ -19,8 +19,8 @@ if (isset($_GET['cid']) && is_numeric($_GET['cid']))
     // Script wurde ueber Ajax aufgerufen
     $cid = $_GET['cid'];
 
-    require("../../system/common.php");
-    require("../../system/classes/ubb_parser.php");
+    require('../../system/common.php');
+    require('../../system/classes/ubb_parser.php');
 
     if ($g_preferences['enable_bbcode'] == 1)
     {
@@ -37,19 +37,18 @@ else
 
 if ($cid > 0)
 {
-    $sql    = "SELECT * FROM ". TBL_GUESTBOOK_COMMENTS. ", ". TBL_GUESTBOOK. "
-                                   WHERE gbo_id     = $cid
+    $sql    = 'SELECT * FROM '. TBL_GUESTBOOK_COMMENTS. ', '. TBL_GUESTBOOK. '
+                                   WHERE gbo_id     = '.$cid.'
                                      AND gbc_gbo_id = gbo_id
-                                     AND gbo_org_id = ". $g_current_organization->getValue("org_id"). "
-                                   ORDER by gbc_timestamp asc";
+                                     AND gbo_org_id = '. $g_current_organization->getValue('org_id'). '
+                                   ORDER by gbc_timestamp asc';
     $comment_result = $g_db->query($sql);
 }
 
 if (isset($comment_result))
 {
 
-    echo"
-    <div id=\"comments_$cid\" style=\"visibility: visible; display: block; text-align: left;\">";
+    echo'<div id="comments_$cid" style="visibility: visible; display: block; text-align: left;">';
 
     //Kommentarnummer auf 1 setzen
     $commentNumber = 1;
@@ -59,40 +58,39 @@ if (isset($comment_result))
     {
         $cid = $row->gbc_gbo_id;
 
-        echo "
-        <div class=\"groupBox\" id=\"gbc_".$row->gbc_id."\" style=\"overflow: hidden; margin-left: 20px; margin-right: 20px;\">
-            <div class=\"groupBoxHeadline\">
-                <div class=\"boxHeadLeft\">
-                    <img src=\"". THEME_PATH. "/icons/comments.png\" style=\"vertical-align: top;\" alt=\"Kommentar ". $commentNumber. "\" />&nbsp;".
-                    "Kommentar von ". $row->gbc_name;
+        echo '
+        <div class="groupBox" id="gbc_'.$row->gbc_id.'" style="overflow: hidden; margin-left: 20px; margin-right: 20px;">
+            <div class="groupBoxHeadline">
+                <div class="boxHeadLeft">
+                    <img src="'. THEME_PATH. '/icons/comments.png" style="vertical-align: top;" alt="Kommentar '. $commentNumber. '" />&nbsp;'.
+                    'Kommentar von '. $row->gbc_name;
 
                 // Falls eine Mailadresse des Users angegeben wurde, soll ein Maillink angezeigt werden...
                 if (isValidEmailAddress($row->gbc_email))
                 {
-                    echo "
-                    <a class=\"iconLink\" href=\"mailto:$row->gbc_email\"><img src=\"". THEME_PATH. "/icons/email.png\" 
-                        alt=\"Mail an $row->gbc_email\" title=\"Mail an $row->gbc_email\" /></a>";
+                    echo '<a class="iconLink" href="mailto:'.$row->gbc_email.'"><img src="'. THEME_PATH. '/icons/email.png" 
+                        alt="Mail an '.$row->gbc_email.'" title="Mail an '.$row->gbc_email.'" /></a>';
                 }
 
-                echo "
+                echo '
                 </div>
 
-                <div class=\"boxHeadRight\">". mysqldatetime("d.m.y h:i", $row->gbc_timestamp);
+                <div class="boxHeadRight">'. mysqldatetime('d.m.y h:i', $row->gbc_timestamp);
 
                 // aendern und loeschen von Kommentaren duerfen nur User mit den gesetzten Rechten
                 if ($g_current_user->editGuestbookRight())
                 {
-                    echo "
-                    <a class=\"iconLink\" href=\"$g_root_path/adm_program/modules/guestbook/guestbook_comment_new.php?cid=$row->gbc_id\"><img 
-                        src=\"". THEME_PATH. "/icons/edit.png\" alt=\"Bearbeiten\" title=\"Bearbeiten\" /></a>
-                    <a class=\"iconLink\" href=\"javascript:deleteObject('gbc', 'gbc_".$row->gbc_id."',".$row->gbc_id.",'".$row->gbc_name."')\"><img 
-                        src=\"". THEME_PATH. "/icons/delete.png\" alt=\"Löschen\" title=\"Löschen\" /></a>";
+                    echo '
+                    <a class="iconLink" href="$g_root_path/adm_program/modules/guestbook/guestbook_comment_new.php?cid='.$row->gbc_id.'"><img 
+                        src="'. THEME_PATH. '/icons/edit.png" alt="Bearbeiten" title="Bearbeiten" /></a>
+                    <a class="iconLink" href="javascript:deleteObject(\'gbc\', \'gbc_'.$row->gbc_id.'\','.$row->gbc_id.',\''.$row->gbc_name.'\')"><img 
+                        src="'. THEME_PATH. '/icons/delete.png" alt="Löschen" title="Löschen" /></a>';
                 }
 
-                echo "</div>
+                echo '</div>
             </div>
 
-            <div class=\"groupBoxBody\">";
+            <div class="groupBoxBody">';
                 // wenn BBCode aktiviert ist, den Text noch parsen, ansonsten direkt ausgeben
                 if ($g_preferences['enable_bbcode'] == 1)
                 {
@@ -109,18 +107,18 @@ if (isset($comment_result))
                     // Userdaten des Editors holen...
                     $user_change = new User($g_db, $row->gbc_usr_id_change);
 
-                    echo "
-                    <div class=\"editInformation\">
-                        Zuletzt bearbeitet von ".
-                        $user_change->getValue("Vorname"). " ". $user_change->getValue("Nachname").
-                        " am ". mysqldatetime("d.m.y h:i", $row->gbc_timestamp_change). "
-                    </div>";
+                    echo '
+                    <div class="editInformation">
+                        Zuletzt bearbeitet von '.
+                        $user_change->getValue('Vorname'). ' '. $user_change->getValue('Nachname').
+                        ' am '. mysqldatetime('d.m.y h:i', $row->gbc_timestamp_change). '
+                    </div>';
                 }
-            echo "
+            echo '
             </div>
         </div>
 
-        <br />";
+        <br />';
 
         // Kommentarnummer um 1 erhoehen
         $commentNumber = $commentNumber + 1;
@@ -130,19 +128,19 @@ if (isset($comment_result))
     if ($g_current_user->commentGuestbookRight() || $g_preferences['enable_gbook_comments4all'] == 1)
     {
         // Bei Kommentierungsrechten, wird der Link zur Kommentarseite angezeigt...
-        $load_url = "$g_root_path/adm_program/modules/guestbook/guestbook_comment_new.php?id=$cid";
-        echo "
-        <div class=\"editInformation\">
-            <span class=\"iconTextLink\">
-                <a href=\"$load_url\"><img src=\"". THEME_PATH. "/icons/comment_new.png\" 
-                alt=\"Kommentieren\" title=\"Kommentieren\" /></a>
-                <a href=\"$load_url\">Einen Kommentar zu diesem Beitrag schreiben.</a>
+        $load_url = $g_root_path.'/adm_program/modules/guestbook/guestbook_comment_new.php?id='.$cid.'';
+        echo '
+        <div class="editInformation">
+            <span class="iconTextLink">
+                <a href="'.$load_url.'"><img src="'. THEME_PATH. '/icons/comment_new.png" 
+                alt="Kommentieren" title="Kommentieren" /></a>
+                <a href="'.$load_url.'">Einen Kommentar zu diesem Beitrag schreiben.</a>
             </span>
-        </div>";
+        </div>';
     }
 
-    echo"
-    </div>";
+    echo'
+    </div>';
 }
 
 ?>
--- source/adm_program/modules/guestbook/guestbook_new.php
+++ source/adm_program/modules/guestbook/guestbook_new.php
@@ -15,8 +15,8 @@
  *
  *****************************************************************************/
 
-require("../../system/common.php");
-require("../../system/classes/table_guestbook.php");
+require('../../system/common.php');
+require('../../system/classes/table_guestbook.php');
 if ($g_preferences['enable_bbcode'] == 1)
 {
     require('../../system/bbcode.php');
@@ -26,47 +26,47 @@ if ($g_preferences['enable_bbcode'] == 1)
 if ($g_preferences['enable_guestbook_module'] == 0)
 {
     // das Modul ist deaktiviert
-    $g_message->show("module_disabled");
+    $g_message->show('module_disabled');
 }
 elseif($g_preferences['enable_guestbook_module'] == 2)
 {
     // nur eingeloggte Benutzer duerfen auf das Modul zugreifen
-    require("../../system/login_valid.php");
+    require('../../system/login_valid.php');
 }
 
 
 // Uebergabevariablen pruefen
 
-if (array_key_exists("id", $_GET))
+if (array_key_exists('id', $_GET))
 {
-    if (is_numeric($_GET["id"]) == false)
+    if (is_numeric($_GET['id']) == false)
     {
-        $g_message->show("invalid");
+        $g_message->show('invalid');
     }
 }
 else
 {
-    $_GET["id"] = 0;
+    $_GET['id'] = 0;
 }
 
-if (array_key_exists("headline", $_GET))
+if (array_key_exists('headline', $_GET))
 {
-    $_GET["headline"] = strStripTags($_GET["headline"]);
+    $_GET['headline'] = strStripTags($_GET['headline']);
 }
 else
 {
-    $_GET["headline"] = "Gästebuch";
+    $_GET['headline'] = 'Gästebuch';
 }
 
 
 // Falls ein Eintrag bearbeitet werden soll muss geprueft weden ob die Rechte gesetzt sind...
-if ($_GET["id"] != 0)
+if ($_GET['id'] != 0)
 {
-    require("../../system/login_valid.php");
+    require('../../system/login_valid.php');
 
     if (!$g_current_user->editGuestbookRight())
     {
-        $g_message->show("norights");
+        $g_message->show('norights');
     }
 }
 
@@ -75,14 +75,14 @@ $_SESSION['navigation']->addUrl(CURRENT_URL);
 // Gaestebuchobjekt anlegen
 $guestbook = new TableGuestbook($g_db);
 
-if($_GET["id"] > 0)
+if($_GET['id'] > 0)
 {
-    $guestbook->readData($_GET["id"]);
+    $guestbook->readData($_GET['id']);
 
     // Pruefung, ob der Eintrag zur aktuellen Organisation gehoert
-    if($guestbook->getValue("gbo_org_id") != $g_current_organization->getValue("org_id"))
+    if($guestbook->getValue('gbo_org_id') != $g_current_organization->getValue('org_id'))
     {
-        $g_message->show("norights");
+        $g_message->show('norights');
     }
 }
 
@@ -92,7 +92,7 @@ if(isset($_SESSION['guestbook_entry_request']))
     // nun die vorher eingegebenen Inhalte auslesen
     foreach($_SESSION['guestbook_entry_request'] as $key => $value)
     {
-        if(strpos($key, "gbo_") == 0)
+        if(strpos($key, 'gbo_') == 0)
         {
             $guestbook->setValue($key, stripslashes($value));
         }
@@ -104,9 +104,9 @@ if(isset($_SESSION['guestbook_entry_request']))
 // Name, Emailadresse und Homepage vorbelegt werden...
 if ($_GET['id'] == 0 && $g_valid_login)
 {
-    $guestbook->setValue("gbo_name", $g_current_user->getValue("Vorname"). " ". $g_current_user->getValue("Nachname"));
-    $guestbook->setValue("gbo_email", $g_current_user->getValue("E-Mail"));
-    $guestbook->setValue("gbo_homepage", $g_current_user->getValue("Homepage"));
+    $guestbook->setValue('gbo_name', $g_current_user->getValue('Vorname'). ' '. $g_current_user->getValue('Nachname'));
+    $guestbook->setValue('gbo_email', $g_current_user->getValue('E-Mail'));
+    $guestbook->setValue('gbo_homepage', $g_current_user->getValue('Homepage'));
 }
 
 if (!$g_valid_login && $g_preferences['flooding_protection_time'] != 0)
@@ -116,31 +116,31 @@ if (!$g_valid_login && $g_preferences['flooding_protection_time'] != 0)
     // einen GB-Eintrag erzeugt hat...
     $ipAddress = $_SERVER['REMOTE_ADDR'];
 
-    $sql = "SELECT count(*) FROM ". TBL_GUESTBOOK. "
-             WHERE unix_timestamp(gbo_timestamp) > unix_timestamp()-". $g_preferences['flooding_protection_time']. "
-               AND gbo_org_id = ". $g_current_organization->getValue("org_id"). "
-               AND gbo_ip_address = '". $guestbook->getValue("gbo_ip_address"). "'";
+    $sql = 'SELECT count(*) FROM '. TBL_GUESTBOOK. '
+             WHERE unix_timestamp(gbo_timestamp) > unix_timestamp()-'. $g_preferences['flooding_protection_time']. '
+               AND gbo_org_id = '. $g_current_organization->getValue('org_id'). '
+               AND gbo_ip_address = "'. $guestbook->getValue('gbo_ip_address'). '"';
     $result = $g_db->query($sql);
     $row = $g_db->fetch_array($result);
     if($row[0] > 0)
     {
           //Wenn dies der Fall ist, gibt es natuerlich keinen Gaestebucheintrag...
-          $g_message->show("flooding_protection", $g_preferences['flooding_protection_time']);
+          $g_message->show('flooding_protection', $g_preferences['flooding_protection_time']);
     }
 }
 
 // Html-Kopf ausgeben
 if ($_GET['id'] > 0)
 {
-    $g_layout['title'] = "Gästebucheintrag ändern";
+    $g_layout['title'] = 'Gästebucheintrag ändern';
 }
 else
 {
-    $g_layout['title'] = "Gästebucheintrag anlegen";
+    $g_layout['title'] = 'Gästebucheintrag anlegen';
 }
 
 //Script für BBCode laden
-$javascript = "";
+$javascript = '';
 if ($g_preferences['enable_bbcode'] == 1)
 {
     $javascript = getBBcodeJS('gbo_text');
@@ -153,132 +153,132 @@ require(THEME_SERVER_PATH. '/overall_header.php');
 // Html des Modules ausgeben
 if ($_GET['id'] > 0)
 {
-    $mode = "3";
+    $mode = '3';
 }
 else
 {
-    $mode = "1";
+    $mode = '1';
 }
 
-echo "
-<form method=\"post\" action=\"$g_root_path/adm_program/modules/guestbook/guestbook_function.php?id=". $_GET["id"]. "&amp;headline=". $_GET['headline']. "&amp;mode=$mode\" >
-<div class=\"formLayout\" id=\"edit_guestbook_form\">
-    <div class=\"formHead\">". $g_layout['title']. "</div>
-    <div class=\"formBody\">
-        <ul class=\"formFieldList\">
+echo '
+<form method="post" action="'.$g_root_path.'/adm_program/modules/guestbook/guestbook_function.php?id='. $_GET['id']. '&amp;headline='. $_GET['headline']. '&amp;mode='.$mode.'" >
+<div class="formLayout" id="edit_guestbook_form">
+    <div class="formHead">'. $g_layout['title']. '</div>
+    <div class="formBody">
+        <ul class="formFieldList">
             <li>
                 <dl>
-                    <dt><label for=\"gbo_name\">Name:</label></dt>
-                    <dd>";
-                        if ($g_current_user->getValue("usr_id") > 0)
+                    <dt><label for="gbo_name">Name:</label></dt>
+                    <dd>';
+                        if ($g_current_user->getValue('usr_id') > 0)
                         {
                             // Eingeloggte User sollen ihren Namen nicht aendern duerfen
-                            echo "<input type=\"text\" id=\"gbo_name\" name=\"gbo_name\" class=\"readonly\" readonly=\"readonly\" tabindex=\"1\" style=\"width: 350px;\" maxlength=\"60\" value=\"". $guestbook->getValue("gbo_name"). "\" />";
+                            echo '<input type="text" id="gbo_name" name="gbo_name" class="readonly" readonly="readonly" tabindex="1" style="width: 350px;" maxlength="60" value="'. $guestbook->getValue('gbo_name'). '" />';
                         }
                         else
                         {
-                            echo "<input type=\"text\" id=\"gbo_name\" name=\"gbo_name\" tabindex=\"1\" style=\"width: 350px;\" maxlength=\"60\" value=\"". $guestbook->getValue("gbo_name"). "\" />
-                            <span class=\"mandatoryFieldMarker\" title=\"Pflichtfeld\">*</span>";
+                            echo '<input type="text" id="gbo_name" name="gbo_name" tabindex="1" style="width: 350px;" maxlength="60" value="'. $guestbook->getValue('gbo_name'). '" />
+                            <span class="mandatoryFieldMarker" title="Pflichtfeld">*</span>';
                         }
-                    echo "</dd>
+                    echo '</dd>
                 </dl>
             </li>
             <li>
                 <dl>
-                    <dt><label for=\"gbo_email\">Emailadresse:</label></dt>
+                    <dt><label for="gbo_email">Emailadresse:</label></dt>
                     <dd>
-                        <input type=\"text\" id=\"gbo_email\" name=\"gbo_email\" tabindex=\"2\" style=\"width: 350px;\" maxlength=\"50\" value=\"". $guestbook->getValue("gbo_email"). "\" />
+                        <input type="text" id="gbo_email" name="gbo_email" tabindex="2" style="width: 350px;" maxlength="50" value="'. $guestbook->getValue('gbo_email'). '" />
                     </dd>
                 </dl>
             </li>
             <li>
                 <dl>
-                    <dt><label for=\"gbo_homepage\">Homepage:</label></dt>
+                    <dt><label for="gbo_homepage">Homepage:</label></dt>
                     <dd>
-                        <input type=\"text\" id=\"gbo_homepage\" name=\"gbo_homepage\" tabindex=\"3\" style=\"width: 350px;\" maxlength=\"50\" value=\"". $guestbook->getValue("gbo_homepage"). "\" />
+                        <input type="text" id="gbo_homepage" name="gbo_homepage" tabindex="3" style="width: 350px;" maxlength="50" value="'. $guestbook->getValue('gbo_homepage'). '" />
                     </dd>
                 </dl>
-            </li>";
+            </li>';
          //BBCode
          if ($g_preferences['enable_bbcode'] == 1)
          {
             printBBcodeIcons();
          }
-         echo "
+         echo '
             <li>
                 <dl>
-                    <dt><label for=\"gbo_text\">Text:</label>";
+                    <dt><label for="gbo_text">Text:</label>';
                         //Einfügen der Smilies
                         if($g_preferences['enable_bbcode'] == 1)
                         {
                             printEmoticons();
                         }
-                    echo "</dt>
+                    echo '</dt>
                     <dd>
-                        <textarea id=\"gbo_text\" name=\"gbo_text\" tabindex=\"4\" style=\"width: 350px;\" rows=\"10\" cols=\"40\">". $guestbook->getValue("gbo_text"). "</textarea>
-                        <span class=\"mandatoryFieldMarker\" title=\"Pflichtfeld\">*</span>
+                        <textarea id="gbo_text" name="gbo_text" tabindex="4" style="width: 350px;" rows="10" cols="40">'. $guestbook->getValue('gbo_text'). '</textarea>
+                        <span class="mandatoryFieldMarker" title="Pflichtfeld">*</span>
                     </dd>
                 </dl>
-            </li>";
+            </li>';
 
             // Nicht eingeloggte User bekommen jetzt noch das Captcha praesentiert,
             // falls es in den Orgaeinstellungen aktiviert wurde...
             if (!$g_valid_login && $g_preferences['enable_guestbook_captcha'] == 1)
             {
-                echo "
+                echo '
                 <li>
                     <dl>
                         <dt>&nbsp;</dt>
                         <dd>
-                            <img src=\"$g_root_path/adm_program/system/classes/captcha.php?id=". time(). "\" alt=\"Captcha\" />
+                            <img src="'.$g_root_path.'/adm_program/system/classes/captcha.php?id='. time(). '" alt="Captcha" />
                         </dd>
                     </dl>
 
                     <dl>
-                       <dt><label for=\"captcha\">Bestätigungscode:</label></dt>
+                       <dt><label for="captcha">Bestätigungscode:</label></dt>
                        <dd>
-                           <input type=\"text\" id=\"captcha\" name=\"captcha\" tabindex=\"5\" style=\"width: 200px;\" maxlength=\"8\" value=\"\" />
-                           <span class=\"mandatoryFieldMarker\" title=\"Pflichtfeld\">*</span>
-                           <img class=\"iconHelpLink\" src=\"". THEME_PATH. "/icons/help.png\" alt=\"Hilfe\" title=\"\" onclick=\"window.open('$g_root_path/adm_program/system/msg_window.php?err_code=captcha_help&amp;window=true','Message','width=600,height=500,left=310,top=200,scrollbars=yes')\" onmouseover=\"ajax_showTooltip(event,'$g_root_path/adm_program/system/msg_window.php?err_code=captcha_help',this)\" onmouseout=\"ajax_hideTooltip()\" />
+                           <input type="text" id="captcha" name="captcha" tabindex="5" style="width: 200px;" maxlength="8" value="" />
+                           <span class="mandatoryFieldMarker" title="Pflichtfeld">*</span>
+                           <img class="iconHelpLink" src="'. THEME_PATH. '/icons/help.png" alt="Hilfe" title="" onclick="window.open(\''.$g_root_path.'/adm_program/system/msg_window.php?err_code=captcha_help&amp;window=true\',\'Message\',\'width=600,height=500,left=310,top=200,scrollbars=yes\')" onmouseover="ajax_showTooltip(event,\''.$g_root_path.'/adm_program/system/msg_window.php?err_code=captcha_help\',this)" onmouseout="ajax_hideTooltip()" />
                        </dd>
                     </dl>
-                </li>";
+                </li>';
             }
-        echo "</ul>
+        echo '</ul>
 
         <hr />
 
-        <div class=\"formSubmit\">
-            <button name=\"speichern\" type=\"submit\" value=\"speichern\" tabindex=\"6\"><img src=\"". THEME_PATH. "/icons/disk.png\" alt=\"Speichern\" />&nbsp;Speichern</button>
+        <div class="formSubmit">
+            <button name="speichern" type="submit" value="speichern" tabindex="6"><img src="'. THEME_PATH. '/icons/disk.png" alt="Speichern" />&nbsp;Speichern</button>
         </div>
     </div>
 </div>
 </form>
 
-<ul class=\"iconTextLinkList\">
+<ul class="iconTextLinkList">
     <li>
-        <span class=\"iconTextLink\">
-            <a href=\"$g_root_path/adm_program/system/back.php\"><img
-            src=\"". THEME_PATH. "/icons/back.png\" alt=\"Zurück\" /></a>
-            <a href=\"$g_root_path/adm_program/system/back.php\">Zurück</a>
+        <span class="iconTextLink">
+            <a href="'.$g_root_path.'/adm_program/system/back.php"><img
+            src="'. THEME_PATH. '/icons/back.png" alt="Zurück" /></a>
+            <a href="'.$g_root_path.'/adm_program/system/back.php">Zurück</a>
         </span>
     </li>
-</ul>";
+</ul>';
 
-if ($g_current_user->getValue("usr_id") == 0)
+if ($g_current_user->getValue('usr_id') == 0)
 {
-    $focusField = "gbo_name";
+    $focusField = 'gbo_name';
 }
 else
 {
-    $focusField = "gbo_text";
+    $focusField = 'gbo_text';
 }
 
-echo"
-<script type=\"text/javascript\"><!--
-    document.getElementById('$focusField').focus();
---></script>";
+echo'
+<script type="text/javascript"><!--
+    document.getElementById(\''.$focusField.'\').focus();
+--></script>';
 
-require(THEME_SERVER_PATH. "/overall_footer.php");
+require(THEME_SERVER_PATH. '/overall_footer.php');
 
 ?>
