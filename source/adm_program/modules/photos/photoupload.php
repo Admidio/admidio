ba8a67124f5d4ab959206bca6264f3c13404961d
Jochen Erkens <jochen.erkens@gmail.com>
2009-02-19 09:20:14 +0000

    Anpassungen für Hilfe-Thickbox: - Bulletpoints klebten am Rand - Im Photomodul waren die boxen zu kurz

--- source/adm_program/modules/photos/photoupload.php
+++ source/adm_program/modules/photos/photoupload.php
@@ -209,9 +209,9 @@ echo '</div>
     </li>    
     <li>
         <span class="iconTextLink">
-            <a class="thickbox" href="'. $g_root_path. '/adm_program/system/msg_window.php?err_code=photo_up_help&amp;window=true&amp;KeepThis=true&amp;TB_iframe=true&amp;height=520&amp;width=580"><img 
+            <a class="thickbox" href="'. $g_root_path. '/adm_program/system/msg_window.php?err_code=photo_up_help&amp;window=true&amp;KeepThis=true&amp;TB_iframe=true&amp;height=540&amp;width=580"><img 
             	src="'. THEME_PATH. '/icons/help.png" alt="Hilfe" /></a>
-            <a class="thickbox" href="'. $g_root_path. '/adm_program/system/msg_window.php?err_code=photo_up_help&amp;window=true&amp;KeepThis=true&amp;TB_iframe=true&amp;height=520&amp;width=580">Hilfe</a>
+            <a class="thickbox" href="'. $g_root_path. '/adm_program/system/msg_window.php?err_code=photo_up_help&amp;window=true&amp;KeepThis=true&amp;TB_iframe=true&amp;height=540&amp;width=580">Hilfe</a>
         </span>
     </li>
 </ul>';
