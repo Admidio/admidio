6dc86630e5b90f1613bf45814690c5e7c424f1d0
Markus Faßbender <markus.fassbender@gmail.com>
2009-04-17 08:12:27 +0000

    So, jetzt sollte es aber klappen...

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
     // wenn noch nicht installiert, dann Install-Dialog anzeigen
-    if(!file_exists('../config.php'))
-    {
-        header('Location: adm_install/index.php');
-    }
-    else
+    if(file_exists('config.php'))
     {
         require_once('adm_program/system/common.php');
         header('Location: '.$g_homepage);
+    }
+    else
+    {
+        header('Location: adm_install/index.php');
     }
 ?>
