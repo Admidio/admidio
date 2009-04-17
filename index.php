095fddbdd40843599f0489288232819290364864
Markus Faßbender <markus.fassbender@gmail.com>
2009-04-17 08:09:52 +0000

    BUG[2769966] Fehlende umleitung index.html bringt error bei Neuinstallation

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-    require_once('adm_program/system/common.php');
-    header('Location: '.$g_homepage);
-    exit();
+    // wenn noch nicht installiert, dann Install-Dialog anzeigen
+    if(!file_exists('../config.php'))
+    {
+        header('Location: adm_install/index.php');
+    }
+    else
+    {
+        require_once('adm_program/system/common.php');
+        header('Location: '.$g_homepage);
+    }
 ?>
