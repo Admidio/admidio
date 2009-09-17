367542782431b26b97201904890ec5dd5931e2b2
rpinto-mpunkt <rpinto-mpunkt@admidio.org>
2009-09-17 14:17:54 +0000

    Hardcodierte Datenbank durch PREFIX ersetzt

--- adm_install/db_scripts/upd_2_2_0_db.sql
+++ adm_install/db_scripts/upd_2_2_0_db.sql
@@ -43,12 +43,12 @@ auto_increment = 1;
 
 -- Tabelle für maximale Zusagen an Termine
 
-CREATE TABLE `admidio_sf`.`adm_date_max_members` (
-    `id`              INT NOT NULL AUTO_INCREMENT ,
-    `dat_id`          INT NOT NULL ,
-    `rol_id`          INT NOT NULL ,
-    `max_members`     INT NOT NULL ,
-    PRIMARY KEY ( `id` )
+CREATE TABLE %PRAEFIX%_date_max_members (
+    id              INT NOT NULL AUTO_INCREMENT ,
+    dat_id          INT NOT NULL ,
+    rol_id          INT NOT NULL ,
+    max_members     INT NOT NULL ,
+    PRIMARY KEY ( id )
 ) 
 ENGINE = InnoDB
 auto_increment = 1;
