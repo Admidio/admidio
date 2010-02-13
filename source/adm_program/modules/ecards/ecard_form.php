bf064afd89ea1ce3c2b1a73f66d8dfca0ea974a4
Roland Eischer <roland_203@yahoo.de>
2010-02-13 18:04:49 +0000

    ecard: kleine Fehler ausgebessert random_photo: durch variable Datumseinstellung fehlte hier noch der korrekte Aufruf zu photo_show.php

--- source/adm_program/modules/ecards/ecard_form.php
+++ source/adm_program/modules/ecards/ecard_form.php
@@ -220,7 +220,7 @@ if ($error_msg != '')
 }
 
 echo '<form id="ecard_form" action="javascript:ecardJS.makePreview();" method="post">
-		<input type="hidden" name="ecard[image_name]" value="'.$g_root_path.'/adm_program/modules/photos/photo_show.php?pho_id='.$pho_id.'&pic_nr='.$photo.'&pho_begin='.$photo_album->getValue('pho_begin').'&max_width='.$g_preferences['ecard_card_picture_width'].'&max_height='.$g_preferences['ecard_card_picture_height'].'" />
+		<input type="hidden" name="ecard[image_name]" value="'.$g_root_path.'/adm_program/modules/photos/photo_show.php?pho_id='.$pho_id.'&amp;pic_nr='.$photo.'&amp;pho_begin='.$photo_album->getValue('pho_begin', 'Y-m-d').'&amp;max_width='.$g_preferences['ecard_view_width'].'&amp;max_height='.$g_preferences['ecard_view_height'].'" />
 		<input type="hidden" name="ecard[image_serverPath]" value="'.SERVER_PATH. '/adm_my_files/photos/'.$photo_album->getValue('pho_begin').'_'.$photo_album->getValue('pho_id').'/'.$photo_nr.'.jpg" />
 		<input type="hidden" name="submit_action" value="" />
 		<ul class="formFieldList">
