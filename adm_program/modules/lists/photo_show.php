87d8f3c2c3cf46be41de57bd5ed1233150da027f
Markus Faßbender <markus.fassbender@gmail.com>
2010-04-11 10:10:41 +0000

    FRE[2985352] Laden der Profilbilder in Listen ändern

--- adm_program/modules/lists/photo_show.php
+++ adm_program/modules/lists/photo_show.php
@@ -12,38 +12,44 @@
  * usr_id : die ID des Users dessen Bild angezeigt werden soll
  *
  *****************************************************************************/
-require("../../system/common.php");
-require("../../system/login_valid.php");
+require_once('../../system/common.php');
+require_once('../../system/login_valid.php');
 
-if(is_numeric($_GET['usr_id']))
+if(isset($_GET['usr_id']) && is_numeric($_GET['usr_id']))
 {
-	if($g_preferences['profile_photo_storage'] == 0)
+    // pruefen, ob Profilfoto aus DB oder Filesystem kommt
+    if($g_preferences['profile_photo_storage'] == 0)
     {
-    	if(isset($_SESSION['profilphoto'][$_GET['usr_id']]) && $_SESSION['profilphoto'][$_GET['usr_id']] != "")
-		{
-			header("Content-Type: image/jpeg");
-			// Ausgabe des Bildes aus dem Sessionarray
-    		echo $_SESSION['profilphoto'][$_GET['usr_id']];
-    		unset($_SESSION['profilphoto'][$_GET['usr_id']]);
-		}
-		else
-		{
-			header("Content-Type: image/png");
-			readfile(THEME_SERVER_PATH. "/images/no_profile_pic.png");
-		}
+        // Profilbild aus DB einlesen
+        $sql = 'SELECT usr_photo FROM '.TBL_USERS.' WHERE usr_id = '.$_GET['usr_id'];
+        $g_db->query($sql);
+        $row = $g_db->fetch_array();
+
+        if(strlen($row['usr_photo']) > 0)
+        {
+            header('Content-Type: image/jpeg');
+            echo $row['usr_photo'];
+        }
+        else
+        {
+            // der Benutzer besitzt kein Bild => Default-Bild anzeigen
+            header('Content-Type: image/png');
+            readfile(THEME_SERVER_PATH. '/images/no_profile_pic.png');
+        }
     }
     else
     {
-     	if(file_exists(SERVER_PATH. "/adm_my_files/user_profile_photos/".$_GET['usr_id'].".jpg"))
-     	{
-     		header("Content-Type: image/jpeg");
-     		readfile(SERVER_PATH. "/adm_my_files/user_profile_photos/".$_GET['usr_id'].".jpg");
-     	}
-     	else
-     	{
-     		header("Content-Type: image/png");
-			readfile(THEME_SERVER_PATH. "/images/no_profile_pic.png");
-     	}   	
+        // Profilbild aus dem Filesystem einlesen bzw. Default-Bild anzeigen
+        if(file_exists(SERVER_PATH. '/adm_my_files/user_profile_photos/'.$_GET['usr_id'].'.jpg'))
+        {
+            header('Content-Type: image/jpeg');
+            readfile(SERVER_PATH. '/adm_my_files/user_profile_photos/'.$_GET['usr_id'].'.jpg');
+        }
+        else
+        {
+            header('Content-Type: image/png');
+            readfile(THEME_SERVER_PATH. '/images/no_profile_pic.png');
+        }
     }
 }
 ?>
