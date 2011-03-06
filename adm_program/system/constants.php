47f7bb0550574e6fb40169cc6f317f09b5092840
Markus Faßbender <markus.fassbender@gmail.com>
2011-03-06 19:21:27 +0000

    Beta-Version auf 4 gesetzt

--- adm_program/system/constants.php
+++ adm_program/system/constants.php
@@ -15,7 +15,7 @@ if ('constants.php' == basename($_SERVER['SCRIPT_FILENAME']))
 
 // Admidio-Versionsnummern und Flags bitte nicht aendern !!!
 define('ADMIDIO_VERSION', '2.2.0');
-define('BETA_VERSION', '3');
+define('BETA_VERSION', '4');
 
 if(BETA_VERSION > 0)
 {
