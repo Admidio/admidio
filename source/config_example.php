6dfcbf963e785f1f7d348fc67d1efd94d33b17f2
Markus Faßbender <markus.fassbender@gmail.com>
2011-03-14 18:18:29 +0000

    - Inhalt der config.php ins englische übersetzt

--- source/config_example.php
+++ source/config_example.php
@@ -6,26 +6,32 @@
  * Homepage     : http://www.admidio.org
  * License      : GNU Public License 2 http://www.gnu.org/licenses/gpl-2.0.html
  *
+ * !!!!!!!!!!!!!!!!!!!!!!!!!! I M P O R T A N T !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * This is an example file. Don't use it with your production environment. 
+ * Our installation wizard will create a specific file with your custom 
+ * preferences.
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *
  *****************************************************************************/
 
 // Table prefix for Admidio-Tables in database
 // Example: 'adm'
-$g_tbl_praefix = '%PREFIX%';
+$g_tbl_praefix = 'adm';
 
 // Access to the database of the MySQL-Server
-$g_adm_srv = '%SERVER%';      // Server
-$g_adm_usr = '%USER%';        // User
-$g_adm_pw  = '%PASSWORD%';    // Password
-$g_adm_db  = '%DATABASE%';    // Database
+$g_adm_srv = 'URL_to_your_MySQL-Server';	// Server
+$g_adm_usr = 'Username';               		// User
+$g_adm_pw  = 'Password';                   	// Password
+$g_adm_db  = 'Databasename';              	// Database
 
 // URL to this Admidio installation 
 // Example: 'http://www.admidio.org/example'
-$g_root_path = '%ROOT_PATH%';
+$g_root_path = 'http://www.your-website.de/admidio';
 
 // Short description of the organization that is running Admidio
 // This short description must correspond to your input in the installtion wizard !!!
 // Example: 'ADMIDIO'
 // Maximun of 10 characters !!!
-$g_organization = '%ORGANIZATION%'; 
+$g_organization = 'Shortcut'; 
 
 ?>
