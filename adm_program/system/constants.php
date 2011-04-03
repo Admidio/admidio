f555daad4cc55a998c590597270b9037a59b0eff
Markus Faßbender <markus.fassbender@gmail.com>
2011-04-03 10:29:20 +0000

    Version schon mal auf 2.3 Beta 1 gesetzt

--- adm_program/system/constants.php
+++ adm_program/system/constants.php
@@ -14,8 +14,8 @@ if ('constants.php' == basename($_SERVER['SCRIPT_FILENAME']))
 }
 
 // Admidio-Versionsnummern und Flags bitte nicht aendern !!!
-define('ADMIDIO_VERSION', '2.2.1');
-define('BETA_VERSION', '5');
+define('ADMIDIO_VERSION', '2.3.0');
+define('BETA_VERSION', '1');
 
 if(BETA_VERSION > 0)
 {
