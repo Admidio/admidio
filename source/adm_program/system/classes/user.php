a6441b40604249ef222c0bedb794596ac8f98bc8
Markus Faßbender <markus.fassbender@gmail.com>
2011-05-01 20:08:00 +0000

    BUG[3295908] Rollenrechte werden für unsichtbare Rollen nicht gesetzt

--- source/adm_program/system/classes/user.php
+++ source/adm_program/system/classes/user.php
@@ -81,7 +81,6 @@ class User extends TableUsers
                            AND mem_begin  <= "'.DATE_NOW.'"
                            AND mem_end     > "'.DATE_NOW.'"
                          WHERE rol_valid   = 1
-                           AND rol_visible = 1
                            AND rol_cat_id  = cat_id
                            AND (  cat_org_id = '. $g_current_organization->getValue('org_id').' 
                                OR cat_org_id IS NULL ) ';
