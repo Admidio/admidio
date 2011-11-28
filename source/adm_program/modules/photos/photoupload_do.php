5bf11789ff13a1b59b4e2413d470dac3bb8373f0
Jochen Erkens <jochen.erkens@gmail.com>
2011-11-28 10:02:42 +0000

    - Multiupload wieder lauffähig gemacht - Ablauf optimiert - Deutschen Text dur Sprachvariable ersezt

--- source/adm_program/modules/photos/photoupload_do.php
+++ source/adm_program/modules/photos/photoupload_do.php
@@ -46,6 +46,46 @@ if(!$gCurrentUser->editPhotoRight())
 {
     $gMessage->show($gL10n->get('PHO_NO_RIGHTS'));
 }
+//var_dump($_FILES['Filedata']['size']);exit();
+//Bei Klassischem upload erstmal testen ob Alle Dateien angekommen sind bei Flex reichen die Kontrollen in der Verarbeitung
+if($getUploadmethod == 1)
+{
+    //zaehlen wieviele Fotos hochgeladen werden sollen und ob alle Uploads Fehlerfrei sind
+    $counter=0;
+    if(isset($_FILES['Filedata']))
+    {
+        for($x=0; $x<=4; $x++)
+        {
+            //Datei wurde hochgeladen
+            if(isset($_FILES['Filedata']['name'][$x]) && $_FILES['Filedata']['size'][$x]!=0)
+            {
+                $counter++;
+    
+                //Die hochgeladene Datei ueberschreitet die in der Anweisung upload_max_filesize in php.ini festgelegte Groesse.
+                if($_FILES['Filedata']['error'][$x]==1)
+                {
+                    $gMessage->show($gL10n->get('PHO_PHOTO_FILES_TO_LARGE', admFuncMaxUploadSize()));
+                    $x = 5;
+                }
+            }
+        }
+    }
+    //Kontrolle ob Fotos ausgewaehlt wurden
+    if($counter==0)
+    {
+        $gMessage->show($gL10n->get('PHO_NO_FILES_SELECTED'));
+    }
+    // Fotos wurden erfolgreich hochgeladen -> Upload-Seite aus der Navi-Klasse entfernen
+    $_SESSION['navigation']->deleteLastUrl();
+}
+
+//bei Bedarf Uploadordner erzeugen
+if(file_exists(SERVER_PATH. '/adm_my_files/photos/upload') == false)
+{
+    require_once('../../system/classes/folder.php');
+    $folder = new Folder(SERVER_PATH. '/adm_my_files/photos');
+    $folder->createFolder('upload', true);
+}
 
 // Fotoalbums-Objekt erzeugen oder aus Session lesen
 if(isset($_SESSION['photo_album']) && $_SESSION['photo_album']->getValue('pho_id') == $getPhotoId)
@@ -63,19 +103,6 @@ else
 if($photo_album->getValue('pho_org_shortname') != $gCurrentOrganization->getValue('org_shortname'))
 {
     $gMessage->show($gL10n->get('SYS_INVALID_PAGE_VIEW'));
-}
-
-if(strlen($_FILES['userfile']['name'][0]) == 0 && $getUploadmethod == 1)
-{
-    $gMessage->show($gL10n->get('SYS_FIELD_EMPTY', $gL10n->get('PHO_PHOTO')));
-}
-
-//bei Bedarf Uploadodner erzeugen
-if(file_exists(SERVER_PATH. '/adm_my_files/photos/upload') == false)
-{
-    require_once('../../system/classes/folder.php');
-    $folder = new Folder(SERVER_PATH. '/adm_my_files/photos');
-    $folder->createFolder('upload', true);
 }
 
 //Ordnerpfad
@@ -97,56 +124,34 @@ if($getUploadmethod == 1)
         '.$gL10n->get('PHO_SHOWN_ON_READY').'<strong>('.$photo_album->getValue('pho_name').')</strong>
     </p>';
 }
-
-//Bei Klassischem upload erstmal testen ob Alle Dateien angekommen sind bei Flex reichen die Kontrollen in der Verarbeitung
-if(isset($_POST['upload']) && $getUploadmethod == 1)
-{
-    //zaehlen wieviele Fotos hochgeladen werden sollen und ob alle Uploads Fehlerfrei sind
-    $counter=0;
-    for($x=0; $x<=4; $x++)
-    {
-        //Datei wurde hochgeladen
-        if(isset($_FILES['userfile']['name'][$x]))
-        {
-            $counter++;
-
-            //Die hochgeladene Datei ueberschreitet die in der Anweisung upload_max_filesize in php.ini festgelegte Groesse.
-            if($_FILES['userfile']['error'][$x]==1)
-            {
-                $gMessage->show($gL10n->get('PHO_PHOTO_FILES_TO_LARGE', admFuncMaxUploadSize()));
-                $x = 5;
-            }
-        }
-    }
-    //Kontrolle ob Fotos ausgewaehlt wurden
-    if($counter==0)
-    {
-        $gMessage->show($gL10n->get('PHO_NO_FILES_SELECTED'));
-    }
-    // Fotos wurden erfolgreich hochgeladen -> Upload-Seite aus der Navi-Klasse entfernen
-    $_SESSION['navigation']->deleteLastUrl();
-}//Kontrollmechanismen
-
+
 //Bildverarbeitung
 $new_quantity = $photo_album->getValue('pho_quantity');
-for($act_upload_nr = 0; $act_upload_nr < 5; $act_upload_nr++)
+//Anzahl der Durchläufe
+$numLoops = 1;
+if($getUploadmethod == 1)
+{
+    $numLoops = 5;
+}
+
+for($act_upload_nr = 0; $act_upload_nr < $numLoops; $act_upload_nr++)
 {
     if($getUploadmethod == 1)
     {
-        $temp_filename = $_FILES['userfile']['tmp_name'][$act_upload_nr];
-        $filename = $_FILES['userfile']['name'][$act_upload_nr];
+        $temp_filename = $_FILES['Filedata']['tmp_name'][$act_upload_nr];
+        $filename = $_FILES['Filedata']['name'][$act_upload_nr];
     }
     else
     {
-        if(!is_uploaded_file($_FILES['userfile']['tmp_name']))
+        if(!is_uploaded_file($_FILES['Filedata']['tmp_name']))
         {
             echo $gL10n->get('SYS_UPLOAD_ERROR');
         }
-        $temp_filename = $_FILES['userfile']['tmp_name'];
-        $filename = $_FILES['userfile']['name'];
+        $temp_filename = $_FILES['Filedata']['tmp_name'];
+        $filename = $_FILES['Filedata']['name'];
     }
     //Datei wurde hochgeladen
-    if(isset($_FILES['userfile']['name']) && is_uploaded_file($temp_filename))
+    if(isset($_FILES['Filedata']['name']) && is_uploaded_file($temp_filename))
     {
         $new_quantity++;
     	
@@ -246,7 +251,6 @@ for($act_upload_nr = 0; $act_upload_nr < 5; $act_upload_nr++)
     	}
     }
 }
-
 
 //Bei Klassischem Upload Reste der Seitenausgabe
 if($getUploadmethod == 1)
