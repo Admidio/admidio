0ab14654295f1b7cbb9b2f5efe8a7be8945d2888
Markus Faßbender <markus.fassbender@gmail.com>
2012-01-01 17:32:21 +0000

    Umbenennung, da Probleme mit Groß-/Kleinschreibung

--- source/adm_program/system/constants.php
+++ source/adm_program/system/constants.php
@@ -15,7 +15,7 @@ if ('constants.php' == basename($_SERVER['SCRIPT_FILENAME']))
 
 // !!! Please do not edit these version numbers !!!
 define('ADMIDIO_VERSION', '2.3.0');
-define('BETA_VERSION', '1');
+define('BETA_VERSION', '2');
 define('MIN_PHP_VERSION', '5.2.0');
 
 if(BETA_VERSION > 0)
