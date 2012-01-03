5ed178b0bf9c7e761e7b8d01835aa61ec81a3a20
Markus Faßbender <markus.fassbender@gmail.com>
2012-01-03 18:05:53 +0000

    - Besseres abfangen von Systemvariablen, so dass bei späteren Änderungen keine Fehlermeldungen kommen können

--- index.php
+++ index.php
@@ -1,12 +1,32 @@
 <?php
-    // wenn noch nicht installiert, dann Install-Dialog anzeigen
-    if(file_exists('config.php'))
-    {
-        require_once('adm_program/system/common.php');
-        header('Location: '.$gHomepage);
-    }
-    else
-    {
-        header('Location: adm_install/index.php');
-    }
+/******************************************************************************
+ * Set the correct startpage for Admidio
+ *
+ * Copyright    : (c) 2004 - 2012 The Admidio Team
+ * Homepage     : http://www.admidio.org
+ * License      : GNU Public License 2 http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ *****************************************************************************/
+
+if(file_exists('config.php'))
+{
+	// if config file exists then show stored homepage
+	require_once('adm_program/system/common.php');
+
+	if(isset($gHomepage))
+	{
+		header('Location: '.$gHomepage);
+	}
+	else
+	{
+		// if parameter gHomepage doesn't exists then show default page
+		header('Location: adm_program/index.php');
+	}
+}
+else
+{
+	// config file doesn't exists then show installation wizard
+	header('Location: adm_install/index.php');
+}
+
 ?>
