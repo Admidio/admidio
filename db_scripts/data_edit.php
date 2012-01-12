cf6ff4d4885c0dba3c1c1231bbe5ac8fd3c8bb69
Markus Faßbender <markus.fassbender@gmail.com>
2012-01-12 21:22:39 +0000

    - Testdaten-Script nun komplett umgestellt

--- db_scripts/data_edit.php
+++ db_scripts/data_edit.php
@@ -7,6 +7,12 @@
  * License      : GNU Public License 2 http://www.gnu.org/licenses/gpl-2.0.html
  *
  *****************************************************************************/
+ 
+function addDaysToDate($days)
+{
+	$time = time() + (60*60*24*$days);
+	return date('Y', $time).'-'.date('m', $time).'-'.date('d', $time);
+}
 
 // set birthday of user to today 25 years ago
 $day   = date('d', time());
@@ -19,38 +25,91 @@ $sql = 'UPDATE '.TBL_USER_DATA.' SET usd_value = \''.$year.'-'.$month.'-'.$day.'
 $db->query($sql);
 
 // set name of role to 4 days in future
-$time = time() + (60*60*24*4);
-$sql = 'UPDATE '.TBL_ROLES.' SET rol_name = \'Termin '.date('Y', $time).'-'.date('m', $time).'-'.date('d', $time).' 19:00 - 4\'
+$sql = 'UPDATE '.TBL_ROLES.' SET rol_name = \'Termin '.addDaysToDate(4).' 19:00 - 4\'
 		 WHERE rol_id = 8 ';
 $db->query($sql);
 
 // set membership of date role
-$time = time() - (60*60*24*4);
-$sql = 'UPDATE '.TBL_MEMBERS.' SET mem_begin = \''.date('Y', $time).'-'.date('m', $time).'-'.date('d', $time).'\'
+$sql = 'UPDATE '.TBL_MEMBERS.' SET mem_begin = \''.addDaysToDate(4).'\'
 		 WHERE mem_id = 500 ';
 $db->query($sql);
 
-$time = time() - (60*60*24*2);
-$sql = 'UPDATE '.TBL_MEMBERS.' SET mem_begin = \''.date('Y', $time).'-'.date('m', $time).'-'.date('d', $time).'\'
+$sql = 'UPDATE '.TBL_MEMBERS.' SET mem_begin = \''.addDaysToDate(2).'\'
 		 WHERE mem_id IN (501, 502, 503) ';
 $db->query($sql);
 
-$time = time() - (60*60*24*1);
-$sql = 'UPDATE '.TBL_MEMBERS.' SET mem_begin = \''.date('Y', $time).'-'.date('m', $time).'-'.date('d', $time).'\'
+$sql = 'UPDATE '.TBL_MEMBERS.' SET mem_begin = \''.addDaysToDate(1).'\'
 		 WHERE mem_id = 504 ';
 $db->query($sql);
 
 // set date of announcements
-$time = time() - (60*60*24*7);
-$sql = 'UPDATE '.TBL_ANNOUNCEMENTS.' SET ann_timestamp_create = \''.date('Y', $time).'-'.date('m', $time).'-'.date('d', $time).'\'
+$sql = 'UPDATE '.TBL_ANNOUNCEMENTS.' SET ann_timestamp_create = \''.addDaysToDate(7).'\'
 		 WHERE ann_id = 1 ';
 $db->query($sql);
 
-$time = time() - (60*60*24*3);
-$time2 = time() - (60*60*24*2);
-$sql = 'UPDATE '.TBL_ANNOUNCEMENTS.' SET ann_timestamp_create = \''.date('Y', $time).'-'.date('m', $time).'-'.date('d', $time).'\'
-                                       , ann_timestamp_change = \''.date('Y', $time2).'-'.date('m', $time2).'-'.date('d', $time2).'\'
+$sql = 'UPDATE '.TBL_ANNOUNCEMENTS.' SET ann_timestamp_create = \''.addDaysToDate(3).'\'
+                                       , ann_timestamp_change = \''.addDaysToDate(2).'\'
 		 WHERE ann_id = 2 ';
 $db->query($sql);
+
+// set dates of events
+$sql = 'UPDATE '.TBL_DATES.' SET dat_begin = \''.addDaysToDate(14).'\'
+                               , dat_end = \''.addDaysToDate(14).'\'
+		 WHERE dat_id = 3 ';
+$db->query($sql);
+
+$sql = 'UPDATE '.TBL_DATES.' SET dat_begin = \''.addDaysToDate(4).'\'
+                               , dat_end = \''.addDaysToDate(4).'\'
+		 WHERE dat_id = 4 ';
+$db->query($sql);
+
+$sql = 'UPDATE '.TBL_DATES.' SET dat_begin = \''.addDaysToDate(30).'\'
+                               , dat_end = \''.addDaysToDate(35).'\'
+		 WHERE dat_id = 5 ';
+$db->query($sql);
+
+$sql = 'UPDATE '.TBL_DATES.' SET dat_begin = \''.addDaysToDate(10).'\'
+                               , dat_end = \''.addDaysToDate(10).'\'
+		 WHERE dat_id = 6 ';
+$db->query($sql);
+
+$sql = 'UPDATE '.TBL_DATES.' SET dat_begin = \''.addDaysToDate(-1).'\'
+                               , dat_end = \''.addDaysToDate(0).'\'
+		 WHERE dat_id = 7 ';
+$db->query($sql);
+
+
+$sql = 'UPDATE '.TBL_FOLDERS.' SET fol_timestamp = \''.addDaysToDate(-7).'\'
+		 WHERE fol_id = 1 ';
+$db->query($sql);
+
+
+$sql = 'UPDATE '.TBL_GUESTBOOK.' SET gbo_timestamp_create = \''.addDaysToDate(-14).'\'
+		 WHERE gbo_id = 1 ';
+$db->query($sql);
+
+$sql = 'UPDATE '.TBL_GUESTBOOK.' SET gbo_timestamp_create = \''.addDaysToDate(-5).'\'
+		 WHERE gbo_id = 2 ';
+$db->query($sql);
+
+
+$sql = 'UPDATE '.TBL_GUESTBOOK_COMMENTS.' SET gbc_timestamp_create = \''.addDaysToDate(-4).'\'
+		 WHERE gbc_id = 1 ';
+$db->query($sql);
+
+
+$sql = 'UPDATE '.TBL_LINKS.' SET lnk_timestamp_create = \''.addDaysToDate(-4).'\'
+                               , lnk_timestamp_change = \''.addDaysToDate(-3).'\'
+		 WHERE lnk_id = 1 ';
+$db->query($sql);
+
+$sql = 'UPDATE '.TBL_LINKS.' SET lnk_timestamp_create = \''.addDaysToDate(-4).'\'
+		 WHERE lnk_id = 2 ';
+$db->query($sql);
+
+$sql = 'UPDATE '.TBL_LINKS.' SET lnk_timestamp_create = \''.addDaysToDate(-4).'\'
+		 WHERE lnk_id = 3 ';
+$db->query($sql);
+
 
 ?>
