87d9d2d4ee87e1c1ed88ff1953da8feea5334472
Thomas Lange <thomas.rcv@googlemail.com>
2012-09-07 17:26:23 +0000

    BUG[3565629] If passed parameter are set to true/false the function admFuncVariableIsValid doesn´t fit to datatype 'boolean' and returns "invalid page view"

--- source/adm_program/system/function.php
+++ source/adm_program/system/function.php
@@ -351,7 +351,8 @@ function admFuncVariableIsValid($array, $variableName, $datatype, $defaultValue 
 		{
 			// boolean type must be 0 or 1 otherwise throw error
 			// do not check with in_array because this function don't work properly
-			if($array[$variableName] != '0' && $array[$variableName] != '1')
+			if($array[$variableName] != '0' && $array[$variableName] != '1'
+                && $array[$variableName] != false && $array[$variableName] != true)
 			{
                 $errorMessage = $gL10n->get('SYS_INVALID_PAGE_VIEW');
 			}
