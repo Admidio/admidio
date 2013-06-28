8167120b2bc14efe62cd7138ce927bb6bb9adee6
Markus Faßbender <markus.fassbender@gmail.com>
2013-06-28 13:50:15 +0000

    BUG#740 Can't save IPv6 addresses in database BUG#741 Result type from fetch_array not compatible with PostgreSQL

--- source/adm_program/system/classes/table_access.php
+++ source/adm_program/system/classes/table_access.php
@@ -284,7 +284,7 @@ class TableAccess
     
 			if($this->db->num_rows($result) == 1)			
 			{
-				$row = $this->db->fetch_array($result, MYSQL_ASSOC);
+				$row = $this->db->fetch_array($result, 'ASSOC');
 				$this->new_record = false;
 				
 				// Daten in das Klassenarray schieben
