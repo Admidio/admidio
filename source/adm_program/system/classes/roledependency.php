473a7841e931c5b172299fbd54a6b733a2784477
Markus Faßbender <markus.fassbender@gmail.com>
2013-10-15 15:45:58 +0000

    BUG#749	Role dependencies could not be assigned with PostgreSql BUG#748	Several role dependencies of one role coult assign one member several times to that role

--- source/adm_program/system/classes/roledependency.php
+++ source/adm_program/system/classes/roledependency.php
@@ -1,6 +1,6 @@
 <?php
 /******************************************************************************
- * Klasse fuer Datenbanktabelle adm_role_dependencies
+ * Class to manage dependencies between different roles.
  *
  * Copyright    : (c) 2004 - 2013 The Admidio Team
  * Homepage     : http://www.admidio.org
@@ -12,14 +12,14 @@ class RoleDependency
 {
     var $db;
 
-    var $role_id_parent;
-    var $role_id_child;
+    var $roleIdParent;
+    var $roleIdChild;
     var $comment;
     var $usr_id;
     var $timestamp;
 
-    var $role_id_parent_orig;
-    var $role_id_child_orig;
+    var $roleIdParentOrig;
+    var $roleIdChildOrig;
 
     var $persisted;
 
@@ -28,6 +28,32 @@ class RoleDependency
     public function __construct(&$db)
     {
         $this->db =& $db;
+        $this->clear();
+    }
+
+    // alle Klassenvariablen wieder zuruecksetzen
+    public function clear()
+    {
+        $this->roleIdParent      = 0;
+        $this->roleIdChild       = 0;
+        $this->comment             = '';
+        $this->usr_id              = 0;
+        $this->timestamp           = '';
+
+        $this->roleIdParentOrig = 0;
+        $this->roleIdChildOrig  = 0;
+
+        $persisted = false;
+    }
+
+    // aktuelle Rollenabhaengigkeit loeschen
+    public function delete()
+    {
+        $sql    = 'DELETE FROM '. TBL_ROLE_DEPENDENCIES.
+                  ' WHERE rld_rol_id_child  = '.$this->roleIdChildOrig.
+                    ' AND rld_rol_id_parent = '.$this->roleIdParentOrig;
+        $this->db->query($sql);
+
         $this->clear();
     }
 
@@ -47,14 +73,14 @@ class RoleDependency
 
             if($row = $this->db->fetch_object())
             {
-                $this->role_id_parent      = $row->rld_rol_id_parent;
-                $this->role_id_child       = $row->rld_rol_id_child;
+                $this->roleIdParent      = $row->rld_rol_id_parent;
+                $this->roleIdChild       = $row->rld_rol_id_child;
                 $this->comment             = $row->rld_comment;
                 $this->timestamp           = $row->rld_timestamp;
                 $this->usr_id              = $row->rld_usr_id;
 
-                $this->role_id_parent_orig = $row->rld_rol_id_parent;
-                $this->role_id_child_orig  = $row->rld_rol_id_child;
+                $this->roleIdParentOrig = $row->rld_rol_id_parent;
+                $this->roleIdChildOrig  = $row->rld_rol_id_child;
             }
             else
             {
@@ -66,101 +92,7 @@ class RoleDependency
             $this->clear();
         }
     }
-
-    // alle Klassenvariablen wieder zuruecksetzen
-    public function clear()
-    {
-        $this->role_id_parent      = 0;
-        $this->role_id_child       = 0;
-        $this->comment             = '';
-        $this->usr_id              = 0;
-        $this->timestamp           = '';
-
-        $this->role_id_parent_orig = 0;
-        $this->role_id_child_orig  = 0;
-
-        $persisted = false;
-    }
-
-    // Es muss die ID des eingeloggten Users uebergeben werden,
-    // damit die Aenderung protokolliert werden kann
-    public function update($login_user_id)
-    {
-        if(!isEmpty() && $login_user_id > 0 && is_numeric($login_user_id))
-        {
-            $sql = 'UPDATE '. TBL_ROLE_DEPENDENCIES. ' SET rld_rol_id_parent = \''.$this->role_id_parent.'\'
-                                                         , rld_rol_id_child  = \''.$this->role_id_child.'\'
-                                                         , rld_comment       = \''.$this->comment.'\'
-                                                         , rld_timestamp     = \''.DATETIME_NOW.'\'
-                                                         , rld_usr_id        = '.$login_user_id.'
-                     WHERE rld_rol_id_parent = '.$this->role_id_parent_orig.'
-                       AND rld_rol_id_child  = '.$this->role_id_child_orig;
-            $this->db->query($sql);
-            $persisted = true;
-            return 0;
-        }
-        return -1;
-    }
-
-    public function insert($login_user_id)
-    {
-        if(!$this->isEmpty() && $login_user_id > 0 && is_numeric($login_user_id))
-        {
-            $sql = 'INSERT INTO '. TBL_ROLE_DEPENDENCIES. ' 
-                                (rld_rol_id_parent,rld_rol_id_child,rld_comment,rld_usr_id,rld_timestamp)
-                         VALUES ('.$this->role_id_parent.', '.$this->role_id_child.', \''.$this->comment.'\', '.$login_user_id.', \''.DATETIME_NOW.'\') ';
-            $this->db->query($sql);
-            $persisted = true;
-            return 0;
-        }
-        return -1;
-    }
-
-    public function isEmpty()
-    {
-        if ($this->role_id_parent == 0 && $this->role_id_child == 0)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-
-    // aktuelle Rollenabhaengigkeit loeschen
-    public function delete()
-    {
-        $sql    = 'DELETE FROM '. TBL_ROLE_DEPENDENCIES.
-                  ' WHERE rld_rol_id_child  = '.$this->role_id_child_orig.
-                    ' AND rld_rol_id_parent = '.$this->role_id_parent_orig;
-        $this->db->query($sql);
-
-        $this->clear();
-    }
-
-    static public function getParentRoles(&$db, $childId)
-    {
-        $allParentIds = array();
-
-        if($childId > 0 && is_numeric($childId))
-        {
-            $sql = 'SELECT rld_rol_id_parent FROM '. TBL_ROLE_DEPENDENCIES.
-                   ' WHERE rld_rol_id_child = '.$childId;
-            $db->query($sql);
-
-            $num_rows = $db->num_rows();
-            if ($num_rows)
-            {
-                while ($row = $db->fetch_object())
-                {
-                    $allParentIds[] = $row->rld_rol_id_parent;
-                }
-            }
-        }
-        return $allParentIds;
-    }
-
+    
     static public function getChildRoles(&$db, $parentId)
     {
         $allChildIds = array();
@@ -182,56 +114,53 @@ class RoleDependency
         }
         return $allChildIds;
     }
-
-    public function setParent($parentId)
+ 
+    static public function getParentRoles(&$db, $childId)
     {
-        if($parentId > 0 && is_numeric($parentId))
-        {
-            $this->role_id_parent = $parentId;
-            $persisted = false;
-            return 0;
-        }
-        return -1;
-    }
+        $allParentIds = array();
 
-    public function setChild($childId)
-    {
         if($childId > 0 && is_numeric($childId))
         {
-            $this->role_id_child = $childId;
-            $persisted = false;
+            $sql = 'SELECT rld_rol_id_parent FROM '. TBL_ROLE_DEPENDENCIES.
+                   ' WHERE rld_rol_id_child = '.$childId;
+            $db->query($sql);
+
+            $num_rows = $db->num_rows();
+            if ($num_rows)
+            {
+                while ($row = $db->fetch_object())
+                {
+                    $allParentIds[] = $row->rld_rol_id_parent;
+                }
+            }
+        }
+        return $allParentIds;
+    }
+ 
+    public function insert($login_user_id)
+    {
+        if(!$this->isEmpty() && $login_user_id > 0 && is_numeric($login_user_id))
+        {
+            $sql = 'INSERT INTO '. TBL_ROLE_DEPENDENCIES. ' 
+                                (rld_rol_id_parent,rld_rol_id_child,rld_comment,rld_usr_id,rld_timestamp)
+                         VALUES ('.$this->roleIdParent.', '.$this->roleIdChild.', \''.$this->comment.'\', '.$login_user_id.', \''.DATETIME_NOW.'\') ';
+            $this->db->query($sql);
+            $persisted = true;
             return 0;
         }
         return -1;
     }
 
-    public function updateMembership()
+    public function isEmpty()
     {
-        if(0 != $this->role_id_parent and 0 != $this->role_id_child )
+        if ($this->roleIdParent == 0 && $this->roleIdChild == 0)
         {
-            $sql = 'SELECT mem_usr_id FROM '. TBL_MEMBERS.
-                   ' WHERE mem_rol_id = '.$this->role_id_child.'
-                       AND mem_begin <= \''.DATE_NOW.'\'
-                       AND mem_end    > \''.DATE_NOW.'\'';
-            $result = $this->db->query($sql);
-
-            $num_rows = $this->db->num_rows($result);
-            if ($num_rows)
-            {
-                $sql = 'INSERT IGNORE INTO '. TBL_MEMBERS. ' (mem_rol_id, mem_usr_id, mem_begin, mem_end, mem_leader) VALUES ';
-
-                while ($row = $this->db->fetch_object($result))
-                {
-                    $sql .= '('.$this->role_id_parent.', '.$row->mem_usr_id.', \''.DATE_NOW.'\', \'9999-12-31\', 0),';
-                }
-                //Das letzte Komma wieder wegschneiden
-                $sql = substr($sql,0,-1);
-                
-                $this->db->query($sql);
-            }
+            return 1;
+        }
+        else
+        {
             return 0;
         }
-        return -1;
     }
 
     static public function removeChildRoles(&$db, $parentId)
@@ -249,5 +178,76 @@ class RoleDependency
         return -1;
     }
 
+    public function setParent($parentId)
+    {
+        if($parentId > 0 && is_numeric($parentId))
+        {
+            $this->roleIdParent = $parentId;
+            $persisted = false;
+            return 0;
+        }
+        return -1;
+    }
+
+    public function setChild($childId)
+    {
+        if($childId > 0 && is_numeric($childId))
+        {
+            $this->roleIdChild = $childId;
+            $persisted = false;
+            return 0;
+        }
+        return -1;
+    }
+    
+    // Es muss die ID des eingeloggten Users uebergeben werden,
+    // damit die Aenderung protokolliert werden kann
+    public function update($login_user_id)
+    {
+        if(!isEmpty() && $login_user_id > 0 && is_numeric($login_user_id))
+        {
+            $sql = 'UPDATE '. TBL_ROLE_DEPENDENCIES. ' SET rld_rol_id_parent = \''.$this->roleIdParent.'\'
+                                                         , rld_rol_id_child  = \''.$this->roleIdChild.'\'
+                                                         , rld_comment       = \''.$this->comment.'\'
+                                                         , rld_timestamp     = \''.DATETIME_NOW.'\'
+                                                         , rld_usr_id        = '.$login_user_id.'
+                     WHERE rld_rol_id_parent = '.$this->roleIdParentOrig.'
+                       AND rld_rol_id_child  = '.$this->roleIdChildOrig;
+            $this->db->query($sql);
+            $persisted = true;
+            return 0;
+        }
+        return -1;
+    }
+
+    /* Adds all active memberships of the child role to the parent role.
+     * If a membership still exists than start date will not be changed. Only
+     * the end date will be set to 31.12.9999.
+     * @return Returns -1 if no parent or child row exists
+     */
+    public function updateMembership()
+    {
+        if($this->roleIdParent != 0 and $this->roleIdChild != 0)
+        {
+            $sql = 'SELECT mem_usr_id FROM '. TBL_MEMBERS.
+                   ' WHERE mem_rol_id = '.$this->roleIdChild.'
+                       AND mem_begin <= \''.DATE_NOW.'\'
+                       AND mem_end    > \''.DATE_NOW.'\'';
+            $result = $this->db->query($sql);
+
+            $num_rows = $this->db->num_rows($result);
+            if ($num_rows)
+            {
+                $member = new TableMembers($this->db);
+
+                while ($row = $this->db->fetch_object($result))
+                {
+                    $member->startMembership($this->roleIdParent, $row->mem_usr_id);
+                }
+            }
+            return 0;
+        }
+        return -1;
+    }
 }
 ?>
