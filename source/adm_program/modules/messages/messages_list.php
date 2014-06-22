07563f83396b0efd3b693d8d62ef23ea18d8c43a
Stefan Weiß <stefan.weiss@uberg.at>
2014-06-22 17:33:11 +0000

    new idea for message history and send of new messages (email and PM)

--- source/adm_program/modules/messages/messages_list.php
+++ source/adm_program/modules/messages/messages_list.php
@@ -26,7 +26,7 @@ if (!$gValidLogin)
     $gMessage->show($gL10n->get('SYS_INVALID_PAGE_VIEW'));
 }
 
-//SQL to find all unread messages
+//SQL to find all unread PM messages
 $sql = "SELECT msg_type, msg_id1, msg_subject, msg_timestamp,
 			CASE WHEN msg_usrid1 = ". $gCurrentUser->getValue('usr_id') ." THEN msg_usrid2
 			ELSE msg_usrid1 
@@ -38,7 +38,7 @@ $sql = "SELECT msg_type, msg_id1, msg_subject, msg_timestamp,
 
 $result = $gDb->query($sql);
 
-//SQL to find all read messages
+//SQL to find all read PM messages
 $sql = "SELECT msg_type, msg_id1, msg_subject, msg_timestamp,
 			CASE WHEN msg_usrid1 = ". $gCurrentUser->getValue('usr_id') ." THEN msg_usrid2
 			ELSE msg_usrid1 
@@ -50,20 +50,87 @@ $sql = "SELECT msg_type, msg_id1, msg_subject, msg_timestamp,
 
 $result1 = $gDb->query($sql);
 
-$headline = $gL10n->get('PMS_MESSAGE');
+//SQL to find all own Email messages
+$sql = "SELECT msg_type, msg_id1, msg_subject, msg_timestamp,
+			CASE WHEN msg_usrid1 = ". $gCurrentUser->getValue('usr_id') ." THEN msg_usrid2
+			ELSE msg_usrid1 
+			END AS user
+        FROM ". TBL_MESSAGES. "
+         WHERE msg_type = 'EMAIL' and (msg_usrid1 = ". $gCurrentUser->getValue('usr_id') ."
+		 or msg_usrid2 = ". $gCurrentUser->getValue('usr_id') ." )
+		 ORDER BY msg_id1 DESC";
+
+$resultMail = $gDb->query($sql);
+
+$headline = 'Messages';
 
 // add current url to navigation stack
 $gNavigation->clear();
 $gNavigation->addUrl(CURRENT_URL, $headline);
 
+// create module menu for emails
+$EmailMenu = new ModuleMenu('admMenuEmail');
+$EmailMenu->addItem('admMenuItemNewEmail', $g_root_path.'/adm_program/modules/messages/messages.php', $gL10n->get('SYS_EMAIL'), '/email.png');
+
+// create module menu for PMs
+$PmMenu = new ModuleMenu('admMenuPm');
+$PmMenu->addItem('admMenuItemNewPm', $g_root_path.'/adm_program/modules/messages/messages.php', $gL10n->get('PMS_MESSAGE'), '/email.png');
+
 // create html page object
 $page = new HtmlPage();
+
+// add some js for page
+//$page->addJavascript(" $('#myTab a').click(function (e) {  e.preventDefault()  $(this).tab('show')}) ", true);
 
 // show headline for Table
 $page->addHeadline($headline);
 
+// make tabs for the page
+$page->addHtml('
+        <ul id="myTab" class="nav nav-tabs" data-tabs="tabs">
+		    <li class="active"><a href="#tabemail" data-toggle="tab">'.$gL10n->get('MAI_SEND_EMAIL').'</a></li>
+            <li><a href="#tabpmsystem" data-toggle="tab">'.$gL10n->get('PMS_MESSAGE').'</a></li>
+        </ul>');
+
+$page->addHtml('<div id="myTabContent" class="tab-content">');
+// part for first tab of the page
+$page->addHtml('<div class="tab-pane active" id="tabemail">');
+
+$page->addHtml($EmailMenu->show(false));
+$page->addHtml('<div class="admBoxHead">list sent emails</div><br>');
+
+$table = new HtmlTable('adm_lists_email_table', $page, true, true);
+
+$table->addAttribute('border', '0');
+$table->addTableHeader();
+
 $table = new HtmlTable('adm_lists_table', $page, true, true);
+
 $table->addRowHeadingByArray(array($gL10n->get('MAI_SUBJECT'), $gL10n->get('SYS_SENDER'), $gL10n->get('SYS_DATE')));
+
+if(isset($result2))
+{
+    while ($row = $gDb->fetch_array($result2)) {
+        $user = new User($gDb, $gProfileFields, $row['user']);
+
+		$table->addRowByArray(array($row['msg_subject'], $user->getValue('FIRST_NAME').' '.$user->getValue('LAST_NAME'), $row['msg_timestamp']), null, array('style' => 'cursor: pointer', 'onclick' => 'window.location.href=\''. $g_root_path. '/adm_program/modules/messages/messages.php?msg_type='.$row['msg_type'].'&usr_id='.$row['user'].'&subject='.addslashes($row['msg_subject']).'&msg_id='. $row['msg_id1']. '\''));
+   }
+}
+$page->addHtml($table->show(false));
+
+$page->addHtml('</div>');
+
+// part for 2 tab of the page
+$page->addHtml('<div class="tab-pane" id="tabpmsystem">');
+$page->addHtml($PmMenu->show(false));
+$page->addHtml('<div class="admBoxHead">list PM History</div><br>');
+
+$table = new HtmlTable('adm_lists_pm_table', $page, true, true);
+
+$table->addAttribute('border', '0');
+$table->addTableHeader();
+$table->addRowHeadingByArray(array($gL10n->get('MAI_SUBJECT'), $gL10n->get('SYS_SENDER'), $gL10n->get('SYS_DATE')));
+$table->addTableBody();
 
 if(isset($result))
 {
@@ -88,6 +155,15 @@ $table->setDatatablesOrderColumns(array(array(3, 'asc')));
 
 // add table to the form
 $page->addHtml($table->show(false));
+
+$page->addHtml("<script>
+   $(function () {
+      $('#myTab li:eq(1) a').tab('show');
+   });
+</script>");
+
+// colse last tab and tab config
+$page->addHtml('</div></div>');
 
 // add form to html page and show page
 $page->show();
