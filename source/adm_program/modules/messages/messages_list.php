c6f4b47d37e0f715bf73c7179775adcba3ccde17
Stefan Weiß <stefan.weiss@uberg.at>
2014-06-25 21:48:26 +0000

    new layout for message_list with all information in one table! I add also a new icon, "email_answer" to indicate if it is possible to answer to message or not.

--- source/adm_program/modules/messages/messages_list.php
+++ source/adm_program/modules/messages/messages_list.php
@@ -62,7 +62,7 @@ $sql = "SELECT msg_type, msg_id1, msg_subject, msg_timestamp,
 
 $resultMail = $gDb->query($sql);
 
-$headline = 'Messages';
+$headline = 'Message History';
 
 // add current url to navigation stack
 $gNavigation->clear();
@@ -71,33 +71,15 @@ $gNavigation->addUrl(CURRENT_URL, $headline);
 // create module menu for emails
 $EmailMenu = new ModuleMenu('admMenuEmail');
 $EmailMenu->addItem('admMenuItemNewEmail', $g_root_path.'/adm_program/modules/messages/messages.php', $gL10n->get('SYS_EMAIL'), '/email.png');
-
-// create module menu for PMs
-$PmMenu = new ModuleMenu('admMenuPm');
-$PmMenu->addItem('admMenuItemNewPm', $g_root_path.'/adm_program/modules/messages/messages.php', $gL10n->get('PMS_MESSAGE'), '/email.png');
+$EmailMenu->addItem('admMenuItemNewPm', $g_root_path.'/adm_program/modules/messages/messages.php', $gL10n->get('PMS_MESSAGE'), '/email.png');
 
 // create html page object
 $page = new HtmlPage();
 
-// add some js for page
-//$page->addJavascript(" $('#myTab a').click(function (e) {  e.preventDefault()  $(this).tab('show')}) ", true);
-
 // show headline for Table
 $page->addHeadline($headline);
 
-// make tabs for the page
-$page->addHtml('
-        <ul id="myTab" class="nav nav-tabs" data-tabs="tabs">
-		    <li class="active"><a href="#tabemail" data-toggle="tab">'.$gL10n->get('MAI_SEND_EMAIL').'</a></li>
-            <li><a href="#tabpmsystem" data-toggle="tab">'.$gL10n->get('PMS_MESSAGE').'</a></li>
-        </ul>');
-
-$page->addHtml('<div id="myTabContent" class="tab-content">');
-// part for first tab of the page
-$page->addHtml('<div class="tab-pane active" id="tabemail">');
-
 $page->addHtml($EmailMenu->show(false));
-$page->addHtml('<div class="admBoxHead">list sent emails</div><br>');
 
 $table = new HtmlTable('adm_lists_email_table', $page, true, true);
 
@@ -106,38 +88,23 @@ $table->addTableHeader();
 
 $table = new HtmlTable('adm_lists_table', $page, true, true);
 
-$table->addRowHeadingByArray(array($gL10n->get('MAI_SUBJECT'), $gL10n->get('SYS_SENDER'), $gL10n->get('SYS_DATE')));
+$table->addRowHeadingByArray(array( 'Typ' ,$gL10n->get('MAI_SUBJECT'), $gL10n->get('SYS_SENDER'), $gL10n->get('SYS_DATE')));
 
-if(isset($result2))
+if(isset($resultMail))
 {
-    while ($row = $gDb->fetch_array($result2)) {
+    while ($row = $gDb->fetch_array($resultMail)) {
         $user = new User($gDb, $gProfileFields, $row['user']);
 
-		$table->addRowByArray(array($row['msg_subject'], $user->getValue('FIRST_NAME').' '.$user->getValue('LAST_NAME'), $row['msg_timestamp']), null, array('style' => 'cursor: pointer', 'onclick' => 'window.location.href=\''. $g_root_path. '/adm_program/modules/messages/messages.php?msg_type='.$row['msg_type'].'&usr_id='.$row['user'].'&subject='.addslashes($row['msg_subject']).'&msg_id='. $row['msg_id1']. '\''));
+		$table->addRowByArray(array( '<img class="iconInformation" src="'. THEME_PATH. '/icons/email.png" alt="'.$gL10n->get('SYS_EMAIL').'" title="'.$gL10n->get('SYS_EMAIL').'" />' ,$row['msg_subject'], $user->getValue('FIRST_NAME').' '.$user->getValue('LAST_NAME'), $row['msg_timestamp']), null, array('style' => 'cursor: pointer'));
    }
 }
-$page->addHtml($table->show(false));
-
-$page->addHtml('</div>');
-
-// part for 2 tab of the page
-$page->addHtml('<div class="tab-pane" id="tabpmsystem">');
-$page->addHtml($PmMenu->show(false));
-$page->addHtml('<div class="admBoxHead">list PM History</div><br>');
-
-$table = new HtmlTable('adm_lists_pm_table', $page, true, true);
-
-$table->addAttribute('border', '0');
-$table->addTableHeader();
-$table->addRowHeadingByArray(array($gL10n->get('MAI_SUBJECT'), $gL10n->get('SYS_SENDER'), $gL10n->get('SYS_DATE')));
-$table->addTableBody();
 
 if(isset($result))
 {
     while ($row = $gDb->fetch_array($result)) {
         $user = new User($gDb, $gProfileFields, $row['user']);
 
-		$table->addRowByArray(array($row['msg_subject'], $user->getValue('FIRST_NAME').' '.$user->getValue('LAST_NAME'), $row['msg_timestamp']), null, array('style' => 'cursor: pointer', 'onclick' => 'window.location.href=\''. $g_root_path. '/adm_program/modules/messages/messages.php?msg_type='.$row['msg_type'].'&usr_id='.$row['user'].'&subject='.addslashes($row['msg_subject']).'&msg_id='. $row['msg_id1']. '\''));
+		$table->addRowByArray(array('<img class="iconInformation" src="'. THEME_PATH. '/icons/email_answer.png" alt="'.$gL10n->get('PMS_MESSAGE').'" title="'.$gL10n->get('PMS_MESSAGE').'" />' ,$row['msg_subject'], $user->getValue('FIRST_NAME').' '.$user->getValue('LAST_NAME'), $row['msg_timestamp']), null, array('style' => 'cursor: pointer', 'onclick' => 'window.location.href=\''. $g_root_path. '/adm_program/modules/messages/messages.php?msg_type='.$row['msg_type'].'&usr_id='.$row['user'].'&subject='.addslashes($row['msg_subject']).'&msg_id='. $row['msg_id1']. '\''));
    }
 }
 
@@ -146,7 +113,7 @@ if(isset($result1))
 	while ($row = $gDb->fetch_array($result1)) {
         $user = new User($gDb, $gProfileFields, $row['user']);
 		
-		$table->addRowByArray(array($row['msg_subject'], $user->getValue('FIRST_NAME').' '.$user->getValue('LAST_NAME'), $row['msg_timestamp']), null, array('style' => 'cursor: pointer', 'onclick' => 'window.location.href=\''. $g_root_path. '/adm_program/modules/messages/messages.php?msg_type='.$row['msg_type'].'&usr_id='.$row['user'].'&subject='.addslashes($row['msg_subject']).'&msg_id='. $row['msg_id1']. '\''));
+		$table->addRowByArray(array('<img class="iconInformation" src="'. THEME_PATH. '/icons/email_answer.png" alt="'.$gL10n->get('PMS_MESSAGE').'" title="'.$gL10n->get('PMS_MESSAGE').'" />' ,$row['msg_subject'], $user->getValue('FIRST_NAME').' '.$user->getValue('LAST_NAME'), $row['msg_timestamp']), null, array('style' => 'cursor: pointer', 'onclick' => 'window.location.href=\''. $g_root_path. '/adm_program/modules/messages/messages.php?msg_type='.$row['msg_type'].'&usr_id='.$row['user'].'&subject='.addslashes($row['msg_subject']).'&msg_id='. $row['msg_id1']. '\''));
     }
 }
 
@@ -155,15 +122,6 @@ $table->setDatatablesOrderColumns(array(array(3, 'asc')));
 
 // add table to the form
 $page->addHtml($table->show(false));
-
-$page->addHtml("<script>
-   $(function () {
-      $('#myTab li:eq(1) a').tab('show');
-   });
-</script>");
-
-// colse last tab and tab config
-$page->addHtml('</div></div>');
 
 // add form to html page and show page
 $page->show();
