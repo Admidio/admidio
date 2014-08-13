c686438f12f1e97438a268896544099bfafd61bc
Stefan Weiß <stefan.weiss@uberg.at>
2014-08-13 09:25:21 +0000

    correction after information about wrong last update

--- source/adm_program/administration/organization/organization_function.php
+++ source/adm_program/administration/organization/organization_function.php
@@ -213,16 +213,12 @@ case 1:
             }
         }
     }
-
+	
     // alle Daten nun speichern
-    $ret_code = $gCurrentOrganization->save();
-    if($ret_code = 0)
-    {
-        $gMessage->show($gL10n->get('SYS_ERROR_DATABASE_ACCESS', $ret_code));
-    }
-
-    $gCurrentOrganization->setPreferences($gPreferences);
-
+	$gCurrentOrganization->save();
+	
+	$gCurrentOrganization->setPreferences($gPreferences);
+	
     // refresh language if neccessary
     if($gL10n->getLanguage() != $gPreferences['system_language'])
     {
