8cfb474397fb6d0b9da83a49d0cccde1e88d5eb2
Markus Faßbender <markus.fassbender@gmail.com>
2014-11-28 19:11:32 +0000

    BUG#792 Incorrect Members assignment to the Organization for event participation

--- adm_program/modules/lists/members_get.php
+++ adm_program/modules/lists/members_get.php
@@ -60,6 +60,7 @@ else
             AND mem_end    > \''.DATE_NOW.'\'
             AND rol_valid  = 1
             AND rol_cat_id = cat_id
+            AND cat_name_intern <> \'CONFIRMATION_OF_PARTICIPATION\'
             AND (  cat_org_id = '. $gCurrentOrganization->getValue('org_id'). '
                 OR cat_org_id IS NULL )) ';
 }
@@ -92,6 +93,7 @@ $sql = 'SELECT DISTINCT usr_id, last_name.usd_value as last_name, first_name.usd
                      FROM '. TBL_ROLES. ' rol2, '. TBL_CATEGORIES. ' cat2, '. TBL_MEMBERS. ' mem2
                     WHERE rol2.rol_valid   = 1
                       AND rol2.rol_cat_id  = cat2.cat_id
+                      AND cat2.cat_name_intern <> \'CONFIRMATION_OF_PARTICIPATION\'
                       AND (  cat2.cat_org_id = '. $gCurrentOrganization->getValue('org_id'). '
                           OR cat2.cat_org_id IS NULL )
                       AND mem2.mem_rol_id  = rol2.rol_id
