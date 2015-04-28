f51a978a7f5924e1be8f2a6da1b6c46d9819741e
sirnone <stefan.weiss@uberg.at>
2015-04-28 23:48:16 +0200

    syntax error, unexpected ';'
    message from user in forum: Fehler Import von ISO-88.csv

--- adm_program/modules/members/import_function.php
+++ adm_program/modules/members/import_function.php
@@ -12,7 +12,7 @@ require_once('../../system/common.php');
 require_once('../../system/login_valid.php');
 
 // Initialize and check the parameters
-$postImportCoding   = admFuncVariableIsValid($_POST, 'import_coding', 'string', array('requireValue' => true, 'validValues' => array('iso-8859-1', 'utf-8'));
+$postImportCoding   = admFuncVariableIsValid($_POST, 'import_coding', 'string', array('requireValue' => true, 'validValues' => array('iso-8859-1', 'utf-8')));
 $postRoleId         = admFuncVariableIsValid($_POST, 'import_role_id', 'numeric');
 $postUserImportMode = admFuncVariableIsValid($_POST, 'user_import_mode', 'numeric', array('requireValue' => true));
 
