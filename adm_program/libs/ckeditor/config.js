ab47b5cdf8e7b84dcf01b27b56ce898bb31ed86a
Thomas Rupprecht <rupprecht.thomas+git@gmail.com>
2015-07-08 00:40:35 +0200

    update lib CKEditor to v4.5.1

--- adm_program/libs/ckeditor/config.js
+++ adm_program/libs/ckeditor/config.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -7,22 +7,4 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-
-    config.toolbar_AdmidioDefault =
-    [
-        ['Format', 'FontSize', 'Bold', 'Italic', 'Underline', 'TextColor', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', '-', 
-        'NumberedList', 'BulletedList', '-','Image', 'Link', 'Unlink', 'Table', '-', 'Maximize']
-    ];
-
-    config.toolbar_AdmidioGuestbook =
-    [
-        ['Font','FontSize', 'Bold', 'Italic', 'Underline', 'TextColor', '-', 'JustifyLeft','JustifyCenter','JustifyRight', '-', 
-        'Smiley', 'Link', 'Unlink', '-', 'NumberedList', 'BulletedList', '-', 'Maximize']
-    ];
-	    
-    config.toolbar_AdmidioPlugin_WC =
-    [
-        ['Format', 'FontSize', 'Bold', 'Italic', 'Underline', 'TextColor', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', '-',
-        'NumberedList', 'BulletedList', '-','-', '-', '-', 'Table', '-', 'Maximize']
-    ];
 };
