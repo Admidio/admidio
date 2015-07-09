506e52d10d7d55c03602f1ecd4bdab88fdc27e5c
Thomas Rupprecht <rupprecht.thomas+git@gmail.com>
2015-07-10 01:34:21 +0200

    check if new username is empty

--- adm_program/system/classes/tableusers.php
+++ adm_program/system/classes/tableusers.php
@@ -210,7 +210,7 @@ class TableUsers extends TableAccess
         // username should not contain special characters
         elseif($columnName === 'usr_login_name')
         {
-            if($newValue !== '' && !strValidCharacters($newValue, 'noSpecialChar'))
+            if($newValue === '' || !strValidCharacters($newValue, 'noSpecialChar'))
             {
                 return false;
             }
