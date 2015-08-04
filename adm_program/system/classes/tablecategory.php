d1cb70a81b024cb8410ca4f716d5161a1e73cfbc
Markus Faßbender <markus.fassbender@gmail.com>
2015-08-04 23:03:42 +0200

    #94 fix for delete of AWA category

--- adm_program/system/classes/tablecategory.php
+++ adm_program/system/classes/tablecategory.php
@@ -70,24 +70,6 @@ class TableCategory extends TableAccess
                        AND cat_type     = \''. $this->getValue('cat_type'). '\'';
             $this->db->query($sql);
 
-            // Abhaenigigkeiten loeschen
-            if($this->getValue('cat_type') == 'DAT')
-            {
-                $object = new TableDate($this->db);
-            }
-            elseif($this->getValue('cat_type') == 'LNK')
-            {
-                $object = new TableWeblink($this->db);
-            }
-            elseif($this->getValue('cat_type') == 'ROL')
-            {
-                $object = new TableRoles($this->db);
-            }
-            elseif($this->getValue('cat_type') == 'USF')
-            {
-                $object = new TableUserField($this->db);
-            }
-
             // alle zugehoerigen abhaengigen Objekte suchen und mit weiteren Abhaengigkeiten loeschen
             $sql    = 'SELECT * FROM '.$this->elementTable.'
                         WHERE '.$this->elementColumn.' = '. $this->getValue('cat_id');
@@ -257,6 +239,11 @@ class TableCategory extends TableAccess
             {
                 $this->elementTable = TBL_DATES;
                 $this->elementColumn = 'dat_cat_id';
+            }
+            elseif($this->getValue('cat_type') == 'AWA')
+            {
+                $this->elementTable  = $g_tbl_praefix.'_user_awards';
+                $this->elementColumn = 'awa_cat_id';
             }
         }
 
