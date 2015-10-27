6c0d6e649e97a08733e87ee9d137338611ce61db
Markus Faßbender <markus.fassbender@gmail.com>
2015-10-27 19:36:30 +0100

    #200 default width of ckeditors table should be 100%

--- adm_program/libs/ckeditor/config.js
+++ adm_program/libs/ckeditor/config.js
@@ -4,25 +4,39 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
 
     config.toolbar_AdmidioDefault =
     [
-        ['Format', 'FontSize', 'Bold', 'Italic', 'Underline', 'TextColor', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', '-', 
+        ['Format', 'FontSize', 'Bold', 'Italic', 'Underline', 'TextColor', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', '-',
         'NumberedList', 'BulletedList', '-','Image', 'Link', 'Unlink', 'Table', '-', 'Maximize']
     ];
 
     config.toolbar_AdmidioGuestbook =
     [
-        ['Font','FontSize', 'Bold', 'Italic', 'Underline', 'TextColor', '-', 'JustifyLeft','JustifyCenter','JustifyRight', '-', 
+        ['Font','FontSize', 'Bold', 'Italic', 'Underline', 'TextColor', '-', 'JustifyLeft','JustifyCenter','JustifyRight', '-',
         'Smiley', 'Link', 'Unlink', '-', 'NumberedList', 'BulletedList', '-', 'Maximize']
     ];
-	    
+
     config.toolbar_AdmidioPlugin_WC =
     [
         ['Format', 'FontSize', 'Bold', 'Italic', 'Underline', 'TextColor', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', '-',
         'NumberedList', 'BulletedList', '-','-', '-', '-', 'Table', '-', 'Maximize']
     ];
 };
+
+/* Set default width of table to 100% instead of 500%. This fits better with a responsive design. */
+CKEDITOR.on('dialogDefinition', function( ev ) {
+
+      var diagName = ev.data.name;
+      var diagDefn = ev.data.definition;
+
+      if(diagName === 'table') {
+        var infoTab = diagDefn.getContents('info');
+
+        var width = infoTab.get('txtWidth');
+        width['default'] = "100%";
+      }
+});
