56a594fb282faf9cefb99ad2773ae079befb281b
Markus Faßbender <markus.fassbender@gmail.com>
2015-12-29 22:32:36 +0100

    #31 LostPassword now works also with username

--- adm_program/system/lost_password.php
+++ adm_program/system/lost_password.php
@@ -47,61 +47,95 @@ if(!empty($_POST['recipient_email']) && !empty($_POST['captcha']))
 {
     try
     {
-        // search for user with the email address that have a valid login and membership to a role
-        $sql = 'SELECT usr_id
-                  FROM '.TBL_MEMBERS.'
-            INNER JOIN '.TBL_ROLES.'
-                    ON rol_id = mem_rol_id
-            INNER JOIN '.TBL_CATEGORIES.'
-                    ON cat_id = rol_cat_id
-            INNER JOIN '.TBL_USERS.'
-                    ON usr_id = mem_usr_id
-            INNER JOIN '.TBL_USER_DATA.' as email
-                    ON email.usd_usr_id = usr_id
-                   AND email.usd_usf_id = '.$gProfileFields->getProperty('EMAIL', 'usf_id').'
-                   AND email.usd_value  = \''.$_POST['recipient_email'].'\'
-                 WHERE LENGTH(usr_login_name) > 0
-                   AND rol_valid  = 1
-                   AND usr_valid  = 1
-                   AND mem_begin <= \''.DATE_NOW.'\'
-                   AND mem_end    > \''.DATE_NOW.'\'
-                   AND (  cat_org_id = '.$gCurrentOrganization->getValue('org_id').'
-                       OR cat_org_id IS NULL )
-                 GROUP BY usr_id';
-        $userStatement = $gDb->query($sql);
-        $count = $userStatement->rowCount();
-
-        // show error if no user found or more than one user found
-        if($count === 0)
+        if(strValidCharacters($_POST['recipient_email'], 'email'))
         {
-            $gMessage->show($gL10n->get('SYS_LOSTPW_EMAIL_ERROR', $_POST['recipient_email']));
+            // search for user with the email address that have a valid login and membership to a role
+            $sql = 'SELECT usr_id
+                      FROM '.TBL_MEMBERS.'
+                INNER JOIN '.TBL_ROLES.'
+                        ON rol_id = mem_rol_id
+                INNER JOIN '.TBL_CATEGORIES.'
+                        ON cat_id = rol_cat_id
+                INNER JOIN '.TBL_USERS.'
+                        ON usr_id = mem_usr_id
+                INNER JOIN '.TBL_USER_DATA.' as email
+                        ON email.usd_usr_id = usr_id
+                       AND email.usd_usf_id = '.$gProfileFields->getProperty('EMAIL', 'usf_id').'
+                       AND email.usd_value  = \''.$_POST['recipient_email'].'\'
+                     WHERE LENGTH(usr_login_name) > 0
+                       AND rol_valid  = 1
+                       AND usr_valid  = 1
+                       AND mem_begin <= \''.DATE_NOW.'\'
+                       AND mem_end    > \''.DATE_NOW.'\'
+                       AND (  cat_org_id = '.$gCurrentOrganization->getValue('org_id').'
+                           OR cat_org_id IS NULL )
+                     GROUP BY usr_id';
+            $userStatement = $gDb->query($sql);
+            $count = $userStatement->rowCount();
         }
-        elseif($count > 1)
+        else
+        {
+            // first try to find user with username. Also an email could be a username.
+            $sql = 'SELECT usr_id
+                      FROM '.TBL_MEMBERS.'
+                INNER JOIN '.TBL_ROLES.'
+                        ON rol_id = mem_rol_id
+                INNER JOIN '.TBL_CATEGORIES.'
+                        ON cat_id = rol_cat_id
+                INNER JOIN '.TBL_USERS.'
+                        ON usr_id = mem_usr_id
+                     WHERE usr_login_name = \''.$_POST['recipient_email'].'\'
+                       AND rol_valid  = 1
+                       AND usr_valid  = 1
+                       AND mem_begin <= \''.DATE_NOW.'\'
+                       AND mem_end    > \''.DATE_NOW.'\'
+                       AND (  cat_org_id = '.$gCurrentOrganization->getValue('org_id').'
+                           OR cat_org_id IS NULL )
+                     GROUP BY usr_id';
+            $userStatement = $gDb->query($sql);
+            $count = $userStatement->rowCount();
+        }
+
+        // show error if more than one user found
+        if($count > 1)
         {
             $gMessage->show($gL10n->get('SYS_LOSTPW_SEVERAL_EMAIL', $_POST['recipient_email']));
         }
+        elseif($count === 1)
+        {
+            // a valid username or email was found then send new password
 
-        $row  = $userStatement->fetch();
-        $user = new User($gDb, $gProfileFields, $row['usr_id']);
+            $row  = $userStatement->fetch();
+            $user = new User($gDb, $gProfileFields, $row['usr_id']);
 
-        // create and save new password and activation id
-        $newPassword  = PasswordHashing::genRandomPassword(8);
-        $activationId = PasswordHashing::genRandomPassword(10);
+            // create and save new password and activation id
+            $newPassword  = PasswordHashing::genRandomPassword(8);
+            $activationId = PasswordHashing::genRandomPassword(10);
 
-        $user->setPassword($newPassword, true);
-        $user->setValue('usr_activation_code', $activationId);
+            $user->setPassword($newPassword, true);
+            $user->setValue('usr_activation_code', $activationId);
 
-        $sysmail = new SystemMail($gDb);
-        $sysmail->addRecipient($user->getValue('EMAIL'), $user->getValue('FIRST_NAME', 'database').' '.$user->getValue('LAST_NAME', 'database'));
-        $sysmail->setVariable(1, $newPassword);
-        $sysmail->setVariable(2, $g_root_path.'/adm_program/system/password_activation.php?usr_id='.$user->getValue('usr_id').'&aid='.$activationId);
-        $sysmail->sendSystemMail('SYSMAIL_ACTIVATION_LINK', $user);
+            $sysmail = new SystemMail($gDb);
+            $sysmail->addRecipient($user->getValue('EMAIL'), $user->getValue('FIRST_NAME', 'database').' '.$user->getValue('LAST_NAME', 'database'));
+            $sysmail->setVariable(1, $newPassword);
+            $sysmail->setVariable(2, $g_root_path.'/adm_program/system/password_activation.php?usr_id='.$user->getValue('usr_id').'&aid='.$activationId);
+            $sysmail->sendSystemMail('SYSMAIL_ACTIVATION_LINK', $user);
 
-        $user->saveChangesWithoutRights();
-        $user->save();
+            $user->saveChangesWithoutRights();
+            $user->save();
+        }
 
+        // always show a positive feedback to prevent hackers to validate an email-address or username
         $gMessage->setForwardUrl($g_root_path.'/adm_program/system/login.php');
-        $gMessage->show($gL10n->get('SYS_LOSTPW_SEND', $_POST['recipient_email']));
+
+        if(strValidCharacters($_POST['recipient_email'], 'email'))
+        {
+            $gMessage->show($gL10n->get('SYS_LOSTPW_SEND_EMAIL', $_POST['recipient_email']));
+        }
+        else
+        {
+            $gMessage->show($gL10n->get('SYS_LOSTPW_SEND_USERNAME', $_POST['recipient_email']));
+        }
     }
     catch(AdmException $e)
     {
@@ -123,7 +157,7 @@ else
 
     // show form
     $form = new HtmlForm('lost_password_form', $g_root_path.'/adm_program/system/lost_password.php', $page);
-    $form->addInput('recipient_email', $gL10n->get('SYS_EMAIL'), null, array('maxLength' => 50, 'property' => FIELD_REQUIRED));
+    $form->addInput('recipient_email', $gL10n->get('SYS_USERNAME_OR_EMAIL'), null, array('maxLength' => 254, 'property' => FIELD_REQUIRED));
 
     // if captchas are enabled then visitors of the website must resolve this
     if (!$gValidLogin && $gPreferences['enable_mail_captcha'] == 1)
