7962e6a46b634bdaeece536961aff23d74b39668
Markus Faßbender <markus.fassbender@gmail.com>
2016-01-15 20:28:20 +0100

    error when showing pdf; consider column alignment in pdf

--- adm_program/modules/lists/lists_show.php
+++ adm_program/modules/lists/lists_show.php
@@ -469,20 +469,17 @@ elseif ($getMode === 'html' || $getMode === 'print')
 }
 elseif ($getMode === 'pdf')
 {
+    $table->setColumnAlignByArray($columnAlign);
     $table->addTableHeader();
     $table->addRow();
     $table->addAttribute('align', 'center');
     $table->addColumn($headline, array('colspan' => count($arrValidColumns)));
     $table->addRow();
-    // Write valid column headings
-    for ($column = 1; $column <= count($arrValidColumns); ++$column)
-    {
-        if ($column === 1)
-        {
-            $table->addColumn($arrValidColumns[0], array('style' => 'text-align: '.$columnAlign[$columnNumber - 1].';font-size:14;background-color:#C7C7C7;'), 'th');
-        }
 
-        $table->addColumn($arrValidColumns[$column], array('style' => 'text-align: '.$columnAlign[$columnNumber - 1].';font-size:14;background-color:#C7C7C7;'), 'th');
+    // Write valid column headings
+    for ($column = 0; $column < count($arrValidColumns); $column++)
+    {
+        $table->addColumn($arrValidColumns[$column], array('style' => 'text-align: '.$columnAlign[$column].';font-size:14;background-color:#C7C7C7;'), 'th');
     }
 }
 else
