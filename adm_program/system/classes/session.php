5b3ca5604f55755046fb6dd45c3fada8f2112c6e
Markus Faßbender <markus.fassbender@gmail.com>
2016-01-18 21:29:41 +0100

    debug print if ip address is not valid

--- adm_program/system/classes/session.php
+++ adm_program/system/classes/session.php
@@ -256,15 +256,19 @@ class Session extends TableAccess
      */
     public function refreshSession()
     {
-        global $gCheckIpAddress;
-
-        $this->readDataById($this->getValue('ses_id'));
+        global $gCheckIpAddress, $gDebug;
 
         // check if current connection has same ip address as of session initialization
         // if config parameter $gCheckIpAddress = 0 then don't check ip address
         if($this->getValue('ses_ip_address') !== $_SERVER['REMOTE_ADDR']
+        && $this->getValue('ses_ip_address') !== ''
         && (!isset($gCheckIpAddress) || $gCheckIpAddress === 1))
         {
+            if($gDebug)
+            {
+                error_log('Admidio stored session ip address: '.$this->getValue('ses_ip_address'). ' :: Remode ip address: '.$_SERVER['REMOTE_ADDR']);
+            }
+
             unset($_SESSION['gCurrentSession']);
             $this->mObjectArray = array();
             $this->clear();
