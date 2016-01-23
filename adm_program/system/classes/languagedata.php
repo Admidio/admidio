31b722379bd315f1a6803ebf44935269432c1cc0
Markus Faßbender <markus.fassbender@gmail.com>
2016-01-23 10:54:21 +0100

    initialize language array if language changed

--- adm_program/system/classes/languagedata.php
+++ adm_program/system/classes/languagedata.php
@@ -128,6 +128,7 @@ class LanguageData
         {
             // initialize all parameters
             $this->textCache = array();
+            $this->countries = array();
 
             $this->language = $language;
         }
