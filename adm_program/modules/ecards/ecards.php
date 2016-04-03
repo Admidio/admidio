66b93ebc6a5488211c7ff1c715713fcc9cd9dbfe
Thomas Rupprecht <rupprecht.thomas+git@gmail.com>
2016-04-03 14:17:52 +0200

    fix intends

--- adm_program/modules/ecards/ecards.php
+++ adm_program/modules/ecards/ecards.php
@@ -154,87 +154,87 @@ $form->addInput('photo_id', null, $getPhotoId, array('type' => 'hidden'));
 $form->addInput('photo_nr', null, $getPhotoNr, array('type' => 'hidden'));
 
 $form->openGroupBox('gb_layout', $gL10n->get('ECA_LAYOUT'));
-    $form->addCustomContent($gL10n->get('SYS_PHOTO'), '
-        <a data-toggle="lightbox" data-type="image" data-title="'.$gL10n->get('SYS_PREVIEW').'"
-            href="'.$g_root_path.'/adm_program/modules/photos/photo_show.php?pho_id='.$getPhotoId.'&amp;photo_nr='.$getPhotoNr.'&amp;max_width='.$gPreferences['photo_show_width'].'&amp;max_height='.$gPreferences['photo_show_height'].'"><img
-            src="'.$g_root_path.'/adm_program/modules/photos/photo_show.php?pho_id='.$getPhotoId.'&amp;photo_nr='.$getPhotoNr.'&amp;max_width='.$gPreferences['ecard_thumbs_scale'].'&amp;max_height='.$gPreferences['ecard_thumbs_scale'].'"
-            class="imageFrame" alt="'.$gL10n->get('ECA_VIEW_PICTURE_FULL_SIZED').'"  title="'.$gL10n->get('ECA_VIEW_PICTURE_FULL_SIZED').'" />
-        </a>');
-    $templates = admFuncGetDirectoryEntries(THEME_SERVER_PATH.'/ecard_templates');
-    foreach($templates as $key => $templateName)
-    {
-        $templates[$key] = ucfirst(preg_replace('/[_-]/', ' ', str_replace('.tpl', '', $templateName)));
-    }
-    $form->addSelectBox('ecard_template', $gL10n->get('ECA_TEMPLATE'), $templates, array('defaultValue' => $template, 'property' => FIELD_REQUIRED, 'showContextDependentFirstEntry' => false));
+$form->addCustomContent($gL10n->get('SYS_PHOTO'), '
+    <a data-toggle="lightbox" data-type="image" data-title="'.$gL10n->get('SYS_PREVIEW').'"
+        href="'.$g_root_path.'/adm_program/modules/photos/photo_show.php?pho_id='.$getPhotoId.'&amp;photo_nr='.$getPhotoNr.'&amp;max_width='.$gPreferences['photo_show_width'].'&amp;max_height='.$gPreferences['photo_show_height'].'"><img
+        src="'.$g_root_path.'/adm_program/modules/photos/photo_show.php?pho_id='.$getPhotoId.'&amp;photo_nr='.$getPhotoNr.'&amp;max_width='.$gPreferences['ecard_thumbs_scale'].'&amp;max_height='.$gPreferences['ecard_thumbs_scale'].'"
+        class="imageFrame" alt="'.$gL10n->get('ECA_VIEW_PICTURE_FULL_SIZED').'"  title="'.$gL10n->get('ECA_VIEW_PICTURE_FULL_SIZED').'" />
+    </a>');
+$templates = admFuncGetDirectoryEntries(THEME_SERVER_PATH.'/ecard_templates');
+foreach($templates as $key => $templateName)
+{
+    $templates[$key] = ucfirst(preg_replace('/[_-]/', ' ', str_replace('.tpl', '', $templateName)));
+}
+$form->addSelectBox('ecard_template', $gL10n->get('ECA_TEMPLATE'), $templates, array('defaultValue' => $template, 'property' => FIELD_REQUIRED, 'showContextDependentFirstEntry' => false));
 $form->closeGroupBox();
 $form->openGroupBox('gb_contact_details', $gL10n->get('SYS_CONTACT_DETAILS'));
 
-    // create list with all possible recipients
+// create list with all possible recipients
 
-    // list all roles where login users could send mails to
-    $arrayMailRoles = $gCurrentUser->getAllMailRoles();
+// list all roles where login users could send mails to
+$arrayMailRoles = $gCurrentUser->getAllMailRoles();
 
-    $sql = 'SELECT rol_id, rol_name
-              FROM '.TBL_ROLES.'
-        INNER JOIN '.TBL_CATEGORIES.'
-                ON cat_id = rol_cat_id
-             WHERE rol_id IN ('.implode(',', $arrayMailRoles).')
-               AND cat_name_intern <> \'CONFIRMATION_OF_PARTICIPATION\'
-          ORDER BY rol_name';
-    $statement = $gDb->query($sql);
+$sql = 'SELECT rol_id, rol_name
+          FROM '.TBL_ROLES.'
+    INNER JOIN '.TBL_CATEGORIES.'
+            ON cat_id = rol_cat_id
+         WHERE rol_id IN ('.implode(',', $arrayMailRoles).')
+           AND cat_name_intern <> \'CONFIRMATION_OF_PARTICIPATION\'
+      ORDER BY rol_name';
+$statement = $gDb->query($sql);
 
-    while($row = $statement->fetch())
-    {
-        $list[] = array('groupID: '.$row['rol_id'], $row['rol_name'], $gL10n->get('SYS_ROLES'));
-    }
+while($row = $statement->fetch())
+{
+    $list[] = array('groupID: '.$row['rol_id'], $row['rol_name'], $gL10n->get('SYS_ROLES'));
+}
 
-    // select all users
-    $arrayRoles = array_merge($arrayMailRoles, $gCurrentUser->getAllVisibleRoles());
-    $arrayUniqueRoles = array_unique($arrayRoles);
+// select all users
+$arrayRoles = array_merge($arrayMailRoles, $gCurrentUser->getAllVisibleRoles());
+$arrayUniqueRoles = array_unique($arrayRoles);
 
-    $sql = 'SELECT usr_id, first_name.usd_value as first_name, last_name.usd_value as last_name,
-                   email.usd_value as email
-              FROM '.TBL_MEMBERS.'
-        INNER JOIN '.TBL_USERS.'
-                ON usr_id = mem_usr_id
-        INNER JOIN '.TBL_USER_DATA.' as email
-                ON email.usd_usr_id = usr_id
-               AND LENGTH(email.usd_value) > 0
-        INNER JOIN '.TBL_USER_FIELDS.' as field
-                ON field.usf_id = email.usd_usf_id
-               AND field.usf_type = \'EMAIL\'
-         LEFT JOIN '.TBL_USER_DATA.' as last_name
-                ON last_name.usd_usr_id = usr_id
-               AND last_name.usd_usf_id = '. $gProfileFields->getProperty('LAST_NAME', 'usf_id'). '
-         LEFT JOIN '.TBL_USER_DATA.' as first_name
-                ON first_name.usd_usr_id = usr_id
-               AND first_name.usd_usf_id = '. $gProfileFields->getProperty('FIRST_NAME', 'usf_id'). '
-             WHERE usr_valid  = 1
-               AND mem_begin <= \''.DATE_NOW.'\'
-               AND mem_end    > \''.DATE_NOW.'\'
-               AND mem_rol_id IN ('.implode(',', $arrayUniqueRoles).')
-          GROUP BY usr_id, first_name.usd_value, last_name.usd_value, email.usd_value
-          ORDER BY last_name, first_name';
-    $statement = $gDb->query($sql);
+$sql = 'SELECT usr_id, first_name.usd_value as first_name, last_name.usd_value as last_name,
+               email.usd_value as email
+          FROM '.TBL_MEMBERS.'
+    INNER JOIN '.TBL_USERS.'
+            ON usr_id = mem_usr_id
+    INNER JOIN '.TBL_USER_DATA.' as email
+            ON email.usd_usr_id = usr_id
+           AND LENGTH(email.usd_value) > 0
+    INNER JOIN '.TBL_USER_FIELDS.' as field
+            ON field.usf_id = email.usd_usf_id
+           AND field.usf_type = \'EMAIL\'
+     LEFT JOIN '.TBL_USER_DATA.' as last_name
+            ON last_name.usd_usr_id = usr_id
+           AND last_name.usd_usf_id = '. $gProfileFields->getProperty('LAST_NAME', 'usf_id'). '
+     LEFT JOIN '.TBL_USER_DATA.' as first_name
+            ON first_name.usd_usr_id = usr_id
+           AND first_name.usd_usf_id = '. $gProfileFields->getProperty('FIRST_NAME', 'usf_id'). '
+         WHERE usr_valid  = 1
+           AND mem_begin <= \''.DATE_NOW.'\'
+           AND mem_end    > \''.DATE_NOW.'\'
+           AND mem_rol_id IN ('.implode(',', $arrayUniqueRoles).')
+      GROUP BY usr_id, first_name.usd_value, last_name.usd_value, email.usd_value
+      ORDER BY last_name, first_name';
+$statement = $gDb->query($sql);
 
-    while ($row = $statement->fetch())
-    {
-        $list[] = array($row['usr_id'], $row['last_name']. ', '.$row['first_name']. ' ('.$row['email'].')', $gL10n->get('SYS_MEMBERS'));
-    }
+while ($row = $statement->fetch())
+{
+    $list[] = array($row['usr_id'], $row['last_name']. ', '.$row['first_name']. ' ('.$row['email'].')', $gL10n->get('SYS_MEMBERS'));
+}
 
-    $form->addSelectBox('ecard_recipients', $gL10n->get('SYS_TO'), $list, array('property'     => FIELD_REQUIRED,
-                                                                                'defaultValue' => $recipients,
-                                                                                'multiselect'  => true));
-    $form->addLine();
-    $form->addInput('name_from', $gL10n->get('MAI_YOUR_NAME'), $gCurrentUser->getValue('FIRST_NAME'). ' '. $gCurrentUser->getValue('LAST_NAME'), array('maxLength' => 50, 'property' => FIELD_DISABLED));
-    $form->addInput('mail_from', $gL10n->get('MAI_YOUR_EMAIL'), $gCurrentUser->getValue('EMAIL'), array('maxLength' => 50, 'property' => FIELD_DISABLED));
+$form->addSelectBox('ecard_recipients', $gL10n->get('SYS_TO'), $list, array('property'     => FIELD_REQUIRED,
+                                                                            'defaultValue' => $recipients,
+                                                                            'multiselect'  => true));
+$form->addLine();
+$form->addInput('name_from', $gL10n->get('MAI_YOUR_NAME'), $gCurrentUser->getValue('FIRST_NAME'). ' '. $gCurrentUser->getValue('LAST_NAME'), array('maxLength' => 50, 'property' => FIELD_DISABLED));
+$form->addInput('mail_from', $gL10n->get('MAI_YOUR_EMAIL'), $gCurrentUser->getValue('EMAIL'), array('maxLength' => 50, 'property' => FIELD_DISABLED));
 $form->closeGroupBox();
 $form->openGroupBox('gb_message', $gL10n->get('SYS_MESSAGE'), 'admidio-panel-editor');
-    $form->addEditor('ecard_message', null, $message, array('property' => FIELD_REQUIRED, 'toolbar' => 'AdmidioGuestbook'));
+$form->addEditor('ecard_message', null, $message, array('property' => FIELD_REQUIRED, 'toolbar' => 'AdmidioGuestbook'));
 $form->closeGroupBox();
 $form->openButtonGroup();
-    $form->addButton('btn_ecard_preview', $gL10n->get('SYS_PREVIEW'), array('icon' => THEME_PATH. '/icons/eye.png'));
-    $form->addSubmitButton('btn_ecard_submit', $gL10n->get('SYS_SEND'), array('icon' => THEME_PATH. '/icons/email.png'));
+$form->addButton('btn_ecard_preview', $gL10n->get('SYS_PREVIEW'), array('icon' => THEME_PATH. '/icons/eye.png'));
+$form->addSubmitButton('btn_ecard_submit', $gL10n->get('SYS_SEND'), array('icon' => THEME_PATH. '/icons/email.png'));
 $form->closeButtonGroup();
 
 // add form to html page and show page
--- adm_program/modules/guestbook/get_comments.php
+++ adm_program/modules/guestbook/get_comments.php
@@ -43,65 +43,64 @@ if ($getGbcId > 0)
                    $conditions.'
           ORDER BY gbc_timestamp_create ASC';
     $commentStatement = $gDb->query($sql);
-}
 
-if($commentStatement->rowCount() > 0)
-{
-    $gbComment = new TableGuestbookComment($gDb);
-
-    // Jetzt nur noch die Kommentare auflisten
-    while ($row = $commentStatement->fetch())
+    if($commentStatement->rowCount() > 0)
     {
-        // GBComment-Objekt initialisieren und neuen DS uebergeben
-        $gbComment->clear();
-        $gbComment->setArray($row);
+        $gbComment = new TableGuestbookComment($gDb);
 
-        $getGbcId = $gbComment->getValue('gbc_gbo_id');
+        // Jetzt nur noch die Kommentare auflisten
+        while ($row = $commentStatement->fetch())
+        {
+            // GBComment-Objekt initialisieren und neuen DS uebergeben
+            $gbComment->clear();
+            $gbComment->setArray($row);
 
-        echo '
-        <div class="panel panel-info admidio-panel-comment" id="gbc_'.$gbComment->getValue('gbc_id').'">
-            <div class="panel-heading">
-                <div class="pull-left">
-                    <img class="admidio-panel-heading-icon" src="'. THEME_PATH. '/icons/comment.png" style="vertical-align: top;" alt="'.$gL10n->get('GBO_COMMENT_BY', $gbComment->getValue('gbc_name')).'" />&nbsp;'.
-                    $gL10n->get('GBO_COMMENT_BY', $gbComment->getValue('gbc_name'));
+            $getGbcId = $gbComment->getValue('gbc_gbo_id');
 
-                    // Falls eine Mailadresse des Users angegeben wurde, soll ein Maillink angezeigt werden...
-                    if(strlen($gbComment->getValue('gbc_email')) > 0)
-                    {
-                        echo '<a class="admidio-icon-link" href="mailto:'.$gbComment->getValue('gbc_email').'"><img src="'. THEME_PATH. '/icons/email.png"
-                            alt="'.$gL10n->get('SYS_SEND_EMAIL_TO', $gbComment->getValue('gbc_email')).'" title="'.$gL10n->get('SYS_SEND_EMAIL_TO', $gbComment->getValue('gbc_email')).'" /></a>';
-                    }
-                echo '</div>
-                <div class="pull-right text-right">'. $gbComment->getValue('gbc_timestamp_create', $gPreferences['system_date'].' '.$gPreferences['system_time']);
+            echo '
+            <div class="panel panel-info admidio-panel-comment" id="gbc_'.$gbComment->getValue('gbc_id').'">
+                <div class="panel-heading">
+                    <div class="pull-left">
+                        <img class="admidio-panel-heading-icon" src="'. THEME_PATH. '/icons/comment.png" style="vertical-align: top;" alt="'.$gL10n->get('GBO_COMMENT_BY', $gbComment->getValue('gbc_name')).'" />&nbsp;'.
+                        $gL10n->get('GBO_COMMENT_BY', $gbComment->getValue('gbc_name'));
 
-                    // aendern und loeschen von Kommentaren duerfen nur User mit den gesetzten Rechten
-                    if ($gCurrentUser->editGuestbookRight())
-                    {
-                        echo '
-                        <a class="admidio-icon-link" href="'.$g_root_path.'/adm_program/modules/guestbook/guestbook_comment_new.php?cid='.$gbComment->getValue('gbc_id').'"><img
-                            src="'. THEME_PATH. '/icons/edit.png" alt="'.$gL10n->get('SYS_EDIT').'" title="'.$gL10n->get('SYS_EDIT').'" /></a>
-                        <a class="admidio-icon-link" data-toggle="modal" data-target="#admidio_modal"
-                            href="'.$g_root_path.'/adm_program/system/popup_message.php?type=gbc&amp;element_id=gbc_'.
-                            $gbComment->getValue('gbc_id').'&amp;database_id='.$gbComment->getValue('gbc_id').'&amp;database_id_2='.$gbComment->getValue('gbo_id').'&amp;name='.urlencode($gL10n->get('GBO_COMMENT_BY', $gbComment->getValue('gbc_name'))).'"><img
-                            src="'. THEME_PATH. '/icons/delete.png" alt="'.$gL10n->get('SYS_DELETE').'" title="'.$gL10n->get('SYS_DELETE').'" /></a>';
-                    }
-                echo '</div>
+            // Falls eine Mailadresse des Users angegeben wurde, soll ein Maillink angezeigt werden...
+            if(strlen($gbComment->getValue('gbc_email')) > 0)
+            {
+                echo '<a class="admidio-icon-link" href="mailto:'.$gbComment->getValue('gbc_email').'"><img src="'. THEME_PATH. '/icons/email.png"
+                    alt="'.$gL10n->get('SYS_SEND_EMAIL_TO', $gbComment->getValue('gbc_email')).'" title="'.$gL10n->get('SYS_SEND_EMAIL_TO', $gbComment->getValue('gbc_email')).'" /></a>';
+            }
+            echo '</div>
+            <div class="pull-right text-right">'. $gbComment->getValue('gbc_timestamp_create', $gPreferences['system_date'].' '.$gPreferences['system_time']);
+
+            // aendern und loeschen von Kommentaren duerfen nur User mit den gesetzten Rechten
+            if ($gCurrentUser->editGuestbookRight())
+            {
+                echo '
+                <a class="admidio-icon-link" href="'.$g_root_path.'/adm_program/modules/guestbook/guestbook_comment_new.php?cid='.$gbComment->getValue('gbc_id').'"><img
+                    src="'. THEME_PATH. '/icons/edit.png" alt="'.$gL10n->get('SYS_EDIT').'" title="'.$gL10n->get('SYS_EDIT').'" /></a>
+                <a class="admidio-icon-link" data-toggle="modal" data-target="#admidio_modal"
+                    href="'.$g_root_path.'/adm_program/system/popup_message.php?type=gbc&amp;element_id=gbc_'.
+                    $gbComment->getValue('gbc_id').'&amp;database_id='.$gbComment->getValue('gbc_id').'&amp;database_id_2='.$gbComment->getValue('gbo_id').'&amp;name='.urlencode($gL10n->get('GBO_COMMENT_BY', $gbComment->getValue('gbc_name'))).'"><img
+                    src="'. THEME_PATH. '/icons/delete.png" alt="'.$gL10n->get('SYS_DELETE').'" title="'.$gL10n->get('SYS_DELETE').'" /></a>';
+            }
+            echo '</div>
             </div>
 
             <div class="panel-body">'.
                 $gbComment->getValue('gbc_text');
 
-                // Buttons zur Freigabe / Loeschen des gesperrten Eintrags
-                if($getModeration)
-                {
-                    echo '
-                    <div class="btn-group" role="group">
-                        <button class="btn btn-default" onclick="callUrlHideElement(\'gbc_'.$gbComment->getValue('gbc_id').'\', \'guestbook_function.php?mode=10&id='.$gbComment->getValue('gbc_id').'\')"><img
-                            src="'. THEME_PATH. '/icons/ok.png" alt="'.$gL10n->get('SYS_UNLOCK').'" />'.$gL10n->get('SYS_UNLOCK').'</button>
-                        <button class="btn btn-default" onclick="callUrlHideElement(\'gbc_'.$gbComment->getValue('gbc_id').'\', \'guestbook_function.php?mode=5&id='.$gbComment->getValue('gbc_id').'\')"><img
-                            src="'. THEME_PATH. '/icons/no.png" alt="'.$gL10n->get('SYS_REMOVE').'" />'.$gL10n->get('SYS_REMOVE').'</button>
-                    </div>';
-                }
+            // Buttons zur Freigabe / Loeschen des gesperrten Eintrags
+            if($getModeration)
+            {
+                echo '
+                <div class="btn-group" role="group">
+                    <button class="btn btn-default" onclick="callUrlHideElement(\'gbc_'.$gbComment->getValue('gbc_id').'\', \'guestbook_function.php?mode=10&id='.$gbComment->getValue('gbc_id').'\')"><img
+                        src="'. THEME_PATH. '/icons/ok.png" alt="'.$gL10n->get('SYS_UNLOCK').'" />'.$gL10n->get('SYS_UNLOCK').'</button>
+                    <button class="btn btn-default" onclick="callUrlHideElement(\'gbc_'.$gbComment->getValue('gbc_id').'\', \'guestbook_function.php?mode=5&id='.$gbComment->getValue('gbc_id').'\')"><img
+                        src="'. THEME_PATH. '/icons/no.png" alt="'.$gL10n->get('SYS_REMOVE').'" />'.$gL10n->get('SYS_REMOVE').'</button>
+                </div>';
+            }
             echo '</div>';
 
             // show information about user who edit the recordset
@@ -109,15 +108,16 @@ if($commentStatement->rowCount() > 0)
             {
                 echo '<div class="panel-footer">'.admFuncShowCreateChangeInfoById(0, '', $gbComment->getValue('gbc_usr_id_change'), $gbComment->getValue('gbc_timestamp_change')).'</div>';
             }
-        echo '</div>';
-    }
+            echo '</div>';
+        }
 
-    if (!$getModeration && ($gCurrentUser->commentGuestbookRight() || $gPreferences['enable_gbook_comments4all'] == 1))
-    {
-        // Bei Kommentierungsrechten, wird der Link zur Kommentarseite angezeigt...
-        echo '
-        <button type="button" class="btn btn-default" onclick="javascript:window.location.href=\''.$g_root_path.'/adm_program/modules/guestbook/guestbook_comment_new.php?id='.$getGbcId.'\'"><img
-            src="'. THEME_PATH. '/icons/comment_new.png" alt="'.$gL10n->get('GBO_WRITE_COMMENT').'"
-            title="'.$gL10n->get('GBO_WRITE_COMMENT').'" />'.$gL10n->get('GBO_WRITE_COMMENT').'</button>';
+        if (!$getModeration && ($gCurrentUser->commentGuestbookRight() || $gPreferences['enable_gbook_comments4all'] == 1))
+        {
+            // Bei Kommentierungsrechten, wird der Link zur Kommentarseite angezeigt...
+            echo '
+            <button type="button" class="btn btn-default" onclick="javascript:window.location.href=\''.$g_root_path.'/adm_program/modules/guestbook/guestbook_comment_new.php?id='.$getGbcId.'\'"><img
+                src="'. THEME_PATH. '/icons/comment_new.png" alt="'.$gL10n->get('GBO_WRITE_COMMENT').'"
+                title="'.$gL10n->get('GBO_WRITE_COMMENT').'" />'.$gL10n->get('GBO_WRITE_COMMENT').'</button>';
+        }
     }
 }
