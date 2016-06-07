584a3bc9df822d27121d62c13856441ad86feaf8
Thomas Rupprecht <rupprecht.thomas+git@gmail.com>
2016-06-07 09:23:00 +0200

    Revert "correction for sorting in tables"
    This reverts commit 28c8f35fc4facb68b4fa9c722bf7f0ca9e44cff2.

--- adm_program/system/classes/htmltable.php
+++ adm_program/system/classes/htmltable.php
@@ -160,7 +160,7 @@ class HtmlTable extends HtmlTableBasic
         }
 
         // if body area wasn't defined until now then do it
-        if($this->tbody == false)
+        if($this->tbody == -1)
         {
             $this->addTableBody();
         }
