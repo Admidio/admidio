e138e90ae258c9bdb5bfd3bcf8e45ba451145fae
Markus Faßbender <markus.fassbender@gmail.com>
2016-11-06 22:47:17 +0100

    this message is not necessary for Admidio
    We should not flush the log file with this message. Sometimes users
    could not change php versions and then they always see this in their
    log file.

--- adm_program/system/logging.php
+++ adm_program/system/logging.php
@@ -39,11 +39,6 @@ $gLogger->notice('##############################################################
 $gLogger->notice('URL: ' . CURRENT_URL);
 $gLogger->notice('MEMORY USAGE: ' . round(memory_get_usage() / 1024, 1) . ' KB');
 
-if (version_compare(PHP_VERSION, '5.6', '<'))
-{
-    $gLogger->warning('PHP-Version is out of date! Please update your PHP-Version!', array('phpVersion' => PHP_VERSION));
-}
-
 // Log Constants
 $constants = array(
     'ADMIDIO_HOMEPAGE' => ADMIDIO_HOMEPAGE,
