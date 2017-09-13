49701aab179be9b37875451bceec9d107b29cf01
Thomas Rupprecht <rupprecht.thomas@gmail.com>
2017-09-13 18:36:02 +0200

    simplify switch case

--- adm_program/system/classes/modulelists.php
+++ adm_program/system/classes/modulelists.php
@@ -192,17 +192,14 @@ class ModuleLists extends Modules
         switch ($this->memberStatus)
         {
             case 'inactive':
-                $sql = ' AND mem_end < \''.DATE_NOW.'\' ';
-                break;
+                return ' AND mem_end < \''.DATE_NOW.'\' ';
             case 'both':
-                $sql ='';
-                break;
+                return '';
             case 'active':
             default:
-                $sql = ' AND mem_begin <= \''.DATE_NOW.'\'
+                return ' AND mem_begin <= \''.DATE_NOW.'\'
                          AND mem_end   >= \''.DATE_NOW.'\' ';
         }
-        return $sql;
     }
 
     /**
