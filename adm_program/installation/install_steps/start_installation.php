58f3db4fb1ab073322f7ae044922fef7fe8616fb
sirnone <stefan.weiss@uberg.at>
2017-12-17 22:10:27 +0100

    correction for Install - final
    now the install is possible again

--- adm_program/installation/install_steps/start_installation.php
+++ adm_program/installation/install_steps/start_installation.php
@@ -218,7 +218,8 @@ $defaultOrgPreferences['system_language']     = $language;
 $benchmarkResults = PasswordHashing::costBenchmark(0.35, 'password', $gPasswordHashAlgorithm);
 $defaultOrgPreferences['system_hashing_cost'] = $benchmarkResults['cost'];
 
-// create all necessary data for this organization
+// create all necessary data for this
+$gCurrentOrganization = new Organization($db, $_SESSION['orga_shortname']);
 $settingsManager =& $gCurrentOrganization->getSettingsManager();
 $settingsManager->setMulti($defaultOrgPreferences, false);
 $gCurrentOrganization->createBasicData((int) $administrator->getValue('usr_id'));
