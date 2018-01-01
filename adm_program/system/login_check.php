042cc173a401477ba365ef0fcaf325de8531121c
Steffen Köhler <steffen@koehlers.de>
2018-01-01 15:50:02 +0100

    As the Klobs client logs in without any Web browser activities, the original Admidio login routine need to be patched.
    This patch splits the authentification logic from any browser based response
    messages, so that the Klobs client can be authentificated even without
    browser interaction.
    
    The normal web based behaviour stays unchanged.
    
    For that, the todays login_check.php is kept as one part still containing
    the browser login response messages, and a new file, login_func.php, which
    now contains the pure authentification.
    
    So existing applications can still call login_check without any differences,
    while non-browser application use login_func.php instead and call
    
    $checkLoginReturn =createUserObjectFromPost();
    if (is_string($checkLoginReturn))
        {
            $gMessage->show($checkLoginReturn);
            // => EXIT
        }
        else
        {..}
    
    to login without any browser interaction.

--- adm_program/system/login_check.php
+++ adm_program/system/login_check.php
@@ -8,100 +8,9 @@
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
  ***********************************************************************************************
  */
-require_once(__DIR__ . '/common.php');
-
-// Initialize parameters
-$bAutoLogin = false;
-$loginname  = '';
-$password   = '';
-$organizationId = (int) $gCurrentOrganization->getValue('org_id');
-
-// Filter parameters
-// parameters could be from login dialog or login plugin !!!
-/**
- * @param string $prefix
- */
-function initLoginParams($prefix)
-{
-    global $bAutoLogin, $loginname, $password, $organizationId, $gSettingsManager;
-
-    $loginname = $_POST[$prefix.'usr_login_name'];
-    $password  = $_POST[$prefix.'usr_password'];
-
-    if($gSettingsManager->getBool('enable_auto_login') && array_key_exists($prefix.'auto_login', $_POST) && $_POST[$prefix.'auto_login'] == 1)
-    {
-        $bAutoLogin = true;
-    }
-    // if user can choose organization then save the selection
-    if(array_key_exists($prefix.'org_id', $_POST) && is_numeric($_POST[$prefix.'org_id']) && $_POST[$prefix.'org_id'] > 0)
-    {
-        $organizationId = (int) $_POST[$prefix.'org_id'];
-    }
-}
-
-if(array_key_exists('usr_login_name', $_POST) && $_POST['usr_login_name'] !== '')
-{
-    initLoginParams('');
-}
-
-if(array_key_exists('plg_usr_login_name', $_POST) && $_POST['plg_usr_login_name'] !== '')
-{
-    initLoginParams('plg_');
-}
-
-if($loginname === '')
-{
-    $gMessage->show($gL10n->get('SYS_FIELD_EMPTY', array($gL10n->get('SYS_USERNAME'))));
-    // => EXIT
-}
-
-if($password === '')
-{
-    $gMessage->show($gL10n->get('SYS_FIELD_EMPTY', array($gL10n->get('SYS_PASSWORD'))));
-    // => EXIT
-}
-
-// TODO Future: check Password min/max Length
-//if(strlen($password) < PASSWORD_MIN_LENGTH)
-//{
-//    $gMessage->show($gL10n->get('PRO_PASSWORD_LENGTH', array($gL10n->get('SYS_PASSWORD'))));
-//}
-
-// Search for username
-$sql = 'SELECT usr_id
-          FROM '.TBL_USERS.'
-         WHERE UPPER(usr_login_name) = UPPER(?)';
-$userStatement = $gDb->queryPrepared($sql, array($loginname));
-
-if ($userStatement->rowCount() === 0)
-{
-    $gLogger->warning('AUTHENTICATION: Incorrect username/password!', array('username' => $loginname, 'password' => '******'));
-
-    $gMessage->show($gL10n->get('SYS_LOGIN_USERNAME_PASSWORD_INCORRECT'));
-    // => EXIT
-}
-else
-{
-    // if login organization is different to organization of config file then create new session variables
-    if($organizationId !== (int) $gCurrentOrganization->getValue('org_id'))
-    {
-        // read organization of config file with their preferences
-        $gCurrentOrganization->readDataById($organizationId);
-
-        // read new profile field structure for this organization
-        $gProfileFields->readProfileFields($organizationId);
-
-        // save new organization id to session
-        $gCurrentSession->setValue('ses_org_id', $organizationId);
-        $gCurrentSession->save();
-    }
-
-    // create user object
-    $gCurrentUser = new User($gDb, $gProfileFields, (int) $userStatement->fetchColumn());
-
-    $checkLoginReturn = $gCurrentUser->checkLogin($password, $bAutoLogin);
-
-    if (is_string($checkLoginReturn))
+require_once(__DIR__ . '/login_func.php');
+$checkLoginReturn =createUserObjectFromPost();
+if (is_string($checkLoginReturn))
     {
         $gMessage->show($checkLoginReturn);
         // => EXIT
@@ -136,4 +45,4 @@ else
         admRedirect($forwardUrl);
         // => EXIT
     }
-}
+
