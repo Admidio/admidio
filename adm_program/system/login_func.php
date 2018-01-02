5cf925739b20d77bb41547f7bd02ba879813d534
Steffen Köhler <steffen@koehlers.de>
2018-01-02 11:58:22 +0100

    Source code reformated, double return at function end removed

--- adm_program/system/login_func.php
+++ adm_program/system/login_func.php
@@ -11,9 +11,9 @@
 require_once(__DIR__ . '/common.php');
 
 // Initialize parameters
-$bAutoLogin = false;
-$loginname  = '';
-$password   = '';
+$bAutoLogin     = false;
+$loginname      = '';
+$password       = '';
 $organizationId = (int) $gCurrentOrganization->getValue('org_id');
 
 // Filter parameters
@@ -24,89 +24,87 @@ $organizationId = (int) $gCurrentOrganization->getValue('org_id');
 function initLoginParams($prefix)
 {
     global $bAutoLogin, $loginname, $password, $organizationId, $gSettingsManager;
-
-    $loginname = $_POST[$prefix.'usr_login_name'];
-    $password  = $_POST[$prefix.'usr_password'];
-
-    if($gSettingsManager->getBool('enable_auto_login') && array_key_exists($prefix.'auto_login', $_POST) && $_POST[$prefix.'auto_login'] == 1)
-    {
+    
+    $loginname = $_POST[$prefix . 'usr_login_name'];
+    $password  = $_POST[$prefix . 'usr_password'];
+    
+    if ($gSettingsManager->getBool('enable_auto_login') && array_key_exists($prefix . 'auto_login', $_POST) && $_POST[$prefix . 'auto_login'] == 1) {
         $bAutoLogin = true;
     }
     // if user can choose organization then save the selection
-    if(array_key_exists($prefix.'org_id', $_POST) && is_numeric($_POST[$prefix.'org_id']) && $_POST[$prefix.'org_id'] > 0)
-    {
-        $organizationId = (int) $_POST[$prefix.'org_id'];
+    if (array_key_exists($prefix . 'org_id', $_POST) && is_numeric($_POST[$prefix . 'org_id']) && $_POST[$prefix . 'org_id'] > 0) {
+        $organizationId = (int) $_POST[$prefix . 'org_id'];
     }
 }
 
-function createUserObjectFromPost(){
+function createUserObjectFromPost()
+{
     
-    global $gSettingsManager, $gCurrentUser,$loginname, $password, $gDb, $gL10n, $gCurrentOrganization, $bAutoLogin, $organizationId, $gProfileFields, $userStatement, $gCurrentSession ;
-
-if(array_key_exists('usr_login_name', $_POST) && $_POST['usr_login_name'] !== '')
-{
-    initLoginParams('');
-}
-
-if(array_key_exists('plg_usr_login_name', $_POST) && $_POST['plg_usr_login_name'] !== '')
-{
-    initLoginParams('plg_');
-}
-
-if($loginname === '')
-{
-    return $gL10n->get('SYS_FIELD_EMPTY', array($gL10n->get('SYS_USERNAME')));
-    // => EXIT
-}
-
-if($password === '')
-{
-    return $gL10n->get('SYS_FIELD_EMPTY', array($gL10n->get('SYS_PASSWORD')));
-    // => EXIT
-}
-
-// TODO Future: check Password min/max Length
-//if(strlen($password) < PASSWORD_MIN_LENGTH)
-//{
-//    return $gL10n->get('PRO_PASSWORD_LENGTH', array($gL10n->get('SYS_PASSWORD')));
-//}
-
-// Search for username
-$sql = 'SELECT usr_id
-          FROM '.TBL_USERS.'
-         WHERE UPPER(usr_login_name) = UPPER(?)';
-$userStatement = $gDb->queryPrepared($sql, array($loginname));
-
-if ($userStatement->rowCount() === 0)
-{
-    $gLogger->warning('AUTHENTICATION: Incorrect username/password!', array('username' => $loginname, 'password' => '******'));
-
-    return $gL10n->get('SYS_LOGIN_USERNAME_PASSWORD_INCORRECT');
-    // => EXIT
-}
-else
-{
-    // if login organization is different to organization of config file then create new session variables
-    if($organizationId !== (int) $gCurrentOrganization->getValue('org_id'))
-    {
-        // read organization of config file with their preferences
-        $gCurrentOrganization->readDataById($organizationId);
-
-        // read new profile field structure for this organization
-        $gProfileFields->readProfileFields($organizationId);
-
-        // save new organization id to session
-        $gCurrentSession->setValue('ses_org_id', $organizationId);
-        $gCurrentSession->save();
+    global $gSettingsManager, $gCurrentUser, $loginname, $password, $gDb, $gL10n, $gCurrentOrganization, $bAutoLogin, $organizationId, $gProfileFields, $userStatement, $gCurrentSession;
+    
+    if (array_key_exists('usr_login_name', $_POST) && $_POST['usr_login_name'] !== '') {
+        initLoginParams('');
     }
-
-    // create user object
-    $gCurrentUser = new User($gDb, $gProfileFields, (int) $userStatement->fetchColumn());
-
-    return  $gCurrentUser->checkLogin($password, $bAutoLogin);
-
-	return $gCurrentUser->checkLogin($password, $bAutoLogin);
-  }
-	
+    
+    if (array_key_exists('plg_usr_login_name', $_POST) && $_POST['plg_usr_login_name'] !== '') {
+        initLoginParams('plg_');
+    }
+    
+    if ($loginname === '') {
+        return $gL10n->get('SYS_FIELD_EMPTY', array(
+            $gL10n->get('SYS_USERNAME')
+        ));
+        // => EXIT
+    }
+    
+    if ($password === '') {
+        return $gL10n->get('SYS_FIELD_EMPTY', array(
+            $gL10n->get('SYS_PASSWORD')
+        ));
+        // => EXIT
+    }
+    
+    // TODO Future: check Password min/max Length
+    //if(strlen($password) < PASSWORD_MIN_LENGTH)
+    //{
+    //    return $gL10n->get('PRO_PASSWORD_LENGTH', array($gL10n->get('SYS_PASSWORD')));
+    //}
+    
+    // Search for username
+    $sql           = 'SELECT usr_id
+          FROM ' . TBL_USERS . '
+         WHERE UPPER(usr_login_name) = UPPER(?)';
+    $userStatement = $gDb->queryPrepared($sql, array(
+        $loginname
+    ));
+    
+    if ($userStatement->rowCount() === 0) {
+        $gLogger->warning('AUTHENTICATION: Incorrect username/password!', array(
+            'username' => $loginname,
+            'password' => '******'
+        ));
+        
+        return $gL10n->get('SYS_LOGIN_USERNAME_PASSWORD_INCORRECT');
+        // => EXIT
+    } else {
+        // if login organization is different to organization of config file then create new session variables
+        if ($organizationId !== (int) $gCurrentOrganization->getValue('org_id')) {
+            // read organization of config file with their preferences
+            $gCurrentOrganization->readDataById($organizationId);
+            
+            // read new profile field structure for this organization
+            $gProfileFields->readProfileFields($organizationId);
+            
+            // save new organization id to session
+            $gCurrentSession->setValue('ses_org_id', $organizationId);
+            $gCurrentSession->save();
+        }
+        
+        // create user object
+        $gCurrentUser = new User($gDb, $gProfileFields, (int) $userStatement->fetchColumn());
+        
+        return $gCurrentUser->checkLogin($password, $bAutoLogin);
+        
+    }
+    
 }
-
