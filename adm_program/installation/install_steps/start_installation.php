76f5362218adb4fe0a1d355b0df0e362428727bd
Thomas Rupprecht <rupprecht.thomas@gmail.com>
2018-01-20 00:35:18 +0100

    fix installation

--- adm_program/installation/install_steps/start_installation.php
+++ adm_program/installation/install_steps/start_installation.php
@@ -129,8 +129,8 @@ $sql = 'INSERT INTO '.TBL_ROLES_RIGHTS.'
 $db->queryPrepared($sql);
 
 // add edit categories right with reference to parent right
-$sql = 'INSERT INTO %PREFIX%_roles_rights (ror_name_intern, ror_table, ror_ror_id_parent)
-        VALUES (\'category_edit\', \'adm_categories\', (SELECT rr.ror_id FROM %PREFIX%_roles_rights rr WHERE rr.ror_name_intern = \'category_view\'))'
+$sql = 'INSERT INTO '.TBL_ROLES_RIGHTS.' (ror_name_intern, ror_table, ror_ror_id_parent)
+        VALUES (\'category_edit\', \'adm_categories\', (SELECT rr.ror_id FROM '.TBL_ROLES_RIGHTS.' rr WHERE rr.ror_name_intern = \'category_view\'))';
 $db->queryPrepared($sql);
 
 // create profile fields of category master data
