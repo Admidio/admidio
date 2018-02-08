e87f3600a559af4de1de1db0647fdf262bb7237f
Thomas Rupprecht <rupprecht.thomas@gmail.com>
2018-02-08 19:23:43 +0100

    remove self called check from phpinfo

--- adm_program/system/phpinfo.php
+++ adm_program/system/phpinfo.php
@@ -8,10 +8,6 @@
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
  ***********************************************************************************************
  */
-if (basename($_SERVER['SCRIPT_FILENAME']) === 'phpinfo.php')
-{
-    exit('This page may not be called directly!');
-}
 
 require_once(__DIR__ . '/common.php');
 require(__DIR__ . '/login_valid.php');
