9ed83a4caee00ec181d4e86854839650f4dd7c91
Thomas Rupprecht <rupprecht.thomas@gmail.com>
2018-02-19 17:45:14 +0100

    remove v2.x update code

--- adm_program/installation/db_scripts/upd_2_4_5_db.sql
+++
@@ -1,9 +0,0 @@
-/**
- ***********************************************************************************************
- * @copyright 2004-2018 The Admidio Team
- * @see https://www.admidio.org/
- * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
- ***********************************************************************************************
- */
-
-UPDATE %PREFIX%_preferences SET prf_value = '0' WHERE prf_name like 'mail_sender_into_to';
