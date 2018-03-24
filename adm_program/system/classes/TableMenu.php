2fcbc94dba17b934e0bd2df314881d90eda5e136
Thomas Rupprecht <rupprecht.thomas@gmail.com>
2018-03-24 19:40:50 +0100

    add missing param types

--- adm_program/system/classes/TableMenu.php
+++ adm_program/system/classes/TableMenu.php
@@ -32,10 +32,10 @@ class TableMenu extends TableAccess
     /**
      * Constructor that will create an object of a recordset of the table adm_category.
      * If the id is set than the specific category will be loaded.
-     * @param \Database $database Object of the class Database. This should be the default global object **$gDb**.
+     * @param Database $database Object of the class Database. This should be the default global object **$gDb**.
      * @param int       $menId    The recordset of the category with this id will be loaded. If id isn't set than an empty object of the table is created.
      */
-    public function __construct($database, $menId = 0)
+    public function __construct(Database $database, $menId = 0)
     {
         parent::__construct($database, TBL_MENU, 'men', $menId);
     }
