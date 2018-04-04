ec5ce5c49cb89d2ba7103ea2b282c304104ccf53
Thomas Rupprecht <rupprecht.thomas@gmail.com>
2018-04-04 23:42:06 +0200

    cleanup const logging

--- adm_program/system/logging.php
+++ adm_program/system/logging.php
@@ -78,7 +78,7 @@ function initLogging(\Psr\Log\LoggerInterface $logger)
             'ADMIDIO' => ADMIDIO_VERSION_TEXT,
             'PHP'     => PHP_VERSION
         ),
-        'ADMIDIO_HOMEPAGE' => ADMIDIO_HOMEPAGE,
+        'SCHEME'           => SCHEME,
         'HTTPS'            => HTTPS,
         'PORT'             => PORT,
         'HOST'             => HOST,
@@ -93,16 +93,6 @@ function initLogging(\Psr\Log\LoggerInterface $logger)
             'WWW_PATH'     => WWW_PATH, // Will get "SERVER_PATH" in v4.0
             'ADMIDIO_PATH' => ADMIDIO_PATH,
             'CURRENT_PATH' => CURRENT_PATH
-        ),
-        'FOLDERS' => array(
-            'FOLDER_DATA'        => FOLDER_DATA,
-            'FOLDER_CLASSES'     => FOLDER_CLASSES,
-            'FOLDER_LIBS_SERVER' => FOLDER_LIBS_SERVER,
-            'FOLDER_LIBS_CLIENT' => FOLDER_LIBS_CLIENT,
-            'FOLDER_LANGUAGES'   => FOLDER_LANGUAGES,
-            'FOLDER_THEMES'      => FOLDER_THEMES,
-            'FOLDER_MODULES'     => FOLDER_MODULES,
-            'FOLDER_PLUGINS'     => FOLDER_PLUGINS
         )
     );
     $logger->info('CONSTANTS: URLS & PATHS & FOLDERS', $constants);
