4767ff8487edce786986ebf55bea80081ecc8312
Thomas Rupprecht <rupprecht.thomas@gmail.com>
2018-04-15 10:59:56 +0200

    fix empty country in dates

--- adm_program/system/classes/TableDate.php
+++ adm_program/system/classes/TableDate.php
@@ -293,7 +293,7 @@ class TableDate extends TableAccess
 
         if ($format !== 'database')
         {
-            if ($columnName === 'dat_country' && $value !== '')
+            if ($columnName === 'dat_country' && $value)
             {
                 // beim Land die sprachabhaengige Bezeichnung auslesen
                 $value = $gL10n->getCountryName($value);
