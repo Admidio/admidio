f03105116397106346eea456713f536b002a38a9
Markus Faßbender <markus.fassbender@gmail.com>
2020-12-12 07:04:24 +0100

    rename download to documents in demo data

--- demo_data/data.sql
+++ demo_data/data.sql
@@ -667,10 +667,10 @@ INSERT INTO %PREFIX%_dates (dat_id, dat_cat_id, dat_begin, dat_end, dat_all_day,
 --
 
 INSERT INTO %PREFIX%_folders (fol_id, fol_org_id, fol_fol_id_parent, fol_type, fol_name, fol_description, fol_path, fol_locked, fol_public, fol_usr_id, fol_timestamp) VALUES
-(1, 1, NULL, 'DOWNLOAD', 'download_demo', NULL, '/adm_my_files', 0, 1, 1, '2014-01-01 22:35:07'),
-(2, 1, 1, 'DOWNLOAD', 'licenses', NULL, '/adm_my_files/download_demo', 0, 1, 1, '2014-02-05 14:05:34'),
-(3, 1, 1, 'DOWNLOAD', 'board-meeting', NULL, '/adm_my_files/download_demo', 0, 0, 354, '2016-04-15 13:25:06'),
-(100, 2, NULL, 'DOWNLOAD', 'download_test', NULL, '/adm_my_files', 0, 1, 1, '2014-01-01 22:35:07');
+(1, 1, NULL, 'DOCUMENTS', 'documents_demo', NULL, '/adm_my_files', 0, 1, 1, '2014-01-01 22:35:07'),
+(2, 1, 1, 'DOCUMENTS', 'licenses', NULL, '/adm_my_files/documents_demo', 0, 1, 1, '2014-02-05 14:05:34'),
+(3, 1, 1, 'DOCUMENTS', 'board-meeting', NULL, '/adm_my_files/documents_demo', 0, 0, 354, '2016-04-15 13:25:06'),
+(100, 2, NULL, 'DOCUMENTS', 'download_test', NULL, '/adm_my_files', 0, 1, 1, '2014-01-01 22:35:07');
 
 
 --
