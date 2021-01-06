daa5869dc616368cd496324ab97a76335e04da1d
Markus Faßbender <markus.fassbender@gmail.com>
2021-01-06 08:11:26 +0100

    happy new year :)

--- adm_program/index.php
+++ adm_program/index.php
@@ -3,7 +3,7 @@
  ***********************************************************************************************
  * index.php for backward compatibility to Admidio 3
  *
- * @copyright 2004-2020 The Admidio Team
+ * @copyright 2004-2021 The Admidio Team
  * @see https://www.admidio.org/
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
  ***********************************************************************************************
