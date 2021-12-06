74dfb4b80582e4b2e1d0d965de14e1f56556e9f4
Markus Faßbender <markus.fassbender@gmail.com>
2021-12-06 18:21:41 +0100

    Cross-site Scripting (XSS) when redirect an url

--- adm_program/system/redirect.php
+++ adm_program/system/redirect.php
@@ -19,7 +19,7 @@
 require_once(__DIR__ . '/common.php');
 
 // Initialize and check the parameters
-$getUrl = admFuncVariableIsValid($_GET, 'url', 'string', array('requireValue' => true));
+$getUrl = admFuncVariableIsValid($_GET, 'url', 'url', array('requireValue' => true));
 
 if (filter_var($getUrl, FILTER_VALIDATE_URL) === false) {
     $gMessage->show($gL10n->get('SYS_REDIRECT_URL_INVALID'));
