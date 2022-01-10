996c0640427ab27a7e50c48a675f77dabc364a5c
Markus Faßbender <markus.fassbender@gmail.com>
2022-01-10 18:05:09 +0100

    first entry of selectbox should be empty string

--- adm_program/system/classes/HtmlForm.php
+++ adm_program/system/classes/HtmlForm.php
@@ -1279,7 +1279,7 @@ class HtmlForm extends HtmlFormBasic
                 $value = Language::translateIfTranslationStrId($value);
 
                 // set the value attribute of the option tag
-                $optionValue = $key;
+                $optionValue = ($key === 0 ? '' : $key);
 
                 if ($optionsAll['arrayKeyIsNotValue']) {
                     $optionValue = $value;
