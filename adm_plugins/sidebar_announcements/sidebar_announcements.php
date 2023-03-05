b5ff07d1c377b36be9652bc5d3b54e52644f1bbd
Markus Faßbender <markus.fassbender@gmail.com>
2023-03-05 08:55:28 +0100

    Special chars like quote not shown in plugin sidebar announcement #1372

--- adm_plugins/sidebar_announcements/sidebar_announcements.php
+++ adm_plugins/sidebar_announcements/sidebar_announcements.php
@@ -6,14 +6,12 @@
  * Plugin that lists the latest announcements in a slim interface and
  * can thus be ideally used in a sidebar
  *
- * Compatible with Admidio version 4.1
- *
  * @copyright 2004-2023 The Admidio Team
  * @see https://www.admidio.org/
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
  ***********************************************************************************************
  */
-$rootPath = dirname(dirname(__DIR__));
+$rootPath = dirname(__DIR__, 2);
 $pluginFolder = basename(__DIR__);
 
 require_once($rootPath . '/adm_program/system/common.php');
@@ -27,7 +25,7 @@ $getCatId    = admFuncVariableIsValid($_GET, 'cat_id', 'int');
 $getDateFrom = admFuncVariableIsValid($_GET, 'date_from', 'date');
 $getDateTo   = admFuncVariableIsValid($_GET, 'date_to', 'date');
 
-// set default values if there no value has been stored in the config.php
+// set default values if no value has been stored in the config.php
 if (!isset($plg_announcements_count) || !is_numeric($plg_announcements_count)) {
     $plg_announcements_count = 2;
 }
@@ -96,8 +94,8 @@ if ($gSettingsManager->getInt('enable_announcements_module') > 0) {
                 if ($plg_max_char_per_word > 0) {
                     $plgNewHeadline = '';
 
-                    // Woerter unterbrechen, wenn sie zu lang sind
-                    $plgWords = explode(' ', SecurityUtils::encodeHTML($plgAnnouncement->getValue('ann_headline')));
+                    // Interrupt words if they are too long
+                    $plgWords = explode(' ', $plgAnnouncement->getValue('ann_headline'));
 
                     foreach ($plgWords as $plgValue) {
                         if (strlen($plgValue) > $plg_max_char_per_word) {
@@ -109,7 +107,7 @@ if ($gSettingsManager->getInt('enable_announcements_module') > 0) {
                     }
                     echo $plgNewHeadline.'</a></h5>';
                 } else {
-                    echo SecurityUtils::encodeHTML($plgAnnouncement->getValue('ann_headline')).'</a></h5>';
+                    echo $plgAnnouncement->getValue('ann_headline').'</a></h5>';
                 }
 
                 // show preview text
