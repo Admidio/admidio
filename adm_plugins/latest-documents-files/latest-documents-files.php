34ac754785a67d2b33b2674e069424401b78e797
Markus Faßbender <markus.fassbender@gmail.com>
2024-03-31 19:45:39 +0200

    remove btn-group-vertical

--- adm_plugins/latest-documents-files/latest-documents-files.php
+++ adm_plugins/latest-documents-files/latest-documents-files.php
@@ -99,7 +99,7 @@ if (Component::isVisible('DOCUMENTS-FILES')) {
                 }
 
                 echo '<li class="list-group-item">
-                    <a class="btn admidio-icon-link" data-bs-toggle="tooltip" data-html="true" title="' . $tooltip . '" href="' . SecurityUtils::encodeUrl(ADMIDIO_URL . FOLDER_MODULES . '/documents-files/get_file.php', array('file_uuid' => $rowFile['fil_uuid'])) . '">' .
+                    <a class="admidio-icon-link" data-bs-toggle="tooltip" data-html="true" title="' . $tooltip . '" href="' . SecurityUtils::encodeUrl(ADMIDIO_URL . FOLDER_MODULES . '/documents-files/get_file.php', array('file_uuid' => $rowFile['fil_uuid'])) . '">' .
                     '<i class="fas ' . $file->getFontAwesomeIcon() . '"></i>' . $fileName . '.' . $file->getFileExtension() . '</a>
                 </li>';
 
@@ -113,7 +113,7 @@ if (Component::isVisible('DOCUMENTS-FILES')) {
 
         if ($countVisibleDownloads > 0) {
             echo '<li class="list-group-item">
-                <a class="btn admidio-icon-link" href="' . ADMIDIO_URL . FOLDER_MODULES . '/documents-files/documents_files.php"><i class="fas fa-list"></i>' . $gL10n->get('PLG_LATEST_FILES_MORE_DOWNLOADS') . '</a>
+                <a class="admidio-icon-link" href="' . ADMIDIO_URL . FOLDER_MODULES . '/documents-files/documents_files.php"><i class="fas fa-list"></i>' . $gL10n->get('PLG_LATEST_FILES_MORE_DOWNLOADS') . '</a>
             </li>';
         }
         echo '</ul>';
