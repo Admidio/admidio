a8b4ad149dfdcac21b88a3d839bd29e22fd1bd48
Markus Faßbender <markus.fassbender@gmail.com>
2024-06-15 12:37:27 +0200

    hide element after deleting

--- adm_program/modules/profile/profile.php
+++ adm_program/modules/profile/profile.php
@@ -869,7 +869,7 @@ if ($gSettingsManager->getBool('contacts_user_relations_enabled')) {
                     class="bi bi-pencil-square" data-bs-toggle="tooltip" title="'.$gL10n->get('SYS_EDIT_USER_IN_RELATION').'"></i></a>';
             }
 
-            $page->addHtml('<li id="row_ure_'.(int) $relation->getValue('ure_id').'" class="list-group-item">');
+            $page->addHtml('<li id="row_ure_'.$relation->getValue('ure_uuid').'" class="list-group-item">');
             $page->addHtml('<div>');
             $page->addHtml('<span>'.$relationName.' - <a href="'.SecurityUtils::encodeUrl(ADMIDIO_URL.FOLDER_MODULES.'/profile/profile.php', array('user_uuid' => $otherUser->getValue('usr_uuid'))).
                            '">'.$otherUser->getValue('FIRST_NAME') . ' ' . $otherUser->getValue('LAST_NAME').'</a> ' . $editUserIcon . '<span>');
