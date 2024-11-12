7d89429153128e71515dc1e2e7443a8f2a23c92e
Markus Faßbender <markus.fassbender@gmail.com>
2024-11-12 20:01:08 +0100

    move code to the preferences module

--- adm_program/modules/preferences.php
+++ adm_program/modules/preferences.php
@@ -19,6 +19,7 @@
  * panel    : The name of the preferences panel that should be shown or saved.
  ***********************************************************************************************
  */
+
 use Admidio\Exception;
 use Admidio\UserInterface\Preferences;
 
@@ -37,25 +38,6 @@ try {
     // only administrators are allowed to view, edit organization preferences or create new organizations
     if (!$gCurrentUser->isAdministrator()) {
         throw new Exception('SYS_NO_RIGHTS');
-    }
-
-    /**
-     * @param string $folder
-     * @param string $templateName
-     * @return string
-     */
-    function getTemplateFileName(string $folder, string $templateName): string
-    {
-        // get all files from the folder
-        $files = array_keys(FileSystemUtils::getDirectoryContent($folder, false, false, array(FileSystemUtils::CONTENT_TYPE_FILE)));
-        $templateFileName = '';
-
-        foreach ($files as $fileName) {
-            if ($templateName === ucfirst(preg_replace('/[_-]/', ' ', str_replace(array('.tpl', '.html', '.txt'), '', $fileName)))) {
-                $templateFileName = $fileName;
-            }
-        }
-        return $templateFileName;
     }
 
     switch ($getMode) {
@@ -77,77 +59,8 @@ try {
             $page->show();
             break;
         case 'save':
-            // check form field input and sanitized it from malicious content
-            $preferencesForm = $gCurrentSession->getFormObject($_POST['admidio-csrf-token']);
-            $formValues = $preferencesForm->validate($_POST);
-
-            // first check the fields of the submitted form
-            switch ($getPanel) {
-                case 'Common':
-                    if (!StringUtils::strIsValidFolderName($_POST['theme'])
-                        || !is_file(ADMIDIO_PATH . FOLDER_THEMES . '/' . $_POST['theme'] . '/index.html')) {
-                        throw new Exception('ORG_INVALID_THEME');
-                    }
-                    break;
-
-                case 'Security':
-                    if (!isset($_POST['enable_auto_login']) && $gSettingsManager->getBool('enable_auto_login')) {
-                        // if auto login was deactivated than delete all saved logins
-                        $sql = 'DELETE FROM ' . TBL_AUTO_LOGIN;
-                        $gDb->queryPrepared($sql);
-                    }
-                    break;
-
-                case 'RegionalSettings':
-                    if (!StringUtils::strIsValidFolderName($_POST['system_language'])
-                        || !is_file(ADMIDIO_PATH . FOLDER_LANGUAGES . '/' . $_POST['system_language'] . '.xml')) {
-                        throw new Exception('SYS_FIELD_EMPTY', array('SYS_LANGUAGE'));
-                    }
-                    break;
-
-                case 'Messages':
-                    // get real filename of the template file
-                    if ($_POST['mail_template'] !== $gSettingsManager->getString('mail_template')) {
-                        $formValues['mail_template'] = getTemplateFileName(ADMIDIO_PATH . FOLDER_DATA . '/mail_templates', $_POST['mail_template']);
-                    }
-                    break;
-
-                case 'Photos':
-                    // get real filename of the template file
-                    if ($_POST['photo_ecard_template'] !== $gSettingsManager->getString('photo_ecard_template')) {
-                        $formValues['photo_ecard_template'] = getTemplateFileName(ADMIDIO_PATH . FOLDER_DATA . '/ecard_templates', $_POST['photo_ecard_template']);
-                    }
-                    break;
-            }
-
-            // then update the database with the new values
-
-            foreach ($formValues as $key => $value) {
-                // Sort out elements that are not stored in adm_preferences here
-                if (!in_array($key, array('save', 'admidio-csrf-token'))) {
-                    if (str_starts_with($key, 'SYSMAIL_')) {
-                        $text = new TableText($gDb);
-                        $text->readDataByColumns(array('txt_org_id' => $gCurrentOrgId, 'txt_name' => $key));
-                        $text->setValue('txt_text', $value);
-                        $text->save();
-                    } elseif ($key === 'enable_auto_login' && $value == 0 && $gSettingsManager->getBool('enable_auto_login')) {
-                        // if deactivate auto login than delete all saved logins
-                        $sql = 'DELETE FROM ' . TBL_AUTO_LOGIN;
-                        $gDb->queryPrepared($sql);
-                        $gSettingsManager->set($key, $value);
-                    } else {
-                        $gSettingsManager->set($key, $value);
-                    }
-                }
-            }
-
-            // refresh language if necessary
-            if ($gL10n->getLanguage() !== $gSettingsManager->getString('system_language')) {
-                $gL10n->setLanguage($gSettingsManager->getString('system_language'));
-            }
-
-            // clean up
-            $gCurrentSession->reloadAllSessions();
+            $preferences = new Admidio\Modules\Preferences();
+            $preferences->save($getPanel, $_POST);
 
             echo json_encode(array('status' => 'success', 'message' => $gL10n->get('SYS_SAVE_DATA')));
             break;
@@ -161,48 +74,19 @@ try {
 
         // set directory protection, write htaccess
         case 'htaccess':
-            if (is_file(ADMIDIO_PATH . FOLDER_DATA . '/.htaccess')) {
+            $preferences = new Admidio\Modules\Preferences();
+            if ($preferences->setHtaccessProtection()) {
                 echo $gL10n->get('SYS_ON');
-                return;
+            } else {
+                echo $gL10n->get('SYS_OFF');
             }
-
-            // create ".htaccess" file for folder "adm_my_files"
-            $htaccess = new Htaccess(ADMIDIO_PATH . FOLDER_DATA);
-            if ($htaccess->protectFolder()) {
-                echo $gL10n->get('SYS_ON');
-                return;
-            }
-
-            $gLogger->warning('htaccess file could not be created!');
-
-            echo $gL10n->get('SYS_OFF');
             break;
 
         // send test email
         case 'test_email':
             $debugOutput = '';
-
-            $email = new Email();
-            $email->setDebugMode(true);
-
-            if ($gSettingsManager->getBool('mail_html_registered_users')) {
-                $email->setHtmlMail();
-            }
-
-            // set email data
-            $email->setSender($gCurrentOrganization->getValue('org_email_administrator'), $gL10n->get('SYS_ADMINISTRATOR'));
-            $email->addRecipientsByUser($gCurrentUser->getValue('usr_uuid'));
-            $email->setSubject($gL10n->get('SYS_EMAIL_FUNCTION_TEST', array($gCurrentOrganization->getValue('org_longname', 'database'))));
-            $email->setTemplateText(
-                $gL10n->get('SYS_EMAIL_FUNCTION_TEST_CONTENT', array($gCurrentOrganization->getValue('org_homepage'), $gCurrentOrganization->getValue('org_longname'))),
-                $gCurrentUser->getValue('FIRSTNAME') . ' ' . $gCurrentUser->getValue('LASTNAME'),
-                $gCurrentUser->getValue('EMAIL'),
-                $gCurrentUser->getValue('usr_uuid'),
-                $gL10n->get('SYS_ADMINISTRATOR')
-            );
-
-            // finally send the mail
-            $sendResult = $email->sendEmail();
+            $preferences = new Admidio\Modules\Preferences();
+            $sendResult = $preferences->sendTestEmail();
 
             if (isset($GLOBALS['phpmailer_output_debug'])) {
                 $debugOutput .= '<br /><br /><h3>' . $gL10n->get('SYS_DEBUG_OUTPUT') . '</h3>' . $GLOBALS['phpmailer_output_debug'];
@@ -237,7 +121,7 @@ try {
             echo $preferences->showUpdateInfo();
             break;
     }
-} catch (Exception $exception) {
+} catch (Throwable $exception) {
     if (in_array($getMode, array('save', 'new_org_create'))) {
         echo json_encode(array('status' => 'error', 'message' => $exception->getMessage()));
     } elseif ($getMode === 'html_form') {
--- src/Modules/Preferences.php
+++ src/Modules/Preferences.php
@@ -1,7 +1,13 @@
 <?php
+
 namespace Admidio\Modules;
 
 use Admidio\Exception;
+use Email;
+use FileSystemUtils;
+use Htaccess;
+use StringUtils;
+use TableText;
 
 /**
  * @brief Class with methods to display the module pages.
@@ -52,6 +58,25 @@ class Preferences
         }
 
         return $update;
+    }
+
+    /**
+     * @param string $folder
+     * @param string $templateName
+     * @return string
+     */
+    static function getTemplateFileName(string $folder, string $templateName): string
+    {
+        // get all files from the folder
+        $files = array_keys(FileSystemUtils::getDirectoryContent($folder, false, false, array(FileSystemUtils::CONTENT_TYPE_FILE)));
+        $templateFileName = '';
+
+        foreach ($files as $fileName) {
+            if ($templateName === ucfirst(preg_replace('/[_-]/', ' ', str_replace(array('.tpl', '.html', '.txt'), '', $fileName)))) {
+                $templateFileName = $fileName;
+            }
+        }
+        return $templateFileName;
     }
 
     /**
@@ -150,22 +175,161 @@ class Preferences
         <p>' . $gL10n->get('SYS_INSTALLED') . ':&nbsp;' . ADMIDIO_VERSION_TEXT . '</p>
         <p>' . $gL10n->get('SYS_AVAILABLE') . ':&nbsp;
             <a href="' . ADMIDIO_HOMEPAGE . 'download.php" title="' . $gL10n->get('SYS_ADMIDIO_DOWNLOAD_PAGE') . '" target="_blank">' .
-                '<i class="bi bi-link"></i>' . $stableVersion . '
+            '<i class="bi bi-link"></i>' . $stableVersion . '
             </a>
             <br />
             ' . $gL10n->get('SYS_AVAILABLE_BETA') . ': &nbsp;';
 
-            if ($versionUpdate !== 99 && $betaVersion !== 'n/a') {
-                $html .= '
-                <a href="' . ADMIDIO_HOMEPAGE . 'download.php" title="' . $gL10n->get('SYS_ADMIDIO_DOWNLOAD_PAGE') . '" target="_blank">' .
-                        '<i class="bi bi-link"></i>' . $betaVersion . ' Beta ' . $betaRelease . '
-                </a>';
-            } else {
-                $html .= $betaVersion;
-            }
+        if ($versionUpdate !== 99 && $betaVersion !== 'n/a') {
             $html .= '
+                <a href="' . ADMIDIO_HOMEPAGE . 'download.php" title="' . $gL10n->get('SYS_ADMIDIO_DOWNLOAD_PAGE') . '" target="_blank">' .
+                '<i class="bi bi-link"></i>' . $betaVersion . ' Beta ' . $betaRelease . '
+                </a>';
+        } else {
+            $html .= $betaVersion;
+        }
+        $html .= '
         </p>
         <strong>' . $versionsText . '</strong>';
         return $html;
+    }
+
+    /**
+     * Save all form data of the panel to the database.
+     * @param string $panel Name of the panel for which the data should be saved.
+     * @param array $formData All form data of the panel.
+     * @return void
+     * @throws Exception
+     */
+    public function save(string $panel, array $formData)
+    {
+        global $gL10n, $gSettingsManager, $gCurrentSession, $gDb, $gCurrentOrgId;
+
+        // check form field input and sanitized it from malicious content
+        $preferencesForm = $gCurrentSession->getFormObject($formData['admidio-csrf-token']);
+        $formValues = $preferencesForm->validate($formData);
+
+        // first check the fields of the submitted form
+        switch ($panel) {
+            case 'Common':
+                if (!StringUtils::strIsValidFolderName($formData['theme'])
+                    || !is_file(ADMIDIO_PATH . FOLDER_THEMES . '/' . $formData['theme'] . '/index.html')) {
+                    throw new Exception('ORG_INVALID_THEME');
+                }
+                break;
+
+            case 'Security':
+                if (!isset($formData['enable_auto_login']) && $gSettingsManager->getBool('enable_auto_login')) {
+                    // if auto login was deactivated than delete all saved logins
+                    $sql = 'DELETE FROM ' . TBL_AUTO_LOGIN;
+                    $gDb->queryPrepared($sql);
+                }
+                break;
+
+            case 'RegionalSettings':
+                if (!StringUtils::strIsValidFolderName($formData['system_language'])
+                    || !is_file(ADMIDIO_PATH . FOLDER_LANGUAGES . '/' . $formData['system_language'] . '.xml')) {
+                    throw new Exception('SYS_FIELD_EMPTY', array('SYS_LANGUAGE'));
+                }
+                break;
+
+            case 'Messages':
+                // get real filename of the template file
+                if ($formData['mail_template'] !== $gSettingsManager->getString('mail_template')) {
+                    $formValues['mail_template'] = $this->getTemplateFileName(ADMIDIO_PATH . FOLDER_DATA . '/mail_templates', $formData['mail_template']);
+                }
+                break;
+
+            case 'Photos':
+                // get real filename of the template file
+                if ($formData['photo_ecard_template'] !== $gSettingsManager->getString('photo_ecard_template')) {
+                    $formValues['photo_ecard_template'] = $this->getTemplateFileName(ADMIDIO_PATH . FOLDER_DATA . '/ecard_templates', $formData['photo_ecard_template']);
+                }
+                break;
+        }
+
+        // then update the database with the new values
+
+        foreach ($formValues as $key => $value) {
+            // Sort out elements that are not stored in adm_preferences here
+            if (!in_array($key, array('save', 'admidio-csrf-token'))) {
+                if (str_starts_with($key, 'SYSMAIL_')) {
+                    $text = new TableText($gDb);
+                    $text->readDataByColumns(array('txt_org_id' => $gCurrentOrgId, 'txt_name' => $key));
+                    $text->setValue('txt_text', $value);
+                    $text->save();
+                } elseif ($key === 'enable_auto_login' && $value == 0 && $gSettingsManager->getBool('enable_auto_login')) {
+                    // if deactivate auto login than delete all saved logins
+                    $sql = 'DELETE FROM ' . TBL_AUTO_LOGIN;
+                    $gDb->queryPrepared($sql);
+                    $gSettingsManager->set($key, $value);
+                } else {
+                    $gSettingsManager->set($key, $value);
+                }
+            }
+        }
+
+        // refresh language if necessary
+        if ($gL10n->getLanguage() !== $gSettingsManager->getString('system_language')) {
+            $gL10n->setLanguage($gSettingsManager->getString('system_language'));
+        }
+
+        // clean up
+        $gCurrentSession->reloadAllSessions();
+    }
+
+    /**
+     * Sends a test email to the email address of the organization.
+     * @return bool Returns **true** if the email could be sent successfully otherwise **false**.
+     * @throws Exception
+     */
+    public function sendTestEmail(): bool
+    {
+        global $gSettingsManager, $gCurrentOrganization, $gCurrentUser, $gL10n;
+
+        $email = new Email();
+        $email->setDebugMode(true);
+
+        if ($gSettingsManager->getBool('mail_html_registered_users')) {
+            $email->setHtmlMail();
+        }
+
+        // set email data
+        $email->setSender($gCurrentOrganization->getValue('org_email_administrator'), $gL10n->get('SYS_ADMINISTRATOR'));
+        $email->addRecipientsByUser($gCurrentUser->getValue('usr_uuid'));
+        $email->setSubject($gL10n->get('SYS_EMAIL_FUNCTION_TEST', array($gCurrentOrganization->getValue('org_longname', 'database'))));
+        $email->setTemplateText(
+            $gL10n->get('SYS_EMAIL_FUNCTION_TEST_CONTENT', array($gCurrentOrganization->getValue('org_homepage'), $gCurrentOrganization->getValue('org_longname'))),
+            $gCurrentUser->getValue('FIRSTNAME') . ' ' . $gCurrentUser->getValue('LASTNAME'),
+            $gCurrentUser->getValue('EMAIL'),
+            $gCurrentUser->getValue('usr_uuid'),
+            $gL10n->get('SYS_ADMINISTRATOR')
+        );
+
+        // finally send the mail
+        return $email->sendEmail();
+    }
+
+    /**
+     * Check if the data folder adm_my_files is protected through a htaccess file. If it's not
+     * protected the function tries to create a htaccess file.
+     * @return bool Returns **true** if the folder is protected otherwise **false**
+     */
+    public function setHtaccessProtection(): bool
+    {
+        global $gLogger;
+
+        if (is_file(ADMIDIO_PATH . FOLDER_DATA . '/.htaccess')) {
+            return true;
+        }
+
+        // create ".htaccess" file for folder "adm_my_files"
+        $htaccess = new Htaccess(ADMIDIO_PATH . FOLDER_DATA);
+        if ($htaccess->protectFolder()) {
+            return true;
+        }
+
+        $gLogger->warning('htaccess file could not be created!');
+        return false;
     }
 }
