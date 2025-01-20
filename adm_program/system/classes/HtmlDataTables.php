3647e31de245f0c6326ce15a61aa5ecae3f65ef1
MightyMCoder <130976036+MightyMCoder@users.noreply.github.com>
2025-01-20 20:23:33 +0100

    Fix HtmlDataTable instance naming to avoid conflicts when adding multiple table instances on one page

--- adm_program/system/classes/HtmlDataTables.php
+++ adm_program/system/classes/HtmlDataTables.php
@@ -172,11 +172,11 @@ class HtmlDataTables
             $javascriptGroupFunction = '
                 // Order by the grouping
                 $("#' . $this->id . ' tbody").on("click", "tr.admidio-group-heading", function() {
-                    const currentOrder = admidioTable.order()[0];
+                    const currentOrder = admidioTable_'. $this->id . '.order()[0];
                     if (currentOrder[0] === ' . $this->groupedColumn . ' && currentOrder[1] === "asc") {
-                        admidioTable.order([' . $this->groupedColumn . ', "desc"]).draw();
+                        admidioTable_'. $this->id . '.order([' . $this->groupedColumn . ', "desc"]).draw();
                     } else {
-                        admidioTable.order([' . $this->groupedColumn . ', "asc"]).draw();
+                        admidioTable_'. $this->id . '.order([' . $this->groupedColumn . ', "asc"]).draw();
                     }
                 });';
         }
@@ -197,7 +197,7 @@ class HtmlDataTables
         }
         $this->htmlPage->addJavascript(
             '
-            const admidioTable = $("#' . $this->id . '").DataTable({' .
+            const admidioTable_'. $this->id . ' = $("#' . $this->id . '").DataTable({' .
             implode(',', $this->datatablesInitParameters) .
             $javascriptGroup . '
             });
