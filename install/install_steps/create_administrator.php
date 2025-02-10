88f0dfb526af7a4a40992b670a39edd0879d0843
MightyMCoder <130976036+MightyMCoder@users.noreply.github.com>
2025-02-10 21:51:20 +0100

    fix: Exception usage "expecting int but string[] given"

--- install/install_steps/create_administrator.php
+++ install/install_steps/create_administrator.php
@@ -9,6 +9,7 @@
  ***********************************************************************************************
  */
 
+use Admidio\Infrastructure\Exception;
 use Admidio\Infrastructure\Utils\PasswordUtils;
 use Admidio\Infrastructure\Utils\SecurityUtils;
 use Admidio\Infrastructure\Utils\StringUtils;
