5d09760465fcc7df4a3df4f9e04a9803ac5e980c
Mathias Huth <matze199730@googlemail.com>
2025-06-02 16:47:08 +0200

    redesign overview

--- modules/overview.php
+++ modules/overview.php
@@ -28,6 +28,20 @@ try {
     // create html page object and load template file
     $page = PagePresenter::withHtmlIDAndHeadline('admidio-overview', $headline);
     $page->setContentFullWidth();
+
+    // create a list of all overview plugins and their visibility
+    $overviewPlugins = array(
+        'login-form' => array('name' => 'login_form', 'file' => 'index.php', 'show' => true),
+        'birthday' => array('name' => 'birthday', 'file' => 'index.php', 'show' => ($gSettingsManager->getInt('overview_plugin_birthday_enabled') === 1 || ($gSettingsManager->getInt('overview_plugin_birthday_enabled') === 2 && $gValidLogin)) ? true : false),
+        'calendar' => array('name' => 'calendar', 'file' => 'index.php', 'show' => ($gSettingsManager->getInt('overview_plugin_calendar_enabled') === 1 || ($gSettingsManager->getInt('overview_plugin_calendar_enabled') === 2 && $gValidLogin)) ? true : false),
+        'random-photo' => array('name' => 'random_photo', 'file' => 'index.php', 'show' => ($gSettingsManager->getInt('overview_plugin_random_photo_enabled') === 1 || ($gSettingsManager->getInt('overview_plugin_random_photo_enabled') === 2 && $gValidLogin)) ? true : false),
+        'latest-documents-files' => array('name' => 'latest-documents-files', 'file' => 'index.php', 'show' => ($gSettingsManager->getInt('overview_plugin_latest_documents_files_enabled') === 1 || ($gSettingsManager->getInt('overview_plugin_latest_documents_files_enabled') === 2 && $gValidLogin)) ? true : false),
+        'announcement-list' => array('name' => 'announcement-list', 'file' => 'index.php', 'show' => ($gSettingsManager->getInt('overview_plugin_announcement_list_enabled') === 1 || ($gSettingsManager->getInt('overview_plugin_announcement_list_enabled') === 2 && $gValidLogin)) ? true : false),
+        'event-list' => array('name' => 'event-list', 'file' => 'index.php', 'show' => ($gSettingsManager->getInt('overview_plugin_event_list_enabled') === 1 || ($gSettingsManager->getInt('overview_plugin_event_list_enabled') === 2 && $gValidLogin)) ? true : false),
+        'who-is-online' => array('name' => 'who-is-online', 'file' => 'index.php', 'show' => ($gSettingsManager->getInt('overview_plugin_who_is_online_enabled') === 1 || ($gSettingsManager->getInt('overview_plugin_who_is_online_enabled') === 2 && $gValidLogin)) ? true : false),
+    );
+    
+    $page->assignSmartyVariable('overviewPlugins', $overviewPlugins);
     $page->addTemplateFile('system/overview.tpl');
 
     $page->show();
