8677c9404f51b5b7fa3b783b1379b5d07fe583e6
Mathias Huth <matze199730@googlemail.com>
2025-06-06 17:39:22 +0200

    fix: update inventory field insertion in createBasicData()

--- src/Organizations/Entity/Organization.php
+++ src/Organizations/Entity/Organization.php
@@ -223,25 +223,25 @@ class Organization extends Entity
         $queryParams = array($orgId, Uuid::uuid4(), Folder::getRootFolderName('documents', $this->getValue('org_shortname')), FOLDER_DATA, $systemUserId, DATETIME_NOW);
         $this->db->queryPrepared($sql, $queryParams);
 
-        // insert inventory fields TODO MH: SYS_STRINGS for description
+        // insert inventory fields
         $sql = 'INSERT INTO ' . TBL_INVENTORY_FIELDS . '
-                       (inf_org_id, inf_type, inf_name_intern, inf_name, inf_description, inf_value_list, inf_system, inf_required_input, inf_sequence, inf_usr_id_create, inf_timestamp_create, inf_usr_id_change, inf_timestamp_change)
-                VALUES (?, \'TEXT\', \'ITEMNAME\', \'SYS_ITEMNAME\', \'Der Name des Gegenstandes\', NULL, 1, 1, 0, ?, ?, NULL, NULL),
-                       (?, \'DROPDOWN\', \'CATEGORY\', \'SYS_CATEGORY\', \'Die Kategorie des Gegenstandes\', \'Allgemein\', 1, 1, 1, ?, ?, NULL, NULL),
-                       (?, \'TEXT\', \'KEEPER\', \'SYS_KEEPER\', \'Der Verwalter des Gegenstandes\', NULL, 1, 0, 2, ?, ?, NULL, NULL),
-                       (?, \'CHECKBOX\', \'IN_INVENTORY\', \'SYS_IN_INVENTORY\', \'Gegenstand befindet sich im Inventar\', NULL, 1, 0, 3, ?, ?, NULL, NULL),
-                       (?, \'TEXT\', \'LAST_RECEIVER\', \'SYS_LAST_RECEIVER\', \'Der letzte Empfänger des Gegenstandes\', NULL, 1, 0, 4, ?, ?, NULL, NULL),
-                       (?, \'DATE\', \'RECEIVED_ON\', \'SYS_RECEIVED_ON\', \'Das Verleihdatum des Gegenstandes an den letzten Empfänger\', NULL, 1, 0, 5, ?, ?, NULL, NULL),
-                       (?, \'DATE\', \'RECEIVED_BACK_ON\', \'SYS_RECEIVED_BACK_ON\', \'Das Datum, an dem der Gegenstand an den Verwalter zurückgegeben wurde\', NULL, 1, 0, 6, ?, ?, NULL, NULL);
+                       (inf_uuid, inf_org_id, inf_type, inf_name_intern, inf_name, inf_description, inf_value_list, inf_system, inf_required_input, inf_sequence, inf_usr_id_create, inf_timestamp_create, inf_usr_id_change, inf_timestamp_change)
+                VALUES (?, ?, \'TEXT\', \'ITEMNAME\', \'SYS_INVENTORY_ITEMNAME\', \'SYS_INVENTORY_ITEMNAME_DESC\', NULL, 1, 1, 0, ?, ?, NULL, NULL),
+                       (?, ?, \'CATEGORY\', \'CATEGORY\', \'SYS_CATEGORY\', \'SYS_INVENTORY_CATEGORY_DESC\', NULL, 1, 1, 1, ?, ?, NULL, NULL),
+                       (?, ?, \'TEXT\', \'KEEPER\', \'SYS_INVENTORY_KEEPER\', \'SYS_INVENTORY_KEEPER_DESC\', NULL, 1, 0, 2, ?, ?, NULL, NULL),
+                       (?, ?, \'CHECKBOX\', \'IN_INVENTORY\', \'SYS_INVENTORY_IN_INVENTORY\', \'SYS_INVENTORY_IN_INVENTORY_DESC\', NULL, 1, 0, 3, ?, ?, NULL, NULL),
+                       (?, ?, \'TEXT\', \'LAST_RECEIVER\', \'SYS_INVENTORY_LAST_RECEIVER\', \'SYS_INVENTORY_LAST_RECEIVER_DESC\', NULL, 1, 0, 4, ?, ?, NULL, NULL),
+                       (?, ?, \'DATE\', \'RECEIVED_ON\', \'SYS_INVENTORY_RECEIVED_ON\', \'SYS_INVENTORY_RECEIVED_ON_DESC\', NULL, 1, 0, 5, ?, ?, NULL, NULL),
+                       (?, ?, \'DATE\', \'RECEIVED_BACK_ON\', \'SYS_INVENTORY_RECEIVED_BACK_ON\', \'SYS_INVENTORY_RECEIVED_BACK_ON_DESC\', NULL, 1, 0, 6, ?, ?, NULL, NULL);
                 ';
         $queryParams = array(
-            $orgId, $systemUserId, DATETIME_NOW,
-            $orgId, $systemUserId, DATETIME_NOW,
-            $orgId, $systemUserId, DATETIME_NOW,
-            $orgId, $systemUserId, DATETIME_NOW,
-            $orgId, $systemUserId, DATETIME_NOW,
-            $orgId, $systemUserId, DATETIME_NOW,
-            $orgId, $systemUserId, DATETIME_NOW
+            Uuid::uuid4(), $orgId, $systemUserId, DATETIME_NOW,
+            Uuid::uuid4(), $orgId, $systemUserId, DATETIME_NOW,
+            Uuid::uuid4(), $orgId, $systemUserId, DATETIME_NOW,
+            Uuid::uuid4(), $orgId, $systemUserId, DATETIME_NOW,
+            Uuid::uuid4(), $orgId, $systemUserId, DATETIME_NOW,
+            Uuid::uuid4(), $orgId, $systemUserId, DATETIME_NOW,
+            Uuid::uuid4(), $orgId, $systemUserId, DATETIME_NOW
         );
         $this->db->queryPrepared($sql, $queryParams);
 
@@ -292,7 +292,6 @@ class Organization extends Entity
         $roleManagement->setValue('rol_announcements', 1);
         $roleManagement->setValue('rol_events', 1);
         $roleManagement->setValue('rol_weblinks', 1);
-        $roleManagement->setValue('rol_inventory_admin', 1);
         $roleManagement->setValue('rol_edit_user', 1);
         $roleManagement->setValue('rol_mail_to_all', 1);
         $roleManagement->setValue('rol_mail_this_role', 2);
