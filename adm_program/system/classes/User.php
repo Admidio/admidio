01cf1bbc25484e5eec5e4710ba512e7d4153c2fa
Markus Faßbender <markus.fassbender@gmail.com>
2025-06-07 07:21:41 +0200

    Former members not shown for non admin users #1816

--- adm_program/system/classes/User.php
+++ adm_program/system/classes/User.php
@@ -1392,11 +1392,11 @@ class User extends TableAccess
     {
         global $gSettingsManager;
 
-        if ((int)$gSettingsManager->get('groups_roles_show_former_members') !== 1
+        if ((int)$gSettingsManager->get('groups_roles_show_former_members') === 1
             && ($this->checkRolesRight('rol_assign_roles')
                 || ($this->isLeaderOfRole($roleId) && in_array($this->rolesMembershipLeader[$roleId], array(1, 3), true)))) {
             return true;
-        } elseif ((int)$gSettingsManager->get('groups_roles_show_former_members') !== 2
+        } elseif ((int)$gSettingsManager->get('groups_roles_show_former_members') === 2
             && ($this->checkRolesRight('rol_edit_user')
                 || ($this->isLeaderOfRole($roleId) && in_array($this->rolesMembershipLeader[$roleId], array(2, 3), true)))) {
             return true;
