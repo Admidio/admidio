49700d57ccf87baed1858afdeeaab2fbe84fc994
Markus Faßbender <markus.fassbender@gmail.com>
2025-09-28 20:56:06 +0200

    fully implement new demo data

--- demo/data_edit.php
+++ demo/data_edit.php
@@ -8,10 +8,10 @@
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
  ***********************************************************************************************
  */
-
 use Admidio\Infrastructure\Database;
 
 require_once(__DIR__ . '/../adm_my_files/config.php');
+require_once(__DIR__ . '/../system/bootstrap/bootstrap.php');
 require_once(__DIR__ . '/DatabaseDateTimeEdit.php');
 
 if (!isset($gDb)) {
@@ -28,13 +28,14 @@ $databaseDateTimeEdit->updateDateTimeField(TBL_ANNOUNCEMENTS, 'ann_timestamp_cre
 $databaseDateTimeEdit->updateTwoRelativeDateTimeField(TBL_EVENTS, 'dat_begin', 'dat_end', 0, 60);
 $databaseDateTimeEdit->updateBirthdays();
 $databaseDateTimeEdit->updateDateTimeField(TBL_REGISTRATIONS, 'reg_timestamp', 0, 3);
+$databaseDateTimeEdit->updateDateTimeField(TBL_LOG_CHANGES, 'log_timestamp_create', 0, 90, true);
 
 if ($gDbType === 'pgsql') {
-    $sql = 'UPDATE ' . TBL_ROLES . ' r
-               SET rol_name = TO_CHAR(e.dat_begin, \'YYYY-MM-DD HH24:MI\') || \' \' || e.dat_headline
-              FROM ' . TBL_EVENTS . ' e
-             WHERE r.dat_rol_id = e.rol_id
-               AND r.dat_rol_id IS NOT NULL ';
+    $sql = 'UPDATE ' . TBL_ROLES . '
+               SET rol_name = TO_CHAR(dat_begin, \'YYYY-MM-DD HH24:MI\') || \' \' || dat_headline
+              FROM ' . TBL_EVENTS . '
+             WHERE dat_rol_id = rol_id
+               AND dat_rol_id IS NOT NULL ';
 } else {
     $sql = 'UPDATE ' . TBL_ROLES . '
           JOIN ' . TBL_EVENTS . ' ON dat_rol_id = rol_id
@@ -42,3 +43,7 @@ if ($gDbType === 'pgsql') {
          WHERE dat_rol_id IS NOT NULL ';
 }
 $gDb->queryPrepared($sql);
+
+if ($gDbType === 'pgsql') {
+    echo '<p>Daten wurden erfolgreich aktualisiert!</p>';
+}
