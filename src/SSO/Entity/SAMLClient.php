59f5874ac5c7e5d33b91e11160b3cb5f5ecc3afb
Reinhold Kainhofer <reinhold@kainhofer.com>
2025-10-16 11:13:38 +0200

    SSO: Fix roles right mixup (wrong right was accessed due to logic flaw in argument handling...
    As the SSO type is accessed in the parent class'es constructur, we need to set the type member before, not after calling the parent constructor...

--- src/SSO/Entity/SAMLClient.php
+++ src/SSO/Entity/SAMLClient.php
@@ -6,8 +6,7 @@ use Admidio\Infrastructure\Database;
 class SAMLClient extends SSOClient 
 {
     public function __construct(Database $database, $client_id = null) {
-        parent::__construct($database, TBL_SAML_CLIENTS, 'smc', $client_id);
-        $this->ssoType = 'saml';
+        parent::__construct($database, 'saml', TBL_SAML_CLIENTS, 'smc', $client_id);
     }
 
 }
